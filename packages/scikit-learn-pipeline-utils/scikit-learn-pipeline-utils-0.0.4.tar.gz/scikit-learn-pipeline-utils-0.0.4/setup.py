b53040ce7c9d2be76479f3182e7ba164cb891df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:55 +0000

    Add package scikit-learn-pipeline-utils/0.0.4/scikit-learn-pipeline-utils-0.0.4.tar.gz

---
+++ packages/scikit-learn-pipeline-utils/scikit-learn-pipeline-utils-0.0.4.tar.gz/scikit-learn-pipeline-utils-0.0.4/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from sklearn_pipeline_utils import __version__
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="scikit-learn-pipeline-utils",
+    version= __version__,
+    description="Custom Pipeline Transformers for Sklearn Pipelines",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deltahedge1/scikit-learn-pipeline-utils",
+    author="Ish Hassan",
+    author_email="ishassan90@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["sklearn_pipeline_utils"],
+    include_package_data=True,
+    install_requires=["scikit_learn", "pandas"],
+)
