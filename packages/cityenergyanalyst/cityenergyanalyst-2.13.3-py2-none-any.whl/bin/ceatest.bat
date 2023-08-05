475f567e1aa5b5def392ac50450edb024a6130a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:54 +0000

    Add package cityenergyanalyst/2.13.3/cityenergyanalyst-2.13.3-py2-none-any.whl

---
+++ packages/cityenergyanalyst/cityenergyanalyst-2.13.3-py2-none-any.whl/bin/ceatest.bat
@@ -0,0 +1,18 @@
+rem script used to test the cea by the jenkins
+rem creates a conda environment (deleting the old one first)
+
+rem removing conda environment creation temporarily
+rem call conda env remove -y -q --name ceatest
+rem call conda env create -q --name ceatest
+
+rem call activate ceatest
+call activate cea
+
+pip.exe install -e .
+
+rem where cea
+
+cea test --reference-cases open --tasks all --verbosity 1
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+call deactivate
