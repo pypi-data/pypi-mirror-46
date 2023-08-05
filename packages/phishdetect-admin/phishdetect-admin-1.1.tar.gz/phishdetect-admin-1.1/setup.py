533f75d6b1eba4cd97ff454043a6ccb6dcd25eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:02 +0000

    Add package phishdetect-admin/1.1/phishdetect-admin-1.1.tar.gz

---
+++ packages/phishdetect-admin/phishdetect-admin-1.1.tar.gz/phishdetect-admin-1.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# PhishDetect
+# Copyright (c) 2018-2019 Claudio Guarnieri.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+from setuptools import setup
+
+description = "Web application to administer a PhishDetect Node"
+requires = (
+    'Flask',
+    'requests',
+    'PyYAML',
+)
+
+setup(
+    name='phishdetect-admin',
+    version='1.1',
+    author='Claudio Guarnieri',
+    author_email='nex@nex.sx',
+    description=description,
+    long_description=description,
+
+    scripts=['bin/phishdetect-admin',],
+    install_requires=requires,
+    packages=['phishdetectadmin',],
+    package_data={'phishdetectadmin': 'phishdetectadmin/templates/*.html'},
+    data_files=[('phishdetectadmin/templates', [os.path.join('phishdetectadmin', 'templates', f) for f in os.listdir('phishdetectadmin/templates/')])],
+
+    zip_safe=False,
+
+    keywords='security phishing phishdetect',
+    license='GPLv3',
+    classifiers=[
+    ],
+)
