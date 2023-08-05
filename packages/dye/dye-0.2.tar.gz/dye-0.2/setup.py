af153dd0e3c218a600fd7fe926ba820b66f038fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:43 +0000

    Add package dye/0.2/dye-0.2.tar.gz

---
+++ packages/dye/dye-0.2.tar.gz/dye-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'dye',
+  packages = ['dye'],
+  version = '0.2',
+  description = 'Lightweight module with ANSI control codes to dye python scripts, the simplest and lightest module to work with colors and formatting.',
+  long_description_content_type="text/markdown",
+  author = 'ax',
+  author_email = 'ax.tryin@gmail.com',
+  url = 'https://github.com/ax-tryin/dye',
+  download_url = 'https://github.com/ax-tryin/dye/archive/0.2.tar.gz',
+  keywords = ['ANSI', 'color', 'terminal', 'lightweight'],
+  setup_cfg=True
+)
