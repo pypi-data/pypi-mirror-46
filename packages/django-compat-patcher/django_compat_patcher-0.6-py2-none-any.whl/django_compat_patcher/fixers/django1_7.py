7bc3db675a382c5e0658bada8f1d7bafb9d3f9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:13 +0000

    Add package django-compat-patcher/0.6/django_compat_patcher-0.6-py2-none-any.whl

---
+++ packages/django-compat-patcher/django_compat_patcher-0.6-py2-none-any.whl/django_compat_patcher/fixers/django1_7.py
@@ -0,0 +1,14 @@
+
+
+from functools import partial
+
+from ..deprecation import *
+from ..registry import register_compatibility_fixer
+
+
+# for backward-compatibility fixers
+django1_7_bc_fixer = partial(register_compatibility_fixer,
+                             fixer_reference_version="1.7",
+                             fixer_applied_from_django="1.7")
+
+
