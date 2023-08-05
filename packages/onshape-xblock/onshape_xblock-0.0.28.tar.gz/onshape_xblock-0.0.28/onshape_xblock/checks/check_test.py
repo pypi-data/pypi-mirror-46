047377ecd8f959c916be302ceb795ea47f524b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:04 +0000

    Add package onshape-xblock/0.0.28/onshape_xblock-0.0.28.tar.gz

---
+++ packages/onshape-xblock/onshape_xblock-0.0.28.tar.gz/onshape_xblock-0.0.28/onshape_xblock/checks/check_test.py
@@ -0,0 +1,16 @@
+from onshape_xblock.check_imports import *
+
+
+class CheckTest(CheckBase):
+
+    failure_message_template = ""
+
+    def __init__(self, test_param="optional"):
+        self.test_param=test_param
+        super(CheckTest, self).__init__(name="Testing",
+                                        max_points=1, client=None,
+                                        onshape_element="https://cad.onshape.com/documents/cca81d10f239db0db9481e6f/v/ca51b7554314d6aab254d2e6/e/69c9eedda86512966b20bc90"
+                                        )
+
+    def execute_check(self):
+        pass
