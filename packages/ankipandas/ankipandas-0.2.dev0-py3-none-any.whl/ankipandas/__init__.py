fa70136ef50e8ac8a67de9d17736b50b1826c1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package ankipandas/0.2.dev0/ankipandas-0.2.dev0-py3-none-any.whl

---
+++ packages/ankipandas/ankipandas-0.2.dev0-py3-none-any.whl/ankipandas/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import ankipandas.raw
+import ankipandas.paths
+import ankipandas.util
+import ankipandas.paths
+from ankipandas.paths import find_db, db_path_input
+from ankipandas.ankidf import AnkiDataFrame
+from ankipandas.util.log import log, set_log_level
