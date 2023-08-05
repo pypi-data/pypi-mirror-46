baf0731b5988d2fb7bc74aea17ed957b82b238e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:32 +0000

    Add package arvados-cwl-runner/1.3.3/arvados-cwl-runner-1.3.3.tar.gz

---
+++ packages/arvados-cwl-runner/arvados-cwl-runner-1.3.3.tar.gz/arvados-cwl-runner-1.3.3/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# Copyright (C) The Arvados Authors. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import absolute_import
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+SETUP_DIR = os.path.dirname(__file__) or '.'
+README = os.path.join(SETUP_DIR, 'README.rst')
+
+import arvados_version
+version = arvados_version.get_version(SETUP_DIR, "arvados_cwl")
+
+setup(name='arvados-cwl-runner',
+      version=version,
+      description='Arvados Common Workflow Language runner',
+      long_description=open(README).read(),
+      author='Arvados',
+      author_email='info@arvados.org',
+      url="https://arvados.org",
+      download_url="https://github.com/curoverse/arvados.git",
+      license='Apache 2.0',
+      packages=find_packages(),
+      package_data={'arvados_cwl': ['arv-cwl-schema.yml']},
+      scripts=[
+          'bin/cwl-runner',
+          'bin/arvados-cwl-runner',
+      ],
+      # Note that arvados/build/run-build-packages.sh looks at this
+      # file to determine what version of cwltool and schema-salad to build.
+      install_requires=[
+          'cwltool==1.0.20181116032456',
+          'schema-salad==2.7.20181116024232',
+          'typing >= 3.6.4',
+          'ruamel.yaml >=0.15.54, <=0.15.77',
+          'arvados-python-client >=1.3.1, <=1.3.99',
+          'setuptools',
+          'ciso8601 >=1.0.6, <2.0.0',
+          'subprocess32>=3.5.1',
+      ],
+      data_files=[
+          ('share/doc/arvados-cwl-runner', ['LICENSE-2.0.txt', 'README.rst']),
+      ],
+      test_suite='tests',
+      tests_require=[
+          'mock>=1.0',
+          'subprocess32>=3.5.1',
+      ],
+      zip_safe=True
+      )
