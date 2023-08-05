8bef6366c1b4e19643311ad9bc3c5c4d8c02a6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:50 +0000

    Add package krasi/0.1/krasi-0.1.tar.gz

---
+++ packages/krasi/krasi-0.1.tar.gz/krasi-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='krasi',
+      version='0.1',
+      description='clinical trial information retriver',
+      url='https://github.com/gastrodia/krasi',
+      author='gastrodia',
+      author_email='ipv6.elata@gmail.com',
+      license='MIT',
+      packages=['krasi','z3'],
+      zip_safe=False)
