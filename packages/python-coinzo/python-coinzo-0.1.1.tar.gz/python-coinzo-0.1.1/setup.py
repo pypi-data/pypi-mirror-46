dd7e155b873f72ffc1249e9a2c70fc9aa9edfc57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:56 +0000

    Add package python-coinzo/0.1.1/python-coinzo-0.1.1.tar.gz

---
+++ packages/python-coinzo/python-coinzo-0.1.1.tar.gz/python-coinzo-0.1.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-coinzo",
+    version="0.1.1",
+    author="tolgamorf",
+    author_email="cryptolga@gmail.com",
+    description="coinzo REST API python implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tolgamorf/python-coinzo",
+    packages=["coinzo"],  # setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=["requests"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="coinzo exchange rest api bitcoin ethereum btc eth neo eos xrp hot",
+)
