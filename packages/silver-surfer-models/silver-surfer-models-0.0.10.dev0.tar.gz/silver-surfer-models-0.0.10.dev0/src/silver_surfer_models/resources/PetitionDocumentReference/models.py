2e5e59ac61516bfd540e37267d779b48442e7281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:12 +0000

    Add package silver-surfer-models/0.0.10.dev0/silver-surfer-models-0.0.10.dev0.tar.gz

---
+++ packages/silver-surfer-models/silver-surfer-models-0.0.10.dev0.tar.gz/silver-surfer-models-0.0.10.dev0/src/silver_surfer_models/resources/PetitionDocumentReference/models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+    UniqueConstraint,
+)
+from datetime import datetime
+
+from ...database import Base
+
+
+class PetitionDocumentReferenceModel(Base):
+    __tablename__ = 'petition_document_references'
+
+    id = Column(Integer, primary_key=True)
+    document_id = Column(
+        Integer,
+        ForeignKey('documents.id'),
+        nullable=False,
+    )
+    type = Column(String(50), nullable=False)
+    value = Column(String(255), nullable=False)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
+
+    __table_args__ = (UniqueConstraint('document_id', 'type', 'value'),)
