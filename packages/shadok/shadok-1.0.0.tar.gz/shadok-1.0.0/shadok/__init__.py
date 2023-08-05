1c1b0a6c5ce36d8f732d1efc4941e4e9c1cf4f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:33 +0000

    Add package shadok/1.0.0/shadok-1.0.0.tar.gz

---
+++ packages/shadok/shadok-1.0.0.tar.gz/shadok-1.0.0/shadok/__init__.py
@@ -0,0 +1,21 @@
+from shadok.shadok_integer import ShadokInteger
+from shadok.shadok_string import ShadokString
+from shadok.magic_faucet import MagicFaucet
+from shadok.path_to_success import (
+    ImproperShadokSyntax,
+    ColanderIsInFactABusError,
+    ShadokBurntByGegeneError,
+    ShadokUpholdingPlanetError,
+    ImproperShadokLogic,
+)
+
+__all__ = [
+    "ShadokString",
+    "ShadokInteger",
+    "ImproperShadokSyntax",
+    "ColanderIsInFactABusError",
+    "ImproperShadokLogic",
+    "ShadokBurntByGegeneError",
+    "ShadokUpholdingPlanetError",
+    "MagicFaucet",
+]
