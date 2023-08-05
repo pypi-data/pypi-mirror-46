a8cc8c62a96f6263549a673497c1a4fe68fb7aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package pypipegraph/0.184/pypipegraph-0.184.tar.gz

---
+++ packages/pypipegraph/pypipegraph-0.184.tar.gz/pypipegraph-0.184/src/pypipegraph/__init__.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del get_distribution, DistributionNotFound
+
+from .graph import run_pipegraph, new_pipegraph
+from .ppg_exceptions import (
+    RuntimeError,
+    RuntimeException,
+    CycleError,
+    JobContractError,
+    PyPipeGraphError,
+    JobDiedException,
+    NothingChanged,
+)
+from . import util
+inside_ppg = util.inside_ppg
+
+from .job import (
+    Job,
+    JobList,
+    FileGeneratingJob,
+    MultiFileGeneratingJob,
+    DataLoadingJob,
+    AttributeLoadingJob,
+    TempFileGeneratingJob,
+    TempFilePlusGeneratingJob,
+    CachedAttributeLoadingJob,
+    CachedDataLoadingJob,
+    PlotJob,
+    CombinedPlotJob,
+    FunctionInvariant,
+    ParameterInvariant,
+    FileTimeInvariant,
+    FileChecksumInvariant,
+    RobustFileChecksumInvariant,
+    FileInvariant,
+    MultiFileInvariant,
+    JobGeneratingJob,
+    DependencyInjectionJob,
+    FinalJob,
+    MemMappedDataLoadingJob,
+    MultiTempFileGeneratingJob,
+    NotebookJob,
+    verify_job_id
+)
+
+assert_uniqueness_of_object = util.assert_uniqueness_of_object
+
+all = [
+    run_pipegraph,
+    new_pipegraph,
+    RuntimeError,
+    RuntimeException,
+    CycleError,
+    JobContractError,
+    PyPipeGraphError,
+    JobDiedException,
+    NothingChanged,
+    Job,
+    JobList,
+    FileGeneratingJob,
+    MultiFileGeneratingJob,
+    DataLoadingJob,
+    AttributeLoadingJob,
+    TempFileGeneratingJob,
+    TempFilePlusGeneratingJob,
+    CachedAttributeLoadingJob,
+    CachedDataLoadingJob,
+    PlotJob,
+    CombinedPlotJob,
+    FunctionInvariant,
+    ParameterInvariant,
+    FileTimeInvariant,
+    FileChecksumInvariant,
+    RobustFileChecksumInvariant,
+    MultiFileInvariant,
+    FileInvariant,
+    JobGeneratingJob,
+    DependencyInjectionJob,
+    FinalJob,
+    MemMappedDataLoadingJob,
+    util,
+    inside_ppg,
+    MultiTempFileGeneratingJob,
+    NotebookJob,
+    verify_job_id
+]
