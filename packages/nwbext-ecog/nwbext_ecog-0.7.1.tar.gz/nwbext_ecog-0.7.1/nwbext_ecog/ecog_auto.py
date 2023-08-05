3ea3a2aadc6023cd27ee7a37f5fe979ba324c78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:34 +0000

    Add package nwbext-ecog/0.7.1/nwbext_ecog-0.7.1.tar.gz

---
+++ packages/nwbext-ecog/nwbext_ecog-0.7.1.tar.gz/nwbext_ecog-0.7.1/nwbext_ecog/ecog_auto.py
@@ -0,0 +1,28 @@
+"""
+import os
+
+from pynwb import load_namespaces
+from to_nwb.extensions.auto_class import get_class, get_multi_container
+
+import numpy as np
+
+filepath = os.path.realpath(__file__)
+basedir = os.path.split(filepath)[0]
+name = 'ecog'
+
+load_namespaces(os.path.join(basedir, name + '.namespace.yaml'))
+
+
+def surface_init_pre(faces, vertices, **kwargs):
+    if np.max(faces) >= len(vertices):
+        raise ValueError('index of faces exceeds number vertices for {}. '
+                         'Faces should be 0-indexed, not 1-indexed'.
+                         format(name))
+    if np.min(faces) < 0:
+        raise ValueError('faces hold indices of vertices and should be non-negative')
+
+
+Surface = get_class(name, 'Surface', surface_init_pre)
+
+CorticalSurfaces = get_multi_container(name, 'CorticalSurfaces', Surface)
+"""
