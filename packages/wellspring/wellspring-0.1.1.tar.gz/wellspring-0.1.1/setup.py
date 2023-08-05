7b36130a46f6a5f7da9f5a5f428294f961ea7e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:01 +0000

    Add package wellspring/0.1.1/wellspring-0.1.1.tar.gz

---
+++ packages/wellspring/wellspring-0.1.1.tar.gz/wellspring-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='wellspring',
+      version='0.1.1',
+      description='Custom pyspark package to work with Databricks',
+      url='https://pypi.org/project/wellspring/',
+      author='Tarun Rana',
+      author_email='tarun.rana@henkel.com',
+      license='MIT',
+      packages=['wellspring'],
+      install_requires=['pyspark','DateTime','pytz','pandas','numpy'],
+      zip_safe=False)
