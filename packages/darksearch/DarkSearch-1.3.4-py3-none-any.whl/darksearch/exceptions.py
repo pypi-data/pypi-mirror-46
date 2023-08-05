268474735bf4bf4c6adaca0cc6b650b6f8e0f3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:02 +0000

    Add package darksearch/1.3.4/DarkSearch-1.3.4-py3-none-any.whl

---
+++ packages/darksearch/DarkSearch-1.3.4-py3-none-any.whl/darksearch/exceptions.py
@@ -0,0 +1,18 @@
+class DarkSearchException(Exception):
+    pass
+
+
+class DarkSearchPageNotFound(DarkSearchException):
+    pass
+
+
+class DarkSearchQuotaExceed(DarkSearchException):
+    pass
+
+
+class DarkSearchRequestException(DarkSearchException):
+    pass
+
+
+class DarkSearchJSONDecodeException(DarkSearchException):
+    pass
