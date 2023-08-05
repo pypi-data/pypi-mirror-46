385f817035692d7224ebcf27c13f05a9f63f8647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:37 +0000

    Add package pyatag/0.1.3/pyatag-0.1.3.tar.gz

---
+++ packages/pyatag/pyatag-0.1.3.tar.gz/pyatag-0.1.3/pyatag/__init__.py
@@ -0,0 +1,11 @@
+from pyatag.const import *
+from pyatag.helpers import *
+from pyatag.errors import *
+from pyatag.helpers import *
+
+import json
+import asyncio 
+import aiohttp
+from datetime import timedelta
+
+from pyatag.gateway import atagDataStore
---
+++ packages/pyatag/pyatag-0.1.3.tar.gz/pyatag-0.1.3/pyatag/input.py
@@ -0,0 +1,11 @@
+TESTDATA = {
+    "_host": "atag.default.svc.cluster.local",
+    "_port": 10000,
+    "_mail": "matsnelissen@gmail.com",
+    "_scan_interval": 30,
+    "_interface":"en0",
+    "_sensors": [
+        "temperature",
+        "current_temp"
+    ]
+}
