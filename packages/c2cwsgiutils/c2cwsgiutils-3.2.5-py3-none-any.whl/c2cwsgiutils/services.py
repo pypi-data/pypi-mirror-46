932b12b2f5f6fb908031921811bd04b7e2dd8dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:38 +0000

    Add package c2cwsgiutils/3.2.5/c2cwsgiutils-3.2.5-py3-none-any.whl

---
+++ packages/c2cwsgiutils/c2cwsgiutils-3.2.5-py3-none-any.whl/c2cwsgiutils/services.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+from cornice import Service
+
+
+def create(name: str, path: str, *args: Any, **kwargs: Any) -> Service:
+    """
+    Create a cornice service with all the default configuration.
+    """
+    kwargs.setdefault('cors_origins', "*")
+    kwargs.setdefault('cors_max_age', 86400)
+    kwargs.setdefault('depth', 2)  # to make venusian find the good frame
+    kwargs.setdefault('http_cache', 0)  # disable client side and proxy caching by default
+    kwargs.setdefault('renderer', 'fast_json')
+    return Service(name, path, *args, **kwargs)
