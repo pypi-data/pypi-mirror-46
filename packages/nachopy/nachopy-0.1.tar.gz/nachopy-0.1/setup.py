1d8e8a83eb950a393b9c53e1db22bd6bdae08363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:22 +0000

    Add package nachopy/0.1/nachopy-0.1.tar.gz

---
+++ packages/nachopy/nachopy-0.1.tar.gz/nachopy-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='nachopy',
+        version='0.1',
+        description='Nachoooooooooooooooooooooooooooo!!',
+        url='https://github.com/chrislauyc/nachopy.git',
+        author='Chris Lau',
+        author_email='chrislyc.lau@gmail.com',
+        packages=['nachopy'],
+        zip_safe=False)
