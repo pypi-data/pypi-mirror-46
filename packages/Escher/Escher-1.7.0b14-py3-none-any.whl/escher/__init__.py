8e7ab4bfb7cf7859d95cd75d54c2609e519ed287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:29 +0000

    Add package Escher/1.7.0b14/Escher-1.7.0b14-py3-none-any.whl

---
+++ packages/Escher/Escher-1.7.0b14-py3-none-any.whl/escher/__init__.py
@@ -0,0 +1,20 @@
+from escher.version import (
+    __version__,
+    __schema_version__,
+    __map_model_version__,
+)
+
+from escher.plots import (
+    Builder,
+    list_available_maps,
+    list_available_models,
+)
+
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'static',
+        'dest': 'jupyter-escher',
+        'require': 'jupyter-escher/extension'
+    }]
