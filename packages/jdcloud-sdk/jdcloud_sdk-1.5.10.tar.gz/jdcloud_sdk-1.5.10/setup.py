93e07717e30d61330a82cc17e3a67a09245be2ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:47 +0000

    Add package jdcloud-sdk/1.5.10/jdcloud_sdk-1.5.10.tar.gz

---
+++ packages/jdcloud-sdk/jdcloud_sdk-1.5.10.tar.gz/jdcloud_sdk-1.5.10/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='jdcloud_sdk',
+    version="1.5.10",
+    description=long_description,
+    author='JDCloud API Gateway Team',
+    url='https://github.com/jdcloud-api/jdcloud-sdk-python',
+    scripts=[],
+    packages=find_packages(),
+    install_requires=['requests'],
+    license="Apache License V2.0"
+)
