447abb8ed9a5602873fd0aa410294cbde45d31c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:00 +0000

    Add package pyoneplatform/0.1.2/pyoneplatform-0.1.2.tar.gz

---
+++ packages/pyoneplatform/pyoneplatform-0.1.2.tar.gz/pyoneplatform-0.1.2/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+__version__ = '0.1.2'
+
+
+def _long_description():
+    with open('README.rst', 'r') as fd:
+        long_description = fd.read()
+    return long_description
+
+
+setup(
+    name="pyoneplatform",
+    version=__version__,
+    author="INET-SDI",
+    author_email="inet-sdi@inet.co.th",
+    maintainer="INET-SDI",
+    maintainer_email="inet-sdi@inet.co.th",
+    url="https://github.com/inetspa/oneplatform-sdk-python",
+    description="OnePlatform API SDK for Python",
+    long_description=_long_description(),
+    license='GNU GPL 3.0',
+    packages=[
+        "oneplatform"
+    ],
+    install_requires=[
+        "requests",
+    ],
+)
