06f1caa1ff78b3c6007ca1d1390e50f1f077080a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:07 +0000

    Add package dynamic-import/0.9.7/dynamic_import-0.9.7.tar.gz

---
+++ packages/dynamic-import/dynamic_import-0.9.7.tar.gz/dynamic_import-0.9.7/dynamic_import/__init__.py
@@ -0,0 +1,4 @@
+from .importer import importer
+
+__version__ = '0.9.7'
+__all__ = ('importer',)
---
+++ packages/dynamic-import/dynamic_import-0.9.7.tar.gz/dynamic_import-0.9.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as file:
+    long_description = file.read()
+
+setup(
+    name='dynamic_import',
+    author='STEALTH',
+    version='0.9.7',
+    author_email='ritesh@stealthcentral.com',
+    description="Dynamically/Lazily import python module on run-time. \
+    Also enables easy to use import path name.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    python_requires='>=3.6',
+    packages=find_packages(),
+    url='https://github.com/YoSTEALTH/Dynamic-Import',
+    classifiers=[
+        # Info: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: Public Domain',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
