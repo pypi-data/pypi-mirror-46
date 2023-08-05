eb8cf421e23cc6834bee4edc2e4433dede05e50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:40 +0000

    Add package apns2/0.5.0/apns2-0.5.0.tar.gz

---
+++ packages/apns2/apns2-0.5.0.tar.gz/apns2-0.5.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+dependencies = [
+    'hyper>=0.7',
+    'PyJWT>=1.4.0',
+    'cryptography>=1.7.2',
+]
+
+try:
+    # noinspection PyUnresolvedReferences
+    import enum
+except ImportError:
+    dependencies.append('enum34')
+
+setup(
+    name='apns2',
+    version='0.5.0',
+    packages=['apns2'],
+    install_requires=dependencies,
+    url='https://github.com/Pr0Ger/PyAPNs2',
+    license='MIT',
+    author='Sergey Petrov',
+    author_email='me@pr0ger.prg',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+    ],
+    description='A python library for interacting with the Apple Push Notification Service via HTTP/2 protocol'
+)
