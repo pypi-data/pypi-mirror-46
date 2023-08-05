83bb64cc87db94051027dcf72b2f171b5bcb2307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:51 +0000

    Add package os-sys/1.7.6/os_sys-1.7.6-py3-none-any.whl

---
+++ packages/os-sys/os_sys-1.7.6-py3-none-any.whl/os_sys/script.php
@@ -0,0 +1,4 @@
+post: {'text': 'hello+world', 'submit': 'Verzenden'} <br> get: {'hello': 'data', 'type': 'test', 'test': 'True', 'country': 'NL', 'dat': '', 'fire': 'true'}<br>python: hello
+{'hello': 'data', 'type': 'test', 'test': 'True', 'country': 'NL', 'dat': '', 'fire': 'true'}
+
+<?php echo('<br>php test');?>
