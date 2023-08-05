1f9242531d028d9e2e36f663ba7a283108c59591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:31 +0000

    Add package mlmmapi/0.0.2/mlmmapi-0.0.2.tar.gz

---
+++ packages/mlmmapi/mlmmapi-0.0.2.tar.gz/mlmmapi-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlmmapi",
+    version="0.0.2",
+    author="Daniel Brandt",
+    author_email="dbbrandt@gmail.com",
+    description="Tools to support the grading of short answer questions using machine learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
