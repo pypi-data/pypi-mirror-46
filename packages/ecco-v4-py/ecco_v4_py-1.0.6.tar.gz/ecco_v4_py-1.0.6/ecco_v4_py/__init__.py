0b3e95027af9453cdb1aa8c7fc8b03d87b326fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:08 +0000

    Add package ecco-v4-py/1.0.6/ecco_v4_py-1.0.6.tar.gz

---
+++ packages/ecco-v4-py/ecco_v4_py-1.0.6.tar.gz/ecco_v4_py-1.0.6/ecco_v4_py/__init__.py
@@ -0,0 +1,79 @@
+from .ecco_utils import make_time_bounds_and_center_times_from_ecco_dataset
+from .ecco_utils import make_time_bounds_from_ds64
+from .ecco_utils import extract_yyyy_mm_dd_hh_mm_ss_from_datetime64
+
+from .ecco_utils import minimal_metadata
+from .ecco_utils import months2days
+
+#from extract_grid_fields_from_mitgrid_files import extract_U_point_grid_fields_from_mitgrid_as_tiles
+#from extract_grid_fields_from_mitgrid_files import extract_G_point_grid_fields_from_mitgrid_as_tiles
+
+from .llc_array_conversion  import llc_compact_to_tiles
+from .llc_array_conversion  import llc_tiles_to_compact
+from .llc_array_conversion  import llc_compact_to_faces
+from .llc_array_conversion  import llc_faces_to_tiles
+from .llc_array_conversion  import llc_tiles_to_faces
+from .llc_array_conversion  import llc_faces_to_compact
+
+from .netcdf_product_generation import create_nc_grid_files_on_native_grid_from_mds
+from .netcdf_product_generation import get_time_steps_from_mds_files
+from .netcdf_product_generation import create_nc_variable_files_on_native_grid_from_mds
+from .netcdf_product_generation import update_ecco_dataset_geospatial_metadata
+from .netcdf_product_generation import update_ecco_dataset_temporal_coverage_metadata
+
+
+from .read_bin_llc import read_llc_to_tiles 
+from .read_bin_llc import read_llc_to_compact
+from .read_bin_llc import read_llc_to_faces
+from .read_bin_llc import load_ecco_vars_from_mds
+
+from .read_bin_gen import load_binary_array
+
+from .resample_to_latlon import resample_to_latlon
+
+from .tile_exchange import append_border_to_tile
+from .tile_exchange import add_borders_to_DataArray_V_points
+from .tile_exchange import add_borders_to_DataArray_U_points
+from .tile_exchange import add_borders_to_DataArray_G_points
+from .tile_exchange import get_llc_tile_border_mapping
+from .tile_exchange import add_borders_to_GRID_tiles
+
+#from .tile_io import load_all_tiles_from_netcdf
+#from .tile_io import load_tile_from_netcdf
+#from .tile_io import load_subset_tiles_from_netcdf
+from .tile_io import load_ecco_grid_tiles_from_nc
+from .tile_io import recursive_load_ecco_var_tiles_from_nc
+from .tile_io import load_ecco_var_tiles_from_nc
+
+from .tile_plot import plot_tile
+from .tile_plot import plot_tiles
+from .tile_plot import unique_color
+
+from .tile_plot_proj import plot_proj_to_latlon_grid
+from .tile_plot_proj import plot_pstereo
+from .tile_plot_proj import plot_global
+
+
+from .tile_rotation import reorient_13_tile_GRID_Dataset_to_latlon_layout
+from .tile_rotation import reorient_13_tile_Dataset_to_latlon_layout_CG_points
+from .tile_rotation import rotate_single_tile_Dataset_CG_points
+from .tile_rotation import rotate_single_tile_DataArray_CG_points
+from .tile_rotation import reorient_13_tile_Dataset_to_latlon_layout_UV_points
+from .tile_rotation import rotate_single_tile_Datasets_UV_points
+from .tile_rotation import rotate_single_tile_DataArrays_UV_points
+
+from .test_llc_array_loading_and_conversion import run_read_bin_and_llc_conversion_test
+
+
+__all__ = ['ecco_utils', 
+           'llc_array_conversion', 
+           'netcdf_product_generation',
+           'read_bin_llc',
+           'read_bin_gen', 
+           'resample_to_latlon', 
+           'tile_exchange', 
+           'tile_io', 
+           'tile_plot',
+           'tile_plot_proj', 
+           'tile_rotation', 
+           'test_llc_array_loading_and_conversion']
