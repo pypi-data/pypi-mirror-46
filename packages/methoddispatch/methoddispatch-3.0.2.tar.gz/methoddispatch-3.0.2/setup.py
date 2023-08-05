d72c496de29e96e3b059a5fd37ee7ec539439e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:02 +0000

    Add package methoddispatch/3.0.2/methoddispatch-3.0.2.tar.gz

---
+++ packages/methoddispatch/methoddispatch-3.0.2.tar.gz/methoddispatch-3.0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+import methoddispatch
+
+
+setuptools.setup(
+    name="methoddispatch",
+    version=methoddispatch.__version__,
+    author=methoddispatch.__author__,
+    author_email="tim.mitchell@seequent.com",
+    description="singledispatch decorator for class methods.",
+    license=methoddispatch.__license__,
+    keywords="single dispatch decorator method",
+    url="https://github.com/seequent/methoddispatch",
+    packages=setuptools.find_packages(),
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
