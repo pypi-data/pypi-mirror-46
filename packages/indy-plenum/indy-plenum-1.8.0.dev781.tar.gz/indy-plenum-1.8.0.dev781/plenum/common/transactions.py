498348b7aa4b63403f29967e39f8c04e1e56fe0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package indy-plenum/1.8.0.dev781/indy-plenum-1.8.0.dev781.tar.gz

---
+++ packages/indy-plenum/indy-plenum-1.8.0.dev781.tar.gz/indy-plenum-1.8.0.dev781/plenum/common/transactions.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class Transactions(Enum):
+    def __str__(self):
+        return self.name
+
+
+class PlenumTransactions(Transactions):
+    #  These numeric constants CANNOT be changed once they have been used,
+    #  because that would break backwards compatibility with the ledger
+    # Also the numeric constants CANNOT collide with transactions in dependent
+    # components.
+    NODE = "0"
+    NYM = "1"
+    AUDIT = "2"
+    GET_TXN = "3"
