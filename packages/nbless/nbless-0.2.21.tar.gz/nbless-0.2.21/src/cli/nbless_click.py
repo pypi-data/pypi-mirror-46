1540a438f61cc5a602d0cafde7d54e761461efb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:32 +0000

    Add package nbless/0.2.21/nbless-0.2.21.tar.gz

---
+++ packages/nbless/nbless-0.2.21.tar.gz/nbless-0.2.21/src/cli/nbless_click.py
@@ -0,0 +1,17 @@
+import sys
+from typing import List
+
+import click
+
+from nbless import nbless, nbsave
+import nbformat
+
+
+@click.command()
+@click.argument("in_files", nargs=-1, required=True,
+                type=click.Path(exists=True))
+@click.option("-o", "--out_file", "out")
+def nbless_click(in_files: List[str], out: str) -> None:
+    """Create an executed Jupyter notebook from markdown and code files."""
+    nb = nbless.nbless(in_files)
+    nbsave.nbsave(out, nb) if out else sys.stdout.write(nbformat.writes(nb))
