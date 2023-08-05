13c9ee90fa4735f7382a31591476fe8c317106fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package cfn-review-bot/1!20190507.80624/cfn-review-bot-1!20190507.80624.tar.gz

---
+++ packages/cfn-review-bot/cfn-review-bot-1!20190507.80624.tar.gz/cfn-review-bot-1!20190507.80624/cfn_review_bot/schema/target.py
@@ -0,0 +1,35 @@
+'''
+Schema for deployment target configuration.
+
+
+A deployment target is typically an account/region combination. Multiple targets
+may be referenced by a single name.
+'''
+
+import schema
+
+from . import aws
+from . import util
+
+
+DEFAULT_ROLE_NAME = 'CfnReviewBot'
+
+
+SingleTargetSchema = schema.Schema({
+  'account-id': aws.AccountId,
+  schema.Optional('region'): util.OneOrMany(aws.Region),
+  schema.Optional('role-name'): str,
+  schema.Optional('tag', default={}): {str: str},
+})
+
+TargetConfigSchema = schema.Schema({
+  schema.Optional('default', default=[]): util.OneOrMany(str),
+  schema.Optional('region', default=[]): util.OneOrMany(aws.Region),
+  schema.Optional('role-name', default=DEFAULT_ROLE_NAME): str,
+  schema.Optional('tag', default={}): {str: str},
+  schema.Optional('target', default={}): {
+    str: util.OneOrMany(SingleTargetSchema),
+  },
+  schema.Optional('stack-root', default='./stack'): str,
+  schema.Optional('template-root', default='./template'): str,
+}, name='Target Configuration')
