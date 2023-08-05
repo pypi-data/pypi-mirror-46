a186a71812b2dcea40fca5597bd0164652d78c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:17 +0000

    Add package nwbext-ecog/0.7.0/nwbext_ecog-0.7.0.tar.gz

---
+++ packages/nwbext-ecog/nwbext_ecog-0.7.0.tar.gz/nwbext_ecog-0.7.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='nwbext_ecog',
+    version='0.7.0',
+    description='Convert data to nwb',
+    long_description=long_description,
+    author='Ben Dichter',
+    author_email='ben.dichter@gmail.com',
+    keywords=['nwb', 'extension'],
+    install_requires=['pynwb'],
+    data_files=[('ndx_specs', ['spec/ecog.namespace.yaml', 'spec/ecog.extensions.yaml'])]
+)
