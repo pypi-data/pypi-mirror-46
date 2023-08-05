0fc532d879c7270d2ddca782ed889b76303741c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:41 +0000

    Add package adaptive/0.8.0/adaptive-0.8.0.tar.gz

---
+++ packages/adaptive/adaptive-0.8.0.tar.gz/adaptive-0.8.0/adaptive/learner/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from contextlib import suppress
+
+from adaptive.learner.average_learner import AverageLearner
+from adaptive.learner.balancing_learner import BalancingLearner
+from adaptive.learner.base_learner import BaseLearner
+from adaptive.learner.data_saver import DataSaver, make_datasaver
+from adaptive.learner.integrator_learner import IntegratorLearner
+from adaptive.learner.learner1D import Learner1D
+from adaptive.learner.learner2D import Learner2D
+from adaptive.learner.learnerND import LearnerND
+
+with suppress(ImportError):
+    # Only available if 'scikit-optimize' is installed
+    from adaptive.learner.skopt_learner import SKOptLearner
