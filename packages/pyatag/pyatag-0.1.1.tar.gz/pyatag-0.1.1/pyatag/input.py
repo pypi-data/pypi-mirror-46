b2a72f20383cb67c7fb0a972d31088d8b0b0d02c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:20 +0000

    Add package pyatag/0.1.1/pyatag-0.1.1.tar.gz

---
+++ packages/pyatag/pyatag-0.1.1.tar.gz/pyatag-0.1.1/pyatag/input.py
@@ -0,0 +1,12 @@
+#omtetesten
+TESTDATA = {
+    "_host": "atag.default.svc.cluster.local",
+    "_port": 10000,
+    "_mail": "matsnelissen@gmail.com",
+    "_scan_interval": 30,
+    "_interface":"en0",
+    "_sensors": [
+        "temperature",
+        "current_temp"
+    ]
+}
