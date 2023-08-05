01cc3a2cb71b2fdae7df0c7e354b5eaf560b4ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:26 +0000

    Add package Terry_toolkit/0.0.1.6.2.2/Terry_toolkit-0.0.1.6.2.2.tar.gz

---
+++ packages/Terry_toolkit/Terry_toolkit-0.0.1.6.2.2.tar.gz/Terry_toolkit-0.0.1.6.2.2/Terry_toolkit/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# __init__.py
+#from .a import A
+#from .b import B
+
+#多文件导入以后只用导入 import Terry-toolkit 就可以使用所有类
+#>>> import mymodule
+#>>> a = mymodule.A()
+#>>> a.spam()
+
+
+from .file import File
+from .text import Text
+from .dict_get import Dict
+from .crawler_baidu import CrawlerBaidu
+from .CxExtractor import CxExtractor
+from .url import Url
+from .SearchBaidu import SearchBaidu
+# from .TextRank4ZH import TextRank4ZH
