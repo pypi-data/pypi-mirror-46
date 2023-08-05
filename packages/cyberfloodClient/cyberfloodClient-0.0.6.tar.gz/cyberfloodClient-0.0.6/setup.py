60d8d5821cd5ccd62bc70792d2bf32206ced1301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:41 +0000

    Add package cyberfloodClient/0.0.6/cyberfloodClient-0.0.6.tar.gz

---
+++ packages/cyberfloodClient/cyberfloodClient-0.0.6.tar.gz/cyberfloodClient-0.0.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cyberfloodClient',
+    version='0.0.6',
+    author="Arnaud Castaner",
+    author_email="arnaud.castaner@outlook.com",
+    description="A CyberFlood REST API client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/acastaner/cf-py-cfclient",
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+)
