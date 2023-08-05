c6d49daa797469e5cf649d54bc49000f197ea02f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:23 +0000

    Add package brownpaperbag/0.1.9/brownpaperbag-0.1.9-py2.py3-none-any.whl

---
+++ packages/brownpaperbag/brownpaperbag-0.1.9-py2.py3-none-any.whl/brownpaperbag/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for brownpaperbag."""
+
+__author__ = """Bruno Paulin"""
+__email__ = "brunopaulin@bpaulin.net"
+__version__ = "0.1.9"
+
+# flake8: noqa
+from .bpbgate import BpbGate, SESSION_EVENT
