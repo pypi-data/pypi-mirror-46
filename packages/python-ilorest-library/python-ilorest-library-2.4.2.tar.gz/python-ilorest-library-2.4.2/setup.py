ae5e180916f0f7772e4a7bb74f4f0dcc44fb6f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:38 +0000

    Add package python-ilorest-library/2.4.2/python-ilorest-library-2.4.2.tar.gz

---
+++ packages/python-ilorest-library/python-ilorest-library-2.4.2.tar.gz/python-ilorest-library-2.4.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name='python-ilorest-library',
+      version='2.4.2',
+      description='iLO Rest Python Library',
+	  author = 'Hewlett Packard Enterprise',
+	  author_email = 'kocurek@hpe.com',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Topic :: Communications'
+      ],
+      keywords='HP Enterprise',
+      url='https://github.com/HewlettPackard/python-ilorest-library',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      install_requires=[
+          'jsonpatch',
+          'jsonpath_rw',
+          'jsonpointer',
+          'urllib3',
+          'six'
+      ])
