c8aae07b3e565b4f96771fddd07c2d0ebb44c67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:34 +0000

    Add package laravalidation/0.4/laravalidation-0.4.tar.gz

---
+++ packages/laravalidation/laravalidation-0.4.tar.gz/laravalidation-0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+setup(
+  name = 'laravalidation',
+  packages=['laravalidation'],
+  version = '0.4',
+  description = 'Laravel like data validation library for python language',
+  author = 'Walid Mashal',
+  author_email = 'walidmashal4@gmail.com',
+  url='https://github.com/kristopherchun/laravalidation.git',
+  download_url = '',
+  keywords = ['validation', 'python', 'laravel'],
+  classifiers = [],
+  long_description=long_description,
+  long_description_content_type='text/markdown'
+)
