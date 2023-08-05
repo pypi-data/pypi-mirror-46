6aac99fe274db351c33cbd6d4bf86afcd5849618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:28 +0000

    Add package miscellaneous/1.0.0/miscellaneous-1.0.0.tar.gz

---
+++ packages/miscellaneous/miscellaneous-1.0.0.tar.gz/miscellaneous-1.0.0/README.rst
@@ -0,0 +1,16 @@
+Description
+==============
+
+Miscelanious of python scripts
+
+Installation
+---------------
+
+not yet
+
+
+Usage
+---------
+
+python -m miscelanious.search_molec_sdfs <your files>  --output molecules_on_both.sdf
+python -m miscelanious.search_molec_sdfs nostrumb_hits*.sdf  --output molecules_intersec.sdf
---
+++ packages/miscellaneous/miscellaneous-1.0.0.tar.gz/miscellaneous-1.0.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(name='miscellaneous',
+      version="1.0.0",
+      url = "https://github.com/nostrumbiodiscovery/analogs_finder", 
+      description='Retrieve analogs given a query molecule and a database.',
+      author='Daniel Soler',
+      author_email='daniel.soler@nostrumbiodiscovery.com',
+      install_requires=["tqdm", ],
+      packages=setuptools.find_packages(),
+      classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+       ],
+     )
