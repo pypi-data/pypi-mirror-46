32adceac41a4dbc1160de3688dd5fd927e76c63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:01 +0000

    Add package preprocessRawText/0.0.27/preprocessRawText-0.0.27.tar.gz

---
+++ packages/preprocessRawText/preprocessRawText-0.0.27.tar.gz/preprocessRawText-0.0.27/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="preprocessRawText",
+    version="0.0.27",
+    author="nguyentho",
+    author_email="nguyentho0207@gmail.com",
+    description="preprocessRawText NLP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nguyenthohy/preprocess.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+
+    package_data={
+            'preprocessRawText': ['vietnamese-stopwords-dash.txt']
+        },
+
+    install_requires=['flashtext', 'gensim', 'pyvi'],
+
+)
