588a14aca90056da29a6a4fb9f49b29b358df8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:21 +0000

    Add package MrCrop/1.0.0/MrCrop-1.0.0-py3-none-any.whl

---
+++ packages/MrCrop/MrCrop-1.0.0-py3-none-any.whl/MrCrop/__version__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__title__ = "MrCrop"
+__description__ = "Crops faces from batches of pictures for websites"
+__author__ = "Dhamodharan Karuppuswamy"
+__version__ = "1.0.0"
