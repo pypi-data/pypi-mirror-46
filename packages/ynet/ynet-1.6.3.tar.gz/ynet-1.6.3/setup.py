2cff4d4e268fd72fc090e8a0d4697461e0c4e760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:13 +0000

    Add package ynet/1.6.3/ynet-1.6.3.tar.gz

---
+++ packages/ynet/ynet-1.6.3.tar.gz/ynet-1.6.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ynet',
+      version='1.6.3',
+      description='ynet.co.il interaction library',
+      author='sl4v',
+      author_email='iamsl4v@protonmail.com',
+      url='https://sl4vkek.github.io/python-ynet',
+      packages=['ynet'],
+     )
