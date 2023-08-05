23df601a4f2d4d4b3291f8982560bc9ceedbb145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:01 +0000

    Add package geoformat/20190508.post2/geoformat-20190508.post2.tar.gz

---
+++ packages/geoformat/geoformat-20190508.post2.tar.gz/geoformat-20190508.post2/README.rst
@@ -0,0 +1,11 @@
+Welcome in  Geoformat
+=====================
+
+
+* Install Geoformat
+
+* How to Use Geoformat
+
+* How is structured Geoformat
+
+* Basical example
---
+++ packages/geoformat/geoformat-20190508.post2.tar.gz/geoformat-20190508.post2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='geoformat',
+    version='20190508-2',
+    packages=['Geoformat'],
+    url='https://framagit.org/Guilhain/Geoformat',
+    license='MIT',
+    author='Guilhain Averlant',
+    author_email='g.averlant@mailfence.com',
+    description='Library GDAL/OGR overlayer'
+)
