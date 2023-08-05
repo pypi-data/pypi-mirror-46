89b743a64b979b40cd352e5c8044b04fb82bc2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:02 +0000

    Add package quienesquien/0.0.1/quienesquien-0.0.1.tar.gz

---
+++ packages/quienesquien/quienesquien-0.0.1.tar.gz/quienesquien-0.0.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+requirements = [
+    'requests'
+]
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name='quienesquien',
+    version='0.0.1',
+    author='Cuenca',
+    author_email='dev@cuenca.com',
+    description='Cliente para listas quienesquien',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cuenca-mx/quienesquien-python',
+    packages=setuptools.find_packages(),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'vcrpy'],
+    extras_require={
+        'dev': [
+            'pytest>=3',
+            'pycodestyle',
+            'coverage'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
