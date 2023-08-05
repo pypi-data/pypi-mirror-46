d5e0643fd9e9bc1459701d05c4e3fcb69f190e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:53 +0000

    Add package guildai/0.6.2/guildai-0.6.2-cp27-cp27m-macosx_10_14_x86_64.whl

---
+++ packages/guildai/guildai-0.6.2-cp27-cp27m-macosx_10_14_x86_64.whl/guildai-0.6.2.data/purelib/guild/tests/copy-source.md
@@ -0,0 +1,126 @@
+skip-windows: yes
+
+# Copying source
+
+Source code is copied for each run according to the model `source`
+attr.
+
+The function that copies the source is `op_util.copy_source`. For our
+tests however, we'll use the private version `_copy_source`, which
+provides an interface suitable for testing.
+
+    >>> from guild.op_util import _copy_source
+
+We'll use the sample project `copy-source` to illustrate the supported
+copy behavior.
+
+    >>> gf = guildfile.from_dir(sample("projects/copy-source"))
+
+The project contains these models:
+
+    >>> sorted(gf.models)
+    ['default', 'exclude-all', 'include-logo', 'only-py', 'py-and-guild']
+
+We'll use temporary run directories to test each copy
+operation. Here's a helper function that copies the source for the
+applicable model and prints the copied source files.
+
+    >>> def copy_model_source(model_name):
+    ...     model = gf.models[model_name]
+    ...     temp_dir = mkdtemp()
+    ...     _copy_source(gf.dir, model.source, temp_dir)
+    ...     copied = find(temp_dir)
+    ...     if not copied:
+    ...         print("<empty>")
+    ...         return
+    ...     for path in copied:
+    ...         print(path, sha256(join_path(temp_dir, path))[:8])
+
+By default, all text files are copied, including links and files
+within linked directories:
+
+    >>> copy_model_source("default")
+    a.txt 90605548
+    empty e3b0c442
+    guild.yml ...
+    hello.py 6ae95c9c
+    subdir/b.txt 43451775
+
+The `include-logo` model explicitly includes `subdir/logo.png`, which
+would otherwise not be copied:
+
+    >>> copy_model_source("include-logo")
+    a.txt 90605548
+    empty e3b0c442
+    guild.yml ...
+    hello.py 6ae95c9c
+    subdir/b.txt 43451775
+    subdir/logo.png 4a9bf008
+
+The `exclude-all` model excludes all source and therefore copies
+nothing:
+
+    >>> copy_model_source("exclude-all")
+    <empty>
+
+The `only-py` model specifies that only `*.py` files be copied:
+
+    >>> copy_model_source("only-py")
+    hello.py 6ae95c9c
+
+The `py-and-guild` model specifies Python source and the Guild file:
+
+    >>> copy_model_source("py-and-guild")
+    guild.yml ...
+    hello.py 6ae95c9c
+
+## Links
+
+The tests below illustrate how copy source handles symlinks. We need
+to generate a test case with links because we can't distribute links
+within the Guild source code (Python wheels, used to package Guild,
+don't support links).
+
+Here's a directory with various files and links:
+
+    >>> project_dir = mkdtemp()
+    >>> def project_file(name, s):
+    ...     open(join_path(project_dir, name), "w").write(s)
+    >>> def project_link(dest, link):
+    ...     symlink(dest, join_path(project_dir, link))
+    >>> def project_subdir(path):
+    ...     mkdir(join_path(project_dir, path))
+
+    >>> project_file("a.txt", "hello")
+    >>> project_link("a.txt", "link-to-a.txt")
+    >>> project_subdir("cycle")
+    >>> project_file("cycle/b.txt", "yo yo yo")
+    >>> project_link("../cycle", "cycle/cycle")
+
+Let's verify that a cycle actually exists:
+
+    >>> exists(join_path(project_dir, *(["cycle"] * 10)))
+    True
+
+A proxy for source:
+
+    >>> class Source(object):
+    ...     def __init__(self, specs):
+    ...         self.specs = specs
+
+A helper to print results:
+
+    >>> def copied_files(dir):
+    ...     for path in find(dir):
+    ...         print(path, sha256(join_path(dir, path))[:8])
+
+Here's out copied source:
+
+    >>> tmp_dir = mkdtemp()
+    >>> _copy_source(project_dir, Source([]), tmp_dir)
+    >>> copied_files(tmp_dir)
+    a.txt 2cf24dba
+    cycle/b.txt c940b581
+    link-to-a.txt 2cf24dba
+
+Note that the cycle is handled in the copy.
---
+++ packages/guildai/guildai-0.6.2-cp27-cp27m-macosx_10_14_x86_64.whl/guildai-0.6.2.data/purelib/guild/tfevent.py
@@ -0,0 +1,144 @@
+# Copyright 2017-2019 TensorHub, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utility module for TensorFlow events.
+
+It's safe to import this module even when TensorFlow isn't installed
+as all required external modules are lazily loaded.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+
+import glob
+import hashlib
+import logging
+import os
+
+log = logging.getLogger("guild")
+
+class ScalarReader(object):
+
+    def __init__(self, dir):
+        self.dir = dir
+
+    def __iter__(self):
+        """Yields (tag, val, step) for scalars.
+        """
+        try:
+            from tensorboard.backend.event_processing import event_accumulator
+        except ImportError:
+            pass
+        else:
+            events = event_accumulator._GeneratorFromPath(self.dir).Load()
+            try:
+                for event in events:
+                    if not event.HasField("summary"):
+                        continue
+                    for val in event.summary.value:
+                        if not val.HasField("simple_value"):
+                            continue
+                        yield val.tag, val.simple_value, event.step
+            except RuntimeError as e:
+                # PEP 479 landed in Python 3.7 and TB triggers this
+                # runtime error when there are no events to read.
+                if e.args[0] != "generator raised StopIteration":
+                    raise
+
+def iter_events(root_path):
+    """Returns an iterator that yields (dir, digest, reader) tuples.
+
+    For each yielded events dir, `digest` changes whenever events have
+    been written to the dir.
+
+    `reader` is an instance of ScalarReader that can be used to read
+    scalars in dir.
+    """
+    ensure_tf_logging_patched()
+    try:
+        from tensorboard.backend.event_processing import io_wrapper
+    except ImportError:
+        pass
+    else:
+        for subdir_path in io_wrapper.GetLogdirSubdirectories(root_path):
+            if _linked_resource_path(subdir_path, root_path):
+                log.debug("skipping linked resource path %s", subdir_path)
+                continue
+            digest = _event_files_digest(subdir_path)
+            yield subdir_path, digest, ScalarReader(subdir_path)
+
+def _linked_resource_path(path, root):
+    """Returns True if path is a linked resource under root.
+
+    This is used to exclude tfevents under root that are linked
+    resources.
+    """
+    if _has_steps(root):
+        return _links_under_root(path, root) > 1
+    else:
+        return not _real_path_under_root(path, root)
+
+def _has_steps(path):
+    return os.path.exists(os.path.join(path, ".guild", "attrs", "steps"))
+
+def _links_under_root(path, root):
+    """Returns the number of links to path uses user root."""
+    links = 0
+    last_path = None
+    while path != root and path != last_path:
+        if os.path.islink(path):
+            links += 1
+        last_path = path
+        path = os.path.dirname(path)
+    return links
+
+def _real_path_under_root(path, root):
+    """Returns True if real path is under root."""
+    real_path = os.path.realpath(path)
+    real_root = os.path.realpath(root)
+    return real_path.startswith(real_root)
+
+def _event_files_digest(dir):
+    """Returns a digest for dir that changes when events change.
+
+    The digest includes the list of event logs and their associated
+    sizes.
+    """
+    event_files = sorted(glob.glob(os.path.join(dir, "*.tfevents.*")))
+    to_hash = "\n".join([
+        "{}\n{}".format(filename, os.path.getsize(filename))
+        for filename in event_files
+        if os.path.isfile(filename)])
+    return hashlib.md5(to_hash.encode("utf-8")).hexdigest()
+
+def ensure_tf_logging_patched():
+    _ensure_tf_oldstyle_logging_patched()
+    _ensure_tf_newstyle_logging_patched()
+
+def _ensure_tf_oldstyle_logging_patched():
+    try:
+        from tensorflow import logging
+    except ImportError:
+        pass
+    else:
+        logging.info = logging.debug = lambda *_arg, **_kw: None
+
+def _ensure_tf_newstyle_logging_patched():
+    try:
+        from tensorboard.util import tb_logging
+    except ImportError:
+        pass
+    else:
+        logger = tb_logging.get_logger()
+        logger.info = logger.debug = lambda *_arg, **_kw: None
