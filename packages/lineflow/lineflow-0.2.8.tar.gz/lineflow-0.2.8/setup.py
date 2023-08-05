d31f59babbbf141703d88edde0fdaad815ec4144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:48 +0000

    Add package lineflow/0.2.8/lineflow-0.2.8.tar.gz

---
+++ packages/lineflow/lineflow-0.2.8.tar.gz/lineflow-0.2.8/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distuils.core import setup
+
+
+setup(
+    name='lineflow',
+    version='0.2.8',
+    description='Framework-Agnostic NLP Data Loader in Python',
+    long_description=open('./README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yasufumy/lineflow',
+    author='Yasufumi Taniguchi',
+    author_email='yasufumi.taniguchi@gmail.com',
+    packages=[
+        'lineflow', 'lineflow.datasets'
+    ],
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    tests_require=['pytest'],
+    extras_require={
+        'allennlp': ['allennlp'],
+        'torchtext': ['torchtext']}
+)
