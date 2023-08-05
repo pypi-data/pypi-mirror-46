c8c5f3218ac1fbca1bb0d399ce7ced03db25a183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:43 +0000

    Add package bio-pyvol/1.0.9/bio-pyvol-1.0.9.tar.gz

---
+++ packages/bio-pyvol/bio-pyvol-1.0.9.tar.gz/bio-pyvol-1.0.9/pyvol/__init__.py
@@ -0,0 +1,10 @@
+
+__version__ = "1.0.9"
+
+def __init_plugin__(app=None):    
+    from pymol import cmd
+    
+
+    from .pymol_interface import pocket, load_spheres
+    cmd.extend('pocket', pocket)
+    cmd.extend('load_spheres', load_spheres)
