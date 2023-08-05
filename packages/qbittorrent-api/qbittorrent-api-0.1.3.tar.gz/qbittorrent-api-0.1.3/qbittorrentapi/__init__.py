2c164ba8c2f6b46eefd819a2e4ba249af5bc420e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:32 +0000

    Add package qbittorrent-api/0.1.3/qbittorrent-api-0.1.3.tar.gz

---
+++ packages/qbittorrent-api/qbittorrent-api-0.1.3.tar.gz/qbittorrent-api-0.1.3/qbittorrentapi/__init__.py
@@ -0,0 +1,4 @@
+VERSION = "0.1.3"
+name = "qbittorrentapi"
+
+from .client import Client
---
+++ packages/qbittorrent-api/qbittorrent-api-0.1.3.tar.gz/qbittorrent-api-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from qbittorrentapi import VERSION
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='qbittorrent-api',
+    version=VERSION,
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    install_requires=['attrdict', 'requests'],
+    url='https://github.com/rmartin16/qbittorrent-api',
+    license='GPL-3',
+    author='Russell Martin',
+    author_email='rmartin16@gmail.com',
+    description='Python client implementation for qBittorrent Web API v2 first available in qBittorrent v4.1.',
+    long_description=long_description,
+    keywords='qbittorrent api',
+    classifiers=["Programming Language :: Python :: 3",
+                 "Programming Language :: Python :: 2",
+                 "Operating System :: OS Independent"]
+)
