debe89e80a45b98e38d25180180b7ae503582c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:05 +0000

    Add package crc-ct/1.0.0b2/crc_ct-1.0.0b2-cp37-cp37m-win_amd64.whl

---
+++ packages/crc-ct/crc_ct-1.0.0b2-cp37-cp37m-win_amd64.whl/crc/_platform/_osx/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) 1994-2019 Adam Karpierz
+# Licensed under the zlib/libpng License
+# http://opensource.org/licenses/zlib/
+
+import sys
+import os
+import platform
+import sysconfig
+import ctypes as ct
+from functools import partial
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+
+dll_suffix = (platform.python_implementation()[:2].lower() +
+              sysconfig.get_python_version().replace(".","") + "-" +
+              sysconfig.get_platform().replace("-","_") +
+              (sysconfig.get_config_var("EXT_SUFFIX") or ".pyd"))
+
+DLL_PATH = os.path.join(os.path.dirname(this_dir), "crc." + dll_suffix)
+
+from ctypes  import CDLL      as DLL
+from ctypes  import CFUNCTYPE as CFUNC
+from _ctypes import dlclose
+
+DLL = partial(DLL, mode=ct.RTLD_GLOBAL)
