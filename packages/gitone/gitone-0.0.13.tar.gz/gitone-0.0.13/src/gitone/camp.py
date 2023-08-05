9c29c8a950496b1aa7cf5c7798980d22d9f38ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:22 +0000

    Add package gitone/0.0.13/gitone-0.0.13.tar.gz

---
+++ packages/gitone/gitone-0.0.13.tar.gz/gitone-0.0.13/src/gitone/camp.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from typing import Optional
+
+import git
+
+
+def camp(commit_message: Optional[str] = None) -> None:
+    """Add and commit changes made to tracked files, then push the commit."""
+
+    repo = git.Repo()
+
+    changed_file_lists = [
+        [file.a_path
+         for file in repo.index.diff(None).iter_change_type(change_type)]
+        for change_type in ('D', 'M')
+    ]
+
+    if any(changed_file_lists):
+        repo.git.add(changed_file_lists)
+
+        if commit_message:
+            print(repo.git.commit(changed_file_lists,
+                                  message=commit_message),
+                  repo.git.push())
+
+        else:
+            prefixes = "Deleted files:", "Modified files:"
+
+            deleted, modified = (
+                f"{prefix} {', '.join(changed)}. " if changed else ""
+                for prefix, changed in zip(prefixes, changed_file_lists)
+            )
+
+            print(repo.git.commit(changed_file_lists,
+                                  message=deleted + modified),
+                  repo.git.push())
+
+    else:
+        print("There are no deleted or modified files.")
+
+
+if __name__ == "__main__":
+    camp()
