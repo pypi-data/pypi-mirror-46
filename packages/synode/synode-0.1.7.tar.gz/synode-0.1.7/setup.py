3877ac51de26a3d72d195afab85694d260576922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:48 +0000

    Add package synode/0.1.7/synode-0.1.7.tar.gz

---
+++ packages/synode/synode-0.1.7.tar.gz/synode-0.1.7/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="synode",
+    version="0.1.7",
+    author="Youngsung Kim",
+    author_email="grnydawn@gmail.com",
+    description="Syntax node Python library for programming languages",
+    long_description=long_description,
+    py_modules=["synode"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
