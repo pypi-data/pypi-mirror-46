87544140a4217993bec1a67f579a9cc84d530d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:42 +0000

    Add package adaptive-scheduler/0.1.2/adaptive_scheduler-0.1.2.zip

---
+++ packages/adaptive-scheduler/adaptive_scheduler-0.1.2.zip/adaptive_scheduler-0.1.2/adaptive_scheduler/__init__.py
@@ -0,0 +1,4 @@
+import adaptive_scheduler.client_support
+import adaptive_scheduler.server_support
+import adaptive_scheduler.slurm  # noqa: F401
+from adaptive_scheduler._version import __version__  # noqa: F401
