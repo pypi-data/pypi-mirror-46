6af82ed4c0ddc5954ae0fed617716321bb9a1c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:29 +0000

    Add package ecd/0.1.192/ecd-0.1.192.tar.gz

---
+++ packages/ecd/ecd-0.1.192.tar.gz/ecd-0.1.192/ecd/__init__.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+"""
+author: sixi
+datetime: 
+python version: 3.x
+summary: 
+install package:
+"""
+from __future__ import absolute_import
+__version__ = "0.1.192"
+
+from .main import main
+
---
+++ packages/ecd/ecd-0.1.192.tar.gz/ecd-0.1.192/ecd/password_manage/app/clipboard_monitor.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+"""
+author: sixi
+datetime: 
+python version: 3.x
+summary: 
+install package:
+"""
+import pyperclip
+import time
+from ecd.password_manage.app.handle_data import HandleData
+
+
+class MonitorClipboard(object):
+    def read(self):
+        return pyperclip.paste()
+
+    def write(self, txt: str):
+        pyperclip.copy(txt)
+
+
+def main():
+    mc = MonitorClipboard()
+    hd = HandleData()
+    pre_txt = None
+    pre_clp_txt = None
+    while 1:
+        time.sleep(0.2)
+        clp_txt = mc.read().strip().lower()
+        if clp_txt != pre_txt and clp_txt != pre_clp_txt:
+            _s_res = hd.search(clp_txt, 1)
+            if _s_res:
+                s_res = _s_res[0]['value']
+                pre_txt = clp_txt
+                pre_clp_txt = s_res
+                mc.write(s_res)
+            else:
+                print(r'D:\\tmp\\password_infos.xlsx 没有数据，不能查找到value')
+
+
+if __name__ == '__main__':
+    main()
