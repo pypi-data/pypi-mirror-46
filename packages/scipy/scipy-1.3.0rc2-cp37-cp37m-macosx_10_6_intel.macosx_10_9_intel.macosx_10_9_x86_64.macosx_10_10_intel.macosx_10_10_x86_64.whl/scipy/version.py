4fbb5318e75288ac331d74862fa1eef944ae73e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:33 +0000

    Add package scipy/1.3.0rc2/scipy-1.3.0rc2-cp37-cp37m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/scipy/scipy-1.3.0rc2-cp37-cp37m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.3.0rc2'
+version = '1.3.0rc2'
+full_version = '1.3.0rc2'
+git_revision = '80b786a4573d665d75ace51c75345a523c5788dd'
+release = True
+
+if not release:
+    version = full_version
