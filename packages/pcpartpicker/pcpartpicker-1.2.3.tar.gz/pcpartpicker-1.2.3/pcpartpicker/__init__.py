eabb920e4dbe287f67fef43003a6c6349c93923f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:43 +0000

    Add package pcpartpicker/1.2.3/pcpartpicker-1.2.3.tar.gz

---
+++ packages/pcpartpicker/pcpartpicker-1.2.3.tar.gz/pcpartpicker-1.2.3/pcpartpicker/__init__.py
@@ -0,0 +1,7 @@
+from .api import API
+from .parts import *
+
+__name__ = ["pcpartpicker"]
+__version__ = '1.2.3'
+__author__ = 'Jonathan Vusich'
+__email__ = 'jonathanvusich@gmail.com'
---
+++ packages/pcpartpicker/pcpartpicker-1.2.3.tar.gz/pcpartpicker-1.2.3/utils/api_test.py
@@ -0,0 +1,15 @@
+from pcpartpicker.api import API
+
+
+def main():
+    api = API()
+    api.set_concurrent_connections(1)
+    api.set_multithreading(False)
+    part_data = api.retrieve_all()
+    for data in part_data.values():
+        for part in data:
+            assert not part.model == ""
+
+
+if __name__ == "__main__":
+    main()
