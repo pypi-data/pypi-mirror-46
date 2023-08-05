472725afa3dbd9c0df78f3a6efdf19aeb55aaeae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package widgetastic.patternfly/0.1.1/widgetastic.patternfly-0.1.1.tar.gz

---
+++ packages/widgetastic.patternfly/widgetastic.patternfly-0.1.1.tar.gz/widgetastic.patternfly-0.1.1/testing/test_buttons.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from widgetastic.widget import View
+from widgetastic_patternfly import Button
+
+
+def test_button_click(browser):
+    class TestView(View):
+        any_button = Button()
+        button1 = Button('Default Normal')
+        button2 = Button(title='Destructive title')
+        button3 = Button(title='noText', classes=[Button.PRIMARY])
+
+    view = TestView(browser)
+    assert view.any_button.is_displayed
+    assert view.button1.is_displayed
+    assert view.button2.is_displayed
+    assert view.button3.is_displayed
+
+    FILL_DICT = {"any_button": True, "button1": True, "button2": False, "button3": False}
+    assert view.fill(FILL_DICT)
