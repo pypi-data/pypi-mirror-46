1c12b2a748df524a1d1696a8e8d493c1a44b2100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package chariot-base/0.9.6/chariot_base-0.9.6.tar.gz

---
+++ packages/chariot-base/chariot_base-0.9.6.tar.gz/chariot_base-0.9.6/chariot_base/utilities/iotlwrap.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from iotl import interpreter
+
+class IoTLWrapper(object):
+    
+    def __init__(self, options):
+        self.filepath = options['filepath']
+        self.IoTState = interpreter.IoTState()
+
+    def load(self, data=None):
+        if data is None:
+            with open(self.filepath, 'r') as f:
+                self.IoTState.parse(f.read())
+        else:
+            self.IoTState.parse(data)
+        return True
+
+    def isSensitive(self, sensor_id):
+        try:
+            return self.IoTState.params[sensor_id]['privacySensitive'] == 1
+        except KeyError:
+            return False
+
+    def params(self, id):
+        try:
+            return self.IoTState.params[id]
+        except KeyError:
+            return {}
+
+    def acl(self, sensor_id):
+        try:
+            return self.IoTState.acl[sensor_id]
+        except KeyError:
+            return []
