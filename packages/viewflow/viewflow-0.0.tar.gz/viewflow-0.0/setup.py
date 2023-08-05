c9e54d0f35008ac7e4b9d6fe8911547d06d05b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:57 +0000

    Add package viewflow/0.0/viewflow-0.0.tar.gz

---
+++ packages/viewflow/viewflow-0.0.tar.gz/viewflow-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='viewflow',
+      version='0.0',
+      description='Package for managing YAML and Rmd views in Airflow.',
+      url='https://github.com/bakera81/viewflow',
+      author='Anthony Baker',
+      author_email='anthony@datacamp.com',
+      license='MIT',
+      packages=['viewflow'],
+      zip_safe=False)
