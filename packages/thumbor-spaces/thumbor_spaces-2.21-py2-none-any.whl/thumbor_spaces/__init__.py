1b25fc8416d55132d9a7f591c8adccdc6eebea7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:29 +0000

    Add package thumbor-spaces/2.21/thumbor_spaces-2.21-py2-none-any.whl

---
+++ packages/thumbor-spaces/thumbor_spaces-2.21-py2-none-any.whl/thumbor_spaces/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+try:
+    from thumbor.config import Config
+    Config.define('SPACES_KEY', 'xxxxxx', 'Spaces api key', 'SPACES')
+    Config.define('SPACES_SECRET', 'xxxxxx', 'Spaces api secret', 'SPACES')
+    Config.define('SPACES_REGION', 'xxxxxx', 'Spaces api region', 'SPACES')
+    Config.define('SPACES_ENDPOINT', 'xxxxxx', 'Spaces api endpoint', 'SPACES')
+    Config.define('SPACES_BUCKET', 'xxxxxx', 'Spaces api endpoint', 'SPACES')
+    Config.define('SPACES_ACL', 'public-read', 'Spaces image acl', 'SPACES')
+    Config.define('SPACES_RESULT_FOLDER', 'result_storage', 'Spaces result storage folder', 'SPACES')
+    Config.define('SPACES_LOADER_FOLDER', 'storage', 'Spaces loader storage folder', 'SPACES')
+    
+except ImportError:
+    #Thumbor isn't installed yet
+    pass
+
+__version__ = "2.21"
