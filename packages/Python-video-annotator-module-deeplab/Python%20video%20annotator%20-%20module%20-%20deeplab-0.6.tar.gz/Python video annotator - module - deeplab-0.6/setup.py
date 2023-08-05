fdbe18ca78ca5adba80abc41c67bee121f7e1bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:56 +0000

    Add package Python-video-annotator-module-deeplab/0.6/Python%20video%20annotator%20-%20module%20-%20deeplab-0.6.tar.gz

---
+++ packages/Python-video-annotator-module-deeplab/Python%20video%20annotator%20-%20module%20-%20deeplab-0.6.tar.gz/Python video annotator - module - deeplab-0.6/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import re
+
+setup(
+	name='Python video annotator - module - deeplab',
+	version=0.6,
+	description="""""",
+	author=['Manuel Manso'],
+	author_email='manel_manso@hotmail.com',
+	url='https://bitbucket.org/fchampalimaud/pythonvideoannotator-module-deeplab',
+	packages=find_packages(),
+	install_requires=['pyyaml']
+)
