15d374e13d530b5b1447d32a5c5e38d2b0d13ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:57 +0000

    Add package ansictrls/0.2.0/ansictrls-0.2.0.tar.gz

---
+++ packages/ansictrls/ansictrls-0.2.0.tar.gz/ansictrls-0.2.0/src/ansictrls/__init__.py
@@ -0,0 +1,26 @@
+from .ctrls import (CS, EraseMode, move, home, erase, hide_cursor,
+                    save_pos, alternate_screen, nestable_save_pos_possible)
+from .sgr import SGR, len_diff, text, fg, bg, sgr_string, sgr_print
+from .spec import set_attributes_delimiters, parse
+
+__version__ = '0.2.0'
+
+__all__ = ['CS',
+           'SGR',
+           'EraseMode',
+           'erase',
+           'move',
+           'home',
+           'hide_cursor',
+           'nestable_save_pos_possible',
+           'save_pos',
+           'alternate_screen',
+           'fg',
+           'bg',
+           'sgr_string',
+           'sgr_print',
+           'text',
+           'len_diff',
+           'set_attributes_delimiters',
+           'parse',
+           ]
