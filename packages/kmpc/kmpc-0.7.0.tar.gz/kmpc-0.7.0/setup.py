b5e53959e03b4e61c2e234cf5c222183e8e7bb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:24 +0000

    Add package kmpc/0.7.0/kmpc-0.7.0.tar.gz

---
+++ packages/kmpc/kmpc-0.7.0.tar.gz/kmpc-0.7.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from kmpc.version import VERSION, VERSION_STR
+
+setup(
+    version=VERSION_STR,
+    download_url='https://gitlab.com/eratosthene/kmpc/archive/'
+                 + VERSION_STR
+                 + '.tar.gz',
+    setup_requires=['setuptools>=38.4.0'],
+    setup_cfg=True
+)
