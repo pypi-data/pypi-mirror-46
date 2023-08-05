4c23312bd7ad19bb296cf5b931a457b9f062630b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:50 +0000

    Add package tarball-deploy/0.0.7.dev6/tarball-deploy-0.0.7.dev6.tar.gz

---
+++ packages/tarball-deploy/tarball-deploy-0.0.7.dev6.tar.gz/tarball-deploy-0.0.7.dev6/tarball_deploy/main.py
@@ -0,0 +1,26 @@
+import argparse
+
+from tarball_deploy import __version__
+from tarball_deploy.workdir import WorkdirType
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {__version__}"
+    )
+    parser.add_argument("--workdir", required=True, type=WorkdirType())
+    action = parser.add_mutually_exclusive_group()
+    action.add_argument(
+        "--from", dest="tar_file", type=argparse.FileType(), default="-"
+    )
+    action.add_argument("--rollback", action="store_true")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    if args.rollback:
+        args.workdir.rollback()
+    else:
+        args.workdir.deploy(args.tar_file)
