2df99a69a869148882e2febf1119efe6fe816ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:08 +0000

    Add package mastercard-p2p/1.0.7/mastercard_p2p-1.0.7-py2-none-any.whl

---
+++ packages/mastercard-p2p/mastercard_p2p-1.0.7-py2-none-any.whl/mastercardp2p/__init__.py
@@ -0,0 +1,13 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardp2p.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardp2p.accountbalance import *
+from mastercardp2p.accountinfo import *
+from mastercardp2p.accountmapping import *
+from mastercardp2p.accountverification import *
+from mastercardp2p.paymenttransfer import *
+from mastercardp2p.transfernotification import *
