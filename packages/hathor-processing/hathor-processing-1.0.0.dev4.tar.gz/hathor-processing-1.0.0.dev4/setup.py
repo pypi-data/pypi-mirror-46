027257269623f6356fd9662aad195a437b4c0e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:09 +0000

    Add package hathor-processing/1.0.0.dev4/hathor-processing-1.0.0.dev4.tar.gz

---
+++ packages/hathor-processing/hathor-processing-1.0.0.dev4.tar.gz/hathor-processing-1.0.0.dev4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='hathor-processing',
+    version='1.0.0.dev4',
+    packages=['hathorprocessing'],
+    url='https://github.com/HathorTechnologies/hathor-processing',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    python_requires='>=3',
+    install_requires=[
+        'pandas',
+        'sqlalchemy',
+        'testcontainers'
+    ],
+)
