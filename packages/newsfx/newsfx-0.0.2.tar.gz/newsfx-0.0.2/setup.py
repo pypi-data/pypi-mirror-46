35faaf0907c4579b3b5d956ee7e70862a205e9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package newsfx/0.0.2/newsfx-0.0.2.tar.gz

---
+++ packages/newsfx/newsfx-0.0.2.tar.gz/newsfx-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='newsfx',
+    version='0.0.2',
+    author="HyperFX Tech Team",
+    author_email="coffee@hyperfx.tech",
+    description="Scraper news article in Viet Nam",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hyperfxtech/newsfx",
+    packages=setuptools.find_packages(),
+    py_modules=['newsfx'],
+    install_requires=[
+        'requests', 'lxml', 'bs4'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
