6f6e4e14a7574aa47d6065de41c38963e22e9e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:42 +0000

    Add package django-fernet-fields/0.6/django-fernet-fields-0.6.tar.gz

---
+++ packages/django-fernet-fields/django-fernet-fields-0.6.tar.gz/django-fernet-fields-0.6/CHANGES.rst
@@ -0,0 +1,40 @@
+CHANGES
+=======
+
+master (unreleased)
+-------------------
+
+0.5 (2017.02.22)
+----------------
+
+* Support Django 1.10.
+
+0.4 (2015.09.18)
+----------------
+
+* Add `isnull` lookup.
+
+
+0.3 (2015.05.29)
+----------------
+
+* Remove DualField and HashField. The only cases where they are useful, they
+  aren't secure.
+
+
+0.2.1 (2015.05.28)
+------------------
+
+* Fix issue getting IntegerField validators.
+
+
+0.2 (2015.05.28)
+----------------
+
+* Extract HashField for advanced lookup needs.
+
+
+0.1 (2015.05.27)
+----------------
+
+* Initial working version.
---
+++ packages/django-fernet-fields/django-fernet-fields-0.6.tar.gz/django-fernet-fields-0.6/setup.py
@@ -0,0 +1,44 @@
+from os.path import join
+from setuptools import setup, find_packages
+
+
+long_description = (
+    open('README.rst').read() + open('CHANGES.rst').read())
+
+
+def get_version():
+    with open(join('fernet_fields', '__init__.py')) as f:
+        for line in f:
+            if line.startswith('__version__ ='):
+                return line.split('=')[1].strip().strip('"\'')
+
+
+setup(
+    name='django-fernet-fields',
+    version=get_version(),
+    description="Fernet-encrypted model fields for Django",
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='ORCAS, Inc',
+    author_email='orcastech@orcasinc.com',
+    url='https://github.com/orcasgit/django-fernet-fields/',
+    packages=find_packages(),
+    install_requires=['Django>=1.11', 'cryptography>=0.9'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+)
