ba12867d71f75b2844fce1ee1ce6ab146256661a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:24 +0000

    Add package nesterpro/1.0.0/nesterpro-1.0.0.tar.gz

---
+++ packages/nesterpro/nesterpro-1.0.0.tar.gz/nesterpro-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterpro',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'rwaa',
+    author_email = 'rororaf15@gmail.com',
+    url = 'https://pypi.org/manage/projects/',
+    description = 'A simple printer of nested lists')
