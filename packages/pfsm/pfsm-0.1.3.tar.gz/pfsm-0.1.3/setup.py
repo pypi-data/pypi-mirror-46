2817d9edd3edb7a2629727af7d67d4990c85c9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package pfsm/0.1.3/pfsm-0.1.3.tar.gz

---
+++ packages/pfsm/pfsm-0.1.3.tar.gz/pfsm-0.1.3/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+setup(
+  name = 'pfsm',
+  packages = ['pfsm'],
+  version = '0.1.3',
+  license='GNU General Public License v3.0',
+  description = 'Python Fast Strings Matching',
+  author = 'ngshya',
+  author_email = 'ngshya@gmail.com',
+  url = 'https://github.com/ngshya/pfsm',
+  download_url = 'https://github.com/ngshya/pfsm/archive/v0.1.3.tar.gz',
+  keywords = ['strings matching', 'strings distance', 'cosine similarity', 'ngrams', 'tf-idf', 'fast', 'python'],
+  install_requires=[
+      'unidecode',
+      'scikit-learn',
+      'numpy', 
+      'scipy',
+      'pandas',
+      'sparse_dot_topn'
+      ],
+  package_data={
+        'notebooks': ['notebooks/*'],
+        'data': ['data/*'],
+        'scripts': ['scripts/*']
+  },
+  classifiers=[
+    'Development Status :: 3 - Alpha', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" 
+    'Intended Audience :: Developers',
+    'Topic :: Text Processing',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3.6'
+  ],
+)
+
+# To upload the package:
+# python setup.py sdist
+# twine upload dist/*
+# rm -R dist
