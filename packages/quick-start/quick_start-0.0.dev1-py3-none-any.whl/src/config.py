1d4a30dbe396754546b31f92277fec01fca8a6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:56 +0000

    Add package quick-start/0.0.dev1/quick_start-0.0.dev1-py3-none-any.whl

---
+++ packages/quick-start/quick_start-0.0.dev1-py3-none-any.whl/src/config.py
@@ -0,0 +1,15 @@
+# Arquivo responsável por gerenciar as configurações do usuário.
+
+from pathlib import Path
+
+from appdirs import user_config_dir
+from appdirs import user_data_dir
+
+
+base_dir = Path(__file__).parent
+project_root = base_dir.parent
+config_ini = project_root / "config.ini"
+current_work_directory = Path.cwd()
+sample_zip = project_root / 'data/sample.zip'
+
+
