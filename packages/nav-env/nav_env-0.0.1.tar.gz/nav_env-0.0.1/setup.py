d897c2a6c5d18b106a467b0e8c0dd7ce481e8414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:56 +0000

    Add package nav-env/0.0.1/nav_env-0.0.1.tar.gz

---
+++ packages/nav-env/nav_env-0.0.1.tar.gz/nav_env-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nav_env',
+      version='0.0.1',
+      install_requires=[
+            'gym',
+            'lgblkb-tools',
+            ]  # And any other dependencies foo needs
+)
