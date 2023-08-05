01490ebad23241b472638c99e39d864dfd28f006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:10 +0000

    Add package randovania/0.26.2/randovania-0.26.2-py3-none-any.whl

---
+++ packages/randovania/randovania-0.26.2-py3-none-any.whl/randovania/__init__.py
@@ -0,0 +1,13 @@
+import sys
+from pathlib import Path
+
+
+def get_data_path() -> Path:
+    if getattr(sys, "frozen", False):
+        file_dir = Path(getattr(sys, "_MEIPASS"))
+    else:
+        file_dir = Path(__file__).parent
+    return file_dir.joinpath("data")
+
+
+VERSION = "0.26.2"
