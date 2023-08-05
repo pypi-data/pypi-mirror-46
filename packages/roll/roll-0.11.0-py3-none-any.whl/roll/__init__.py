0c7a19212b4fb67931f6df6379fc4e03b6e33888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package roll/0.11.0/roll-0.11.0-py3-none-any.whl

---
+++ packages/roll/roll-0.11.0-py3-none-any.whl/roll/__init__.py
@@ -0,0 +1,146 @@
+"""Howdy fellow developer!
+
+We are glad you are taking a look at our code :-)
+Make sure to check out our documentation too:
+http://roll.readthedocs.io/en/latest/
+
+If you do not understand why something is not working as expected,
+please submit an issue (or even better a pull-request with at least
+a test failing): https://github.com/pyrates/roll/issues/new
+"""
+
+import inspect
+from collections import namedtuple
+from http import HTTPStatus
+
+from autoroutes import Routes
+
+from .http import Cookies, Files, Form, HttpError, HTTPProtocol, Query
+from .io import Request, Response
+from .websocket import ConnectionClosed  # noqa. Exposed for convenience.
+from .websocket import WSProtocol
+
+Route = namedtuple('Route', ['payload', 'vars'])
+HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'TRACE', 'OPTIONS', 'CONNECT',
+                'PATCH']
+
+
+class Roll(dict):
+    """Deal with routes dispatching and events listening.
+
+    You can subclass it to set your own `Protocol`, `Routes`, `Query`, `Form`,
+    `Files`, `Request`, `Response` and/or `Cookies` class(es).
+    """
+    HttpProtocol = HTTPProtocol
+    WebsocketProtocol = WSProtocol
+    Routes = Routes
+    Query = Query
+    Form = Form
+    Files = Files
+    Request = Request
+    Response = Response
+    Cookies = Cookies
+
+    def __init__(self):
+        self.routes = self.Routes()
+        self.hooks = {}
+
+    async def startup(self):
+        await self.hook('startup')
+
+    async def shutdown(self):
+        await self.hook('shutdown')
+
+    async def __call__(self, request: Request, response: Response):
+        try:
+            if not await self.hook('request', request, response):
+                if not request.route.payload:
+                    raise HttpError(HTTPStatus.NOT_FOUND, request.path)
+                # Uppercased in order to only consider HTTP verbs.
+                if request.method.upper() not in request.route.payload:
+                    raise HttpError(HTTPStatus.METHOD_NOT_ALLOWED)
+                handler = request.route.payload[request.method]
+                await handler(request, response, **request.route.vars)
+        except Exception as error:
+            await self.on_error(request, response, error)
+        try:
+            # Views exceptions should still pass by the response hooks.
+            await self.hook('response', request, response)
+        except Exception as error:
+            await self.on_error(request, response, error)
+        return response
+
+    async def on_error(self, request: Request, response: Response, error):
+        if not isinstance(error, HttpError):
+            error = HttpError(HTTPStatus.INTERNAL_SERVER_ERROR,
+                              str(error).encode())
+        response.status = error.status
+        response.body = error.message
+        try:
+            await self.hook('error', request, response, error)
+        except Exception as e:
+            response.status = HTTPStatus.INTERNAL_SERVER_ERROR
+            response.body = str(e)
+
+    def factory(self):
+        return self.HttpProtocol(self)
+
+    def lookup(self, request):
+        request.route = Route(*self.routes.match(request.path))
+
+    def _get_protocol_class(self, protocol):
+        klass_attr = protocol.title() + 'Protocol'
+        klass = getattr(self, klass_attr, None)
+        assert klass, (f'No class handler declared for {protocol} protocol. '
+                       f'Add a {klass_attr} key to your Roll app.')
+        return klass
+
+    def route(self, path: str, methods: list=None,
+              protocol: str='http', **extras: dict):
+
+        protocol_class = self._get_protocol_class(protocol)
+        # Computed at load time for perf.
+        extras['protocol'] = protocol
+        extras['_protocol_class'] = protocol_class
+
+        def add_route(view):
+            nonlocal methods
+            if inspect.isclass(view):
+                inst = view()
+                if methods is not None:
+                    raise AttributeError("Can't use `methods` with class view")
+                payload = {}
+                for method in HTTP_METHODS:
+                    key = f"on_{method.lower()}"
+                    func = getattr(inst, key, None)
+                    if func:
+                        payload[method] = func
+                if not payload:
+                    raise ValueError(f"Empty view: {view}")
+            else:
+                if methods is None:
+                    methods = ['GET']
+                payload = {method: view for method in methods}
+            payload.update(extras)
+            if protocol_class.ALLOWED_METHODS:
+                assert set(methods) <= set(protocol_class.ALLOWED_METHODS)
+            self.routes.add(path, **payload)
+            return view
+
+        return add_route
+
+    def listen(self, name: str):
+        def wrapper(func):
+            self.hooks.setdefault(name, [])
+            self.hooks[name].append(func)
+        return wrapper
+
+    async def hook(self, name: str, *args, **kwargs):
+        try:
+            for func in self.hooks[name]:
+                result = await func(*args, **kwargs)
+                if result:  # Allows to shortcut the chain.
+                    return result
+        except KeyError:
+            # Nobody registered to this event, let's roll anyway.
+            pass
---
+++ packages/roll/roll-0.11.0-py3-none-any.whl/roll/testing.py
@@ -0,0 +1,233 @@
+import http.client
+import json
+import mimetypes
+import pytest
+
+from functools import partial
+from io import BytesIO
+from urllib.parse import urlencode
+from uuid import uuid4
+
+
+def encode_multipart(data, charset='utf-8'):
+    # Ported from Werkzeug testing.
+    boundary = f'---------------Boundary{uuid4().hex}'
+    body = BytesIO()
+
+    def write(string):
+        body.write(string.encode(charset))
+
+    if isinstance(data, dict):
+        data = data.items()
+
+    for key, values in data:
+        if not isinstance(values, (list, tuple)):
+            values = [values]
+        for value in values:
+            write(f'--{boundary}\r\n'
+                  f'Content-Disposition: form-data; name="{key}"')
+            reader = getattr(value, 'read', None)
+            if reader is not None:
+                filename = getattr(value, 'filename',
+                                   getattr(value, 'name', None))
+                content_type = getattr(value, 'content_type', None)
+                if content_type is None:
+                    content_type = filename and \
+                        mimetypes.guess_type(filename)[0] or \
+                        'application/octet-stream'
+                if filename is not None:
+                    write(f'; filename="{filename}"\r\n')
+                else:
+                    write('\r\n')
+                write(f'Content-Type: {content_type}\r\n\r\n')
+                while 1:
+                    chunk = reader(16384)
+                    if not chunk:
+                        break
+                    body.write(chunk)
+            else:
+                if not isinstance(value, str):
+                    value = str(value)
+                else:
+                    value = value.encode(charset)
+                write('\r\n\r\n')
+                body.write(value)
+            write('\r\n')
+    write(f'--{boundary}--\r\n')
+
+    body.seek(0)
+    content_type = f'multipart/form-data; boundary={boundary}'
+    return body.read(), content_type
+
+
+class Transport:
+
+    def __init__(self):
+        self.data = b''
+        self._closing = False
+
+    def is_closing(self):
+        return self._closing
+
+    def write(self, data):
+        self.data += data
+
+    def close(self):
+        self._closing = True
+
+
+class Client:
+
+    # Default content type for request body encoding, change it to your own
+    # taste if needed.
+    content_type = 'application/json; charset=utf-8'
+
+    def __init__(self, app):
+        self.app = app
+
+    def handle_files(self, kwargs):
+        kwargs.setdefault('headers', {})
+        files = kwargs.pop('files', None)
+        if files:
+            kwargs['headers']['Content-Type'] = 'multipart/form-data'
+            if isinstance(files, dict):
+                files = files.items()
+            for key, els in files:
+                if not els:
+                    continue
+                if not isinstance(els, (list, tuple)):
+                    # Allow passing a file instance.
+                    els = [els]
+                file_ = els[0]
+                if isinstance(file_, str):
+                    file_ = file_.encode()
+                if isinstance(file_, bytes):
+                    file_ = BytesIO(file_)
+                if len(els) > 1:
+                    file_.name = els[1]
+                if len(els) > 2:
+                    file_.charset = els[2]
+                kwargs['body'][key] = file_
+
+    def encode_body(self, body, headers):
+        content_type = headers.get('Content-Type')
+        if not body or isinstance(body, (str, bytes)):
+            return body, headers
+        if not content_type:
+            if self.content_type:
+                headers['Content-Type'] = content_type = self.content_type
+        if content_type:
+            if 'application/x-www-form-urlencoded' in content_type:
+                body = urlencode(body)
+            elif 'application/json' in content_type:
+                body = json.dumps(body)
+            elif 'multipart/form-data' in content_type:
+                body, headers['Content-Type'] = encode_multipart(body)
+            else:
+                raise NotImplementedError('Content-Type not supported')
+        return body, headers
+
+    async def request(self, path, method='GET', body=b'', headers=None,
+                      content_type=None):
+        headers = headers or {}
+        if content_type:
+            headers['Content-Type'] = content_type
+        body, headers = self.encode_body(body, headers)
+        if isinstance(body, str):
+            body = body.encode()
+        if body and 'Content-Length' not in headers:
+            headers['Content-Length'] = len(body)
+        self.protocol = self.app.factory()
+        self.protocol.connection_made(Transport())
+        headers = '\r\n'.join(f'{k}: {v}' for k, v in headers.items())
+        data = b'%b %b HTTP/1.1\r\n%b\r\n\r\n%b' % (
+            method.encode(), path.encode(), headers.encode(), body or b'')
+
+        self.protocol.data_received(data)
+        if self.protocol.task:
+            await self.protocol.task
+        return self.protocol.response
+
+    async def get(self, path, **kwargs):
+        return await self.request(path, method='GET', **kwargs)
+
+    async def head(self, path, **kwargs):
+        return await self.request(path, method='HEAD', **kwargs)
+
+    async def post(self, path, data=None, **kwargs):
+        kwargs.setdefault('body', data or {})
+        self.handle_files(kwargs)
+        return await self.request(path, method='POST', **kwargs)
+
+    async def put(self, path, data=None, **kwargs):
+        kwargs.setdefault('body', data or {})
+        self.handle_files(kwargs)
+        return await self.request(path, method='PUT', **kwargs)
+
+    async def patch(self, path, data=None, **kwargs):
+        kwargs.setdefault('body', data or {})
+        self.handle_files(kwargs)
+        return await self.request(path, method='PATCH', **kwargs)
+
+    async def delete(self, path, **kwargs):
+        return await self.request(path, method='DELETE', **kwargs)
+
+    async def options(self, path, **kwargs):
+        return await self.request(path, method='OPTIONS', **kwargs)
+
+    async def connect(self, path, **kwargs):
+        return await self.request(path, method='CONNECT', **kwargs)
+
+
+@pytest.fixture
+def client(app, event_loop):
+    app.loop = event_loop
+    app.loop.run_until_complete(app.startup())
+    yield Client(app)
+    app.loop.run_until_complete(app.shutdown())
+
+
+class LiveClient:
+
+    def __init__(self, app):
+        self.app = app
+        self.url = None
+        self.wsl = None
+
+    def start(self):
+        self.app.loop.run_until_complete(self.app.startup())
+        self.server = self.app.loop.run_until_complete(
+            self.app.loop.create_server(self.app.factory, '127.0.0.1', 0))
+        self.port = self.server.sockets[0].getsockname()[1]
+        self.url = f'http://127.0.0.1:{self.port}'
+        self.wsl = f'ws://127.0.0.1:{self.port}'
+
+    def stop(self):
+        self.server.close()
+        self.port = self.url = self.wsl = None
+        self.app.loop.run_until_complete(self.server.wait_closed())
+        self.app.loop.run_until_complete(self.app.shutdown())
+
+    def execute_query(self, method, uri, headers):
+        self.conn.request(method, uri, headers=headers)
+        response = self.conn.getresponse()
+        return response
+
+    async def query(self, method, uri, headers: dict=None):
+        if headers is None:
+            headers = {}
+
+        self.conn = http.client.HTTPConnection('127.0.0.1', self.port)
+        requester = partial(self.execute_query, method.upper(), uri, headers)
+        response = await self.app.loop.run_in_executor(None, requester)
+        self.conn.close()
+        return response
+
+
+@pytest.fixture()
+def liveclient(app, event_loop):
+    app.loop = event_loop
+    client = LiveClient(app)
+    client.start()
+    yield client
+    client.stop()
