0a1d886879eaa4bd8764d11e6cfdfd02440a16bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package workflowwebtools/0.10.0/workflowwebtools-0.10.0.tar.gz

---
+++ packages/workflowwebtools/workflowwebtools-0.10.0.tar.gz/workflowwebtools-0.10.0/setup.py
@@ -0,0 +1,40 @@
+import glob
+import setuptools
+
+import workflowwebtools
+
+setuptools.setup(
+    name='workflowwebtools',
+    version=workflowwebtools.__version__,
+    packages=setuptools.find_packages(),
+    author='Daniel Abercrombie',
+    author_email='dabercro@mit.edu',
+    description='Provides a server for Production and Reprocessing operations',
+    url='https://github.com/CMSCompOps/WorkflowWebTools',
+    scripts=[s for s in glob.glob('bin/*') if not s.endswith('~')],
+    package_data={
+        'workflowwebtools': ['default/config.yml'],
+        'workflowwebtools.web': ['static/*/*',
+                                 'templates/*']
+        },
+    install_requires=[
+        'cmstoolbox>=0.12.0',
+        'more-itertools<6.0.0',
+        'cherrypy<18.0.0',
+        'mako',
+        'numpy>=1.6.1',
+        'scipy==1.1.0',
+        'scikit-learn==0.20.3',
+        'passlib>=1.6',
+        'bcrypt',
+        'pyOpenSSL',
+        'pyyaml',
+        'validators',
+        'tabulate',
+        'pymongo<3.5.0',
+        'cx_Oracle',
+        'pandas',
+        'keras',
+        'tensorflow<2.0'
+        ]
+    )
