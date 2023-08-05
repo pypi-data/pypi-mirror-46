3499fd3b9dae47a5ba00af4d33ba71b3b29ff0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:02 +0000

    Add package spiketoolkit/0.2.2/spiketoolkit-0.2.2.tar.gz

---
+++ packages/spiketoolkit/spiketoolkit-0.2.2.tar.gz/spiketoolkit-0.2.2/spiketoolkit/comparison/__init__.py
@@ -0,0 +1,5 @@
+from .sortingcomparison import compare_two_sorters
+from .multisortingcomparison import compare_multiple_sorters
+from .gathersortingcomparison import gather_sorting_comparison
+from .comparisontools import do_matching, do_score_labels, do_counting, do_confusion_matrix, compare_spike_trains
+
