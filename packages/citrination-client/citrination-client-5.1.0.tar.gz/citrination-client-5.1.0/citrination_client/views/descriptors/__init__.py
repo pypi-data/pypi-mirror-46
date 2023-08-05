18a11a3f07ddef02aa5719039887eba5bd26c0ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:01 +0000

    Add package citrination-client/5.1.0/citrination-client-5.1.0.tar.gz

---
+++ packages/citrination-client/citrination-client-5.1.0.tar.gz/citrination-client-5.1.0/citrination_client/views/descriptors/__init__.py
@@ -0,0 +1,6 @@
+from .alloy_composition_descriptor import AlloyCompositionDescriptor
+from .categorical_descriptor import CategoricalDescriptor
+from .inorganic_descriptor import InorganicDescriptor
+from .organic_descriptor import OrganicDescriptor
+from .real_descriptor import RealDescriptor
+from .formulation_descriptor import FormulationDescriptor
