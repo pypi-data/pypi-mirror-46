e7c30639507bed5b7aedb41fb96dd8605820f789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:46 +0000

    Add package terminology/1.0.0/terminology-1.0.0.tar.gz

---
+++ packages/terminology/terminology-1.0.0.tar.gz/terminology-1.0.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['terminology']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'terminology',
+    'version': '1.0.0',
+    'description': 'An intuitive way to color terminal text',
+    'long_description': '',
+    'author': 'Juan Gonzalez',
+    'author_email': 'jrg2156@gmail.com',
+    'url': 'https://github.com/juanrgon/terminology',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
