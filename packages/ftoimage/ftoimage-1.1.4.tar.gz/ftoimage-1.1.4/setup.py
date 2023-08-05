bd843210e35288440a31950798bb9d91eb5ea875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:55 +0000

    Add package ftoimage/1.1.4/ftoimage-1.1.4.tar.gz

---
+++ packages/ftoimage/ftoimage-1.1.4.tar.gz/ftoimage-1.1.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ftoimage',
+    version='1.1.4',
+    packages=['', 'resume_text', 'image_convertion'],
+    url='https://pypi.org/project/ftoimage/',
+    license='',
+    author='crow',
+    author_email='',
+    description='Convert a file to .jpg',
+    long_description=open('README.md').read()
+)
