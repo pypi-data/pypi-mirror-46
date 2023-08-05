5f8b2009fc63cc8b42bdf6b10c49573db96716b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:39 +0000

    Add package xyz01/0.2/xyz01-0.2.tar.gz

---
+++ packages/xyz01/xyz01-0.2.tar.gz/xyz01-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='xyz01',
+      version='0.2',
+      description='The test for pypi',
+      url='https://github.com',
+      author='Test ',
+      author_email='test@test.com',
+      license='MIT',
+      packages=['xyz01'],
+      zip_safe=False)
