0117eace7a9836f2b01505cded641dfd1df2e816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package easytoolset/0.8/easytoolset-0.8.tar.gz

---
+++ packages/easytoolset/easytoolset-0.8.tar.gz/easytoolset-0.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='easytoolset',
+      version='0.8',
+      description='The easy tools in the world',
+      long_description=readme(),
+      url='http://github.com/maxis1314',
+      author='Daniel',
+      author_email='maxis1314@163.com',
+      #scripts=['bin/funniest-joke'],
+      entry_points = {
+        'console_scripts': ['joke=easytoolset.command_line:main'],
+      },
+      license='MIT',
+      packages=['easytoolset'],
+      install_requires=[
+          'markdown',
+      ],
+      zip_safe=False)
