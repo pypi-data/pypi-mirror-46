4324bedcb3c5f3416f5baaf588c5a15c93173290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:24 +0000

    Add package vmi/0.1.4/vmi-0.1.4-py3-none-any.whl

---
+++ packages/vmi/vmi-0.1.4-py3-none-any.whl/vmi/vocc.py
@@ -0,0 +1,427 @@
+from OCC.Core.AIS import *
+from OCC.Core.Adaptor2d import *
+from OCC.Core.Adaptor3d import *
+from OCC.Core.Addons import *
+from OCC.Core.AdvApp2Var import *
+from OCC.Core.AdvApprox import *
+from OCC.Core.AppBlend import *
+from OCC.Core.AppCont import *
+from OCC.Core.AppDef import *
+from OCC.Core.AppParCurves import *
+from OCC.Core.AppStd import *
+from OCC.Core.AppStdL import *
+from OCC.Core.Approx import *
+from OCC.Core.ApproxInt import *
+from OCC.Core.Aspect import *
+from OCC.Core.BOPAlgo import *
+from OCC.Core.BOPCol import *
+from OCC.Core.BOPDS import *
+from OCC.Core.BOPInt import *
+from OCC.Core.BOPTools import *
+from OCC.Core.BRep import *
+from OCC.Core.BRepAdaptor import *
+from OCC.Core.BRepAlgo import *
+from OCC.Core.BRepAlgoAPI import *
+from OCC.Core.BRepApprox import *
+from OCC.Core.BRepBlend import *
+from OCC.Core.BRepBndLib import *
+from OCC.Core.BRepBuilderAPI import *
+from OCC.Core.BRepCheck import *
+from OCC.Core.BRepClass import *
+from OCC.Core.BRepClass3d import *
+from OCC.Core.BRepExtrema import *
+from OCC.Core.BRepFeat import *
+from OCC.Core.BRepFill import *
+from OCC.Core.BRepFilletAPI import *
+from OCC.Core.BRepGProp import *
+from OCC.Core.BRepIntCurveSurface import *
+from OCC.Core.BRepLProp import *
+from OCC.Core.BRepLib import *
+from OCC.Core.BRepMAT2d import *
+from OCC.Core.BRepMesh import *
+from OCC.Core.BRepOffset import *
+from OCC.Core.BRepOffsetAPI import *
+from OCC.Core.BRepPrim import *
+from OCC.Core.BRepPrimAPI import *
+from OCC.Core.BRepProj import *
+from OCC.Core.BRepSweep import *
+from OCC.Core.BRepTools import *
+from OCC.Core.BRepTopAdaptor import *
+from OCC.Core.BSplCLib import *
+from OCC.Core.BSplSLib import *
+from OCC.Core.BiTgte import *
+from OCC.Core.Bisector import *
+from OCC.Core.Blend import *
+from OCC.Core.BlendFunc import *
+from OCC.Core.Bnd import *
+from OCC.Core.BndLib import *
+from OCC.Core.CDF import *
+from OCC.Core.CDM import *
+from OCC.Core.CPnts import *
+from OCC.Core.CSLib import *
+from OCC.Core.ChFi2d import *
+from OCC.Core.ChFi3d import *
+from OCC.Core.ChFiDS import *
+from OCC.Core.ChFiKPart import *
+from OCC.Core.Contap import *
+from OCC.Core.Convert import *
+from OCC.Core.Dico import *
+from OCC.Core.Draft import *
+from OCC.Core.DsgPrs import *
+from OCC.Core.Dynamic import *
+from OCC.Core.ElCLib import *
+from OCC.Core.ElSLib import *
+from OCC.Core.Expr import *
+from OCC.Core.ExprIntrp import *
+from OCC.Core.Extrema import *
+from OCC.Core.FEmTool import *
+from OCC.Core.FSD import *
+from OCC.Core.FairCurve import *
+from OCC.Core.FilletSurf import *
+from OCC.Core.GC import *
+from OCC.Core.GCE2d import *
+from OCC.Core.GCPnts import *
+from OCC.Core.GEOMAlgo import *
+from OCC.Core.GProp import *
+from OCC.Core.GccAna import *
+from OCC.Core.GccEnt import *
+from OCC.Core.GccGeo import *
+from OCC.Core.GccInt import *
+from OCC.Core.GccIter import *
+from OCC.Core.Geom import *
+from OCC.Core.Geom2d import *
+from OCC.Core.Geom2dAPI import *
+from OCC.Core.Geom2dAdaptor import *
+from OCC.Core.Geom2dConvert import *
+from OCC.Core.Geom2dGcc import *
+from OCC.Core.Geom2dHatch import *
+from OCC.Core.Geom2dInt import *
+from OCC.Core.Geom2dLProp import *
+from OCC.Core.GeomAPI import *
+from OCC.Core.GeomAbs import *
+from OCC.Core.GeomAdaptor import *
+from OCC.Core.GeomConvert import *
+from OCC.Core.GeomFill import *
+from OCC.Core.GeomInt import *
+from OCC.Core.GeomLProp import *
+from OCC.Core.GeomLib import *
+from OCC.Core.GeomPlate import *
+from OCC.Core.GeomProjLib import *
+from OCC.Core.GeomToStep import *
+from OCC.Core.GeomTools import *
+from OCC.Core.GraphDS import *
+from OCC.Core.GraphTools import *
+from OCC.Core.Graphic3d import *
+from OCC.Core.HLRAlgo import *
+from OCC.Core.HLRAppli import *
+from OCC.Core.HLRBRep import *
+from OCC.Core.HLRTopoBRep import *
+from OCC.Core.Hatch import *
+from OCC.Core.HatchGen import *
+from OCC.Core.Hermit import *
+from OCC.Core.IFSelect import *
+from OCC.Core.IGESCAFControl import *
+from OCC.Core.IGESControl import *
+from OCC.Core.Image import *
+from OCC.Core.IncludeLibrary import *
+from OCC.Core.IntAna import *
+from OCC.Core.IntAna2d import *
+from OCC.Core.IntCurve import *
+from OCC.Core.IntCurveSurface import *
+from OCC.Core.IntCurvesFace import *
+from OCC.Core.IntImp import *
+from OCC.Core.IntImpParGen import *
+from OCC.Core.IntPatch import *
+from OCC.Core.IntPoly import *
+from OCC.Core.IntPolyh import *
+from OCC.Core.IntRes2d import *
+from OCC.Core.IntStart import *
+from OCC.Core.IntSurf import *
+from OCC.Core.IntTools import *
+from OCC.Core.IntWalk import *
+from OCC.Core.Interface import *
+from OCC.Core.InterfaceGraphic import *
+from OCC.Core.Intf import *
+from OCC.Core.Intrv import *
+from OCC.Core.LProp import *
+from OCC.Core.LProp3d import *
+from OCC.Core.Law import *
+from OCC.Core.LocOpe import *
+from OCC.Core.LocalAnalysis import *
+from OCC.Core.MAT import *
+from OCC.Core.MAT2d import *
+from OCC.Core.MMgt import *
+from OCC.Core.Materials import *
+from OCC.Core.MeshVS import *
+from OCC.Core.Message import *
+from OCC.Core.NCollection import *
+from OCC.Core.NIS import *
+from OCC.Core.NLPlate import *
+from OCC.Core.OSD import *
+from OCC.Core.PCDM import *
+from OCC.Core.PLib import *
+from OCC.Core.Plate import *
+from OCC.Core.Plugin import *
+from OCC.Core.Poly import *
+from OCC.Core.Precision import *
+from OCC.Core.Primitives import *
+from OCC.Core.ProjLib import *
+from OCC.Core.Prs3d import *
+from OCC.Core.PrsMgr import *
+from OCC.Core.Quantity import *
+from OCC.Core.RWStepAP203 import *
+from OCC.Core.RWStepAP214 import *
+from OCC.Core.RWStepBasic import *
+from OCC.Core.RWStepDimTol import *
+from OCC.Core.RWStepElement import *
+from OCC.Core.RWStepFEA import *
+from OCC.Core.RWStepGeom import *
+from OCC.Core.RWStepRepr import *
+from OCC.Core.RWStepShape import *
+from OCC.Core.RWStepVisual import *
+from OCC.Core.RWStl import *
+from OCC.Core.Resource import *
+from OCC.Core.STEPCAFControl import *
+from OCC.Core.STEPConstruct import *
+from OCC.Core.STEPControl import *
+from OCC.Core.STEPEdit import *
+from OCC.Core.STEPSelections import *
+from OCC.Core.Select3D import *
+from OCC.Core.SelectBasics import *
+from OCC.Core.SelectMgr import *
+from OCC.Core.ShapeAlgo import *
+from OCC.Core.ShapeAnalysis import *
+from OCC.Core.ShapeBuild import *
+from OCC.Core.ShapeConstruct import *
+from OCC.Core.ShapeCustom import *
+from OCC.Core.ShapeExtend import *
+from OCC.Core.ShapeFix import *
+from OCC.Core.ShapeProcess import *
+from OCC.Core.ShapeProcessAPI import *
+from OCC.Core.ShapeUpgrade import *
+from OCC.Core.SortTools import *
+from OCC.Core.Standard import *
+from OCC.Core.StdFail import *
+from OCC.Core.StdPrs import *
+from OCC.Core.StdSelect import *
+from OCC.Core.StepAP203 import *
+from OCC.Core.StepAP209 import *
+from OCC.Core.StepAP214 import *
+from OCC.Core.StepBasic import *
+from OCC.Core.StepDimTol import *
+from OCC.Core.StepElement import *
+from OCC.Core.StepFEA import *
+from OCC.Core.StepGeom import *
+from OCC.Core.StepRepr import *
+from OCC.Core.StepShape import *
+from OCC.Core.StepToGeom import *
+from OCC.Core.StepToTopoDS import *
+from OCC.Core.StepVisual import *
+from OCC.Core.StlAPI import *
+from OCC.Core.StlMesh import *
+from OCC.Core.StlTransfer import *
+from OCC.Core.Storage import *
+from OCC.Core.Sweep import *
+from OCC.Core.TColGeom import *
+from OCC.Core.TColGeom2d import *
+from OCC.Core.TColQuantity import *
+from OCC.Core.TColStd import *
+from OCC.Core.TColgp import *
+from OCC.Core.TCollection import *
+from OCC.Core.TDF import *
+from OCC.Core.TDataStd import *
+from OCC.Core.TDataXtd import *
+from OCC.Core.TDocStd import *
+from OCC.Core.TFunction import *
+from OCC.Core.TNaming import *
+from OCC.Core.TPrsStd import *
+from OCC.Core.TShort import *
+from OCC.Core.TopAbs import *
+from OCC.Core.TopBas import *
+from OCC.Core.TopClass import *
+from OCC.Core.TopCnx import *
+from OCC.Core.TopExp import *
+from OCC.Core.TopLoc import *
+from OCC.Core.TopOpeBRep import *
+from OCC.Core.TopOpeBRepBuild import *
+from OCC.Core.TopOpeBRepDS import *
+from OCC.Core.TopOpeBRepTool import *
+from OCC.Core.TopTools import *
+from OCC.Core.TopTrans import *
+from OCC.Core.TopoDS import *
+from OCC.Core.TopoDSToStep import *
+from OCC.Core.Transfer import *
+from OCC.Core.TransferBRep import *
+from OCC.Core.UTL import *
+from OCC.Core.Units import *
+from OCC.Core.UnitsAPI import *
+from OCC.Core.V3d import *
+from OCC.Core.Visual3d import *
+from OCC.Core.Visualization import *
+from OCC.Core.Voxel import *
+from OCC.Core.XBRepMesh import *
+from OCC.Core.XCAFApp import *
+from OCC.Core.XCAFDoc import *
+from OCC.Core.XCAFPrs import *
+from OCC.Core.XSControl import *
+from OCC.Core.gce import *
+from OCC.Core.gp import *
+from OCC.Core.math import *
+
+import tempfile
+import pathlib
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+import vtk
+from vmi.vask import *
+from vmi.view import *
+
+
+class Shape(QObject, Menu):
+    def __init__(self, name=None):
+        QObject.__init__(self)
+        self.name = name if name else self.tr('形状 （Shape)')
+
+        Menu.__init__(self, name=self.name)
+        self.actions['ExportSTEP'] = self.menu.addAction(self.tr('导出 (Export) STEP'))
+        self.actions['ExportSTEP'].triggered.connect(self.exportSTEP)
+        self.actions['ExportSTL'] = self.menu.addAction(self.tr('导出 (Export) STL'))
+        self.actions['ExportSTL'].triggered.connect(self.exportSTL)
+
+        self._Shape = TopoDS_Shape()
+        self._Dataset = vtk.vtkPolyData()
+
+    def __setstate__(self, s):
+        self.__init__(s['name'])
+        self.__dict__.update(s)
+        s = self.__dict__
+
+    def __getstate__(self):
+        s = self.__dict__.copy()
+        for kw in ['menu', 'actions', '__METAOBJECT__']:
+            if kw in s:
+                del s[kw]
+        return s
+
+    def importSTEP(self, file=None):
+        if file is None:
+            file = askOpenFile(self.tr('导入 (Import) STEP'), '*.stp')
+            if file is None:
+                return
+        r = STEPControl_Reader()
+        if IFSelect_RetDone == r.ReadFile(file):
+            r.TransferRoots()
+            self._Shape = r.OneShape()
+            return True
+        return False
+
+    def exportSTEP(self, file=None):
+        if file is None:
+            file = askSaveFile(self.tr('导出 (Export) STEP'), '*.stp')
+            if file is None:
+                return
+        w = STEPControl_Writer()
+        w.Transfer(self._Shape, STEPControl_AsIs)
+        status = w.Write(file)
+        if status == IFSelect_RetDone:
+            return True
+        return False
+
+    def exportSTL(self, file=None):
+        if file is None:
+            file = askSaveFile(self.tr('导出 (Export) STL'), '*.stl')
+            if file is None:
+                return
+        return stlapi.Write(self._Shape, file, False)
+
+    def remesh(self):
+        shape = self._Shape
+        if shape is None:
+            return
+
+        pts = vtk.vtkPoints()
+        polydata = vtk.vtkPolyData()
+        polydata.SetPoints(pts)
+        polydata.Allocate()
+
+        # 1. 离散化
+        breptools.Clean(shape)
+
+        discrete = BRepMesh_IncrementalMesh(shape, 1e-3, True, 0.5, True)
+        if not discrete.IsNull():
+            discrete.Perform()
+
+        # 2. 转换Vertex
+        exp = TopExp_Explorer(shape, TopAbs_VERTEX)
+        while exp.More():
+            vertex = topods.Vertex(exp.Current())
+            exp.Next()
+
+            pnt = BRep_Tool.Pnt(vertex)
+            id = polydata.GetPoints().InsertNextPoint(pnt.X(), pnt.Y(), pnt.Z())
+            polydata.InsertNextCell(vtk.VTK_VERTEX, 1, [id])
+
+        # 3. 转换Edge
+        exp = TopExp_Explorer(shape, TopAbs_EDGE)
+        while exp.More():
+            edge = topods.Edge(exp.Current())
+            exp.Next()
+
+            if edge.IsNull() or BRep_Tool.Degenerated(edge):
+                continue
+
+            p_on_tri = Handle_Poly_PolygonOnTriangulation()
+            triangulation = Handle_Poly_Triangulation()
+            loc = TopLoc_Location()
+            BRep_Tool.PolygonOnTriangulation(edge, p_on_tri, triangulation, loc, 1)
+            p_3d = None
+
+            if p_on_tri is None:
+                p_3d = BRep_Tool.Polygon3D(edge, loc)
+
+            if p_3d is None and p_on_tri is None:
+                continue
+
+            trsf = loc.Transformation()
+
+            if not p_3d is None:
+                n = p_3d.NbNodes()
+                pnts = p_3d.Nodes()
+            else:
+                n = p_on_tri.NbNodes()
+                pnts = triangulation.Nodes()
+
+            if n > 1:
+                ids = vtk.vtkIdList()
+                for i in range(1, n + 1):
+                    pnt = pnts[i]
+                    pnt.Transform(trsf)
+                    id = polydata.GetPoints().InsertNextPoint(pnt.X(), pnt.Y(), pnt.Z())
+                    ids.InsertNextId(id)
+                polydata.InsertNextCell(vtk.VTK_POLY_LINE, ids)
+
+        # 4. 转换Face并合并
+        with tempfile.TemporaryDirectory() as p:
+            p = pathlib.Path(p) / '.stl'
+            stlapi.Write(shape, str(p), False)
+
+            r = vtk.vtkSTLReader()
+            r.SetFileName(str(p))
+            r.Update()
+
+            a = vtk.vtkAppendPolyData()
+            a.AddInputData(r.GetOutput())
+            a.AddInputData(polydata)
+            a.Update()
+
+            c = vtk.vtkCleanPolyData()
+            c.SetInputData(a.GetOutput())
+            c.Update()
+
+            self._Dataset.DeepCopy(c.GetOutput())
+
+
+if __name__ == '__main__':
+    s = Shape()
+    s.importSTEP('C:/Users/Medraw/Desktop/1.stp')
+    s.remesh()
