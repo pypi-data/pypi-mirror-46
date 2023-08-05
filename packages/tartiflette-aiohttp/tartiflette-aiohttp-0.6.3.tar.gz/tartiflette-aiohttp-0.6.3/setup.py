85fe8c5880067ca7ed08c71804e4f4861cba996e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:25 +0000

    Add package tartiflette-aiohttp/0.6.3/tartiflette-aiohttp-0.6.3.tar.gz

---
+++ packages/tartiflette-aiohttp/tartiflette-aiohttp-0.6.3.tar.gz/tartiflette-aiohttp-0.6.3/setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+
+
+_TEST_REQUIRE = [
+    "pytest",
+    "pytest-cov",
+    "pytest-asyncio",
+    "asynctest",
+    "pytz",
+    "pylint==2.3.1",
+    "xenon",
+    "black==19.3b0",
+]
+
+_VERSION = "0.6.3"
+
+_PACKAGES = find_packages(exclude=["tests*"])
+
+setup(
+    name="tartiflette-aiohttp",
+    version=_VERSION,
+    description="Runs a Tartiflette GraphQL Engine through aiohttp",
+    long_description=open("README.md").read(),
+    url="https://github.com/dailymotion/tartiflette-aiohttp",
+    author="Dailymotion Core API Team",
+    author_email="team@tartiflette.io",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    keywords="api graphql protocol api rest relay tartiflette dailymotion",
+    packages=_PACKAGES,
+    install_requires=[
+        "aiohttp~=3.4",
+        "tartiflette<0.10.0,>=0.6.5,!=0.7.0,!=0.7.1,!=0.7.2",
+    ],
+    tests_require=_TEST_REQUIRE,
+    extras_require={"test": _TEST_REQUIRE},
+    include_package_data=True,
+)
