74611fc3820374457f84157884b0585c65f52ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:15 +0000

    Add package skafos/0.1.3/skafos-0.1.3.tar.gz

---
+++ packages/skafos/skafos-0.1.3.tar.gz/skafos-0.1.3/setup.py
@@ -0,0 +1,42 @@
+import os
+import setuptools
+
+
+def read(fname):
+  """Utility function to read the README and VERSION files."""
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+with open('requirements.txt', 'r') as r:
+    REQS = r.read().splitlines()
+
+
+setuptools.setup(
+  name="skafos",
+  packages=setuptools.find_packages(),
+  version=read('skafos/VERSION'),
+  license="Apache Software License",
+  description="Python wrapper for loading, fetching, and listing model versions with the Skafos platform.",
+  long_description=read('README.md'),
+  long_description_content_type="text/markdown",
+  author="Skafos, LLC",
+  author_email="skafos@skafos.ai",
+  url="https://github.com/skafos/skafossdk",
+  download_url='',
+  keywords=['machine learning delivery', 'mobile deployment', 'model versioning'],
+  install_requires=REQS,
+  include_package_data=True,
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3'
+  ],
+  project_urls={
+    "Dashboard": "https://dashboard.skafos.ai",
+    "SDK Documentation": "https://sdk.skafos.ai",
+    "Source": "https://github.com/skafos/skafossdk",
+    "Website": "https://skafos.ai"
+  },
+  python_requires=">=3"
+)
