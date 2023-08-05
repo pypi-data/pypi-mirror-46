17ba78c9b1b57fcd2bb05cb9b31c7b5338de1daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:19 +0000

    Add package ptdc/1.2.1/ptdc-1.2.1.tar.gz

---
+++ packages/ptdc/ptdc-1.2.1.tar.gz/ptdc-1.2.1/ptdc/__init__.py
@@ -0,0 +1,28 @@
+"""
+Python Twitter Data Collector library initialization
+
+:copyright: Copyright since 2019 Lamparelli Andrea, all rights reserved
+:license: MIT, see LICENSE for more details.
+"""
+import logging
+
+from ptdc.collector import Collector, AccountCollector, StatusCollector
+from ptdc.streamer import OnlineStreamer
+from ptdc.support import authenticate
+from ptdc import data
+
+__version__ = '1.2.1'
+__author__ = 'Andrea Lamparelli'
+__license__ = "MIT"
+
+__all__ = [
+    'AccountCollector',
+    'StatusCollector',
+    'Collector',
+    'data',
+    'OnlineStreamer',
+    'authenticate',
+    '__version__'
+]
+
+logging.basicConfig(level=logging.WARNING, format='%(levelname)s:%(module)s.%(funcName)s.%(lineno)d:%(message)s')
