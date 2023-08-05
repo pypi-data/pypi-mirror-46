f2661457d5415bfaa47aa3ce5ab8ca814dd3331e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:04 +0000

    Add package pybase100/0.3a0/pybase100-0.3a0.tar.gz

---
+++ packages/pybase100/pybase100-0.3a0.tar.gz/pybase100-0.3a0/setup.py
@@ -0,0 +1,18 @@
+import setuptools 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='pybase100',
+      version='0.3a',
+      description='A Python version of Base100 encoding mechanism',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/MasterGroosha/pybase100',
+      author='Evgeny Petrov',
+      author_email='groosha@protonmail.com',
+      license='The Unlicence',
+      packages=setuptools.find_packages(),
+      zip_safe=False
+)
