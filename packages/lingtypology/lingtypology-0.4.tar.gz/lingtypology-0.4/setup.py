81d1a4694444134b7ec68f950366091b0aac61c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:52 +0000

    Add package lingtypology/0.4/lingtypology-0.4.tar.gz

---
+++ packages/lingtypology/lingtypology-0.4.tar.gz/lingtypology-0.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='lingtypology',
+    version='0.4',
+    description='A linguistic tool for interactive mapping.',
+    url='https://github.com/OneAdder/lingtypology',
+    author='Michael Voronov',
+    author_email='mikivo@list.ru',
+    license='GPLv3',
+    packages=['lingtypology'],
+    python_requires='>=3.5',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+          'folium',
+          'branca',
+          'jinja2',
+          'pandas',
+          'pyglottolog',
+    ],
+    extras_require={
+        'test': [
+            'pytest>=3.6',
+            'pytest-cov',
+            'coverage>=4.2',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+) 
