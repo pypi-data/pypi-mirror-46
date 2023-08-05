3e7f9d15c179ff48148429dbe5a4bf683fb2805d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package pyface/6.1.0/pyface-6.1.0.tar.gz

---
+++ packages/pyface/pyface-6.1.0.tar.gz/pyface-6.1.0/pyface/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM PYFACE SETUP.PY
+version = '6.1.0'
+full_version = '6.1.0'
+git_revision = '4c2513c6'
+is_released = True
+
+if not is_released:
+    version = full_version
