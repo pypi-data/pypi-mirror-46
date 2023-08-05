2f018cf0ca0bc748c4dcc192102a32e0262019aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:50 +0000

    Add package star-lighting/0.5/star-lighting-0.5.tar.gz

---
+++ packages/star-lighting/star-lighting-0.5.tar.gz/star-lighting-0.5/StarLighting/__init__.py
@@ -0,0 +1,4 @@
+from .excel import Excel
+from .mail import Server
+
+name = 'StarLight'
---
+++ packages/star-lighting/star-lighting-0.5.tar.gz/star-lighting-0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='star-lighting',
+    version='0.5',
+    author='duxinn',
+    author_email="xiamin_1990@yahoo.com",
+    description='simplely use python',
+    long_description='simplely use python',
+    packages=['StarLighting'],
+    install_requires=['numpy', 'xlwt', 'xlrd', 'openpyxl']
+)
+
+
+
+
+
