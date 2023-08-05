07bc050814a802a445c0923c5301d8f52887df87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:11 +0000

    Add package django-identity-external/0.6.1/django-identity-external-0.6.1.tar.gz

---
+++ packages/django-identity-external/django-identity-external-0.6.1.tar.gz/django-identity-external-0.6.1/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+	README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+	name = 'django-identity-external',
+	version = '0.6.1',
+	packages = find_packages(),
+	include_package_data = True,
+	license = 'Apache 2.0 License',
+	description = 'Django middleware for handling of external identities.',
+	long_description = README,
+	long_description_content_type = 'text/x-rst',
+	url = 'https://github.com/adelton/django-identity-external',
+	author = 'Jan Pazdziora',
+	author_email = 'jan.pazdziora@django.adelton.com',
+	install_requires = [
+		'django>=2.0',
+	],
+	classifiers = [
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		# New PersistentRemoteUserMiddleware in Django 1.9
+		'Framework :: Django :: 2.0',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: Apache Software License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Topic :: Internet :: WWW/HTTP',
+		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+	],
+)
