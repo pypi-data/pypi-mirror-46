32a6210853ae6b6553f65784a76807cb6e82d7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:27 +0000

    Add package kuber/1.7.3/kuber-1.7.3-py2.py3-none-any.whl

---
+++ packages/kuber/kuber-1.7.3-py2.py3-none-any.whl/kuber/latest/__init__.py
@@ -0,0 +1,18 @@
+import datetime as _datetime
+
+from kuber import versioning
+
+# Information about the kubernetes API library version associated with this
+# package and kuber metadata around its creation and labeling within the
+# kuber library.
+KUBERNETES_VERSION = versioning.KubernetesVersion(
+    label='latest',
+    version='v1.14.1',
+    major='1',
+    minor='14',
+    patch='1',
+    pre_release='',
+    build='',
+    created_at=_datetime.datetime(2019, 5, 11),
+    commit_sha='b7394102d6ef778017f2ca4046abbaa23b88c290'
+)
