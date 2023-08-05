233de8704923da0d02eda25b7f15c3b46427453c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:43 +0000

    Add package catalyst/19.6rc1/catalyst-19.6rc1-py2.py3-none-any.whl

---
+++ packages/catalyst/catalyst-19.6rc1-py2.py3-none-any.whl/catalyst/contrib/criterion/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from torch.nn.modules.loss import *
+from .bcece import *
+from .ce import *
+from .center_loss import *
+from .contrastive import *
+from .dice import *
+from .focal_loss import *
+from .huber import *
+from .unet import *
+from .iou import *
