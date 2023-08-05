0396badfe50b1060a806910cbb71792b18d2a9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:45 +0000

    Add package amtool/0.1.0/amtool-0.1.0.tar.gz

---
+++ packages/amtool/amtool-0.1.0.tar.gz/amtool-0.1.0/README.rst
@@ -0,0 +1,37 @@
+========================
+Artifact Management Tool
+========================
+
+
+.. image:: https://img.shields.io/pypi/v/amtool.svg
+        :target: https://pypi.python.org/pypi/amtool
+
+.. image:: https://img.shields.io/travis/bellockk/amtool.svg
+        :target: https://travis-ci.org/bellockk/amtool
+
+.. image:: https://readthedocs.org/projects/amtool/badge/?version=latest
+        :target: https://amtool.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+
+
+The Artifacts Management Tool is meant to be a generic means of storing and manipulating artifact data in a human readable text format ideal for colaborative work.
+
+
+* Free software: MIT license
+* Documentation: https://amtool.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ packages/amtool/amtool-0.1.0.tar.gz/amtool-0.1.0/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=6.0', ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Kenneth E. Bellock",
+    author_email='ken@bellock.net',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="The Artifacts Management Tool is meant to be a generic means of storing and manipulating artifact data in a human readable text format ideal for colaborative work.",
+    entry_points={
+        'console_scripts': [
+            'amtool=amtool.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='amtool',
+    name='amtool',
+    packages=find_packages(include=['amtool']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/bellockk/amtool',
+    version='0.1.0',
+    zip_safe=False,
+)
