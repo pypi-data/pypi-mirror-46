9bd7452bdbff9e87c584110d87f3dbfb6d655f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:58 +0000

    Add package dcd-sdk/0.0.14/dcd-sdk-0.0.14.tar.gz

---
+++ packages/dcd-sdk/dcd-sdk-0.0.14.tar.gz/dcd-sdk-0.0.14/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    README = f.read()
+
+with open('LICENSE') as f:
+    sdk_license = f.read()
+
+setup(
+    name='dcd-sdk',
+    version='0.0.14',
+    author='Jacky Bourgeois',
+    author_email='jacky@datacentricdesign.org',
+    description='A Python SDK to interact with the Data-Centric Design Hub',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/datacentricdesign/dcd-sdk-python',
+    license=sdk_license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    data_files = [('', ['LICENSE','requirements.txt'])],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Environment :: Console',
+        'Topic :: Software Development :: Build Tools',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ]
+)
