bd26ec01871f6027c90aff67ff9dca086b84497e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:02 +0000

    Add package lino/19.5.1/lino-19.5.1.tar.gz

---
+++ packages/lino/lino-19.5.1.tar.gz/lino-19.5.1/bash/pull.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright 2015-2016 Luc Saffre
+# License: BSD (see file COPYING for details)
+#
+# Run `git pull` on all repositories used by this project. Also remove
+# all `*.pyc` files.
+#
+
+set -e
+
+date >> freeze.log
+pip freeze >> freeze.log
+
+function pull_here() {
+    pwd
+    git pull
+    find -name '*.pyc' -exec rm -f {} +
+}
+
+REPOS=/path/to/your/repositories
+
+cd $REPOS/lino ; pull_here
+cd $REPOS/xl ; pull_here
+cd $REPOS/cosi ; pull_here
+cd $REPOS/voga ; pull_here
+
