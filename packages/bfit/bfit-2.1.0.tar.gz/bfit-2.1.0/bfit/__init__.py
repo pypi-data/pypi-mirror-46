078c8186d06522b16054b4789f586564de57f422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:08 +0000

    Add package bfit/2.1.0/bfit-2.1.0.tar.gz

---
+++ packages/bfit/bfit-2.1.0.tar.gz/bfit-2.1.0/bfit/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['gui','fitting']
+__version__ = '2.1.0'
+__author__ = 'Derek Fujimoto'
+logger_name = 'bfit'
---
+++ packages/bfit/bfit-2.1.0.tar.gz/bfit-2.1.0/bfit/fitting/__init__.py
@@ -0,0 +1,8 @@
+__all__=['functions',
+         'global_fitter',
+         'global_bdata_fitter',
+         'PulsedFns',
+         'pulsed',
+         'continuous',
+         'fitter',
+         'fit_bdata']
