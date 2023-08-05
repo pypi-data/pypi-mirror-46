e5ff8d0323a2c4a27acb45af408bd87f067837e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:09 +0000

    Add package MASlite/2019.5.9.45023/MASlite-2019.5.9.45023.tar.gz

---
+++ packages/MASlite/MASlite-2019.5.9.45023.tar.gz/MASlite-2019.5.9.45023/setup.py
@@ -0,0 +1,29 @@
+"""
+MasLite
+"""
+from datetime import datetime
+from setuptools import setup
+
+v = datetime.now()
+__version__ = "{}.{}.{}.{}".format(v.year, v.month, v.day, v.hour * 3600 + v.minute*60 + v.second)
+
+setup(
+    name="MASlite",
+    version=__version__,
+    url="https://github.com/root-11/maslite",
+    license="MIT",
+    author="Bjorn Madsen",
+    author_email="bjorn.madsen@operationsresearchgroup.com",
+    description="A lightweight multi-agent systems",
+    packages=["maslite"],
+    platforms="any",
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
