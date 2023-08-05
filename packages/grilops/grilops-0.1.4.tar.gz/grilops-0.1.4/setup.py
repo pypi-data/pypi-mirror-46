0fdfc2bfc04599cede952f3b134cb66333502df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:45 +0000

    Add package grilops/0.1.4/grilops-0.1.4.tar.gz

---
+++ packages/grilops/grilops-0.1.4.tar.gz/grilops-0.1.4/setup.py
@@ -0,0 +1,20 @@
+"""grilops setup"""
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+    name="grilops",
+    version="0.1.4",
+    description="GRId LOgic Puzzle Solver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/obijywk/grilops",
+    author="Matt Gruskin",
+    author_email="matthew.gruskin@gmail.com",
+    license="MIT",
+    packages=["grilops"],
+    zip_safe=False
+)
