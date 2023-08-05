f5868a4bb93c555c194fec83e973cfa5ae12ec9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:56 +0000

    Add package ictrainer/0.2.6/ictrainer-0.2.6.tar.gz

---
+++ packages/ictrainer/ictrainer-0.2.6.tar.gz/ictrainer-0.2.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('readme.md', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+setup(name="ictrainer",
+      version="0.2.6",
+      description="cli tool for training your own image classifier with one line command!",
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      url="https://github.com/koji/ictrainer",
+      author='koji kanao',
+      author_email='koji.kanao@nyu.edu',
+      license="MIT",
+      packages=["ictrainer"],
+      scripts=["bin/ictrainer"],
+      install_requires=["docopt==0.6.2", "keras", "numpy", "tensorflow", "icrawler", "Pillow", "opencv-python", "numpy", "matplotlib"])
