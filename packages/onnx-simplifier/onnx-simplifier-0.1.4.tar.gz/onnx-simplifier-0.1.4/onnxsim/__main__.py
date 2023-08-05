36391ae48ffbaa4df3bdb97cd4ffe905af0654e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:37 +0000

    Add package onnx-simplifier/0.1.4/onnx-simplifier-0.1.4.tar.gz

---
+++ packages/onnx-simplifier/onnx-simplifier-0.1.4.tar.gz/onnx-simplifier-0.1.4/onnxsim/__main__.py
@@ -0,0 +1,22 @@
+import argparse
+
+import onnx
+import onnxsim
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_model', help='Input ONNX model')
+    parser.add_argument('output_model', help='Output ONNX model')
+    parser.add_argument('check_n', help='Check whether the output is correct with n random inputs',
+                        nargs='?', type=int, default=0)
+    args = parser.parse_args()
+    print("Simplifying...")
+    model_opt = onnxsim.simplify(args.input_model, check_n=args.check_n)
+
+    onnx.save(model_opt, args.output_model)
+    print("Ok!")
+
+
+if __name__ == '__main__':
+    main()
