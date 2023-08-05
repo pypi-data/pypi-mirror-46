13eebd39898dca0acdbab050afa05e89d1229b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:15 +0000

    Add package DevlObject/1.1.2/DevlObject-1.1.2.tar.gz

---
+++ packages/DevlObject/DevlObject-1.1.2.tar.gz/DevlObject-1.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="DevlObject",
+	version="1.1.2",
+	description="Convert a Json To a Object with Pretty Print",
+	author="DevL Aditya Nugraha",
+	author_email="adityana987@gmail.com",
+	url="https://github.com/devladityanugraha/DevlObject",
+	classifiers=[
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Utilities",
+		"Topic :: Internet",
+	]
+)
