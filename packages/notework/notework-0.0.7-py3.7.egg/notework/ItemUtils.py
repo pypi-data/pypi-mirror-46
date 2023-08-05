a3cdd0b5d107a5cb08a9f5a5c819ce3e9361e583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package notework/0.0.7/notework-0.0.7-py3.7.egg

---
+++ packages/notework/notework-0.0.7-py3.7.egg/notework/ItemUtils.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/05/08 18:56
+# @Author  : niuliangtao
+# @Site    : 
+# @File    : ItemUtils.py
+# @Software: PyCharm
+
+
+import json
+import logging
+from urllib import request
+
+logger = logging.getLogger('ItemUtils')
+
+__all__ = ['fill_item_info']
+
+
+def fill_item_info(item_list=None):
+    if item_list is None or not isinstance(item_list, list):
+        raise Exception('入参 item_list 是list')
+    items = ','.join(item_list)
+    url = 'http://pluto.vdian.net/solution/query?solutionId=1004&itemIdList=' + items
+    logger.debug('url:' + url)
+
+    f = request.urlopen(url)
+
+    response = json.loads(f.read())
+
+    logger.debug('response:' + str(response))
+    result = response['result']['result']
+    for res in result:
+        res['priceInfo'] = res['price']
+        res['price'] = res['priceInfo']['price']
+    return result
+
+
+def test():
+    res = fill_item_info(['2540122701'])
+    print(res)
