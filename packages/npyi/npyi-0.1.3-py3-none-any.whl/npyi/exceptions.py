eafd4a0b30087f4e89cf60baafc20cea9d0d062e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package npyi/0.1.3/npyi-0.1.3-py3-none-any.whl

---
+++ packages/npyi/npyi-0.1.3-py3-none-any.whl/npyi/exceptions.py
@@ -0,0 +1,38 @@
+class NPyIException(Exception):
+    """
+    Base exception for module
+    """
+
+    pass
+
+
+class InvalidSearchParamException(NPyIException):
+    """
+    Exception for invalid search parameters
+    """
+
+    pass
+
+
+class InvalidVersionException(NPyIException):
+    """
+    Exception for invalid NPPES API versions
+    """
+
+    pass
+
+
+class InvalidUseFirstNameAliasException(NPyIException):
+    """
+    Exception for invalid used_first_name_alias values
+    """
+
+    pass
+
+
+class InvalidAddressPurposeException(NPyIException):
+    """
+    Exception for invalid address_purpose values
+    """
+
+    pass
