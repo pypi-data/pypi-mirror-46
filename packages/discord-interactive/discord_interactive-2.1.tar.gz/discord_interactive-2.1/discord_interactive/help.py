89dc1de0b791726544899bf75017c8bd6558d92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:08 +0000

    Add package discord-interactive/2.1/discord_interactive-2.1.tar.gz

---
+++ packages/discord-interactive/discord_interactive-2.1.tar.gz/discord_interactive-2.1/discord_interactive/help.py
@@ -0,0 +1,136 @@
+from discord_interactive.page import Page
+import asyncio
+
+DEFAULT_QUIT_REACT = '❌'
+
+class Help:
+    """ Class representing the whole Help system.
+
+    Attributes:
+        client (Discord.Client): Discord client (to send messages). 
+        page_graph (Page): Page graph representing the whole help pages.
+        quit_react (str): Reaction used to leave the help system.. 
+    """
+
+    def __init__(self, client, page_graph, quit_react=DEFAULT_QUIT_REACT):
+        """ Help constructor
+
+        Args:
+            client (Discord.Client): Discord client (to send messages). 
+            page_graph (Page): Page graph representing the whole help pages.
+            quit_react (str): Reaction used to leave the help system.
+        """
+        self.client = client
+        self.page_graph = page_graph
+        self.quit_react = quit_react
+
+    async def display(self, member):
+        """ Main function of the Help system.
+
+        This function is the main function of the help system. When a user 
+        request help, simply call this function. 
+        It will display the first message of the help, and then wait the user to
+        react. Depending on the reaction, it will display the next page, etc...
+
+        Args:
+            member (Discord.Member): Member who called help. Help will be 
+                displayed as a private message to him.
+        """
+
+        current_page = self.page_graph
+        prev_input = []
+
+        # Never stop displaying help
+        while True:
+            # Run basic callbacks before displaying the page
+            for callback in current_page.get_callbacks():
+                await callback(current_page, member, prev_input)
+
+            # Send the current page to the user as private message
+            if member.dm_channel is None:
+                await member.create_dm()
+            bot_message = await member.dm_channel.send(current_page.content())
+
+            # Display possible reactions
+            for react in current_page.get_react_list() + [self.quit_react]:
+                await bot_message.add_reaction(react)
+
+            next_page = None
+            # While user give wrong reaction/input, keep waiting for better input
+            while next_page is None:
+                # Get user input
+                reaction, message = await self._get_user_input(member, \
+                                                               bot_message, \
+                                                               current_page)
+
+                # 2 cases : reaction or message
+                if reaction is not None and message is None:
+                    # If the user wants to quit, quit
+                    if reaction.emoji == self.quit_react:
+                        # Clean and quit. This is the only way to quit for now
+                        await bot_message.delete()
+                        return
+
+                    # Retrieve page based on reaction
+                    next_page = current_page.next_page(reaction.emoji)
+
+                elif reaction is None and message is not None:
+                    # Retrieve page
+                    next_page = current_page.next_page()
+
+            # Before going to next page, remember the input of the user if given
+            if message is not None:
+                prev_input.append(message)
+
+            # Here the next page is valid. Clean current message and loop
+            await bot_message.delete()
+            current_page = next_page
+
+    async def _get_user_input(self, member, message, current_page):
+        """ Function retrieving the user input.
+
+        This function retrieve the user input. The user input is either a 
+        message or a reaction (if the page does not need user input, only 
+        reaction will be an acceptable input). 
+        If both input and reaction are correct input, return the first event met.
+
+        Args:
+            member (Discord.Member): Member who called help. Help will be 
+                displayed as a private message to him.
+            message (Discord.Message): Message sent by the bot, where the user 
+                should react.
+            current_page (Page): Page being displayed. We know what kind of 
+                input we need from this page.
+
+        Returns:
+            reaction (Discord.Reaction): Reaction of the user, or None if the 
+                correct input was a user message.
+            message (Discord.Message): Message of the user, or None if the 
+                correct input was a user reaction.
+        """
+        def check_reaction(reaction, user):
+            return user == member
+
+        def check_message(m):
+            return m.author == member
+
+        task_react = asyncio.ensure_future(self.client.wait_for('reaction_add', \
+                        check=check_reaction))
+        task_answer = asyncio.ensure_future(self.client.wait_for('message', \
+                        check=check_message))
+        tasks = [task_react]    # Always wait for user reaction
+        if current_page.need_user_input():
+            tasks.append(task_answer)   # Sometimes need to expect input too
+        
+        # Wait the actual user input
+        done, _ = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+
+        # Depending on what the user did, return the right thing
+        if task_react in done:
+            # User reacted
+            reaction, _ = done.pop().result()
+            return reaction, None
+        else:
+            # User answered
+            msg = done.pop().result()
+            return None, msg
---
+++ packages/discord-interactive/discord_interactive-2.1.tar.gz/discord_interactive-2.1/discord_interactive/page.py
@@ -0,0 +1,179 @@
+import collections
+
+DEFAULT_PARENT_REACT = '🔙'
+DEFAULT_ROOT_REACT = '🔝'
+DEFAULT_LINK_REACTS = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣']
+
+class Page:
+    """ Class representing a page of the help.
+
+    This class represents a page of the help. A page is displayed to the user, 
+    and the user can naviguate through page using reaction. 
+    A page have several attributes : a message, and a map of linked pages, 
+    based on reaction of the user.
+
+    Attributes:
+        msg (str): Message to display to the user when displaying the page.
+        connector (str): String used to connect each description of each link.
+        links (dict of str to Page): List of linked pages, where each reaction 
+            ID is linkied to the appropriate page.
+        links_msg (list of str): List of messages for each link. 
+        meta_links (dict of str to Page): Similar to links, but contain special
+            links (root, parent) : reaction should be displayed at the end.
+        input_link (Page): Page to display if user input a message.
+        callbacks (list): List of callbacks to call when displaying the page.
+    """
+
+    def __init__(self, msg, root=None, parent=None, 
+            root_react=DEFAULT_ROOT_REACT, parent_react=DEFAULT_PARENT_REACT,
+            connector='\n\n', callbacks=[]):
+        """ Page constructor
+
+        Constructor of the class Page. Create a Page with a message. 
+        and optionally a root Page and a parent Page.
+        A page without root is a root itself. A page can have no parent if you
+        don't want to have a return function.
+
+        Args:
+            msg (str): Message to display to the user when displaying the page.
+            root (Page, optional): Root Page. Defaults to None.
+            parent (Page, optional): Parent Page. Defaults to None.
+            root_react (str, optional): Reaction to access the root page.
+            parent_react (str, optional): Reaction to access the parent page.
+            connector (str, optional): Connector to join each description of 
+                each link. Defaults to `\n`.
+            callbacks (list, optional): List of callbacks to be called when 
+                displaying page. Defaults to empty list.
+        """
+        self.msg = msg
+        self.connector = connector
+        self.links = collections.OrderedDict()
+        self.links_msg = []
+        self.meta_links = collections.OrderedDict()
+        self.input_link = None
+
+        if parent:
+            self.meta_links[parent_react] = parent
+        if root:
+            self.meta_links[root_react] = root
+
+        self.callbacks = callbacks
+
+    def add_link(self, page, msg=None, react=None, after_msg=False):
+        """ Method to add a link to the page.
+
+        Args:
+            page (Page): Page to link.
+            msg (str, optional): Description of this link. This message is 
+                supposed to help the user understand to which page he is going
+                to be redirected if he clicks this link. Defaults to None.
+            react (str, optional): Reaction to access the page. If None is given,
+                use defaults emoji (numbers). Defaults to None.
+            after_msg (bool, optional): If True, this link represent the next
+                page to display after a user input a message. Mutually exclusive
+                with the parameter react, and the parameter message (description
+                of the page should explain to user what to input). Defaults to 
+                False.
+        """
+        if after_msg:
+            if react is not None:
+                raise ValueError("No reaction should be given in the case of " \
+                                 "a link after a user input")
+            if msg is not None:
+                raise ValueError("No message should be given to describe a link" \
+                                 "after a user input")
+            self.input_link = page
+            return
+
+        if react is None:
+            # Use default emoji for reaction 
+            assert len(self.links) < len(DEFAULT_LINK_REACTS), \
+                "There is not enough default reaction emoji. Please specify " \
+                "emoji explicitly."
+            react = DEFAULT_LINK_REACTS[len(self.links)]
+
+        self.links[react] = page
+
+        # Add links message
+        if msg is None:
+            self.links_msg.append('')
+        else:
+            self.links_msg.append(msg)
+
+    def content(self):
+        """ Method to get the content of a page.
+
+        This method return the content of a page, concatenating the message of 
+        the page and the descriptions of each links of the page.
+
+        Returns:
+            str: The content of the page.
+        """
+        content = self.msg
+        for reaction, msg in zip(self.get_react_list(), self.links_msg):
+            content += self.connector + reaction + ' ' + msg
+        return content
+
+    def get_react_list(self):
+        """ Method to access the list of reactions of linked pages.
+
+        Returns:
+            list: List of str where each str represent a reaction of a linked 
+                page.
+        """
+        return list(self._all_links().keys())
+
+    def need_user_input(self):
+        """ Method to know if the Help display needs to wait for the user to 
+        input something. 
+
+        If the page contains a link to a page waiting for an input, then the 
+        helper needs to wait the user to input something. In other cases, just 
+        do the default behavior (wait for a reaction of the user).
+
+        Returns:
+            bool: True if there is some links with input, False otherwise.
+        """
+        return self.input_link is not None
+
+    def next_page(self, react=None):
+        """Accessing the next Page.
+
+        This function access the next page based on the reaction given. If the 
+        choice is not valid, None is returned.
+
+        Args:
+            react (str, optional): Reaction chosen by the user, representing a 
+                linked Page. If None, the page returned is the special input 
+                page. Default to None. 
+
+        Returns:
+            Page or None: The next page to display, based on the reaction of 
+                the user, or None if the choice of the user is not valid.
+        """
+        if react is None:
+            return self.input_link
+
+        try:
+            return self._all_links()[react]
+        except KeyError:
+            return None
+
+    def get_callbacks(self):
+        """Accessing the list of basic callbacks.
+
+        Returns:
+            List of callbacks: Basics callbacks to call before page is displayed.
+        """
+        return self.callbacks
+
+    def _all_links(self):
+        """ Private function
+
+        Return a concatenated dictionnary between links and meta-links.
+
+        Returns:
+            OrderedDict: Concatenated links and meta-links.
+        """
+        return collections.OrderedDict(list(self.links.items()) + \
+                                       list(self.meta_links.items()))
