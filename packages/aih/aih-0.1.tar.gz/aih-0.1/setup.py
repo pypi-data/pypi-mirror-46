679d7b4e97078b1bd1db18a0aa141fe9f160679e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:53 +0000

    Add package aih/0.1/aih-0.1.tar.gz

---
+++ packages/aih/aih-0.1.tar.gz/aih-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='aih',
+      version='0.1',
+      description='',
+      author='huy.ha',
+      author_email='huy.hq@vmodev.com',
+      license='MIT',
+      packages=['aih'],
+      install_requires=[
+          'PyJWT',
+          'requests'
+      ],
+      zip_safe=False)
