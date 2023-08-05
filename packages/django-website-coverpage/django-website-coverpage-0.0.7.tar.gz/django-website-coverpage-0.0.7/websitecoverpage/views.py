c9160a6c369c1db604b50f25ecf20616715b8c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:27 +0000

    Add package django-website-coverpage/0.0.7/django-website-coverpage-0.0.7.tar.gz

---
+++ packages/django-website-coverpage/django-website-coverpage-0.0.7.tar.gz/django-website-coverpage-0.0.7/websitecoverpage/views.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
+
+from .forms import CoverPageViewForm
+
+
+class CoverPageView(TemplateView):
+    def get_template_names(self):
+        config = getattr(settings, 'WEBSITE_COVERPAGE', {})
+        return [config.get('template', 'coverpage/coverpage.html')]
+
+    def post(self, request):
+        form = CoverPageViewForm(request.POST)
+        if form.is_valid():
+            # get config
+            config = getattr(settings, 'WEBSITE_COVERPAGE', {})
+            cookiename = config.get('cookiename', 'coverpage')
+
+            # set cookie and redirect
+            r = form.cleaned_data['redirect']
+            if len(r) == 0:
+                r = request.COOKIES.get('%s_referrer' % cookiename, '/')
+            response = redirect(r)
+            response.delete_cookie('%s_referrer' % cookiename)
+            response.set_cookie(cookiename, 1)
+            return response
+
+        # fallback
+        return self.get(request)
