ef86d2c8d6577fe17d3af534af7263387a5c4eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:12 +0000

    Add package ml-core/19.5.1198/ml_core-19.5.1198-py2.py3-none-any.whl

---
+++ packages/ml-core/ml_core-19.5.1198-py2.py3-none-any.whl/missinglink/core/exceptions.py
@@ -0,0 +1,17 @@
+# -*- coding: utf8 -*-
+
+
+class MissingLinkException(Exception):
+    pass
+
+
+class NonRetryException(MissingLinkException):
+    pass
+
+
+class AccessDenied(NonRetryException):
+    pass
+
+
+class NotFound(NonRetryException):
+    pass
