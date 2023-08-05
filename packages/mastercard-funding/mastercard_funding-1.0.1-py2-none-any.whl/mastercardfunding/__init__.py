e3e8ee56359dd76d18864e0a7322b32dd7d90988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:40 +0000

    Add package mastercard-funding/1.0.1/mastercard_funding-1.0.1-py2-none-any.whl

---
+++ packages/mastercard-funding/mastercard_funding-1.0.1-py2-none-any.whl/mastercardfunding/__init__.py
@@ -0,0 +1,9 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardfunding.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardfunding.funding import *
+from mastercardfunding.fundingreversal import *
