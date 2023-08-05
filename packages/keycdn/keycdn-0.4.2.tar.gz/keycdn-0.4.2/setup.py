bd21dbecf6b47e602d4b19ed7cf4d1ba155801e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package keycdn/0.4.2/keycdn-0.4.2.tar.gz

---
+++ packages/keycdn/keycdn-0.4.2.tar.gz/keycdn-0.4.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+  name = 'keycdn',
+  packages = ['keycdn'],
+  version = '0.4.2',
+  description = 'A Python REST Client for KeyCDN API',
+  long_description = open('README.md').read(),
+  long_description_content_type = 'text/markdown',
+  author = 'KeyCDN',
+  url = 'https://github.com/keycdn/python-keycdn-api',
+  keywords = ['cdn', 'content delivery network', 'keycdn'],
+  classifiers = [
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3'
+  ],
+)
