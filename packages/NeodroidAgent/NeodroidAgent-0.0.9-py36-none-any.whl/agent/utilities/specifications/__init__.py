b6916a3b3c99a264cc1583b30b5fb0df1ef0278c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:43 +0000

    Add package NeodroidAgent/0.0.9/NeodroidAgent-0.0.9-py36-none-any.whl

---
+++ packages/NeodroidAgent/NeodroidAgent-0.0.9-py36-none-any.whl/agent/utilities/specifications/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'cnheider'
+
+from agent.utilities.specifications.exclude.optimiser_specification import *
+from .training_resume import *
+from agent.utilities.specifications.exclude.architecture_specification import *
