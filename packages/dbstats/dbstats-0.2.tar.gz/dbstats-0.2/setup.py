c432f1755799bf91c35094bd87b66c31cd035fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:00 +0000

    Add package dbstats/0.2/dbstats-0.2.tar.gz

---
+++ packages/dbstats/dbstats-0.2.tar.gz/dbstats-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='dbstats',
+		version='0.2',
+		description='dbstats',
+        url='https://github.com/jgeofil/dbstats',
+        author='Jeremy Georges-Filteau',
+        author_email='jeremy@thehyve.nl',
+        license='MIT',
+        packages=['dbstats'],
+		install_requires=[
+			'psycopg2',
+			'sparse',
+			'tqdm',
+			'numpy'
+        ],
+        scripts=['bin/get-transmart-stats.py'],
+        zip_safe=False)
