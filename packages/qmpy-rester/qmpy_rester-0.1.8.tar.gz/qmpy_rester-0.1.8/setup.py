3ffb5578d073afc7e65da3353da57b3237bf8f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:42 +0000

    Add package qmpy-rester/0.1.8/qmpy_rester-0.1.8.tar.gz

---
+++ packages/qmpy-rester/qmpy_rester-0.1.8.tar.gz/qmpy_rester-0.1.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name="qmpy_rester",
+      version='0.1.8',
+      description="A python code to query OQMD data through oqmd-api",
+      url="https://github.com/mohanliu/qmpy_rester",
+      author="Mohan Liu",
+      author_email="mohan@u.northwestern.edu",
+      license="LICENSE",
+      packages=['qmpy_rester'],
+      long_description=open('README.md').read(),
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      install_requires=["requests"],
+      )
+
+    
