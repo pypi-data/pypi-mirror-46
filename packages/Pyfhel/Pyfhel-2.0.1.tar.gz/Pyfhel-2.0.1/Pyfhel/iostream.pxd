76a95f6d8b5d2df9941db029e1feb1f6e622127f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:02 +0000

    Add package Pyfhel/2.0.1/Pyfhel-2.0.1.tar.gz

---
+++ packages/Pyfhel/Pyfhel-2.0.1.tar.gz/Pyfhel-2.0.1/Pyfhel/iostream.pxd
@@ -0,0 +1,30 @@
+# distutils: language = c++
+#cython: language_level=3, boundscheck=False
+# -------------------------------- CIMPORTS ------------------------------------
+from libcpp.string cimport string
+
+# ---------------------------- CYTHON DECLARATION ------------------------------
+cdef extern from "<iostream>" namespace "std":
+    cdef cppclass ostream:
+        ostream& write(const char*, int) except +
+    cdef cppclass istream:
+        istream& read(char*, int) except +
+
+
+cdef extern from "<fstream>" namespace "std":
+    cdef cppclass ofstream(ostream):
+        # constructors
+        ofstream()except +
+        ofstream(const char*) except +
+        ofstream(const string&) except +
+        void open(const char*) except +
+        void open(const string&) except +
+        void close() except +
+    cdef cppclass ifstream(istream):
+        # constructors
+        ifstream()except +
+        ifstream(const char*) except +
+        ifstream(const string&) except +
+        void open(const char&) except +
+        void open(const string&) except +
+        void close() except +
---
+++ packages/Pyfhel/Pyfhel-2.0.1.tar.gz/Pyfhel-2.0.1/setup.py
@@ -0,0 +1,140 @@
+# --------------------------------- IMPORTS -----------------------------------
+# Create Extension modules written in C for Python
+from setuptools import setup, Extension, find_packages
+
+# Get directories for includes of both Python and Numpy
+from distutils.sysconfig import get_python_inc
+import numpy
+
+
+# Scan a directory searching for all C++ files
+import os
+def scan(dir, files=[]):
+    for file in os.listdir(dir):
+        path = os.path.join(dir, file)
+        if os.path.isfile(path) and path.endswith(".cpp"):
+            files.append(path)
+    return files
+
+# Reading version info from Readme and hardcoding it in the __init__.py file
+import fileinput
+import re
+
+v_readme_regex = r'\[\_*v([0-9]+\.[0-9]+\.[0-9a-z]+)\_*\]'
+with open('README.md') as readme:
+    VERSION = re.findall(v_readme_regex,readme.read())[0]
+    
+v_init_regex   = r'\"([0-9]+\.[0-9]+\.[0-9a-z]+)\"'
+with fileinput.FileInput('Pyfhel/__init__.py', inplace=True) as file:
+    for line in file:
+        print(re.sub(v_init_regex, '"'+VERSION+'"', line), end='')
+
+# Including Readme in the module as long description.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# ---------------------------------- OPTIONS ----------------------------------
+import sys
+
+LIBS= False
+if "--LIBS" in sys.argv:
+    LIBS= True
+    del sys.argv[sys.argv.index("--LIBS")]
+
+CYTHONIZE= False
+if "--CYTHONIZE" in sys.argv:
+    CYTHONIZE= True
+    del sys.argv[sys.argv.index("--CYTHONIZE")]
+    
+    
+    
+# ------------------------------- SETUP CONFIG --------------------------------
+# could run setup from anywhere
+
+PYFHEL_PATH = 'Pyfhel'
+AFHEL_PATH = os.path.join(PYFHEL_PATH, 'Afhel')
+SEAL_PATH = os.path.join(PYFHEL_PATH, 'SEAL', 'SEAL', 'seal')
+
+# ---------------------------- COMPILATION CONFIG -----------------------------
+# Including shared libraries
+# TODO: include libpython.a only in windows ? " -D MS_WIN64"
+libraries = ["seal", "afhel"] if LIBS else []
+local_sources = [] if LIBS else scan(SEAL_PATH,
+                                     [os.path.join(AFHEL_PATH, 'Afseal.cpp')])
+
+# Compile flags for extensions
+language            = "c++"
+include_dirs        = [get_python_inc(),numpy.get_include(),
+                       PYFHEL_PATH, AFHEL_PATH, SEAL_PATH]
+extra_compile_flags = ["-std=c++17", "-O3", "-DHAVE_CONFIG_H"]
+
+# -------------------------------- EXTENSIONS ---------------------------------
+ext = ".pyx" if CYTHONIZE else ".cpp"
+ext_modules = [
+         Extension(
+             name="Pyfhel.Pyfhel",
+             sources=[os.path.join(PYFHEL_PATH,"Pyfhel"+ext)]+local_sources,
+             libraries=libraries,
+             include_dirs=include_dirs,
+             language=language,
+             extra_compile_args=extra_compile_flags,
+         ),
+         Extension(
+             name="Pyfhel.PyPtxt",
+             sources=[os.path.join(PYFHEL_PATH,"PyPtxt"+ext)]+local_sources,
+             libraries=libraries,
+             include_dirs=include_dirs,
+             language=language,
+             extra_compile_args=extra_compile_flags,
+         ),
+         Extension(
+             name="Pyfhel.PyCtxt",
+             sources=[os.path.join(PYFHEL_PATH,"PyCtxt"+ext)]+local_sources,
+             libraries=libraries,
+             include_dirs=include_dirs,
+             language=language,
+             extra_compile_args=extra_compile_flags,
+         ),   
+]
+if CYTHONIZE:
+    from Cython.Build import cythonize
+    ext_modules=cythonize(ext_modules)
+
+# -------------------------------- INSTALLER ----------------------------------
+setup(
+    name            = "Pyfhel",
+    version         = VERSION,
+    author          = "Alberto Ibarrondo",
+    author_email    = "ibarrond@eurecom.fr",
+    description     = "Python for Homomorphic Encryption Libraries",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    keywords        = "homomorphic encryption cython cryptography",
+    license         = "GNU GPLv3",
+    url             = "https://github.com/ibarrond/Pyfhel",     
+    setup_requires  =["setuptools>=30.0",
+                      "numpy>=1.14.0"],
+    install_requires=["cython>=0.25.1",
+                      "numpy>=1.14.0"],
+    classifiers     =(
+        "Programming Language :: C++",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+    ),
+    zip_safe=False,
+    packages=find_packages(),
+    ext_modules=ext_modules,  
+    test_suite=os.path.join(PYFHEL_PATH,"test.py"),
+)
