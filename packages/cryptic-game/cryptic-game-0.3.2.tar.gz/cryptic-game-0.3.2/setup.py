27fb4a20ac9e6183803d2359eab8fd263048cfbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:25 +0000

    Add package cryptic-game/0.3.2/cryptic-game-0.3.2.tar.gz

---
+++ packages/cryptic-game/cryptic-game-0.3.2.tar.gz/cryptic-game-0.3.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("./README.md", "r") as f:
+    file = f.read()
+
+setup(name='cryptic-game',
+      version='0.3.2',
+      description='Microservice handler for cryptic-game',
+      author='cryptic-game',
+      author_email='faq@cryptic-game.net',
+      long_description=file,
+      long_description_content_type='text/markdown',
+      url='https://github.com/cryptic-game/python3-lib',
+      packages=find_packages(),
+      )
