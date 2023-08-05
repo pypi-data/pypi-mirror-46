56e99089358c6132bf11a392b5ef975aa59aab82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:51 +0000

    Add package miniworldmaker/0.9.58/miniworldmaker-0.9.58.tar.gz

---
+++ packages/miniworldmaker/miniworldmaker-0.9.58.tar.gz/miniworldmaker-0.9.58/miniworldmaker/tokens/number_token.py
@@ -0,0 +1,19 @@
+from miniworldmaker.tokens import text_token
+
+
+class NumberToken(text_token.TextToken):
+
+    def __init__(self, number, position):
+        super().__init__(str(number), position)
+        self.number = number
+
+    def inc(self):
+        self.number+= 1
+        self.set_text(str(self.number))
+
+    def set_number(self, number):
+        self.number = number
+        self.set_text(str(self.number))
+
+    def get_text(self):
+        return self.costume.text
