aeedaf3d2d72bba2932c05e787b43e4157a71bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:31 +0000

    Add package solaredge-local/0.1.4/solaredge_local-0.1.4.tar.gz

---
+++ packages/solaredge-local/solaredge_local-0.1.4.tar.gz/solaredge_local-0.1.4/solaredge_local/solaredge.py
@@ -0,0 +1,12 @@
+from uplink import Consumer, get
+from .maintenance_pb2 import Maintenance
+from .status_pb2 import Status
+
+class SolarEdge(Consumer):
+    @get("/web/v1/status")
+    def get_status(self) -> Status:
+        pass
+
+    @get("/web/v1/maintenance")
+    def get_optimizers(self) -> Maintenance:
+        pass
