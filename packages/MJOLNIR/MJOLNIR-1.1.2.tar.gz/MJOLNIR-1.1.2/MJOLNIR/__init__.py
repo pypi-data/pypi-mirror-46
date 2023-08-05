13a9a5c50033639d335406cfdc1ae5513c42a557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:49 +0000

    Add package MJOLNIR/1.1.2/MJOLNIR-1.1.2.tar.gz

---
+++ packages/MJOLNIR/MJOLNIR-1.1.2.tar.gz/MJOLNIR-1.1.2/MJOLNIR/__init__.py
@@ -0,0 +1,12 @@
+"""
+MJOLNIR Module
+^^^^^^^^^^^^^^
+
+Further documentation? Version is 1.0.10?
+"""
+#import Geometry, Statistic, Data
+
+import sys,os
+sys.path.append('.')
+
+__version__ = '1.1.2'
