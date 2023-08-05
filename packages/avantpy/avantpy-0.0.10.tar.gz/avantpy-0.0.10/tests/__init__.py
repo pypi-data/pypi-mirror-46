b78971790e9c4c6362261efa26966b36293023ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:08 +0000

    Add package avantpy/0.0.10/avantpy-0.0.10.tar.gz

---
+++ packages/avantpy/avantpy-0.0.10.tar.gz/avantpy-0.0.10/tests/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+
+# set up import hook
+import avantpy  # noqa
