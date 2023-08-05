5db50f6fe78d6976646dbf8f9a512efcf3f93cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:24 +0000

    Add package KuaiShouGetCnString/0.1.1/KuaiShouGetCnString-0.1.1.tar.gz

---
+++ packages/KuaiShouGetCnString/KuaiShouGetCnString-0.1.1.tar.gz/KuaiShouGetCnString-0.1.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: mage
+# Mail: mage@woodcol.com
+# Created Time:  2018-1-23 19:17:34
+#############################################
+
+
+from setuptools import setup, find_packages
+print(find_packages())
+setup(
+    name = "KuaiShouGetCnString",
+    version = "0.1.1",
+    keywords = ("pip", "KuaiShouGetCnString"),
+    description = "A font processing function ",
+    long_description = "A font processing function ",
+    license = "MIT Licence",
+
+    url = "https://github.com/jinmingzhou/KuaiShouGetCnString",
+    author = "robin",
+    author_email = "1983273232@qq.com",
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
