ef2a82493840efb20e9c05454da1f89e0612e309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package aegis/1.0.0/aegis-1.0.0.tar.gz

---
+++ packages/aegis/aegis-1.0.0.tar.gz/aegis-1.0.0/aegis/__init__.py
@@ -0,0 +1,35 @@
+"""
+aegis allows to protect endpoints
+and also provides authentication scoping.
+"""
+
+__version__ = '1.0.0'
+
+__all__ = [
+    "BaseAuthenticator", "JWTAuth", "BasicAuth",
+    "login_required", "permissions",
+    "AuthException", "AuthRequiredException",
+    "AuthenticationFailedException",
+    "TokenExpiredException",
+    "InvalidRefreshTokenException",
+    "ForbiddenException",
+    "InvalidTokenException",
+]
+from .authenticators.base import BaseAuthenticator
+from .authenticators.jwt import JWTAuth
+from .authenticators.basic import BasicAuth
+
+from .decorators import (
+    login_required,
+    permissions
+)
+
+from .exceptions import (
+    AuthException,
+    AuthRequiredException,
+    AuthenticationFailedException,
+    TokenExpiredException,
+    InvalidRefreshTokenException,
+    ForbiddenException,
+    InvalidTokenException,
+)
