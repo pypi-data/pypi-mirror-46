e14430614efb505ae275f3a0069070c0e316508e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:43 +0000

    Add package git-hooks-1c/9.1.1/git-hooks-1c-9.1.1.tar.gz

---
+++ packages/git-hooks-1c/git-hooks-1c-9.1.1.tar.gz/git-hooks-1c-9.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['git_hooks_1c']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cjk-commons>=3.3,<4.0', 'fleep>=1.0,<2.0', 'parse-1c-build>=5.5,<6.0']
+
+entry_points = \
+{'console_scripts': ['gh1c = git_hooks_1c.__main__:run']}
+
+setup_kwargs = {
+    'name': 'git-hooks-1c',
+    'version': '9.1.1',
+    'description': 'Git hooks utilities for 1C:Enterprise',
+    'long_description': '',
+    'author': 'Cujoko',
+    'author_email': 'cujoko@gmail.com',
+    'url': 'https://github.com/Cujoko/git-hooks-1c',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
