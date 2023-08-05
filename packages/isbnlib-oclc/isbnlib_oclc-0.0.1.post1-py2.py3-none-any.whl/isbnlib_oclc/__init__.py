4e6752d4c89ccb05f29f3ab61d0f01a472dc6b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:24 +0000

    Add package isbnlib-oclc/0.0.1.post1/isbnlib_oclc-0.0.1.post1-py2.py3-none-any.whl

---
+++ packages/isbnlib-oclc/isbnlib_oclc-0.0.1.post1-py2.py3-none-any.whl/isbnlib_oclc/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+from ._oclc import query
+
+__version__ = '0.0.1.post1'
+
+__plugin_main_language__ = 'en'
+__plugin_service_name__ = 'oclc'
+__plugin_type__ = 'metadata'
