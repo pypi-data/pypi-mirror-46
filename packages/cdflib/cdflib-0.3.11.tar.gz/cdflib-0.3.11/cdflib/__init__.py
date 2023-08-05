5d5a2b64ada5611f8821da3abbffb66020414882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:46 +0000

    Add package cdflib/0.3.11/cdflib-0.3.11.tar.gz

---
+++ packages/cdflib/cdflib-0.3.11.tar.gz/cdflib-0.3.11/cdflib/__init__.py
@@ -0,0 +1,18 @@
+import os
+from . import cdfread
+from . import cdfwrite
+from .epochs import CDFepoch as cdfepoch
+
+# This function determines if we are reading or writing a file
+
+
+def CDF(path, cdf_spec=None, delete=False, validate=None):
+    path = os.path.expanduser(path)
+    if (os.path.exists(path)):
+        if delete:
+            os.remove(path)
+            return
+        else:
+            return cdfread.CDF(path, validate=validate)
+    else:
+        return cdfwrite.CDF(path, cdf_spec=cdf_spec, delete=delete)
