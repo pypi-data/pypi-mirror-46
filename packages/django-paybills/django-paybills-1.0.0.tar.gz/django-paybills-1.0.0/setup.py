6c5ad44e9e76f64f488718c4f844ca03f03bfe90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:54 +0000

    Add package django-paybills/1.0.0/django-paybills-1.0.0.tar.gz

---
+++ packages/django-paybills/django-paybills-1.0.0.tar.gz/django-paybills-1.0.0/setup.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name="django-paybills",
+      version="1.0.0",
+      description="Django app for paypal integration.",
+      packages=["paybills"],
+      author="Iury O. G. Figueiredo",
+      author_email="ioliveira@id.uff.br",
+      url='',
+      download_url='',
+      keywords=[],
+      classifiers=[])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
