bcd5910222f9bcd734518b8754469a86b3274d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:45 +0000

    Add package jf-distributions/0.1/jf_distributions-0.1.tar.gz

---
+++ packages/jf-distributions/jf_distributions-0.1.tar.gz/jf_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jf_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jf_distributions'],
+      author='Jesse Fredrickson',
+      author_email = 'jfreds@bu.edu',
+      zip_safe=False)
