9297b1dd651e084f1d2789c44ea4ae721306cdd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:14 +0000

    Add package releasecmd/0.0.20/releasecmd-0.0.20.tar.gz

---
+++ packages/releasecmd/releasecmd-0.0.20.tar.gz/releasecmd-0.0.20/README.rst
@@ -0,0 +1,69 @@
+releasecmd
+============================================
+.. image:: https://badge.fury.io/py/releasecmd.svg
+    :target: https://badge.fury.io/py/releasecmd
+    :alt: PyPI package version
+
+.. image:: https://img.shields.io/pypi/pyversions/releasecmd.svg
+    :target: https://pypi.org/project/releasecmd
+    :alt: Supported Python versions
+
+Summary
+---------
+``releasecmd`` is a ``release`` subcommand for ``setup.py`` (``setuptools.setup``).
+The subcommand create a git tag and push, and upload packages to ``PyPI``.
+
+The subcommand class (``releasecmd.ReleaseCommand``) is implemented as
+a subclass of ``setuptools.Command`` class.
+The ``release`` subcommand does the followings:
+
+1. Find a file that defined the package version
+2. Create a git tag from the package version information
+3. Push git tags
+4. Upload package files to PyPI by using ``twine``
+
+
+Example
+============================================
+
+.. code-block::
+
+    $ python setup.py release
+    running release
+    [get the version from ./releasecmd/__version__.py]
+    [create a git tag: v0.0.15]
+    [push git tags]
+    [upload the package to PyPI]
+    ...
+
+Before execute, need to exist uploading binaries in ``dist/`` directory.
+
+Create a GPG signed tag
+---------------------------
+.. code-block::
+
+    $ python setup.py release --sign
+    running release
+    [get the version from ./releasecmd/__version__.py]
+    [create a git tag with gpg signing: v0.0.15]
+    [push git tags]
+    [upload the package to PyPI]
+    ...
+
+Skip create a git tag
+---------------------------
+.. code-block::
+
+    $ python setup.py release --skip-tagging
+    running release
+    [get the version from ./releasecmd/__version__.py]
+    [push git tags]
+    [upload the package to PyPI]    
+    ...
+
+
+Dependencies
+============================================
+Python 2.7+ or 3.5+
+
+- `twine <https://twine.readthedocs.io/>`__
---
+++ packages/releasecmd/releasecmd-0.0.20.tar.gz/releasecmd-0.0.20/releasecmd/__init__.py
@@ -0,0 +1,175 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import print_function, unicode_literals
+
+import errno
+import io
+import os
+import re
+import subprocess
+import sys
+
+import setuptools
+
+from .__version__ import __author__, __copyright__, __email__, __license__, __version__
+
+
+class ReleaseCommand(setuptools.Command):
+    description = "create a Git tag and push, and then upload packages to PyPI"
+
+    # command class must provide 'user_options' attribute (a list of tuples)
+    user_options = [
+        ("skip-tagging", None, "skip a git tag creation"),
+        ("dry-run", None, "do no harm"),
+        ("sign", None, "make a GPG-signed tag"),
+    ]
+
+    __DIST_DIR_NAME = "dist"
+
+    def initialize_options(self):
+        self.skip_tagging = False
+        self.dry_run = False
+        self.sign = False
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """
+        1. create a git tag from version information in __version__.py
+        2. push git tags
+        3. upload package files to PyPI by using twine
+        """
+
+        self.__validate_dist_dir()
+
+        version = self.__get_version()
+        self.__validate_version(version)
+
+        upload_file_list = self.__get_upload_files(version)
+        if not upload_file_list:
+            print(
+                (
+                    "package files not found in '{dir:s}/' that matches the version ({version:s}) "
+                    + "to upload"
+                ).format(dir=self.__DIST_DIR_NAME, version=version),
+                file=sys.stderr,
+            )
+            sys.exit(errno.ENOENT)
+
+        self.__push_git_tag(version)
+        self.__upload_package(upload_file_list)
+
+    def __validate_dist_dir(self):
+        if os.path.isdir(self.__DIST_DIR_NAME):
+            return
+
+        print(
+            "{:s}/ directory not found. build the package first.".format(self.__DIST_DIR_NAME),
+            file=sys.stderr,
+        )
+        sys.exit(errno.ENOENT)
+
+    def __validate_version(self, version):
+        from pkg_resources import parse_version
+        from pkg_resources.extern.packaging.version import Version, LegacyVersion
+
+        if not isinstance(parse_version(version), Version):
+            print("invalid version string: {}".format(version), file=sys.stderr)
+            sys.exit(errno.EINVAL)
+
+    def __get_version(self):
+        for version_file in self.__traverse_version_file():
+            version = self.__extract_version_from_file(version_file)
+
+            if version:
+                print("[get the version from {}]".format(version_file))
+                return version
+
+        print("version not found in the curent directory", file=sys.stderr)
+        sys.exit(errno.ENOENT)
+
+    def __extract_version_from_file(self, filepath):
+        pkg_info = {}
+
+        if not filepath:
+            print("require a file path", file=sys.stderr)
+            sys.exit(errno.ENOENT)
+
+        if not os.path.isfile(filepath):
+            print("file not found: {}".format(filepath), file=sys.stderr)
+            sys.exit(errno.ENOENT)
+
+        with io.open(filepath, encoding="utf8") as f:
+            try:
+                exec(f.read(), pkg_info)
+            except KeyError:
+                return None
+
+        return pkg_info.get("__version__")
+
+    def __call(self, command):
+        if self.dry_run:
+            print("dry run: {}".format(command))
+            return
+
+        return_code = subprocess.call(command, shell=True)
+        if return_code != 0:
+            sys.exit(return_code)
+
+    def __push_git_tag(self, version):
+        tag = "v{}".format(version)
+
+        if not self.skip_tagging:
+            command_items = ["git", "tag"]
+            extra_log = ""
+
+            if self.sign:
+                command_items.extend(["--sign", "-m", "'GPG signed {} tag'".format(version)])
+                extra_log = " with gpg signing"
+
+            print("[create a git tag{}: {}]".format(extra_log, tag))
+
+            command_items.append(tag)
+
+            self.__call(" ".join(command_items))
+
+        print("[push git tags]")
+        self.__call("git push --tags")
+
+    def __get_upload_files(self, version):
+        version_regexp = re.compile(re.escape(version))
+        upload_file_list = []
+
+        for filename in os.listdir(self.__DIST_DIR_NAME):
+            if not version_regexp.search(filename):
+                continue
+
+            upload_file_list.append(os.path.join(self.__DIST_DIR_NAME, filename))
+
+        return upload_file_list
+
+    def __upload_package(self, upload_file_list):
+        print("[upload the package to PyPI]")
+        self.__call("twine upload {:s}".format(" ".join(upload_file_list)))
+
+    @staticmethod
+    def __traverse_version_file():
+        exclude_regexp_list = [re.compile("/build/.+"), re.compile(re.escape("/.eggs/"))]
+        ver_file_candidate_regexp = re.compile("^_.+_\.py$")
+
+        for root, dirs, files in os.walk("."):
+            for filename in files:
+                if ver_file_candidate_regexp.search(filename) is None:
+                    continue
+
+                if any([exclude_regexp.search(root) for exclude_regexp in exclude_regexp_list]):
+                    continue
+
+                yield os.path.join(root, filename)
+
+        return None
