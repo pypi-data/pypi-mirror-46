c8bbebd78920ec82c2640089fd0620939c60f633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:56 +0000

    Add package jupyterlab-bokeh-server/0.2.0/jupyterlab-bokeh-server-0.2.0.tar.gz

---
+++ packages/jupyterlab-bokeh-server/jupyterlab-bokeh-server-0.2.0.tar.gz/jupyterlab-bokeh-server-0.2.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="jupyterlab-bokeh-server",
+    version='0.2.0',
+    url="https://github.com/ian-r-rose/jupyterlab-bokeh-server",
+    author="Matt Rocklin and Ian Rose",
+    description="projectjupyter@gmail.com",
+    packages=setuptools.find_packages(),
+	keywords=['Jupyter'],
+	classifiers=['Framework :: Jupyter'],
+    install_requires=[
+        'jupyter-server-proxy'
+    ],
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'bokeh-dashboard = jupyterlab_bokeh_server:launch_server',
+        ]
+    },
+    package_data={
+        'jupyterlab_bokeh_server': ['icons/*'],
+    },
+)
