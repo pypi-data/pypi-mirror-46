02e84aed7b176165d9cace173763abdbf58a9827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package thcolor/0.2/thcolor-0.2.tar.gz

---
+++ packages/thcolor/thcolor-0.2.tar.gz/thcolor-0.2/thcolor/_builtin/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#******************************************************************************
+# Copyright (C) 2019 Thomas "Cakeisalie5" Touhey <thomas@touhey.fr>
+# This file is part of the thcolor project, which is MIT-licensed.
+#******************************************************************************
+""" Named colors references, using various sources. """
+
+from ._css import (CSS1Reference, CSS2Reference, CSS3Reference,
+	CSS4Reference)
+
+__all__ = ["CSS1Reference", "CSS2Reference", "CSS3Reference",
+	"CSS4Reference"]
+
+# End of file.
