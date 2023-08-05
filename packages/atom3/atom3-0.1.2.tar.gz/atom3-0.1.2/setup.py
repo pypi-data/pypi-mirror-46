25adebbafd31c9a8cdcec9e64943d396a4867c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:18 +0000

    Add package atom3/0.1.2/atom3-0.1.2.tar.gz

---
+++ packages/atom3/atom3-0.1.2.tar.gz/atom3-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='atom3',
+    packages=['atom3'],
+    url='https://github.com/raphtown/atom3',
+    version='0.1.2',
+    description='3D Atomic Data Processing',
+    long_description=open("README.rst").read(),
+    author='Raphael Townshend',
+    license='MIT',
+    install_requires=[
+        'biopython',
+        'easy-parallel',
+        'h5py',
+        'pandas',
+        'scipy',
+    ],
+    entry_points={
+        'console_scripts': [
+            "atom3 = atom3.main:main",
+        ]
+    }
+)
