85a4c5b1f83503ec2d4b190cd06f03e044cfcb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:58 +0000

    Add package get-variable-name/0.0.2/get_variable_name-0.0.2.tar.gz

---
+++ packages/get-variable-name/get_variable_name-0.0.2.tar.gz/get_variable_name-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="get_variable_name",
+    version="0.0.2",
+    author="Farinnako Adeyanju Victor",
+    author_email="ennythan006@gmail.com",
+    description="Returns variable name as a string",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sorxcode/get-variable-name",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
