559a068edcbb956315cd93f13340140a58adf13b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:45 +0000

    Add package txlog/0.0.0.27a0/txlog-0.0.0.27a0-py3-none-any.whl

---
+++ packages/txlog/txlog-0.0.0.27a0-py3-none-any.whl/txlog/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .txlog import TxLog, Transaction
+import logging
+
+__version__ = '0.0.0.27a'
+
+logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig(format='%(asctime)-15s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.info(f'TxLog v{__version__}')
