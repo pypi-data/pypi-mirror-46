aed54730c7e3322dc651714f91d543c82ec0c769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:49 +0000

    Add package simqle/0.2.0/simqle-0.2.0.tar.gz

---
+++ packages/simqle/simqle-0.2.0.tar.gz/simqle-0.2.0/simqle/__init__.py
@@ -0,0 +1,15 @@
+"""Import commonly used functions."""
+
+__version__ = '0.2.0'
+
+
+from .functions import recordset, execute_sql
+from .connections import get_connection, load_connections, reset_connections
+
+__all__ = [
+    "recordset",
+    "get_connection",
+    "execute_sql",
+    "load_connections",
+    "reset_connections",
+]
---
+++ packages/simqle/simqle-0.2.0.tar.gz/simqle-0.2.0/simqle/constants.py
@@ -0,0 +1,8 @@
+"""Load Constants."""
+
+
+DEV_MAP = {
+    # test mode: connection heading in yaml file
+    True: "test-connections",
+    False: "connections"
+}
