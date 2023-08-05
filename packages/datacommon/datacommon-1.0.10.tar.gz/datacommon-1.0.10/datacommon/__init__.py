7a8ed389835ee0ad651ceb1f225b04e7121e3791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package datacommon/1.0.10/datacommon-1.0.10.tar.gz

---
+++ packages/datacommon/datacommon-1.0.10.tar.gz/datacommon-1.0.10/datacommon/__init__.py
@@ -0,0 +1,13 @@
+
+
+__title__ = '通用数据处理工具'
+__description__ = '通用数据处理工具'
+__author__ = 'pengr'
+__author_email__ = 'pengrui55555@163.com'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2019 Pengr'
+
+from . import util
+from .constants import non
+from .reader import getCsvData, getXlsData
+from .dataSet import DataSet
