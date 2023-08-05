1462c91a4954c63368c4dde8d36741fb2999f311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:25 +0000

    Add package userdatamodel/1.4.3/userdatamodel-1.4.3.tar.gz

---
+++ packages/userdatamodel/userdatamodel-1.4.3.tar.gz/userdatamodel-1.4.3/userdatamodel/models.py
@@ -0,0 +1,9 @@
+from user import (
+    User, GoogleProxyGroup, Project, Group, UserToGroup, AccessPrivilege,
+    IdentityProvider, Application, Certificate,
+    CloudProvider, StorageAccess, Bucket,
+    ComputeAccess, UserToBucket,
+    AuthorizationProvider, S3Credential,
+    HMACKeyPair, HMACKeyPairArchive, ProjectToBucket, Tag, Policy,
+    users_to_policies,
+)
