1b8097dd4daaf564b81550f4739f47b096707486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:03 +0000

    Add package aws-cdk.aws-sqs/0.31.0/aws-cdk.aws-sqs-0.31.0.tar.gz

---
+++ packages/aws-cdk.aws-sqs/aws-cdk.aws-sqs-0.31.0.tar.gz/aws-cdk.aws-sqs-0.31.0/src/aws_cdk/aws_sqs/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+from jsii.python import classproperty
+
+import aws_cdk.aws_autoscaling_api
+import aws_cdk.aws_cloudwatch
+import aws_cdk.aws_iam
+import aws_cdk.aws_kms
+import aws_cdk.aws_s3_notifications
+import aws_cdk.cdk
+__all__ = []
+
+publication.publish()
