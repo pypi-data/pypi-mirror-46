37c3fd562ff3cfe8da3b5690039b5c59aef89669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:54 +0000

    Add package uiuc-mas/1/uiuc-mas-1.tar.gz

---
+++ packages/uiuc-mas/uiuc-mas-1.tar.gz/uiuc-mas-1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='uiuc-mas',
+    version='1',
+    packages=['mas'],
+    author="Evan Widloski",
+    author_email="evan@evanw.org",
+    description="Milliarc-second Photon Sieve Simulations",
+    long_description=open('README.md').read(),
+    license="GPLv3",
+    keywords="photonsieve csbs sieve uiuc",
+    url="https://github.com/uiuc-sine/mas",
+    install_requires=[
+        "matplotlib",
+        "h5py",
+        "numpy",
+        "scipy",
+        "imageio",
+        "pybm3d"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
