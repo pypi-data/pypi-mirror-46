0e2e95ac4b1d189c853392024db5b927111848d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:17 +0000

    Add package opsdroid-get-image-size/0.2.2/opsdroid_get_image_size-0.2.2.tar.gz

---
+++ packages/opsdroid-get-image-size/opsdroid_get_image_size-0.2.2.tar.gz/opsdroid_get_image_size-0.2.2/setup.py
@@ -0,0 +1,33 @@
+import os
+import codecs
+from setuptools import setup, find_packages
+import versioneer
+
+
+def read_long_description():
+    long_desc = []
+    with codecs.open('README.rst', 'r', 'utf8') as longdesc:
+        long_desc.append(longdesc.read())
+    if os.path.exists("HISTORY.rst"):
+        with codecs.open('HISTORY.rst', 'r', 'utf8') as history:
+            long_desc.append(history.read())
+    return u'\n\n'.join(long_desc)
+
+LONG_DESCRIPTION = read_long_description()
+
+setup(
+    name='opsdroid_get_image_size',
+    url='https://github.com/opsdroid/image_size',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    long_description=LONG_DESCRIPTION,
+    author='github.com/scardine',
+    author_email=' ',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'get-image-size = get_image_size:main',
+            ],
+    },
+)
