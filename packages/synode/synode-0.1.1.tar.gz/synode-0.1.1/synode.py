fb9c1894cc0b4bb0b1567e29c1c948ada795d1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:39 +0000

    Add package synode/0.1.1/synode-0.1.1.tar.gz

---
+++ packages/synode/synode-0.1.1.tar.gz/synode-0.1.1/synode.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals
+
+import pickle
+
+class Node(object):
+    """Syntax node Python library for programming languages
+
+"""
+
+    def __new__(cls, node, uppernode, **kwargs):
+
+        obj = super(Node, cls).__new__(cls)
+
+        obj.nodename = cls.__name__
+        obj.uppernode = uppernode
+        obj.subnodes = []
+        obj.nodetypes = []
+
+        constructor = getattr(self, "construct", None)
+
+        if constructor is None:
+            raise Exception("'construct' bound method is not found.")
+
+        elif callable(constructor):
+            constructor(node) 
+
+        else:
+            raise Exception("'construct' attribute is not callable.")
+
+        return obj
+
+    def add_nodetype(self, nodetype):
+        self.nodetypes.append(nodetype)
+
+    def add_subnode(self, node):
+        subnode = Node(node, self)
+        self.subnodes.append(subnode)
+        return subnode
+
+    def write(self, output):
+
+        with open(output, "wb") as f:
+            pickle.dump(self, f)
