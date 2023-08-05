e17bd4279b066802b1f9967a1a8b91d7fcd34c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package ecd/0.1.17/ecd-0.1.17.tar.gz

---
+++ packages/ecd/ecd-0.1.17.tar.gz/ecd-0.1.17/ecd/concurrent_p_t/example/__init__.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+"""
+author: Allen
+datetime: 
+python version: 3.x
+summary: 
+install package:
+"""
---
+++ packages/ecd/ecd-0.1.17.tar.gz/ecd-0.1.17/ecd/main.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+"""
+author: Allen
+datetime: 
+python version: 3.x
+summary: 
+install package:
+"""
+from ecd.password_manage.run import main as password_app
+
+def main():
+    password_app()
+
+if __name__ == '__main__':
+    main()
