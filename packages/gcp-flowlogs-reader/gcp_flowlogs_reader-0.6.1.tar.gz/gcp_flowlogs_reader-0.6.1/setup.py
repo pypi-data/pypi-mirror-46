52f879e28a264c5a0789c1557c574c392cc7c66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:13 +0000

    Add package gcp-flowlogs-reader/0.6.1/gcp_flowlogs_reader-0.6.1.tar.gz

---
+++ packages/gcp-flowlogs-reader/gcp_flowlogs_reader-0.6.1.tar.gz/gcp_flowlogs_reader-0.6.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gcp_flowlogs_reader',
+    version='0.6.1',
+    license='Apache',
+    url='https://github.com/obsrvbl/gcp-flowlogs-reader',
+
+    description='Reader for Google Cloud VPC Flow Logs',
+    long_description=(
+        "This project provides a convenient interface for accessing "
+        "VPC Flow Logs stored in Google Cloud's Stackdriver Logging service."
+    ),
+
+    author='Cisco Stealthwatch Cloud',
+    author_email='support@observable.net',
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gcp_flowlogs_reader = gcp_flowlogs_reader.__main__:main',
+        ],
+    },
+
+    packages=find_packages(exclude=[]),
+    test_suite='tests',
+
+    install_requires=[
+        'google-cloud-logging>=1.6.0',
+        'google-cloud-resource-manager>=0.28.3',
+    ],
+    tests_require=[],
+)
