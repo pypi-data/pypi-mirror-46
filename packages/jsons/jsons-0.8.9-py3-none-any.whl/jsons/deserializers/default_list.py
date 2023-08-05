46674ad858d989a41996885e585b2f1ce092b34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:54 +0000

    Add package jsons/0.8.9/jsons-0.8.9-py3-none-any.whl

---
+++ packages/jsons/jsons-0.8.9-py3-none-any.whl/jsons/deserializers/default_list.py
@@ -0,0 +1,19 @@
+from jsons._load_impl import load
+
+
+def default_list_deserializer(obj: list, cls: type = None, **kwargs) -> list:
+    """
+    Deserialize a list by deserializing all items of that list.
+    :param obj: the list that needs deserializing.
+    :param cls: the type optionally with a generic (e.g. List[str]).
+    :param kwargs: any keyword arguments.
+    :return: a deserialized list instance.
+    """
+    cls_ = None
+    kwargs_ = {**kwargs}
+    if cls and hasattr(cls, '__args__'):
+        cls_ = cls.__args__[0]
+        # Mark the cls as 'inferred' so that later it is known where cls came
+        # from and the precedence of classes can be determined.
+        kwargs_['_inferred_cls'] = True
+    return [load(x, cls_, **kwargs_) for x in obj]
