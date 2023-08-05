5b2c9861dc4ad5c379600364c1dbb7d865d2c10a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:46 +0000

    Add package rf-sclibrary/1.3.4/rf-sclibrary-1.3.4.tar.gz

---
+++ packages/rf-sclibrary/rf-sclibrary-1.3.4.tar.gz/rf-sclibrary-1.3.4/src/SCLibrary/base/__init__.py
@@ -0,0 +1,2 @@
+from .robotlibcore import DynamicCore, keyword
+from .hook import hook_zh
---
+++ packages/rf-sclibrary/rf-sclibrary-1.3.4.tar.gz/rf-sclibrary-1.3.4/src/SCLibrary/builtin/__init__.py
@@ -0,0 +1,8 @@
+from .db import DBKeywords
+from .loglistener import LogListener
+from .requester import RequesterKeywords, get_doraemon_headers
+from .util import UtilKeywords, get_robot_variable_value
+from .randomkeywords import RandomKeywords
+from .select import SelectKeywords
+from .locust import LocustKeyword
+from .env import Env
