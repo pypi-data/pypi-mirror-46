85ae6cd51016994223ea0bb762b65e3e7dd2db51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package kidx-nlu/0.0.1a6/kidx_nlu-0.0.1a6-py2.py3-none-any.whl

---
+++ packages/kidx-nlu/kidx_nlu-0.0.1a6-py2.py3-none-any.whl/kidx_nlu/training_data/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from kidx_nlu.training_data.loading import load_data
+from kidx_nlu.training_data.message import Message
+from kidx_nlu.training_data.training_data import TrainingData
