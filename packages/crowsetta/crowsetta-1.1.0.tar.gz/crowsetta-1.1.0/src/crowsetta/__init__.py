71cb6ce2b941701c0e9d963b8bbeffc6c82c73ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:23 +0000

    Add package crowsetta/1.1.0/crowsetta-1.1.0.tar.gz

---
+++ packages/crowsetta/crowsetta-1.1.0.tar.gz/crowsetta-1.1.0/src/crowsetta/__init__.py
@@ -0,0 +1,12 @@
+from .transcriber import Transcriber
+from .segment import Segment
+from .sequence import Sequence
+from .meta import Meta
+from . import csv
+from . import data
+from . import formats
+
+# built-in formats
+from . import notmat
+from . import koumura
+from . import textgrid
