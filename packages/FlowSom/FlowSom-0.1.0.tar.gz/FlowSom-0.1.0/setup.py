6372b095e900af19845f5b79be52c092b9f861d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:05 +0000

    Add package FlowSom/0.1.0/FlowSom-0.1.0.tar.gz

---
+++ packages/FlowSom/FlowSom-0.1.0.tar.gz/FlowSom-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='FlowSom',
+    url='https://github.com/Hatchin/FlowSOM',
+    author='Sangyu Shen',
+    author_email='sangyushen@gmail.com',
+    packages=['flowsom'],
+    install_requires=['FlowCytometryTools', 'matplotlib', 'networkx', 'minisom', 'scikit-learn', 'numpy', 'pandas'],
+    version='0.1.0',
+    license='MIT',
+    description='A Python implementation of FlowSOM algorithm for clustering and visualizing a mass cytometry data set.'
+)
