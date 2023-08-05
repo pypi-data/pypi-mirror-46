647dd059760695c9b5d6b572e42733f50ec15cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:52 +0000

    Add package autodzee/2.1/autodzee-2.1-py3-none-any.whl

---
+++ packages/autodzee/autodzee-2.1-py3-none-any.whl/mega/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+
+def put(local,remote):
+    cmd="mega-put -c --ignore-quota-warn {} {}".format(local,remote)
+    return os.popen(cmd).read()
+def get(local,remote):
+    cmd="mega-get -m --ignore-quota-warn {} {}".format(remote,local)
+    print(cmd)
+    return os.popen(cmd).read()
+def find(name,remote="/"):
+    cmd="mega-find {} --pattern={}".format(remote,name)
+    return os.popen(cmd).read()
+def is_exist(name,remote="/"):
+    return remote in find(name,remote)
