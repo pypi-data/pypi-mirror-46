f6916757be72ca2eef9bd24129b3b0ffa67328d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package easytoolset/0.6/easytoolset-0.6.tar.gz

---
+++ packages/easytoolset/easytoolset-0.6.tar.gz/easytoolset-0.6/easytoolset/text.py
@@ -0,0 +1,10 @@
+from markdown import markdown
+
+def joke():
+    return markdown(u'Wenn ist das Nunst\u00fcck git und Slotermeyer?'
+                    u'Ja! ... **Beiherhund** das Oder die Flipperwaldt '
+                    u'gersput.')
+
+
+def addtwo(i,j):
+    return i+j
