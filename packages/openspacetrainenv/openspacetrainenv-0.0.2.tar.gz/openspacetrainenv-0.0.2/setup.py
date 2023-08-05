57d461b5a342165b2c418502cab501e12b009a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:13 +0000

    Add package openspacetrainenv/0.0.2/openspacetrainenv-0.0.2.tar.gz

---
+++ packages/openspacetrainenv/openspacetrainenv-0.0.2.tar.gz/openspacetrainenv-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openspacetrainenv",
+    version="0.0.2",
+    author="OpenSpace",
+    description="The OpenSpace train environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.ugent.be/OpenSpace-DP/train_env",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+)
