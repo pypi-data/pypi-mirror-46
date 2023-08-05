39ebad19b5ac63f95eb518320d11a1db7664bd60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:02 +0000

    Add package xyscript/0.1.7.4/xyscript-0.1.7.4.tar.gz

---
+++ packages/xyscript/xyscript-0.1.7.4.tar.gz/xyscript-0.1.7.4/xyscript/config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+#-*- encoding:utf-8 -*-
+# from __future__ import print_function
+
+__version__ = '0.1.7.4'
+
+class Config:
+    def get(self,project):
+        print(project)
+        # pass
+    def get_version(self):
+        return __version__
+        
