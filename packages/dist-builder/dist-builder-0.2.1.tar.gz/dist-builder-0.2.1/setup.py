a8cae7b0441097ed991af23731edca25ddf3df04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:29 +0000

    Add package dist-builder/0.2.1/dist-builder-0.2.1.tar.gz

---
+++ packages/dist-builder/dist-builder-0.2.1.tar.gz/dist-builder-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='dist-builder',
+	version='0.2.1',
+	description='Build a wheel and source distribution and bundle with other files in a zip',
+	url='https://github.com/dnut/dist-builder/',
+	author='Drew Nutter',
+	author_email='drew@drewnutter.com',
+	license='GPLv3',
+	py_modules=['dist_builder'],
+	install_requires=[],
+	zip_safe=False
+)
