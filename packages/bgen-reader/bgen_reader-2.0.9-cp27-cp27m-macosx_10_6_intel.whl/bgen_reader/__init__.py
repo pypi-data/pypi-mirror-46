12e63ca97a3a4e67fb330d200086a850817be96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:23 +0000

    Add package bgen-reader/2.0.9/bgen_reader-2.0.9-cp27-cp27m-macosx_10_6_intel.whl

---
+++ packages/bgen-reader/bgen_reader-2.0.9-cp27-cp27m-macosx_10_6_intel.whl/bgen_reader/__init__.py
@@ -0,0 +1,56 @@
+"""
+BGEN file format reader
+=======================
+
+`BGEN <http://www.well.ox.ac.uk/~gav/bgen_format/>`_ is a file format for storing large
+genetic datasets. It supports both unphased genotypes and phased haplotype data with
+variable ploidy and number of alleles. It was designed to provides a compact data
+representation without sacrificing variant access performance.
+
+Functions
+---------
+create_metadata_file  Create variants metadata file.
+read_bgen             Read a given BGEN file.
+test                  Verify this package's integrity.
+
+Documentation can be found at <https://github.com/limix/bgen-reader-py>.
+"""
+
+from __future__ import absolute_import
+
+from ._dosage import (
+    allele_expectation,
+    convert_to_dosage,
+    compute_dosage,
+    allele_frequency,
+)
+from ._example import example_files
+from ._metadata import create_metadata_file
+from ._reader import read_bgen
+from ._testit import test
+
+try:
+    from ._ffi import ffi as _
+
+    assert _ is not None
+except Exception as e:
+    msg = "\nIt is likely caused by a broken installation of this package."
+    msg += "\nPlease, make sure you have a C compiler and try to uninstall"
+    msg += "\nand reinstall the package again."
+    e.msg = e.msg + msg
+    raise e
+
+
+__version__ = "2.0.9"
+
+__all__ = [
+    "__version__",
+    "test",
+    "read_bgen",
+    "create_metadata_file",
+    "convert_to_dosage",
+    "allele_expectation",
+    "example_files",
+    "compute_dosage",
+    "allele_frequency",
+]
