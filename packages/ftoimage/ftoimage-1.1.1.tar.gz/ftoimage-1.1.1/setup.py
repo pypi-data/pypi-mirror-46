3e0a14ce306a4fd855783b96ea78dc7f64f1e3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:51 +0000

    Add package ftoimage/1.1.1/ftoimage-1.1.1.tar.gz

---
+++ packages/ftoimage/ftoimage-1.1.1.tar.gz/ftoimage-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ftoimage',
+    version='1.1.1',
+    packages=['', 'resume_text', 'image_convertion'],
+    url='',
+    license='',
+    author='crow',
+    author_email='',
+    description=''
+)
