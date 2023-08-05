955beb3891b19d5ef8f1347b611021bc7a43be32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:56 +0000

    Add package mastercard-p2m/1.0.4/mastercard_p2m-1.0.4-py2-none-any.whl

---
+++ packages/mastercard-p2m/mastercard_p2m-1.0.4-py2-none-any.whl/mastercardp2m/__init__.py
@@ -0,0 +1,19 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardp2m.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardp2m.digitalaccountreferencenumber import *
+from mastercardp2m.digitalaccountreferencenumberretrieval import *
+from mastercardp2m.funding import *
+from mastercardp2m.fundingreversal import *
+from mastercardp2m.merchantrefund import *
+from mastercardp2m.merchantretrieval import *
+from mastercardp2m.merchanttransferfundingandpayment import *
+from mastercardp2m.merchanttransferpayment import *
+from mastercardp2m.testpaymentnotification import *
+from mastercardp2m.testrefundnotification import *
+from mastercardp2m.transfernotification import *
+from mastercardp2m.transfernotificationregistration import *
