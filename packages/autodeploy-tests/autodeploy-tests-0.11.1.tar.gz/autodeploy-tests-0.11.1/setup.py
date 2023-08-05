f8bd03f2e29653b4c6ebea2326eb4669d9b296dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:34 +0000

    Add package autodeploy-tests/0.11.1/autodeploy-tests-0.11.1.tar.gz

---
+++ packages/autodeploy-tests/autodeploy-tests-0.11.1.tar.gz/autodeploy-tests-0.11.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['autodeploy-tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['black>=18.3-alpha.0,<19.0', 'django>=2.2,<3.0']
+
+setup_kwargs = {
+    'name': 'autodeploy-tests',
+    'version': '0.11.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Marco Acierno',
+    'author_email': 'marcoaciernoemail@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
