c58dfdf3cbd8ad6caff2c64d62f4dfe32884fdfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:58 +0000

    Add package megad2/0.2/megad2-0.2.tar.gz

---
+++ packages/megad2/megad2-0.2.tar.gz/megad2-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='megad2',
+    python_requires='>3.7',
+    install_requirements=['aiohttp'],
+    version='0.2',
+    packages=['megad'],
+    url='',
+    license='BSD',
+    author='andrewgermanovich',
+    author_email='andvikt@gmail.com',
+    description='Simple API for ab-log.ru mega-d'
+)
