b92a48567ed83148da144e3784f5463484304b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:02 +0000

    Add package django-redis-admin/0.2.0/django_redis_admin-0.2.0-py3-none-any.whl

---
+++ packages/django-redis-admin/django_redis_admin-0.2.0-py3-none-any.whl/redis_admin/__about__.py
@@ -0,0 +1,11 @@
+__title__ = 'Django Redis Admin'
+__package_name__ = 'django-redis-admin'
+__author__ = 'Rick van Hattem (Wolph)'
+__description__ = ' '.join('''
+A Django Admin interface for Redis servers with optional Redis Sentinel support
+'''.strip().split())
+__email__ = 'wolph@wol.ph'
+__version__ = '0.2.0'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2019 Rick van Hattem (Wolph)'
+__url__ = 'https://github.com/WoLpH/django-redis-admin'
