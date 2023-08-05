a9c911605bc0053d468cc96fe9740a308b13bff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:08 +0000

    Add package djaodjin-rules/0.1.9/djaodjin-rules-0.1.9.tar.gz

---
+++ packages/djaodjin-rules/djaodjin-rules-0.1.9.tar.gz/djaodjin-rules-0.1.9/rules/api/rules.py
@@ -0,0 +1,253 @@
+# Copyright (c) 2019, DjaoDjin inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from django.db import transaction
+from django.db.models import F, Q, Max
+from django.db.utils import IntegrityError
+from rest_framework.generics import (
+    ListCreateAPIView, RetrieveUpdateDestroyAPIView)
+from rest_framework import serializers
+
+from .serializers import RuleSerializer
+from ..mixins import AppMixin
+from ..models import Rule
+
+#pylint: disable=no-init
+#pylint: disable=old-style-class
+
+
+class UpdateRuleSerializer(RuleSerializer):
+
+    class Meta:
+        model = Rule
+        fields = ('rank', 'path', 'allow', 'is_forward', 'engaged')
+        read_only_fields = ('path',)
+
+
+class RuleMixin(AppMixin):
+
+    model = Rule
+    serializer_class = RuleSerializer
+    lookup_field = 'path'
+    lookup_url_kwarg = 'rule'
+
+    def get_queryset(self):
+        return self.model.objects.get_rules(self.app)
+
+    def perform_create(self, serializer):
+        # We don't compact ranks after DELETE
+        last_rank = self.get_queryset().aggregate(
+            Max('rank')).get('rank__max', 0)
+        # If the key exists and return None the default value is not applied
+        last_rank = last_rank + 1 if last_rank is not None else 1
+        serializer.save(app=self.app, rank=last_rank)
+
+    def perform_update(self, serializer):
+        serializer.save(app=self.app)
+
+
+class RuleListAPIView(RuleMixin, ListCreateAPIView):
+    """
+    Queries a page (``PAGE_SIZE`` records) of ``Rule``.
+
+    **Tags: rbac
+
+    **Examples
+
+    .. code-block:: http
+
+        GET /api/proxy/rules/ HTTP/1.1
+
+    responds
+
+    .. code-block:: json
+
+        {
+            "count": 1,
+            "next": null,
+            "previous": null,
+            "results": [
+                {
+                    "rank": 0,
+                    "path": "/",
+                    "allow": "authenticated",
+                    "is_forward": true,
+                    "engaged": ""
+                }
+            ]
+        }
+    """
+    def get_queryset(self):
+        return super(RuleListAPIView, self).get_queryset().order_by('rank')
+
+    @staticmethod
+    def check_path(request):
+        if 'path' not in request.data:
+            request.data.update({'path': "/"})
+        if not request.data['path'].endswith('/'):
+            request.data['path'] += '/'
+        if not request.data['path'].startswith('/'):
+            request.data['path'] = '/' + request.data['path']
+
+    def perform_create(self, serializer):
+        try:
+            return super(RuleListAPIView, self).perform_create(serializer)
+        except IntegrityError as err:
+            if 'uniq' in str(err).lower():
+                raise serializers.ValidationError({'detail':
+                    "Rule with path prefix '%s' already exists."
+                    % serializer.validated_data['path']})
+            raise
+
+    def post(self, request, *args, **kwargs):
+        """
+        Creates a new ``Rule``.
+
+        **Tags: rbac
+
+        **Examples
+
+        .. code-block:: http
+
+            POST /api/proxy/rules/ HTTP/1.1
+
+        .. code-block:: json
+
+            {
+                "rank": 0,
+                "path": "/",
+                "allow": "authenticated",
+                "is_forward": true,
+                "engaged": ""
+            }
+        """
+        self.check_path(request)
+        return self.create(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        """
+        Update the rank of rules.
+
+        When receiving a request like [{u'newpos': 1, u'oldpos': 3}],
+        it will move the rule at position 3 to position 1, updating all
+        rules ranks in-between.
+
+        **Tags: rbac
+        """
+        with transaction.atomic():
+            for move in request.data:
+                oldpos = move['oldpos']
+                newpos = move['newpos']
+                queryset = self.get_queryset()
+                updated = queryset.get(rank=oldpos)
+                if newpos < oldpos:
+                    queryset.filter(Q(rank__gte=newpos)
+                                    & Q(rank__lt=oldpos)).update(
+                        rank=F('rank') + 1, moved=True)
+                else:
+                    queryset.filter(Q(rank__lte=newpos)
+                                    & Q(rank__gt=oldpos)).update(
+                        rank=F('rank') - 1, moved=True)
+                updated.rank = newpos
+                updated.moved = True
+                updated.save(update_fields=['rank', 'moved'])
+                queryset.filter(moved=True).update(moved=False)
+        return self.get(request, *args, **kwargs)
+
+
+class RuleDetailAPIView(RuleMixin, RetrieveUpdateDestroyAPIView):
+    """
+    Retrieves details on a ``Rule``.
+
+    **Tags: rbac
+
+    **Examples
+
+    .. code-block:: http
+
+        GET /api/proxy/rules/app/ HTTP/1.1
+
+    responds
+
+    .. code-block:: json
+
+        {
+            "rank": 0,
+            "path": "/app/",
+            "allow": "authenticated",
+            "is_forward": true,
+            "engaged": ""
+        }
+    """
+    serializer_class = UpdateRuleSerializer
+
+    def put(self, request, *args, **kwargs):
+        """
+        Updates a ``Rule``.
+
+        **Tags: rbac
+
+        **Examples
+
+        .. code-block:: http
+
+            PUT /api/proxy/rules/app/ HTTP/1.1
+
+        .. code-block:: json
+
+            {
+                "rank": 0,
+                "path": "/app/",
+                "allow": "authenticated",
+                "is_forward": true,
+                "engaged": ""
+            }
+
+        responds
+
+        .. code-block:: json
+
+            {
+                "rank": 0,
+                "path": "/app/",
+                "allow": "authenticated",
+                "is_forward": true,
+                "engaged": ""
+            }
+        """
+        return super(RuleDetailAPIView, self).put(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        """
+        Deletes a ``Rule``.
+
+        **Tags: rbac
+
+        **Examples
+
+        .. code-block:: http
+
+            DELETE /api/proxy/rules/app/ HTTP/1.1
+        """
+        return super(RuleDetailAPIView, self).delete(request, *args, **kwargs)
---
+++ packages/djaodjin-rules/djaodjin-rules-0.1.9.tar.gz/djaodjin-rules-0.1.9/rules/static/js/djaodjin-rules-vue.js
@@ -0,0 +1,245 @@
+Vue.directive('sortable', {
+  inserted: function (el, binding) {
+    new Sortable(el, binding.value || {})
+  }
+})
+
+var itemListMixin = {
+    data: function(){
+        return this.getInitData();
+    },
+    methods: {
+        getInitData: function(){
+            data = {
+                url: '',
+                itemsLoaded: false,
+                items: {
+                    results: [],
+                    count: 0
+                },
+                params: {},
+            }
+            return data;
+        },
+        resetDefaults: function(overrides){
+            if(!overrides) overrides = {}
+            var data = Object.assign(this.getInitData(), overrides);
+            Object.assign(this.$data, data);
+        },
+        get: function(){
+            var vm = this;
+            if(!vm.url) return
+            $.get(vm.url, vm.getParams(), function(res){
+                vm.items = res
+                vm.itemsLoaded = true;
+            });
+        },
+        getParams: function(){
+            return this.params
+        }
+    },
+}
+
+var paginationMixin = {
+    data: function(){
+        return {
+            params: {
+                page: 1,
+            },
+            itemsPerPage: djaodjinSettings.itemsPerPage,
+        }
+    },
+    computed: {
+        totalItems: function(){
+            return this.items.count
+        },
+        pageCount: function(){
+            return Math.ceil(this.totalItems / this.itemsPerPage)
+        }
+    }
+}
+
+if($('#rules-table').length > 0){
+var rtable = new Vue({
+    el: "#rules-table",
+    data: function(){
+        return {
+            url: djaodjinSettings.urls.rules.api_rules,
+            itemsLoaded: false,
+            items: {
+                results: [],
+                count: 0
+            },
+            params: {},
+            ruleModalOpen: false,
+            newRule: {
+                path: '',
+                rank: 0,
+                is_forward: false,
+            },
+            edit_description: [],
+        }
+    },
+    mixins: [
+        itemListMixin,
+        paginationMixin,
+    ],
+    methods: {
+        moved: function(e){
+            var vm = this;
+            // updating local order
+            vm.items.results.splice(e.newIndex, 0,
+                vm.items.results.splice(e.oldIndex, 1)[0]);
+            var pos = [{oldpos: e.oldIndex+1, newpos: e.newIndex+1}];
+            $.ajax({
+                method: 'PATCH',
+                url: vm.url,
+                contentType: 'application/json',
+                data: JSON.stringify(pos),
+            }).done(function (resp) {
+                vm.items = resp;
+            }).fail(function(resp){
+                showErrorMessages(resp);
+            });
+        },
+        create: function(){
+            var vm = this;
+            $.ajax({
+                method: 'POST',
+                url: vm.url,
+                contentType: 'application/json',
+                data: JSON.stringify(vm.newRule),
+            }).done(function (resp) {
+                vm.get();
+                vm.newRule = {
+                    path: '',
+                    rank: 0,
+                    is_forward: false,
+                }
+                vm.ruleModalOpen = false;
+            }).fail(function(resp){
+                vm.ruleModalOpen = false;
+                showErrorMessages(resp);
+            });
+        },
+        update: function(rule){
+            var vm = this;
+            var url = vm.url + rule.path;
+            $.ajax({
+                method: 'PUT',
+                url: url,
+                contentType: 'application/json',
+                data: JSON.stringify(rule),
+            }).done(function (resp) {
+                vm.ruleModalOpen = false;
+            }).fail(function(resp){
+                vm.ruleModalOpen = false;
+                showErrorMessages(resp);
+            });
+        },
+        remove: function(idx){
+            var vm = this;
+            var rule = vm.items.results[idx]
+            var url = vm.url + rule.path;
+            $.ajax({
+                method: 'DELETE',
+                url: url,
+            }).done(function (resp) {
+                vm.params.page = 1;
+                vm.get();
+            }).fail(function(resp){
+                showErrorMessages(resp);
+            });
+        },
+        editDescription: function(idx){
+            var vm = this;
+            vm.edit_description = Array.apply(
+                null, new Array(vm.items.results.length)).map(function() {
+                return false;
+            });
+            vm.$set(vm.edit_description, idx, true)
+            // at this point the input is rendered and visible
+            vm.$nextTick(function(){
+                vm.$refs.edit_description_input[idx].focus();
+            });
+        },
+        saveDescription: function(coupon, idx, event){
+            if (event.which === 13 || event.type === "blur" ){
+                this.$set(this.edit_description, idx, false)
+                this.update(this.items.results[idx])
+            }
+        },
+    },
+    mounted: function(){
+        this.get();
+    }
+});
+
+$('#new-rule').on('shown.bs.modal', function(){
+    var self = $(this);
+    self.find('[name="new_rule_path"]').focus();
+});
+
+} // $('#rules-table').length > 0
+
+if($('#rule-list-container').length > 0){
+var app = new Vue({
+    el: "#rule-list-container",
+    data: {
+        sessionKey: gettext('Generating...'),
+        testUsername: '',
+        forward_session: '',
+        forward_session_header: '',
+        forward_url: '',
+    },
+    methods: {
+        generateKey: function(){
+            var vm = this;
+            $.ajax({
+                method: 'PUT',
+                url: djaodjinSettings.urls.rules.api_generate_key,
+            }).done(function (resp) {
+                vm.sessionKey = resp.enc_key;
+            }).fail(function(resp){
+                vm.sessionKey = gettext("ERROR");
+                showErrorMessages(resp);
+            });
+        },
+        getSessionData: function(){
+            var vm = this;
+            var url = djaodjinSettings.urls.rules.api_session_data + "/" + vm.testUsername;
+            $.ajax({
+                method: 'GET',
+                url: url,
+            }).done(function(resp) {
+                vm.forward_session = resp.forward_session;
+                vm.forward_session_header = resp.forward_session_header;
+                vm.forward_url = resp.forward_url;
+            }).fail(function(resp){
+                showErrorMessages(resp);
+            });
+        },
+        update: function(submitEntryPoint) {
+            var vm = this;
+            var data = {
+                authentication: vm.$refs.authentication.value,
+                welcome_email: vm.$refs.welcomeEmail.checked,
+                session_backend: vm.$refs.sessionBackend.value,
+            }
+            if( submitEntryPoint ) {
+                data['entry_point'] = vm.$refs.entryPoint.value;
+            }
+            $.ajax({
+                method: 'PUT',
+                url: djaodjinSettings.urls.rules.api_detail,
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+            }).done(function (resp) {
+                showMessages([gettext("Update successful.")], "success");
+            }).fail(function(resp){
+                showErrorMessages(resp);
+            });
+        },
+    },
+})
+}
