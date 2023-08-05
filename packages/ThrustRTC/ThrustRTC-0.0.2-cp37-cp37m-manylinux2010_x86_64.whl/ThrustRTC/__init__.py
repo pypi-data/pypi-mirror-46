37c5c131197a0cd160ffc99660d3df9b8451e935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package ThrustRTC/0.0.2/ThrustRTC-0.0.2-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/ThrustRTC/ThrustRTC-0.0.2-cp37-cp37m-manylinux2010_x86_64.whl/ThrustRTC/__init__.py
@@ -0,0 +1,20 @@
+from . import PyThrustRTC as native
+import os
+
+def set_libnvrtc_path(path):
+	native.n_set_libnvrtc_path(path)
+
+def set_ptx_cache(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    native.n_set_ptx_cache(path)
+
+from .Context import *
+from .DeviceViewable import *
+from .DVVector import device_vector, device_vector_from_numpy, device_vector_from_dvs, device_vector_from_list
+from .FakeVectors import *
+from .Functor import *
+from .Transformations import *
+from .Copying import *
+from .Reductions import *
+from .PrefixSums import *
