10296875b56aa5f58dac156416112760fd9cc95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:34 +0000

    Add package wkcuber/0.2.8/wkcuber-0.2.8.tar.gz

---
+++ packages/wkcuber/wkcuber-0.2.8.tar.gz/wkcuber-0.2.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wkcuber",
+    packages=find_packages(exclude=("tests",)),
+    version="0.2.8",
+    install_requires=["scipy", "numpy", "pillow", "pyyaml", "wkw"],
+    description="A cubing tool for webKnossos",
+    author="Norman Rzepka",
+    author_email="norman.rzepka@scalableminds.com",
+    url="https://scalableminds.com",
+)
