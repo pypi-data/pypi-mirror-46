109e2dbfe237f4957a60dd50585c11c863b9ccdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:07 +0000

    Add package bigtiger-admin-owl/0.0.1rc2/bigtiger_admin_owl-0.0.1rc2-py2-none-any.whl

---
+++ packages/bigtiger-admin-owl/bigtiger_admin_owl-0.0.1rc2-py2-none-any.whl/owl_admin/__init__.py
@@ -0,0 +1,3 @@
+default_app_config = 'owl_admin.apps.OwlAdminConfig'
+
+__version__ = "0.0.1rc2"
---
+++ packages/bigtiger-admin-owl/bigtiger_admin_owl-0.0.1rc2-py2-none-any.whl/owl_admin/templatetags/virtual.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2014-5-14
+@author: hshl Co.,Ltd.
+'''
+
+from django import template
+from django.utils.safestring import mark_safe
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.simple_tag
+def virtual(value):
+    virtual_url = ''
+    if hasattr(settings, 'VIRTUAL_URL'):
+        virtual_url = settings.VIRTUAL_URL
+    return mark_safe(u'{0}{1}'.format(virtual_url, value))
