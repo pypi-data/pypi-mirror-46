a26fbe048f8d66f4f79e947dfc25db38b77a030f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:33 +0000

    Add package PyBtMesh/0.1.1/PyBtMesh-0.1.1.tar.gz

---
+++ packages/PyBtMesh/PyBtMesh-0.1.1.tar.gz/PyBtMesh-0.1.1/setup.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+
+from setuptools import setup
+
+setup(
+    name='PyBtMesh',
+    version='0.1.1',
+    description='Python module for bluetooth mesh',
+    author='samuelli2019',
+    author_email='lijyigac@gmail.com',
+    url='https://github.com/samuelli2019/PyBtMesh',
+    packages=['btmesh'],
+    requires=["bitstring", "cryptography"],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
