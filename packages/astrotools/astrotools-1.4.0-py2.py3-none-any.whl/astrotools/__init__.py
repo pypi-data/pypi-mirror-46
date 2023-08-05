f7cb41ff1fc19b26379cd2ece5bd14871f4e7c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:01 +0000

    Add package astrotools/1.4.0/astrotools-1.4.0-py2.py3-none-any.whl

---
+++ packages/astrotools/astrotools-1.4.0-py2.py3-none-any.whl/astrotools/__init__.py
@@ -0,0 +1,23 @@
+# noinspection PyPep8Naming
+__all__ = [
+    "auger",
+    "coord",
+    "cosmic_rays",
+    "gamale",
+    "obs",
+    "simulations",
+    "skymap",
+    "stat",
+    "nucleitools",
+    "healpytools"]
+
+import astrotools.auger
+import astrotools.coord
+import astrotools.cosmic_rays
+import astrotools.gamale
+import astrotools.obs
+import astrotools.simulations
+import astrotools.skymap
+import astrotools.stat
+import astrotools.nucleitools
+import astrotools.healpytools
