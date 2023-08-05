b60b2ec1045a077097a12eee43f1ac6602b89eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:36 +0000

    Add package eth-brownie/1.0.0b4/eth_brownie-1.0.0b4-py3-none-any.whl

---
+++ packages/eth-brownie/eth_brownie-1.0.0b4-py3-none-any.whl/brownie/cli/test.py
@@ -0,0 +1,272 @@
+#!/usr/bin/python3
+
+from docopt import docopt
+import importlib
+import json
+import os
+from pathlib import Path
+import re
+from requests.exceptions import ReadTimeout
+import sys
+import time
+
+from brownie.cli.utils import color
+from brownie.test.coverage import merge_coverage, analyze_coverage
+from brownie.exceptions import ExpectedFailing, VirtualMachineError
+import brownie.network as network
+from brownie.network.history import TxHistory
+import brownie.network.transaction as transaction
+from brownie.project.build import Build, get_ast_hash
+from brownie._config import ARGV, CONFIG
+
+COVERAGE_COLORS = [
+    (0.5, "bright red"),
+    (0.85, "bright yellow"),
+    (1, "bright green")
+]
+
+__doc__ = """Usage: brownie test [<filename>] [<range>] [options]
+
+Arguments:
+  <filename>              Only run tests from a specific file or folder
+  <range>                 Number or range of tests to run from file
+
+Options:
+  
+  --coverage -c          Evaluate test coverage and display a report
+  --update -u            Only run tests where changes have occurred
+  --gas -g               Display gas profile for function calls
+  --verbose -v           Enable verbose reporting
+  --tb -t                Show entire python traceback on exceptions
+  --help -h              Display this message
+
+By default brownie runs every script found in the tests folder as well as any
+subfolders. Files and folders beginning with an underscore will be skipped."""
+
+
+def _run_test(module, fn_name, count, total):
+    fn = getattr(module, fn_name)
+    desc = fn.__doc__ or fn_name
+    sys.stdout.write("   {0} - {1} ({0}/{2})...".format(count, desc, total))
+    sys.stdout.flush()
+    if fn.__defaults__:
+        args = dict(zip(
+            fn.__code__.co_varnames[:len(fn.__defaults__)],
+            fn.__defaults__
+        ))
+        if 'skip' in args and args['skip']:
+            sys.stdout.write(
+                "\r {0[pending]}\u229d{0[dull]} {1} - ".format(color, count) +
+                "{1} ({0[pending]}skipped{0[dull]}){0}\n".format(color, desc)
+            )
+            return []
+    else:
+        args = {}
+    try:
+        stime = time.time()
+        if ARGV['coverage'] and 'always_transact' in args:
+            ARGV['always_transact'] = args['always_transact']
+        fn()
+        if ARGV['coverage']:
+            ARGV['always_transact'] = True
+        if 'pending' in args and args['pending']:
+            raise ExpectedFailing("Test was expected to fail")
+        sys.stdout.write("\r {0[success]}\u2713{0} {1} - {2} ({3:.4f}s) \n".format(
+            color, count, desc, time.time()-stime
+        ))
+        sys.stdout.flush()
+        return []
+    except Exception as e:
+        if type(e) != ExpectedFailing and 'pending' in args and args['pending']:
+            c = [color('success'), color('dull'), color()]
+        else:
+            c = [color('error'), color('dull'), color()]
+        sys.stdout.write("\r {0[0]}{1}{0[1]} {4} - {2} ({0[0]}{3}{0[1]}){0[2]}\n".format(
+            c,
+            '\u2717' if type(e) in (AssertionError, VirtualMachineError) else '\u203C',
+            desc,
+            type(e).__name__,
+            count
+        ))
+        sys.stdout.flush()
+        if type(e) != ExpectedFailing and 'pending' in args and args['pending']:
+            return []
+        filename = str(Path(module.__file__).relative_to(CONFIG['folders']['project']))
+        fn_name = filename[:-2]+fn_name
+        return [(fn_name, color.format_tb(sys.exc_info(), filename), type(e))]
+
+
+def run_test(filename, network, idx):
+    network.rpc.reset()
+    if type(CONFIG['test']['gas_limit']) is int:
+        network.gas_limit(CONFIG['test']['gas_limit'])
+
+    module = importlib.import_module(filename.replace(os.sep, '.'))
+    test_names = [
+        i for i in dir(module) if i not in dir(sys.modules['brownie']) and
+        i[0] != "_" and callable(getattr(module, i))
+    ]
+    code = Path(CONFIG['folders']['project']).joinpath("{}.py".format(filename)).open().read()
+    test_names = re.findall('(?<=\ndef)[\s]{1,}[^(]*(?=\([^)]*\)[\s]*:)', code)
+    test_names = [i.strip() for i in test_names if i.strip()[0] != "_"]
+    duplicates = set([i for i in test_names if test_names.count(i) > 1])
+    if duplicates:
+        raise ValueError(
+            "tests/{}.py contains multiple tests of the same name: {}".format(
+                filename, ", ".join(duplicates)
+            )
+        )
+    traceback_info = []
+    test_history = set()
+    if not test_names:
+        print("\n{0[error]}WARNING{0}: No test functions in {0[module]}{1}.py{0}".format(color, filename))
+        return [], []
+
+    print("\nRunning {0[module]}{1}.py{0} - {2} test{3}".format(
+            color,
+            filename,
+            len(test_names)-1,
+            "s" if len(test_names) != 2 else ""
+    ))
+    if 'setup' in test_names:
+        test_names.remove('setup')
+        traceback_info += _run_test(module, 'setup', 0, len(test_names))
+        if traceback_info:
+            return TxHistory().copy(), traceback_info
+    network.rpc.snapshot()
+    for c, t in enumerate(test_names[idx], start=idx.start + 1):
+        network.rpc.revert()
+        traceback_info += _run_test(module, t, c, len(test_names))
+        if traceback_info and traceback_info[-1][2] == ReadTimeout:
+            print(" {0[error]}WARNING{0}: RPC crashed, terminating test".format(color))
+            network.rpc.kill(False)
+            network.rpc.launch()
+            break
+        test_history.update(TxHistory().copy())
+    return test_history, traceback_info
+
+
+def get_test_files(path):
+    if not path:
+        path = ""
+    if path[:6] != "tests/":
+        path = "tests/"+path
+    path = Path(CONFIG['folders']['project']).joinpath(path)
+    if not path.is_dir():
+        if not path.suffix:
+            path = Path(str(path)+".py")
+        if not path.exists():
+            sys.exit("{0[error]}ERROR{0}: Cannot find {0[module]}tests/{1}{0}".format(color, path.name))
+        result = [path]
+    else:
+        result = [i for i in path.glob('**/*.py') if i.name[0]!="_" and "/_" not in str(i)]
+    return [str(i.relative_to(CONFIG['folders']['project']))[:-3] for i in result]
+
+
+def main():
+    args = docopt(__doc__)
+    ARGV._update_from_args(args)
+    if ARGV['coverage']:
+        ARGV['always_transact'] = True
+    traceback_info = []
+    test_files = get_test_files(args['<filename>'])
+
+    if len(test_files) == 1 and args['<range>']:
+        try:
+            idx = args['<range>']
+            if ':' in idx:
+                idx = slice(*[int(i)-1 for i in idx.split(':')])
+            else:
+                idx = slice(int(idx)-1, int(idx))
+        except:
+            sys.exit("{0[error]}ERROR{0}: Invalid range. Must be an integer or slice (eg. 1:4)".format(color))
+    elif args['<range>']:
+        sys.exit("{0[error]}ERROR:{0} Cannot specify a range when running multiple tests files.".format(color))
+    else:
+        idx = slice(0, None)
+
+    network.connect(ARGV['network'])
+    coverage_files = []
+
+    try:
+        for filename in test_files:
+            coverage_json = Path(CONFIG['folders']['project'])
+            coverage_json = coverage_json.joinpath("build/coverage"+filename[5:]+".json")
+            coverage_files.append(coverage_json)
+            if coverage_json.exists():
+                coverage_eval = json.load(coverage_json.open())['coverage']
+                if ARGV['update'] and (coverage_eval or not ARGV['coverage']):
+                    continue
+            else:
+                coverage_eval = {}
+                for p in list(coverage_json.parents)[::-1]:
+                    if not p.exists():
+                        p.mkdir()
+
+            test_history, tb = run_test(filename, network, idx)
+            if tb:
+                traceback_info += tb
+                if coverage_json.exists():
+                    coverage_json.unlink()
+                continue
+
+            if args['--coverage']:
+                stime = time.time()
+                sys.stdout.write("     - Evaluating test coverage...")
+                sys.stdout.flush()
+                coverage_eval = analyze_coverage(test_history)
+                sys.stdout.write(
+                    "\r {0[success]}\u2713{0}   - ".format(color) +
+                    "Evaluating test coverage ({:.4f}s)\n".format(time.time()-stime)
+                )
+                sys.stdout.flush()
+            build_folder = Path(CONFIG['folders']['project']).joinpath('build/contracts')
+            build_files = set(build_folder.joinpath(i+'.json') for i in coverage_eval)
+            coverage_eval = {
+                'coverage': coverage_eval,
+                'sha1': dict((str(i), Build()[i.stem]['bytecodeSha1']) for i in build_files)
+            }
+            if args['<range>']:
+                continue
+
+            test_path = Path(CONFIG['folders']['project']).joinpath(filename+".py")
+            coverage_eval['sha1'][str(test_path)] = get_ast_hash(test_path)
+
+            json.dump(
+                coverage_eval,
+                coverage_json.open('w'),
+                sort_keys=True,
+                indent=4,
+                default=sorted
+            )
+    except KeyboardInterrupt:
+        print("\n\nTest execution has been terminated by KeyboardInterrupt.")
+        sys.exit()
+    finally:
+        if traceback_info:
+            print("\n{0[error]}WARNING{0}: {1} test{2} failed.{0}".format(
+                color, len(traceback_info), "s" if len(traceback_info) > 1 else ""
+            ))
+            for err in traceback_info:
+                print("\nException info for {0[0]}:\n{0[1]}".format(err))
+            sys.exit()
+
+    print("\n{0[success]}SUCCESS{0}: All tests passed.".format(color))
+
+    if args['--coverage']:
+        print("\nCoverage analysis:\n")
+        coverage_eval = merge_coverage(coverage_files)
+
+        for contract in coverage_eval:
+            print("  contract: {0[contract]}{1}{0}".format(color, contract))
+            for fn_name, pct in [(x,v[x]['pct']) for v in coverage_eval[contract].values() for x in v]:
+                c = next(i[1] for i in COVERAGE_COLORS if pct<=i[0])
+                print("    {0[contract_method]}{1}{0} - {2}{3:.1%}{0}".format(
+                    color, fn_name, color(c), pct
+                ))
+            print()
+
+    if args['--gas']:
+        print('\nGas Profile:')
+        for i in sorted(transaction.gas_profile):
+            print("{0} -  avg: {1[avg]:.0f}  low: {1[low]}  high: {1[high]}".format(i, transaction.gas_profile[i]))
---
+++ packages/eth-brownie/eth_brownie-1.0.0b4-py3-none-any.whl/brownie/project/compiler.py
@@ -0,0 +1,382 @@
+#!/usr/bin/python3
+
+from hashlib import sha1
+from pathlib import Path
+import re
+import solcx
+
+from .sources import Sources
+from brownie.exceptions import CompilerError
+from brownie._config import CONFIG
+
+STANDARD_JSON = {
+    'language': "Solidity",
+    'sources': {},
+    'settings': {
+        'outputSelection': {'*': {
+            '*': [
+                "abi",
+                "evm.assembly",
+                "evm.bytecode",
+                "evm.deployedBytecode"
+            ],
+            '': ["ast"]
+        }},
+        "optimizer": {
+            "enabled": CONFIG['solc']['optimize'],
+            "runs": CONFIG['solc']['runs']
+        }
+    }
+}
+
+sources = Sources()
+
+
+def set_solc_version():
+    '''Sets the solc version based on the project config file.'''
+    try:
+        solcx.set_solc_version(CONFIG['solc']['version'])
+    except solcx.exceptions.SolcNotInstalled:
+        solcx.install_solc(CONFIG['solc']['version'])
+        solcx.set_solc_version(CONFIG['solc']['version'])
+    CONFIG['solc']['version'] = solcx.get_solc_version_string().strip('\n')
+
+
+def compile_contracts(contract_paths):
+    '''Compiles the contract files and returns a dict of build data.'''
+    print("Compiling contracts...")
+    print("Optimizer: {}".format(
+        "Enabled  Runs: "+str(CONFIG['solc']['runs']) if
+        CONFIG['solc']['optimize'] else "Disabled"
+    ))
+
+    print("\n".join(" - {}...".format(Path(i).name) for i in contract_paths))
+    input_json = STANDARD_JSON.copy()
+    input_json['sources'] = dict((
+        str(i),
+        {'content': sources[i]}
+    ) for i in contract_paths)
+    return _compile_and_format(input_json)
+
+
+def compile_source(code):
+    '''Compiles the contract source and returns a dict of build data.'''
+    path = sources.add_source(code)
+    input_json = STANDARD_JSON.copy()
+    input_json['sources'] = {path: {'content': code}}
+    return _compile_and_format(input_json)
+
+
+def _compile_and_format(input_json):
+    try:
+        compiled = solcx.compile_standard(
+            input_json,
+            optimize=CONFIG['solc']['optimize'],
+            optimize_runs=CONFIG['solc']['runs'],
+            allow_paths="."
+        )
+    except solcx.exceptions.SolcError as e:
+        raise CompilerError(e)
+    compiled = _generate_pcMap(compiled)
+    result = {}
+
+    for filename, name in [(k,x) for k,v in compiled['contracts'].items() for x in v]:
+        data = compiled['contracts'][filename][name]
+        evm = data['evm']
+        ref = [
+            (k, x) for v in evm['bytecode']['linkReferences'].values()
+            for k, x in v.items()
+        ]
+        # standardize unlinked library tags
+        for n, loc in [(i[0], x['start']*2) for i in ref for x in i[1]]:
+            evm['bytecode']['object'] = "{}__{:_<36}__{}".format(
+                evm['bytecode']['object'][:loc],
+                n[:36],
+                evm['bytecode']['object'][loc+40:]
+            )
+        result[name] = {
+            'abi': data['abi'],
+            'allSourcePaths': sorted(set(i['contract'] for i in evm['pcMap'] if i['contract'])),
+            'ast': compiled['sources'][filename]['ast'],
+            'bytecode': evm['bytecode']['object'],
+            'bytecodeSha1': sha1(evm['bytecode']['object'][:-68].encode()).hexdigest(),
+            'compiler': CONFIG['solc'],
+            'contractName': name,
+            'deployedBytecode': evm['deployedBytecode']['object'],
+            'deployedSourceMap': evm['deployedBytecode']['sourceMap'],
+            # 'networks': {},
+            'opcodes': evm['deployedBytecode']['opcodes'],
+            'sha1': sources.get_hash(name),
+            'source': input_json['sources'][filename]['content'],
+            'sourceMap': evm['bytecode']['sourceMap'],
+            'sourcePath': filename,
+            'type': sources.get_type(name),
+            'pcMap': evm['pcMap']
+        }
+        result[name]['coverageMap'] = _generate_coverageMap(result[name])
+    return result
+
+
+def _generate_pcMap(compiled):
+    '''
+    Generates an expanded sourceMap useful for debugging.
+    [{
+        'contract': relative path of the contract source code
+        'jump': jump instruction as supplied in the sourceMap (-,i,o)
+        'op': opcode string
+        'pc': program counter as given by debug_traceTransaction
+        'start': source code start offset
+        'stop': source code stop offset
+        'value': value of the instruction, if any
+    }, ... ]
+    '''
+    id_map = dict((v['id'], k) for k, v in compiled['sources'].items())
+    for filename, name in [(k, x) for k, v in compiled['contracts'].items() for x in v]:
+        bytecode = compiled['contracts'][filename][name]['evm']['deployedBytecode']
+
+        if not bytecode['object']:
+            compiled['contracts'][filename][name]['evm']['pcMap'] = []
+            continue
+        pcMap = []
+        opcodes = bytecode['opcodes']
+        source_map = bytecode['sourceMap']
+        while True:
+            try:
+                i = opcodes[:-1].rindex(' STOP')
+            except ValueError:
+                break
+            if 'JUMPDEST' in opcodes[i:]:
+                break
+            opcodes = opcodes[:i+5]
+        opcodes = opcodes.split(" ")[::-1]
+        pc = 0
+        last = source_map.split(';')[0].split(':')
+        for i in range(3):
+            last[i] = int(last[i])
+        pcMap.append({
+            'start': last[0],
+            'stop': last[0]+last[1],
+            'op': opcodes.pop(),
+            'contract': id_map[last[2]],
+            'jump': last[3],
+            'pc': 0
+        })
+        pcMap[0]['value'] = opcodes.pop()
+        for value in source_map.split(';')[1:]:
+            pc += 1
+            if pcMap[-1]['op'][:4] == "PUSH":
+                pc += int(pcMap[-1]['op'][4:])
+            if value:
+                value = (value+":::").split(':')[:4]
+                for i in range(3):
+                    value[i] = int(value[i] or last[i])
+                value[3] = value[3] or last[3]
+                last = value
+            pcMap.append({
+                'start': last[0],
+                'stop': last[0]+last[1],
+                'op': opcodes.pop(),
+                'contract': id_map[last[2]] if last[2] != -1 else False,
+                'jump': last[3],
+                'pc': pc
+            })
+            if opcodes[-1][:2] == "0x":
+                pcMap[-1]['value'] = opcodes.pop()
+        compiled['contracts'][filename][name]['evm']['pcMap'] = pcMap
+    return compiled
+
+
+def _generate_coverageMap(build):
+    """Given the compiled project as supplied by compiler.compile_contracts(),
+    returns the function and line based coverage maps for unit test coverage
+    evaluation.
+
+    A coverage map item is structured as follows:
+
+    {
+        "/path/to/contract/file.sol":{
+            "functionName":{
+                "fn": {},
+                "line": [{}, {}, {}],
+                "total": int
+            }
+        }
+    }
+
+    Each dict in fn/line is as follows:
+
+    {
+        'jump': pc of the JUMPI instruction, if it is a jump
+        'pc': list of opcode program counters tied to the map item
+        'start': source code start offset
+        'stop': source code stop offset
+    }
+    """
+
+    fn_map = dict((x, {}) for x in build['allSourcePaths'])
+
+    for i in _isolate_functions(build):
+        fn_map[i.pop('contract')][i.pop('method')] = {'fn': i, 'line': []}
+    line_map = _isolate_lines(build)
+    if not line_map:
+        return {}
+
+    # future me - i'm sorry for this line
+    for source, fn_name, fn in [(k, x, v[x]['fn']) for k, v in fn_map.items() for x in v]:
+        for ln in [
+            i for i in line_map if i['contract'] == source and
+            i['start'] == fn['start'] and i['stop'] == fn['stop']
+        ]:
+            # remove duplicate mappings
+            line_map.remove(ln)
+        for ln in [
+            i for i in line_map if i['contract'] == source and
+            i['start'] >= fn['start'] and i['stop'] <= fn['stop']
+        ]:
+            # apply method names to line mappings
+            line_map.remove(ln)
+            fn_map[ln.pop('contract')][fn_name]['line'].append(ln)
+        fn_map[source][fn_name]['total'] = sum([1 if not i['jump'] else 2 for i in fn_map[source][fn_name]['line']])
+    return fn_map
+
+
+def _isolate_functions(compiled):
+    '''Identify function level coverage map items.'''
+    pcMap = compiled['pcMap']
+    fn_map = {}
+    for op in _oplist(pcMap, "JUMPDEST"):
+        s = _get_source(op)
+        if s[:8] in ("contract", "library ", "interfac"):
+            continue
+        if s[:8] == "function":
+            fn = s[9:s.index('(')]
+        elif " public " in s:
+            fn = s[s.index(" public ")+8:].split(' =')[0].strip()
+        else:
+            continue
+        if fn not in fn_map:
+            fn_map[fn] = _base(op)
+            fn_map[fn]['method'] = fn
+        fn_map[fn]['pc'].add(op['pc'])
+
+    fn_map = _sort(fn_map.values())
+    if not fn_map:
+        return []
+    for op in _oplist(pcMap):
+        try:
+            f = _next(fn_map, op)
+        except StopIteration:
+            continue
+        if op['stop'] > f['stop']:
+            continue
+        f['pc'].add(op['pc'])
+    return fn_map
+
+
+def _isolate_lines(compiled):
+    '''Identify line based coverage map items.
+
+    For lines where a JUMPI is not present, coverage items will merge
+    to include as much of the line as possible in a single item. Where a
+    JUMPI is involved, no merge will happen and overlapping non-jump items
+    are discarded.'''
+    pcMap = compiled['pcMap']
+    line_map = {}
+
+    # find all the JUMPI opcodes
+    for i in [pcMap.index(i) for i in _oplist(pcMap, "JUMPI")]:
+        op = pcMap[i]
+        if op['contract'] not in line_map:
+            line_map[op['contract']] = []
+        # if followed by INVALID or the source contains public, ignore it
+        if pcMap[i+1]['op'] == "INVALID" or " public " in _get_source(op):
+            continue
+        try:
+            # JUMPI is to the closest previous opcode that has
+            # a different source offset and is not a JUMPDEST
+            req = next(
+                x for x in pcMap[i-2::-1] if x['contract'] and
+                x['op'] != "JUMPDEST" and
+                x['start'] + x['stop'] != op['start'] + op['stop']
+            )
+        except StopIteration:
+            continue
+        line_map[op['contract']].append(_base(req))
+        line_map[op['contract']][-1].update({'jump': op['pc']})
+
+    # analyze all the opcodes
+    for op in _oplist(pcMap):
+        # ignore code that spans multiple lines
+        if ';' in _get_source(op):
+            continue
+        if op['contract'] not in line_map:
+            line_map[op['contract']] = []
+        # find existing related coverage map item, make a new one if none exists
+        try:
+            ln = _next(line_map[op['contract']], op)
+        except StopIteration:
+            line_map[op['contract']].append(_base(op))
+            continue
+        if op['stop'] > ln['stop']:
+            # if coverage map item is a jump, do not modify the source offsets
+            if ln['jump']:
+                continue
+            ln['stop'] = op['stop']
+        ln['pc'].add(op['pc'])
+
+    # sort the coverage map and merge overlaps where possible
+    for contract in line_map:
+        line_map[contract] = _sort(line_map[contract])
+        ln_map = line_map[contract]
+        i = 0
+        while True:
+            if len(ln_map) <= i + 1:
+                break
+            if ln_map[i]['jump']:
+                i += 1
+                continue
+            # JUMPI overlaps cannot merge
+            if ln_map[i+1]['jump']:
+                if ln_map[i]['stop'] > ln_map[i+1]['start']:
+                    del ln_map[i]
+                else:
+                    i += 1
+                continue
+            if ln_map[i]['stop'] >= ln_map[i+1]['start']:
+                ln_map[i]['pc'] |= ln_map[i+1]['pc']
+                ln_map[i]['stop'] = max(ln_map[i]['stop'], ln_map[i+1]['stop'])
+                del ln_map[i+1]
+                continue
+            i += 1
+    return [x for v in line_map.values() for x in v]
+
+
+def _get_source(op):
+    return sources[op['contract']][op['start']:op['stop']]
+
+
+def _next(coverage_map, op):
+    '''Given a coverage map and an item from pcMap, returns the related
+    coverage map item (based on source offset overlap).'''
+    return next(
+        i for i in coverage_map if i['contract'] == op['contract'] and
+        i['start'] <= op['start'] < i['stop']
+    )
+
+
+def _sort(list_):
+    return sorted(list_, key=lambda k: (k['contract'], k['start'], k['stop']))
+
+
+def _oplist(pcMap, op=None):
+    return [i for i in pcMap if i['contract'] and (not op or op == i['op'])]
+
+
+def _base(op):
+    return {
+        'contract': op['contract'],
+        'start': op['start'],
+        'stop': op['stop'],
+        'pc': set([op['pc']]),
+        'jump': False
+    }
