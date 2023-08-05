8839391c1054b3f9a4325f3d4d755dbba8e78847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:05 +0000

    Add package simple-pyueye/0.0.7/simple_pyueye-0.0.7.tar.gz

---
+++ packages/simple-pyueye/simple_pyueye-0.0.7.tar.gz/simple_pyueye-0.0.7/setup.py
@@ -0,0 +1,24 @@
+"""
+Usage: python setup.py sdist bdist_wheel
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fr:
+    requirements = fr.read().splitlines()
+
+setuptools.setup(
+    name="simple_pyueye",
+    version="0.0.7",
+    author="Joao Vital",
+    author_email="joao.vital@frontwave.com",
+	license='3-Clause BSD License',
+    description="High-level wrapper for the python bindings package pyueye",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://frontwave.pt/",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+)
---
+++ packages/simple-pyueye/simple_pyueye-0.0.7.tar.gz/simple_pyueye-0.0.7/simple_pyueye/__init__.py
@@ -0,0 +1,4 @@
+from .camera import CameraObj
+from .camera import ImageBuffer
+
+name = 'simple_pyueye'
