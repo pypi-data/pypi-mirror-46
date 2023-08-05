170aff3de46c56a9ebc87071d73f24c0e256f803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:24 +0000

    Add package drf-advanced-auth/0.2.3/drf_advanced_auth-0.2.3.tar.gz

---
+++ packages/drf-advanced-auth/drf_advanced_auth-0.2.3.tar.gz/drf_advanced_auth-0.2.3/README.md
@@ -0,0 +1,74 @@
+# DRF Advanced Authentication
+
+[Django REST Framework](https://www.django-rest-framework.org/) provides just about everything needed to turn your Django app into a REST-based web application. One area that DRF lacks functionality is in account management, specifically all the non-happy path scenarios related to authentication, namely password changes and password resets. This module aims to simplify setting up a full auth subsystem for your REST application.
+
+The funcitons provided by this package are:
+
+* login: an endpoint for authenticating a user
+* logout: an endpoint for ending a user's session
+* change password: an endpoint for changing the password of a logged-in user
+* reset password: two endpoints that togehter can be used to reset the password of a logged-out user (lost password, etc)
+
+**Note:** This package is a work in progress (that's why it's not yet at version 1.0). I am active seeking contributions to help with making it more usable, see ["Contributing"](#contributing) below.
+
+
+## Installation
+
+Install the package:
+
+```bash
+$ pip install drf-advanced-auth
+```
+
+Add it to `INSTALLED_APPS`:
+
+```python
+INSTALLED_APPS = [
+    ...
+    'drf_advanced_auth',
+    ...
+]
+```
+
+Update your URLs by adding the `drf_advanced_auth` urls under whatever prefix you want, and another url named `password_reset_confirm` that you want users to be redirected to when they click the link in the password reset email:
+
+```python
+urlpatterns = [
+    url(r'^auth/', include('drf_advanced_auth.urls', namespace='authentication')),
+    url(r'^password-reset/update/(?P<uidb64>.*)/(?P<token>.*)', FakeView.as_view(), name='password_reset_confirm'),
+]
+```
+
+
+## Usage
+
+(IN PROGRESS)
+
+
+## Sample App
+
+You can see a sample app using these fields buy running the following:
+
+```bash
+$ python manage.py migrate
+$ python manage.py loaddata fixtures/base.json
+$ python manage.py runserver
+```
+
+This app has the following endpoints (assuming they are under the `auth` prefix as shown above):
+
+* `/auth/login/`
+* `/auth/logout/`
+* `/auth/change_password/`
+* `/auth/reset_password_request/`
+* `/auth/reset_password_complete/`
+
+The username for the admin user is `admin`, and the password is `pass`.
+
+
+<a name="contributing"></a>
+## Contributing
+
+I am actively seeking contributions to this package. Check the "Issues" section of the repository for my current hit list.
+
+If you have suggestions for other features I am open to hearing them. Use the "Issues" section of the repository to start a conversation.
---
+++ packages/drf-advanced-auth/drf_advanced_auth-0.2.3.tar.gz/drf_advanced_auth-0.2.3/drf_advanced_auth/serializers.py
@@ -0,0 +1,94 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError as ModelValidationError
+from rest_framework import serializers
+
+from . import utils
+
+
+_authentication_failure_error = dict(error='authentication error')
+
+
+class PasswordField(serializers.CharField):
+
+    def __init__(self, *args, **kwargs):
+        super(PasswordField, self).__init__(*args, **kwargs)
+        style = self.style
+        if 'input_type' in style:
+            raise Exception('do not override field_type for password fields')
+        self.style['input_type'] = 'password'
+
+
+NullSerializer = serializers.Serializer
+
+
+class NewPasswordBase(serializers.Serializer):
+
+    new_password = PasswordField()
+    repeat_password = PasswordField()
+
+    def validate_new_password(self, value):
+        user = self.context['request'].user
+        try:
+            validate_password(value, user=user)
+        except ModelValidationError as e:
+            raise serializers.ValidationError(e)
+        return value
+
+    def validate(self, data):
+        if data['repeat_password'] != data['new_password']:
+            raise serializers.ValidationError('passwords mismatch')
+        return data
+
+
+class ChangePasswordSerializer(NewPasswordBase):
+
+    current_password = PasswordField()
+
+    def validate_current_password(self, value):
+        user = self.context['request'].user
+        if not authenticate(username=user.username, password=value):
+            raise serializers.ValidationError('current password incorrect')
+        return value
+
+    def save(self):
+        user = self.context['request'].user
+        user.set_password(self.data['new_password'])
+        user.save()
+
+
+class LoginSerializer(serializers.Serializer):
+
+    username = serializers.CharField()
+    password = PasswordField()
+
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if user is None:
+            raise serializers.ValidationError(_authentication_failure_error)
+        data['user'] = user
+        return data
+
+
+class ResetPasswordCompleteSerializer(NewPasswordBase):
+
+    uidb64 = serializers.CharField()
+    token = serializers.CharField()
+
+    def validate(self, data):
+        data = super(ResetPasswordCompleteSerializer, self).validate(data)
+        try:
+            self.user = utils.check_token(data['uidb64'], data['token'])
+        except Exception as e:
+            raise serializers.ValidationError(e)
+        return data
+
+    def save(self):
+        user = self.user
+        user.set_password(self.data['new_password'])
+        user.save()
+
+
+class ResetPasswordRequestSerializer(serializers.Serializer):
+
+    email = serializers.EmailField()
