e0b2ac0c1c24f03a2840987bfcead6a65826fee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:52 +0000

    Add package bentso/0.2.1/bentso-0.2.1.tar.gz

---
+++ packages/bentso/bentso-0.2.1.tar.gz/bentso-0.2.1/CHANGES.md
@@ -0,0 +1,19 @@
+### 0.2.1 (2019-05-12)
+
+Delete debugging statement
+
+## 0.2 (2019-05-12)
+
+Added ability to clean dataframes
+
+### 0.1.1 (2019-03-22)
+
+Fix missing package metadata files
+
+## 0.1 (2019-03-22)
+
+First release.
+
+### 0.0.1 (2019-03-20)
+
+Initial development version
---
+++ packages/bentso/bentso-0.2.1.tar.gz/bentso-0.2.1/setup.py
@@ -0,0 +1,71 @@
+from setuptools import setup
+import os
+
+packages = []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+# Probably should be changed, __init__.py is no longer required for Python 3
+for dirpath, dirnames, filenames in os.walk('bentso'):
+    # Ignore dirnames that start with '.'
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+setup(
+    name='bentso',
+    version="0.2.1",
+    packages=packages,
+    author="Chris Mutel",
+    author_email="cmutel@gmail.com",
+    license=open('LICENSE').read(),
+    # Only if you have non-python data (CSV, etc.). Might need to change the directory name as well.
+    # package_data={'bentso': package_files(os.path.join('bentso', 'data'))},
+    # entry_points = {
+    #     'console_scripts': [
+    #         'bentso-cli = bentso.bin.bentso_cli:main',
+    #     ]
+    # },
+    install_requires=[
+        'appdirs',
+        'beautifulsoup4',
+        'docopt',
+        'entsoe-py',
+        'pandas',
+        'peewee',
+        'pytz',
+        'requests',
+    ],
+    url="https://github.com/BONSAMURAIS/bentso",
+    long_description=open('README.md').read(),
+    description='BONSAI living model for European electricity via ENTSO-E API',
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
