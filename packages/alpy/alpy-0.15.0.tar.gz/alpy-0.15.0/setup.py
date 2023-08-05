151db4e414c64b8a455569bb067afb0d9951f0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:05 +0000

    Add package alpy/0.15.0/alpy-0.15.0.tar.gz

---
+++ packages/alpy/alpy-0.15.0.tar.gz/alpy-0.15.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import pathlib
+
+import setuptools
+
+import alpy
+
+setuptools.setup(
+    name="alpy",
+    version=alpy.__version__,
+    description="Library for testing network virtual appliances using Docker",
+    url="https://gitlab.com/abogdanenko/alpy",
+    packages=["alpy"],
+    install_requires=["docker", "pexpect", "qmp"],
+    author="Alexey Bogdanenko",
+    author_email="alexey@bogdanenko.com",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
