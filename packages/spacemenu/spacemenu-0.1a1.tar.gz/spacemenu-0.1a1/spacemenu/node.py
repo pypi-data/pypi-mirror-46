2c4e30d3760036d7d04e5c2a544de65a284a0b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:58 +0000

    Add package spacemenu/0.1a1/spacemenu-0.1a1.tar.gz

---
+++ packages/spacemenu/spacemenu-0.1a1.tar.gz/spacemenu-0.1a1/spacemenu/node.py
@@ -0,0 +1,34 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class _Node:
+    def __init__(self, label):
+        self.label = label
+        self.shortcut = None
+
+    def _on_click(self, button, args):
+        type = self.__class__.__name__.lower()
+        window = button.get_parent().get_parent()
+        window.emit(type, self)
+
+    def set_shortcut(self, shortcut):
+        self.shortcut = shortcut
+
+
+    def get_button(self):
+        button = Gtk.Button.new_with_label('{shortcut:1}{separator:2}{label:10}'.format(
+            shortcut = self.shortcut,
+            separator = '->',
+            label= self.label
+        ))
+
+        button.connect('clicked', self._on_click, None)
+
+        for c in button.get_children():
+            c.set_justify(Gtk.Justification.LEFT)
+            c.set_halign(Gtk.Align.START)
+            c.set_use_markup(True)
+
+        return button
+
