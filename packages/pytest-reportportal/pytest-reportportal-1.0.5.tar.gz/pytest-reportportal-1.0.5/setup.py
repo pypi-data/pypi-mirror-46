ec2758171fbb9fdeba4471aa83d245a3b7152843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:23 +0000

    Add package pytest-reportportal/1.0.5/pytest-reportportal-1.0.5.tar.gz

---
+++ packages/pytest-reportportal/pytest-reportportal-1.0.5.tar.gz/pytest-reportportal-1.0.5/setup.py
@@ -0,0 +1,46 @@
+import os
+
+from setuptools import setup
+
+
+def read_file(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+def static_url(url):
+    return os.path.join(STATIC_URL, url)
+		
+version = '1.0.5'
+
+requirements = [
+    'reportportal-client>=3.1.0',
+    'pytest>=3.0.7',
+    'six>=1.10.0',
+    'dill>=0.2.7.1',
+]
+
+setup(
+    name='pytest-reportportal',
+    version=version,
+    description='Agent for Reporting results of tests to the Report Portal',
+    long_description=read_file('README.rst'),
+    author='',
+    author_email='SupportEPMC-TSTReportPortal@epam.com',
+    url='https://github.com/reportportal/agent-python-pytest',
+    packages=['pytest_reportportal'],
+    install_requires=requirements,
+    license='Apache License Version 2.0',
+    keywords=['testing', 'reporting', 'reportportal', 'pytest'],
+    classifiers=[
+        'Framework :: Pytest',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+        ],
+    entry_points={
+        'pytest11': [
+            'pytest_reportportal = pytest_reportportal.plugin',
+        ]
+    },
+)
