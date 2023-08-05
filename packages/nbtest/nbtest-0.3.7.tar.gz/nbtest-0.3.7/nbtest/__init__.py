dbd7a328658f30e3ae9a6be520b06f70d34efb56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:26 +0000

    Add package nbtest/0.3.7/nbtest-0.3.7.tar.gz

---
+++ packages/nbtest/nbtest-0.3.7.tar.gz/nbtest-0.3.7/nbtest/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, print_function
+__version__ = '0.3.7' # add path from nbtest import
+
+from . import Utils
+from . import assertpyx
+from .assertpyx import AX
+from .jsonpath import jpFinds
