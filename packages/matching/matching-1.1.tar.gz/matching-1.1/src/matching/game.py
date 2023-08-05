7c3288b2a245a2bee6f4bc269a97f8c47044ed49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:26 +0000

    Add package matching/1.1/matching-1.1.tar.gz

---
+++ packages/matching/matching-1.1.tar.gz/matching-1.1/src/matching/game.py
@@ -0,0 +1,51 @@
+""" The base matchmaker class for facilitating and solving matching games. """
+
+
+class Game:
+    """ A class to store information about, and facilitate the solving of, a
+    matching game.
+
+    This is a base class and is not intended for uses other than inheritance.
+
+    Attributes
+    ==========
+    matching : `None`
+        Initialised to be `None`. After solving the game instance, a dictionary
+        storing the final matches is given here.
+    blocking_pairs : `None`.
+        Initialised to be `None`. After solving and checking the stability of
+        the game instance, a list of any pairs that block the stability of the
+        matching.
+    """
+
+    def __init__(self):
+
+        self._matching = None
+        self.blocking_pairs = None
+
+    @property
+    def matching(self):
+        """ Property method to stop direct write access. """
+
+        return self._matching
+
+    @matching.getter
+    def matching(self):
+        """ Property getter. """
+
+        return self._matching
+
+    def solve(self):
+        """ Placeholder for solving the given matching game. """
+
+        raise NotImplementedError()
+
+    def check_stability(self):
+        """ Placeholder for checking the stability of the current matching. """
+
+        raise NotImplementedError()
+
+    def check_validity(self):
+        """ Placeholder for checking the validity of the current matching. """
+
+        raise NotImplementedError()
---
+++ packages/matching/matching-1.1.tar.gz/matching-1.1/src/matching/games/__init__.py
@@ -0,0 +1,5 @@
+""" Make the games and their algorithms accessible. """
+
+from .hospital_resident import HospitalResident, hospital_resident
+from .stable_marriage import StableMarriage, stable_marriage
+from .student_allocation import StudentAllocation, student_allocation
