ae7de91410cbfea0b9d5fc5db76ce3d6c21e0fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:09 +0000

    Add package pyuplift/0.0.3.4/pyuplift-0.0.3.4.tar.gz

---
+++ packages/pyuplift/pyuplift-0.0.3.4.tar.gz/pyuplift-0.0.3.4/pyuplift/variable_selection/__init__.py
@@ -0,0 +1,3 @@
+from .two_model import TwoModel
+from .dummy import Dummy
+from .econometric import Econometric
---
+++ packages/pyuplift/pyuplift-0.0.3.4.tar.gz/pyuplift-0.0.3.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyuplift',
+    version='0.0.3.4',
+    author='Artem Kuchumov',
+    author_email='kuchumov7@gmail.com',
+    url='https://github.com/duketemon/pyuplift',
+    license='MIT License',
+    description='Uplift modeling implementation',
+    keywords=['uplift modeling', 'machine learning', 'true-response-modeling', 'incremental-value-marketing'],
+    packages=find_packages(),
+    install_requires=["pandas>=0.23.4", "scikit-learn>=0.20.0", "requests>=2.19.1"],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
