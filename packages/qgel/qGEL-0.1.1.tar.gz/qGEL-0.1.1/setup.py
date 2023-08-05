374173e2e43256dd41b49da2bd9438f9e472691b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:23 +0000

    Add package qgel/0.1.1/qGEL-0.1.1.tar.gz

---
+++ packages/qgel/qGEL-0.1.1.tar.gz/qGEL-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='qGEL',
+      version='0.1.1',
+      description='A quick way to qGEL',
+      long_description='A quick way to use the qGEL embedding : http://github.com/egolinko/qGELpy',
+      url='http://github.com/egolinko/qGELpy',
+      author='Eric Golinko',
+      author_email='egolinko@gmail.com',
+      license='MIT',
+      packages=['qGEL'],
+      zip_safe=False)
