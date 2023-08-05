eb3a3549fa294cef06bf3a26f3d46ea9697496ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:14 +0000

    Add package afs2-datasource/1.0.11/afs2-datasource-1.0.11.tar.gz

---
+++ packages/afs2-datasource/afs2-datasource-1.0.11.tar.gz/afs2-datasource-1.0.11/setup.py
@@ -0,0 +1,20 @@
+import os
+import setuptools
+
+setuptools.setup(
+    name='afs2-datasource',
+    version='1.0.11',
+    author='Stacy.yeh',
+    author_email='stacy.yeh@advantech.com.tw',
+    packages=setuptools.find_packages(),
+    install_requires=[
+      "pymongo==3.7.2",
+      "pandas==0.24.2",
+      "psycopg2-binary==2.8.1",
+      "influxdb==5.2.2"
+    ],
+    description='For AFS developer to access Datasource',
+    keywords=['AFS']
+)
+
+# python setup.py bdist_wheel
