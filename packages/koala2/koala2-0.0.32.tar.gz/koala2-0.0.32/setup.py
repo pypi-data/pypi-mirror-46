74eb6751af87ed3161f46177417b5bcde404d6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:44 +0000

    Add package koala2/0.0.32/koala2-0.0.32.tar.gz

---
+++ packages/koala2/koala2-0.0.32.tar.gz/koala2-0.0.32/setup.py
@@ -0,0 +1,32 @@
+"""Setup script."""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+if __name__ == '__main__':
+    setup(
+        name="koala2",
+
+        version="0.0.32",
+
+        author="Ants, open innovation lab",
+        author_email="contact@weareants.fr",
+
+        packages=find_packages(),
+
+        include_package_data=True,
+
+        url="https://github.com/anthill/koala",
+
+        license="GNU GPL3",
+        install_requires=[
+            'networkx >= 2.1',
+            'openpyxl >= 2.5.3',
+            'numpy >= 1.14.2',
+            'Cython >= 0.28.2',
+            'lxml >= 4.1.1',
+            'six >= 1.11.0',
+        ]
+    )
