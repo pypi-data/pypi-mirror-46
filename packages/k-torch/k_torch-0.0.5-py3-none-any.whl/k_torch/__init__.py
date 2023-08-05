5b47fe9da4f2461d904637c4e6b33d586ec8e946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:52 +0000

    Add package k-torch/0.0.5/k_torch-0.0.5-py3-none-any.whl

---
+++ packages/k-torch/k_torch-0.0.5-py3-none-any.whl/k_torch/__init__.py
@@ -0,0 +1,12 @@
+# K torch init file
+
+
+### version 0.0.4 : 10-05-19
+# Fixed layer returning 3 params
+
+### version 0.0.3 : 10-05-19
+# changed nb_epochs to epochs in models.fit
+# changed validation_data from dict to list in models.fit
+# added type casting to np.float64 to X and y in models.fit
+
+name = 'k_torch'
