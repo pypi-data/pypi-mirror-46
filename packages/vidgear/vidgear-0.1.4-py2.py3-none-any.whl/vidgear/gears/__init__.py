b790a34d1a2d8b48e1e6e0b4f45ba8e36ecee023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:51 +0000

    Add package vidgear/0.1.4/vidgear-0.1.4-py2.py3-none-any.whl

---
+++ packages/vidgear/vidgear-0.1.4-py2.py3-none-any.whl/vidgear/gears/__init__.py
@@ -0,0 +1,7 @@
+# import the necessary packages
+from .pigear import PiGear
+from .camgear import CamGear
+from .videogear import VideoGear
+from .writegear import WriteGear
+
+__all__ = ["PiGear", "CamGear", "VideoGear", "WriteGear"]
