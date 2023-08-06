c451079ca334f1c48616a171b1ce271a7e16cb2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:26 +0000

    Add package genie.libs.filetransferutils/19.5.0/genie.libs.filetransferutils-19.5.0-py3-none-any.whl

---
+++ packages/genie.libs.filetransferutils/genie.libs.filetransferutils-19.5.0-py3-none-any.whl/genie/libs/filetransferutils/__init__.py
@@ -0,0 +1,27 @@
+'''
+Module:
+    genie.libs.filetransferutils
+
+Description:
+	This is the library sub-component of Genie for `genie.libs.filetransferutils`.
+    A utility package for file transfer to/from remote servers using different protocols
+    (tftp/ftp/scp...etc.)
+
+'''
+
+__version__ = '19.5.0'
+__author__ = 'Cisco Systems Inc.'
+__contact__ = ['pyats-support@cisco.com', 'pyats-support-ext@cisco.com']
+__copyright__ = 'Copyright (c) 2018, Cisco Systems Inc.'
+
+from .fileutils import FileUtils
+
+try:
+    from ats.cisco.stats import CesMonitor
+    CesMonitor(action = 'geniefiletransferutilslib', application='Genie').post()
+except Exception:
+    try:
+        from ats.utils.stats import CesMonitor
+        CesMonitor(action = 'geniefiletransferutilslib', application='Genie').post()
+    except Exception:
+        pass
