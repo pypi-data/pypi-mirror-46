cf58316c8a68caaffba6a02d530ca26c4b8a6fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:07 +0000

    Add package defaultdict-NickNackGus/1.0.1/defaultdict-NickNackGus-1.0.1.tar.gz

---
+++ packages/defaultdict-NickNackGus/defaultdict-NickNackGus-1.0.1.tar.gz/defaultdict-NickNackGus-1.0.1/defaultdict/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+"""NickNackGus's DefaultDict package."""
+name = "defaultdict"
+
---
+++ packages/defaultdict-NickNackGus/defaultdict-NickNackGus-1.0.1.tar.gz/defaultdict-NickNackGus-1.0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="defaultdict-NickNackGus",
+    version="1.0.1",
+    author="NickNackGus",
+    author_email="NickNackGus@gmail.com",
+    description="defaultdict with deepcopy support",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NickNackGus/pydefaultdict",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
