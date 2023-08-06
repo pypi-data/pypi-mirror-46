f3265faac81072012733d27d2b123e14df606eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:58 +0000

    Add package NBFS/0.0.5/NBFS-0.0.5.tar.gz

---
+++ packages/NBFS/NBFS-0.0.5.tar.gz/NBFS-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'NBFS',
+    version = '0.0.5',
+    keywords='NBFS',
+    description = 'a tool for analysis NBFS protocol',
+    license = '',
+    url = 'https://github.com/hero576/pynbfs',
+    author = 'hero576',
+    author_email = '812041531@qq.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [
+        ],
+)
