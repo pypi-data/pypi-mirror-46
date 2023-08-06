53e7ba1fed04351bbad399159cb59d64c4dade90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:44 +0000

    Add package qreport/0.3/qreport-0.3.tar.gz

---
+++ packages/qreport/qreport-0.3.tar.gz/qreport-0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(name='qreport',
+      version='0.3',
+      description='Simple Report',
+      url='https://github.com/rvettori/qreport',
+      author='Rafael Vettori',
+      author_email='rafael.vettori@gmail.com',
+      license='MIT',
+      packages=['qreport'],
+      install_requires=[
+            'dataset',
+            'qfilter',
+            'jinja2',
+      ],
+      zip_safe=False,
+      package_data={'qreport': [
+          'templates/index.html',
+          'templates/_filter.html',
+          'templates/_forms.html',
+          'templates/_table.html',
+          'templates/_title.html',
+          'templates/_script.js',
+      ]}
+)
