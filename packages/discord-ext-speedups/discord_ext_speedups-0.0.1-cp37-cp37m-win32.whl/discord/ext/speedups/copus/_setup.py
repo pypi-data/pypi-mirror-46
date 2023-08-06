4f5d2cf2fb56ef3f2a35f9668d37acd1a8b46719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:06 +0000

    Add package discord-ext-speedups/0.0.1/discord_ext_speedups-0.0.1-cp37-cp37m-win32.whl

---
+++ packages/discord-ext-speedups/discord_ext_speedups-0.0.1-cp37-cp37m-win32.whl/discord/ext/speedups/copus/_setup.py
@@ -0,0 +1,36 @@
+import pathlib
+import struct
+import sys
+
+# System info
+platform = sys.platform
+arch = struct.calcsize('P') * 8
+
+# Paths
+cwd = pathlib.Path('.')
+extension_path = pathlib.Path(__file__).parent
+binary_path = extension_path / 'bin' / f'x{arch}'
+
+# Compile options
+include_dirs = [extension_path]
+library_dirs = [binary_path]
+runtime_lib_dirs = [binary_path]
+
+libraries = {
+    'win32': ['opus'],
+    'linux': ['opus']
+}
+
+if platform == 'win32':
+    runtime_lib_dirs = []
+    include_dirs.append(cwd / 'include')
+
+def get_extension_data():
+    return {
+        'name': 'discord.ext.speedups.copus._copus',
+        'sources': [extension_path / '_copus.pyx'],
+        'libraries': libraries[platform],
+        'include_dirs': include_dirs,
+        'library_dirs': library_dirs,
+        'runtime_library_dirs': runtime_lib_dirs
+    }
