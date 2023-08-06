885cb69a0824612466bda56af693253ef7c552a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:09 +0000

    Add package pySMP/0.1.3/pySMP-0.1.3.tar.gz

---
+++ packages/pySMP/pySMP-0.1.3.tar.gz/pySMP-0.1.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="pySMP",
+	version="0.1.3",
+	author="King Abdullah Petroleum Studies and Research Center",
+	author_email="ktab@kapsarc.org",
+	description="KTAB",
+	long_description=long_description,
+	long_description_conent_type="text/markdown",
+	url="https://github.com/kapsarc/pySMP",
+	packages=['pySMP'],
+	package_dir={'pySMP': 'pySMP'},
+	package_data={'pySMP': ['lib/*.so', 'lib/*.dll', 'lib/*.dylib']},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent"
+	]
+)
