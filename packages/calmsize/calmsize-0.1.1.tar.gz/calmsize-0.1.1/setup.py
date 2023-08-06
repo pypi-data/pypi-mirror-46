a694b9d34bc835f8e86c6a314134c2e666ea6a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:26 +0000

    Add package calmsize/0.1.1/calmsize-0.1.1.tar.gz

---
+++ packages/calmsize/calmsize-0.1.1.tar.gz/calmsize-0.1.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+try:
+    long_description = '{}\n{}\nDownload\n======\n'.format(
+        open('README.md').read(),
+        open('CHANGES.md').read()
+    )
+except FileNotFoundError:
+    long_description = ''
+
+
+setup(
+    name="calmsize",
+    version="0.1.1",
+    description="Fork from hurry.filesize, A simple Python library for human readable file sizes (or anything sized in bytes).",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    keywords='file size bytes',
+    author='Kaiyu Shi',
+    author_email='skyisno.1@gmail.com',
+    url='https://github.com/Stonesjtu/calmsize',
+    license='ZPL 2.1',
+    packages=['calmsize'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+    ],
+)
