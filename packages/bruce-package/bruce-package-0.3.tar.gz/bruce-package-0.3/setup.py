18ff9c63015b7c220f0568dcae126396b307f6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:34 +0000

    Add package bruce-package/0.3/bruce-package-0.3.tar.gz

---
+++ packages/bruce-package/bruce-package-0.3.tar.gz/bruce-package-0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, Extension
+
+setup(
+    name = 'bruce-package',
+    version = '0.3',
+    description = 'GPU-accelerated binary star model',
+    url = None,
+    author = 'Samuel Gill et al',
+    author_email = 'samuel.gill@warwick.ac.uk',
+    license = 'GNU',
+    packages=['bruce','bruce/binarystar'],
+    scripts=['ngtsfit/ngtsfit'],
+    install_requires=['celerite', 'numba', 'numpy', 'emcee']
+)
