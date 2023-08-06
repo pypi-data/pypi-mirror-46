cbf15c63170de78d52f8529d286f44fed6258586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:24 +0000

    Add package ifolder/1.1.6/ifolder-1.1.6.tar.gz

---
+++ packages/ifolder/ifolder-1.1.6.tar.gz/ifolder-1.1.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ifolder',
+    version='1.1.6',
+    description=(
+        'ifolder is a python package for transferring files and folders with tcp socket'
+    ),
+    long_description=open('README.rst').read(),
+    author='vinct',
+    author_email='vt.y@qq.com',
+    maintainer='vt',
+    maintainer_email='vt.y@qq.com',
+    license='MIT License',
+    install_requires=[
+          'wonderbits',
+          'socket',],
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/vincent770/ifolder.git',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+)
