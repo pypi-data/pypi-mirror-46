e418db1eb783ab619211e4d0aa7de7a2e55c02ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:51 +0000

    Add package t2kdm/1.6.0/t2kdm-1.6.0-py2.py3-none-any.whl

---
+++ packages/t2kdm/t2kdm-1.6.0-py2.py3-none-any.whl/t2kdm/__init__.py
@@ -0,0 +1,47 @@
+"""T2K Data Manager
+
+Helpful tools to manage the T2K data on the grid.
+"""
+
+import configuration
+import backends
+import storage
+import utils
+import sys
+
+if sys.argv[0].endswith('t2kdm-config'):
+    # Someone is calling t2kdm-config.
+    # Do not try to load any configuration, as that is what they might be trying to fix!
+    pass
+else:
+    # Load configuration
+    config = configuration.load_config()
+    # Get backend according to configuration
+    backend = backends.get_backend(config)
+
+    # Get functions from backend
+    ls = backend.ls
+    iter_ls = backend.iter_ls
+    ls_se = backend.ls_se
+    iter_ls_se = backend.iter_ls_se
+    is_dir = backend.is_dir
+    is_dir_se = backend.is_dir_se
+    replicas = backend.replicas
+    is_file = backend.is_file
+    is_file_se = backend.is_file_se
+    exists = backend.exists
+    is_online = backend.is_online
+    checksum = backend.checksum
+    state = backend.state
+    replicate = backend.replicate
+    remove = backend.remove
+    rmdir = backend.rmdir
+    move = backend.move
+    rename = backend.rename
+    get = backend.get
+    put = backend.put
+
+    # And from utils
+    check_checksums = utils.check_checksums
+    check_replicas = utils.check_replicas
+    check_replica_states = utils.check_replica_states
