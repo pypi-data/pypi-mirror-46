28b2b60de21e1e9556adeb05924f86950636e2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:34 +0000

    Add package aws-default-cleaner/1.0.2/aws-default-cleaner-1.0.2.tar.gz

---
+++ packages/aws-default-cleaner/aws-default-cleaner-1.0.2.tar.gz/aws-default-cleaner-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='aws-default-cleaner',
+    version='1.0.2',
+    description='AWS Default Cleaner - default VPC, default subnets, default internet gateway and etc',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ashkue/aws-default-cleaner',
+    author='Pavel Morshenyuk',
+    author_email='ashkue@outlook.com',
+    license='MIT',
+    scripts=['bin/aws-default-cleaner'],
+    install_requires=[
+        'click',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ])
