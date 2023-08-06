b815fcd43b73a4217057bdb1133ef5c20f2f1708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:02 +0000

    Add package flask-censor/0.1/flask-censor-0.1.tar.gz

---
+++ packages/flask-censor/flask-censor-0.1.tar.gz/flask-censor-0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import flask_censor
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name = "flask-censor",
+	version = flask_censor.__version__,
+	author = "Robert Heath",
+	author_email = "rob@robheath.me.uk",
+	description = "A profanity censor with hooks for Flask and Jinja2",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url = "https://github.com/Laukei/flask-censor",
+	packages = ['flask_censor'],
+    #data_files=[('flask_censor/data', ['flask_censor/data/wordlist.txt']), ],
+ 	package_data={
+ 	    '': ['flask_censor/data/wordlist.txt'],
+	 },
+	include_package_data=True,
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Development Status :: 3 - Alpha"]
+	)
