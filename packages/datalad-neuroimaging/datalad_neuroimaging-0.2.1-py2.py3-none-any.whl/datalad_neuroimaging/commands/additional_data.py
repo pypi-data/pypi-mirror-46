1d4aeef2b9779fc388481ac988959341f85ec2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:03:00 +0000

    Add package datalad-neuroimaging/0.2.1/datalad_neuroimaging-0.2.1-py2.py3-none-any.whl

---
+++ packages/datalad-neuroimaging/datalad_neuroimaging-0.2.1-py2.py3-none-any.whl/datalad_neuroimaging/commands/additional_data.py
@@ -0,0 +1,6 @@
+some = {
+        'type': '',
+        'status': None,  # TODO: process state convention; flags
+        'location': ds.path, # would be SESSION/dicom or raw ds root;
+                             # So: needs to be SESSION/physio or sth.
+}
---
+++ packages/datalad-neuroimaging/datalad_neuroimaging-0.2.1-py2.py3-none-any.whl/datalad_neuroimaging/commands/spec_handler.py
@@ -0,0 +1,14 @@
+# type: level(study, session, whatever)
+#
+
+# 'type'
+# 'location'
+# 'status'
+# 'comment'
+
+
+class StudySpecHandler(object):
+
+    def __init__(self):
+        pass
+
