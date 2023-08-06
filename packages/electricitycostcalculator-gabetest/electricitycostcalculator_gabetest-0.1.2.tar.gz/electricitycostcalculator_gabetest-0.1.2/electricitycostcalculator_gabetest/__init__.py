46399c5c51e90c93f33adf6a8753624c5e43d7d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:09 +0000

    Add package electricitycostcalculator-gabetest/0.1.2/electricitycostcalculator_gabetest-0.1.2.tar.gz

---
+++ packages/electricitycostcalculator-gabetest/electricitycostcalculator_gabetest-0.1.2.tar.gz/electricitycostcalculator_gabetest-0.1.2/electricitycostcalculator_gabetest/__init__.py
@@ -0,0 +1,4 @@
+__author__ = 'Olivier Van Cutsem'
+
+from electricitycostcalculator_gabetest import cost_calculator, oadr_signal, openei_tariff
+__all__ = ["cost_calculator", "oadr_signal", "openei_tariff"]
