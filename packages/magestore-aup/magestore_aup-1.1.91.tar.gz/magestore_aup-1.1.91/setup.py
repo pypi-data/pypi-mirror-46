c3cb3a0d259576d79716365482d7169d42f71d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:54 +0000

    Add package magestore-aup/1.1.91/magestore_aup-1.1.91.tar.gz

---
+++ packages/magestore-aup/magestore_aup-1.1.91.tar.gz/magestore_aup-1.1.91/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+package_name = 'magestore_aup'
+packages = setuptools.find_packages(
+    include=[package_name, "{}.*".format(package_name)]
+)
+
+setuptools.setup(
+    name=package_name,
+    version="1.1.91",
+    author="Mars",
+    author_email="mars@trueplus.vn",
+    description="Update README",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://magestore.com",
+    packages=packages,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3',
+    install_requires=[
+        # install specific crytography 2.4.2 to fix error with 2.5 version
+        # ref: https://github.com/paramiko/paramiko/issues/1369
+        'cryptography==2.4.2',
+        'fabric',
+    ],
+    include_package_data=True
+)
