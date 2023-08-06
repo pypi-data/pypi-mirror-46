43cf77c0d0a6685499835f1ecf7d240362be8dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:17 +0000

    Add package markdown2pdf3/0.2.2/markdown2pdf3-0.2.2-py3-none-any.whl

---
+++ packages/markdown2pdf3/markdown2pdf3-0.2.2-py3-none-any.whl/markdown2pdf3/utils.py
@@ -0,0 +1,16 @@
+import os
+import functools
+
+
+def save_cwd(function):
+    functools.wraps(function)
+
+    def wrapper(*args, **kwargs):
+        current_dir = os.getcwd()
+        try:
+            result = function(*args, **kwargs)
+            return result
+        finally:
+            os.chdir(current_dir)
+
+    return wrapper
