54ded5bc82c8d7a3fc8136030c87501c7907f09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:40 +0000

    Add package onc/2.0.2/onc-2.0.2-py3-none-any.whl

---
+++ packages/onc/onc-2.0.2-py3-none-any.whl/onc/modules/_OncDelivery.py
@@ -0,0 +1,232 @@
+from datetime import datetime, timedelta
+import requests
+import os
+import humanize
+from time import sleep, time
+from .util import printErrorMessage
+from ._DataProductFile import _DataProductFile
+from ._PollLog import _PollLog
+from ..Exceptions import MaxRetriesException
+
+class _OncDelivery:
+    """
+    Holds the methods that wrap the API data product delivery services
+    """
+    pollPeriod = 2.0 # Default seconds to wait between consecutive download tries of a file (when no estimate processing time is available)
+
+    
+    def orderDataProduct(self, filters, maxRetries: int = 0, downloadResultsOnly: bool = False, includeMetadataFile: bool = True, overwrite: bool = False):
+        config = {
+            'maxRetries': maxRetries,
+            'overwrite' : overwrite,
+            'outPath'   : self.outPath,
+            'token'     : self.token,
+            'baseUrl'   : self.baseUrl,
+            'timeout'   : self.timeout,
+            'download'  : not downloadResultsOnly,
+            'metadata'  : includeMetadataFile
+        }
+        url = '{:s}api/dataProductDelivery'.format(self.baseUrl)
+
+        try:
+            # Request the product
+            dpRequestId = self.requestProduct(url, filters)
+            config['pollPeriod'] = self.pollPeriod
+            
+            # Run the product and get the dpRunIds
+            runInfo = self.runProduct(url, dpRequestId)
+            
+            # For all runIds, wait for generation and download if required
+            fileList = []
+            for runId in runInfo['runIds']:
+                if config['download']:
+                    fileList.extend(self.downloadProductFiles(config, runId))
+                else:
+                    fileList.extend(self.infoForProductFiles(config, runId, runInfo['fileCount']))
+
+            # Print the final stats
+            self.printTotalStats(fileList, runInfo)
+
+        except Exception: raise
+
+        return self.generateStats(fileList, runInfo)
+
+        
+    def requestProduct(self, url, filters):
+        """
+        Data product request
+        """
+        filters['method'] = 'request'
+        filters['token']  = self.token
+        try:
+            response = self._doRequest(url, filters)
+        except Exception: raise
+    
+        self.estimatePollPeriod(response)
+        self.printProductRequest(response)
+        return response['dpRequestId']
+
+    
+    def runProduct(self, url, dpRequestId):
+        """
+        Run a product request and wait until the product generation is complete
+        Return a dictionary with information of the run process
+        """
+        status = ''
+        log = _PollLog(True)
+        runResult = {'runIds': [], 'fileCount': 0, 'runTime': 0, 'requestCount': 0}
+        try:
+            start = time()
+            while status != 'complete':
+                response = requests.get(url, {'method': 'run', 'token': self.token, 'dpRequestId': dpRequestId}, timeout=self.timeout)
+                runResult['requestCount'] += 1
+
+                if response.ok:
+                    data = response.json()
+                else:
+                    code = response.status_code
+                    if self.showInfo: util.printErrorMessage(response, params)
+                    raise Exception('The server request failed with HTTP status {:d}.'.format(code), code)
+                
+                status = data[0]['status']
+                log.logMessage(data)
+                sleep(self.pollPeriod)
+            
+            runResult['fileCount'] = data[0]['fileCount']
+            runResult['runTime'] = time() - start
+        except Exception: raise
+        
+        # gather a list of runIds
+        for run in data:
+            runResult['runIds'].append(run['dpRunId'])
+
+        return runResult
+            
+    
+    def downloadProductFiles(self, config: list, runId: int):
+        fileList = []
+        index = 1
+        try:
+            # wait until product has been generated
+            status = 200
+            print('Downloading data product files with runId {:d}...'.format(runId))
+            while status == 200:
+                pf = _DataProductFile(runId, str(index), config)
+                status = pf.download()
+                if status != 404:
+                    fileList.append(pf.getInfo())
+                index += 1
+
+            # get metadata if required
+            if config['metadata']:
+                pf = _DataProductFile(runId, 'meta', config)
+                status = pf.download()
+                if status != 404:
+                    fileList.append(pf.getInfo(download=True))
+
+        except Exception: raise
+
+        return fileList
+
+
+    def infoForProductFiles(self, config: list, runId: int, fileCount: int):
+        fileList = []
+        self.print(fileCount)
+        for index in range(1, fileCount + 1):
+            pf = _DataProductFile(runId, str(index), config)
+            self.print(index)
+            fileList.append(pf.getInfo(download=False))
+
+        if config['metadata']:
+            pf = _DataProductFile(runId, 'meta', config)
+            fileList.append(pf.getInfo(download=False))
+
+        return fileList
+
+
+    def printProductRequest(self, response):
+        """
+        Prints the information from a response given after a data product request
+        The request response format might differ depending on the product source (archive or generated on the fly)
+        """
+        isGenerated = ('estimatedFileSize' in response)
+        print('Request Id: {:d}'.format(response['dpRequestId']))
+        
+        if isGenerated:
+            print('Estimated File Size: {:s}'.format(response['estimatedFileSize']))
+            print('Estimated Processing Time: {:s}'.format(response['estimatedProcessingTime']))
+        else:
+            print('File Size: {:s}'.format(response['fileSize']))
+            print('Data product is ready for download.')
+
+
+    def estimatePollPeriod(self, response):
+        """
+        Sets a poll period adequate to the estimated processing time
+        Longer processing times require longer poll periods to avoid going over maxRetries
+        """
+        # Parse estimated processing time
+        txtEstimated = response['estimatedProcessingTime']
+        parts = txtEstimated.split(' ')
+        if len(parts) == 2:
+            unit = parts[1]
+            factor = 1
+            if unit   == 'min':
+                factor = 60
+            elif unit == 'hour':
+                factor = 3600
+            total = factor * int(parts[0])
+            self.pollPeriod = max(0.02 * total, 1.0) # poll every 2%
+        else:
+            # No estimated processing time available, so we keep the default
+            pass
+
+
+    def printTotalStats(self, fileList: list, runInfo: dict):
+        """
+        Prints a formatted representation of the total time and size downloaded
+        after the product order finishes
+        """
+        downloadCount = 0
+        downloadTime = 0
+        size = 0
+        
+        for file in fileList:
+            size += file["size"]
+            if file["downloaded"]:
+                downloadCount += 1
+                downloadTime  += file['fileDownloadTime']
+
+        # Print run time
+        runTime = timedelta(seconds=runInfo['runTime'])
+        print('Total run time: {:s}'.format(humanize.naturaldelta(runTime)))
+
+        # Print download time
+        if downloadTime < 1.0:
+            txtDownTime = '{:.3f} seconds'.format(downloadTime)
+        else:
+            txtDownTime = humanize.naturaldelta(downloadTime)
+        print('Total download Time: {:s}'.format(txtDownTime))
+
+        # Print size and count of files
+        print('{:d} files ({:s}) downloaded'.format(downloadCount, humanize.naturalsize(size)))
+        
+
+    def generateStats(self, fileList: list, runInfo: dict):
+        size = 0
+        downloadTime = 0
+        for file in fileList:
+            downloadTime += file['fileDownloadTime']
+            size += file['size']
+
+        result = {
+            'downloadResults': fileList,
+            'stats': {
+                'runTime'     : round(runInfo['runTime'], 3),
+                'downloadTime': round(downloadTime, 3),
+                'requestCount': runInfo['requestCount'],
+                'totalSize'   : size
+            }
+        }
+
+        return result
---
+++ packages/onc/onc-2.0.2-py3-none-any.whl/onc/onc.py
@@ -0,0 +1,147 @@
+import requests
+import json
+from datetime import timedelta
+import errno
+import puremagic
+import humanize
+from urllib import parse
+from .modules._util import printErrorMessage
+from .modules._OncDiscovery import _OncDiscovery
+from .modules._OncDelivery import _OncDelivery
+from .modules._OncRealTime import _OncRealTime
+from .modules._OncArchive import _OncArchive
+
+
+class ONC(_OncDiscovery, _OncDelivery, _OncRealTime, _OncArchive):
+    
+    def __init__(self, token, production: bool=True, showInfo: bool=False, outPath: str='output', timeout: int=60):
+        self.token      = token
+        self.showInfo   = showInfo
+        self.timeout    = timeout
+
+        # sanitize outPath
+        outPath = outPath.replace('\\', '/')
+        if outPath[-1] == '/':
+            outPath = outPath[:-1]
+        self.outPath = outPath
+        
+        if production:
+            self.baseUrl = "https://data.oceannetworks.ca/"
+        else:
+            self.baseUrl = "https://qa.oceannetworks.ca/"
+
+
+    def print(self, obj, filename: str=""):
+        text = json.dumps(obj, indent=4)
+        if filename == "":
+            print(text)
+        else:
+            with open(filename, "w+") as file:
+                file.write(text)
+
+
+    def decodeJsonFromUrl(self, url: str):
+        '''
+        Return a dictionary from the json response of a json data product download url.
+        '''
+        try:
+            return self._doRequest(url, filters)
+        except Exception: raise
+
+
+    def _doRequest(self, url: str, filters: dict=None):
+        """
+        Generic request wrapper for making simple web service requests
+        @param url:    String full url to request
+        @param params: Dictionary of parameters to append to the request
+        @return:       JSON object obtained on a successful request
+        @throws:       Exception if the HTTP request fails with status 400, as a tuple with
+                       the error description and the error JSON structure returned
+                       by the API, or a generic exception otherwise
+        """
+        if filters is None: filters = {}
+        try:
+            self._log('Requesting URL: {:s}{:s}'.format(url, parse.urlencode(filters)))
+            response = requests.get(url, filters, timeout=self.timeout)
+            
+            if response.ok:
+                jsonResult = response.json()
+            else:
+                status = response.status_code
+                if status == 400:
+                    if self.showInfo:
+                        printErrorMessage(response, filters)
+                    raise Exception('The request failed with HTTP status 400.', response.json())
+                else:
+                    raise Exception('The request failed with HTTP status {:d}.'.format(status), response.text)
+
+            elapsed = response.elapsed.total_seconds()
+            self._log('Web Service response time: {:s}'.format(self._formatDuration(elapsed)))
+        
+        except Exception: raise
+
+        self._sanitizeBooleans(jsonResult)
+        return jsonResult
+
+
+    def _sanitizeBooleans(self, data: list):
+        """
+        For all rows in data, enforce that fields expected to have bool values have the right type
+            Will modify the data array
+        @param data:   Usually an array of dictionaries
+        """
+        if not(isinstance(data, list)): return
+        if len(data) == 0: return
+
+        fixHasDeviceData = False
+        fixHasPropertyData = False
+
+        # check hasDeviceData only if present and of the wrong type
+        # for now we only check the first row
+        if "hasDeviceData" in data[0]:
+            if (type(data[0]["hasDeviceData"]) != bool):   fixHasDeviceData = True
+        
+        if "hasPropertyData" in data[0]:
+            if (type(data[0]["hasPropertyData"]) != bool): fixHasPropertyData = True
+
+        # same for hasPropertyData
+        if fixHasDeviceData or fixHasPropertyData:
+            for row in data:
+                if fixHasDeviceData:
+                    row["hasDeviceData"]   = (row["hasDeviceData"] == "true")
+                if fixHasPropertyData:
+                    row["hasPropertyData"] = (row["hasPropertyData"] == "true")
+
+
+    
+    def _log(self, message: str):
+        """
+        Prints message to console only when self.showInfo is true
+        @param message: String
+        """
+        if self.showInfo:
+            print(message)
+
+
+    def _formatSize(self, size: float):
+        """
+        Returns a formatted file size string representation
+        @param size: {float} Size in bytes
+        """
+        return humanize.naturalsize(size)
+
+
+    def _formatDuration(self, secs: float):
+        """
+        Returns a formatted time duration string representation of a duration in seconds
+        @param seconds: float
+        """
+        if secs < 1.0:
+            txtDownTime = '{:.3f} seconds'.format(secs)
+        else:
+            d = timedelta(seconds=secs)
+            txtDownTime = humanize.naturaldelta(d)
+
+        return txtDownTime
+
+
