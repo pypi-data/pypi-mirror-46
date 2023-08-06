5c15594e0b4e86b0a3bc31647542cc675d9031cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:22 +0000

    Add package Binomial-prob-pckg/0.1/Binomial_prob_pckg-0.1.tar.gz

---
+++ packages/Binomial-prob-pckg/Binomial_prob_pckg-0.1.tar.gz/Binomial_prob_pckg-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Binomial_prob_pckg',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Binomial_prob_pckg'],
+      author = 'Tomiwa_salau',
+      author_email = 'ayotomiwasalau@gmail.com',
+      zip_safe=False)
