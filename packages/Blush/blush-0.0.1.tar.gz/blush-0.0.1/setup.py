f17cafdb30508ccdbc0c0efc16ca9a62ebaeb390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:48 +0000

    Add package Blush/0.0.1/blush-0.0.1.tar.gz

---
+++ packages/Blush/blush-0.0.1.tar.gz/blush-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+  name = 'blush',
+  packages = ['blush'],
+  version = '0.0.1',
+  description = '',
+  long_description = '',
+  author = '',
+  license = '',
+  package_data={},
+  url = 'https://github.com/alvations/blush',
+  keywords = [],
+  classifiers = [],
+  install_requires = ['six', 'click', 'joblib', 'tqdm'],
+)
