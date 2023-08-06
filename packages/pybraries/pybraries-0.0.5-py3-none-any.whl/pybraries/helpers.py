1997b1f0f65b3cc83d71ed4dce7bf9652c6b60c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:09 +0000

    Add package pybraries/0.0.5/pybraries-0.0.5-py3-none-any.whl

---
+++ packages/pybraries/pybraries-0.0.5-py3-none-any.whl/pybraries/helpers.py
@@ -0,0 +1,32 @@
+import os
+import requests
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+
+
+class APIKeyMissingError(Exception):
+    pass
+
+
+LIBRARIES_API_KEY = os.environ.get("LIBRARIES_API_KEY", None)
+
+if LIBRARIES_API_KEY is None:
+    raise APIKeyMissingError(
+        "All methods require an API key. "
+        "See https://libraries.io to get your free key. "
+        "Then set the key to the environment variable: LIBRARIES_API_KEY"
+    )
+
+# session retry settings
+retries = Retry(total=3, backoff_factor=0.2, status_forcelist=[500, 502, 503, 504])
+
+# session object common properties
+sess = requests.Session()
+sess.params = {}
+sess.params["api_key"] = LIBRARIES_API_KEY
+sess.mount("https://", HTTPAdapter(max_retries=retries))
+
+
+def clear_params():
+    sess.params.clear()
+    sess.params["api_key"] = LIBRARIES_API_KEY
