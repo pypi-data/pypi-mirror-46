c7ca99a3261c771250e4a6760242a3d4ef034687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:09 +0000

    Add package pydicom-tools/0.0.6/pydicom_tools-0.0.6.tar.gz

---
+++ packages/pydicom-tools/pydicom_tools-0.0.6.tar.gz/pydicom_tools-0.0.6/README.md
@@ -0,0 +1,25 @@
+# pydicom tools
+
+pydicomを用いてDICOM RTを解析するときのツール群です。
+
+## 3次元CTデータの構築
+
+以下の方法で変数`ct`に3次元Volumeを読み込みます。
+
+```python
+from pydicom_tools import CTImage
+ct = CTImage()
+ct.load(path_to_ct)
+```
+
+## DRRの生成
+
+以下の方法でDRRを生成します。
+
+```python
+from pydicom_tools import DRR
+drr = DRR(ct, beam, size)
+```
+
+`ct`は`CTImage`クラスのインスタンス、`beam`はBeamSequenceのオブジェクト、
+`size`はDRRのmm単位のサイズです。
---
+++ packages/pydicom-tools/pydicom_tools-0.0.6.tar.gz/pydicom_tools-0.0.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README_en.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydicom_tools",
+    version="0.0.6",
+    author="Akihisa Wakita",
+    author_email="wakita84@gmail.com",
+    description="Tools for dicom RT analysis with pydicom",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wkt84/pydicom-tools",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pydicom",
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
