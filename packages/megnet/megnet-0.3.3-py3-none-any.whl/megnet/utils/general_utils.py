afa593373eb88cc06d2b0e9bc864a41c63d836d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:21 +0000

    Add package megnet/0.3.3/megnet-0.3.3-py3-none-any.whl

---
+++ packages/megnet/megnet-0.3.3-py3-none-any.whl/megnet/utils/general_utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def to_list(x):
+    """
+    If x is not a list, convert it to list
+    """
+    if isinstance(x, list) or isinstance(x, tuple):
+        return x
+    elif isinstance(x, np.ndarray):
+        return to_list(x.tolist())
+    else:
+        return [x]
+
+
+def expand_1st(x):
+    """
+    Adding an extra first dimension
+
+    Args:
+        x: (np.array)
+    Returns:
+         (np.array)
+    """
+    return np.expand_dims(x, axis=0)
