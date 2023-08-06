eb525fe3c5482e04ea0628644eaa97d5ef2b670f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:38 +0000

    Add package movierecomender/0.0.4/movierecomender-0.0.4.tar.gz

---
+++ packages/movierecomender/movierecomender-0.0.4.tar.gz/movierecomender-0.0.4/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name= 'movierecomender',
+    version='0.0.4',
+    author='Andres Duenas',
+    description='Recomends a movie given two files',
+    url='https://github.com/Malandru/Recomender',
+    packages=setuptools.find_packages()
+)
