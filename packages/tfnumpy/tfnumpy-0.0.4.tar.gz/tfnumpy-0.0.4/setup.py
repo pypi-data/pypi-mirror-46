437c1cd51de446baa87f882c4f5c6e48e2934df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:38 +0000

    Add package tfnumpy/0.0.4/tfnumpy-0.0.4.tar.gz

---
+++ packages/tfnumpy/tfnumpy-0.0.4.tar.gz/tfnumpy-0.0.4/setup.py
@@ -0,0 +1,42 @@
+
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='tfnumpy',
+      version="0.0.4",
+      description="Collection of Simple Numerical Routines using TensorFlow",
+      long_description="Collection of simple numerical routines that uses TensorFlow",
+      classifiers=[
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "License :: OSI Approved :: MIT License",
+      ],
+      keywords="numerics computation tensorflow",
+      url="https://github.com/stephenhky/TFNumPy",
+      author="Kwan-Yuet Ho",
+      author_email="stephenhky@yahoo.com.hk",
+      license='MIT',
+      packages=['tfnumpy',
+                'tfnumpy.embed',
+                'tfnumpy.regression',
+                'tfnumpy.utils',
+                'tfnumpy.tensor',
+                ],
+      install_requires=[
+          'numpy', 'tensorflow',
+      ],
+      tests_require=[
+          'unittest2', 'scikit-learn',
+      ],
+      scripts=['bin/tfsammon'],
+      include_package_data=True,
+      test_suite="test",
+      zip_safe=False)
+
