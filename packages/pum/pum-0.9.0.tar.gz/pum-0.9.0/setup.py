33f2c89089c73c0d80c40385f2647da94bfceb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:35 +0000

    Add package pum/0.9.0/pum-0.9.0.tar.gz

---
+++ packages/pum/pum-0.9.0.tar.gz/pum-0.9.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+import sys
+if sys.version_info < (3,3):
+    sys.exit('Pum requires at least Python version 3.3.\nYou are currently running this installation with\n\n{}'.format(sys.version))
+
+setup(
+    name = 'pum',
+    packages = [
+        'pum',
+        'pum/core',
+        'pum/utils'
+    ],
+    scripts = [
+        'scripts/pum'
+    ],
+    version = '0.9.0',
+    description = 'Postgres upgrade manager',
+    author = 'Mario Baranzini',
+    author_email = 'mario@opengis.ch',
+    url = 'https://github.com/opengisch/pum',
+    download_url = 'https://github.com/opengisch/pum/archive/0.9.0.tar.gz', # I'll explain this in a second
+    keywords = [
+        'postgres',
+        'migration',
+        'upgrade'
+    ],
+    classifiers = [
+        'Topic :: Database',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development :: Version Control',
+        'Development Status :: 5 - Production/Stable'
+    ],
+    install_requires = [
+        'psycopg2-binary>=2.7.3',
+        'PyYAML'
+    ],
+    python_requires=">=3.3",
+)
