077de03df7f24a74e5a8d2e79f8dae90c2ce2f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:59 +0000

    Add package xtgeo/2.0.4/xtgeo-2.0.4-cp34-cp34m-manylinux1_x86_64.whl

---
+++ packages/xtgeo/xtgeo-2.0.4-cp34-cp34m-manylinux1_x86_64.whl/xtgeo/xyz/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, absolute_import
+from __future__ import print_function
+
+# flake8: noqa
+from xtgeo.xyz._xyz import XYZ
+from xtgeo.xyz.points import Points
+from xtgeo.xyz.polygons import Polygons
