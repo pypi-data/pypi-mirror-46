4c0a344fecb4a580b989ae8a19757bfbe30e8ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:53 +0000

    Add package django-oscar-cybersource/4.0.0b1/django_oscar_cybersource-4.0.0b1-py3-none-any.whl

---
+++ packages/django-oscar-cybersource/django_oscar_cybersource-4.0.0b1-py3-none-any.whl/cybersource/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.PaymentToken)
+class PaymentTokenAdmin(admin.ModelAdmin):
+    list_filter = ['card_type', 'log__date_created']
+    search_fields = ['token', 'card_type', 'masked_card_number']
+    fields = ['token', 'card_type', 'masked_card_number', 'log']
+    list_display = ['token', 'card_type', 'masked_card_number', 'log']
+    readonly_fields = fields
+
+
+@admin.register(models.CyberSourceReply)
+class CyberSourceReplyAdmin(admin.ModelAdmin):
+    list_filter = ['date_modified', 'date_created']
+    list_display = ['date_created', 'user', 'order', 'date_modified']
+    fields = ['user', 'order', 'data', 'date_modified', 'date_created']
+    readonly_fields = fields
+
+
+@admin.register(models.SecureAcceptanceProfile)
+class SecureAcceptanceProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'hostname', 'profile_id', 'is_default']
+    fields = ['hostname', 'profile_id', 'access_key', 'secret_key', 'is_default']
