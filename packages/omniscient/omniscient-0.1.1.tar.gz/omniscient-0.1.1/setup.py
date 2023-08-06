7d0b25f024206e78f376a4db8cab031ce376791c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:51 +0000

    Add package omniscient/0.1.1/omniscient-0.1.1.tar.gz

---
+++ packages/omniscient/omniscient-0.1.1.tar.gz/omniscient-0.1.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from setuptools.command.install import install
+from subprocess import check_call
+
+
+REQUIRED_PACKAGES = [
+    'scikit-learn',
+    'pandas',
+    'deeppavlov',
+    'tensorflow'
+]
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        # check_call("python -m deeppavlov download deeppavlov/configs/squad/squad_bert.json".split())
+        from deeppavlov import build_model, configs
+        model = build_model(configs.squad.squad_bert, download=True)
+
+        install.run(self)
+
+
+setup(name='omniscient',
+      version='0.1.1',
+      description='Omniscient question answering system',
+      url='',
+      author='Pasan Karunaratne',
+      author_email='pasankarunaratne@gmail.com',
+      license='MIT',
+      packages=['omniscient'],
+      install_requires=REQUIRED_PACKAGES,
+      zip_safe=False,
+      # cmdclass={
+      #     'install': PostInstallCommand,
+      # }
+      )
