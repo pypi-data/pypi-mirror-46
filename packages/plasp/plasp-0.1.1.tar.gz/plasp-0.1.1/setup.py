1dff883926ec069c813e5ffb908f2210a138a084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:58 +0000

    Add package plasp/0.1.1/plasp-0.1.1.tar.gz

---
+++ packages/plasp/plasp-0.1.1.tar.gz/plasp-0.1.1/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='plasp',
+    version='0.1.1',
+    description='Partial Labeling Approached through Structured Prediction',
+    license='MIT',
+    packages=find_packages(),
+    author='Vivien Cabannes',
+    author_email='vivien.cabannes@gmail.com',
+    url='https://github.com/VivienCabannes/plasp',
+    download_url='https://github.com/VivienCabannes/plasp/archive/master.zip',
+    keywords='partial labeling weakly supervised learning plasp scalable method structured prediction'
+)
