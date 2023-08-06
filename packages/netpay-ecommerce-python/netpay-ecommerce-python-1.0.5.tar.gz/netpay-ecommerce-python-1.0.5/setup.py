8d2af2a8e4c9ebe8becbf98dce89440266299098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:59 +0000

    Add package netpay-ecommerce-python/1.0.5/netpay-ecommerce-python-1.0.5.tar.gz

---
+++ packages/netpay-ecommerce-python/netpay-ecommerce-python-1.0.5.tar.gz/netpay-ecommerce-python-1.0.5/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+     name='netpay-ecommerce-python',  
+     version='1.0.5',
+     author="Netpay",
+     author_email="info@netpay.com.mx",
+     description="A Python scripts for consume eccomerce Api",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
