6c77dfb5385ef7c5262f600d67ec70a34b997c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:12 +0000

    Add package EasyProcess/0.2.7/EasyProcess-0.2.7.tar.gz

---
+++ packages/EasyProcess/EasyProcess-0.2.7.tar.gz/EasyProcess-0.2.7/tests/coverage/fast/test_examples.py
@@ -0,0 +1,17 @@
+from easyprocess import EasyProcess
+from nose.tools import eq_
+from nose.tools import ok_
+import sys
+
+
+def test():
+    eq_(EasyProcess([sys.executable, '-m', 'easyprocess.examples.ver']
+                    ).call().return_code, 0)
+    eq_(EasyProcess([sys.executable, '-m', 'easyprocess.examples.log']
+                    ).call().return_code, 0)
+    eq_(EasyProcess([sys.executable, '-m', 'easyprocess.examples.cmd']
+                    ).call().return_code, 0)
+    eq_(EasyProcess([sys.executable, '-m', 'easyprocess.examples.hello']
+                    ).call().return_code, 0)
+    eq_(EasyProcess([sys.executable, '-m', 'easyprocess.examples.timeout']
+                    ).call().return_code, 0)
