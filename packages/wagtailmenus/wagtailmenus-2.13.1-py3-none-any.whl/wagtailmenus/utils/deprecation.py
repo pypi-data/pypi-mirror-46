6ea68e7b2af718686abce7ccdb3a3b931e447080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:32 +0000

    Add package wagtailmenus/2.13.1/wagtailmenus-2.13.1-py3-none-any.whl

---
+++ packages/wagtailmenus/wagtailmenus-2.13.1-py3-none-any.whl/wagtailmenus/utils/deprecation.py
@@ -0,0 +1,13 @@
+class RemovedInWagtailMenus3Warning(DeprecationWarning):
+    pass
+
+
+removed_in_next_version_warning = RemovedInWagtailMenus3Warning
+
+
+class RemovedInWagtailMenus31Warning(PendingDeprecationWarning):
+    pass
+
+
+class RemovedInWagtailMenus32Warning(PendingDeprecationWarning):
+    pass
