d8e168581313ecbb3229c7153467dc017636cc48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:03 +0000

    Add package sklearn-fuzzy/0.0.6/sklearn_fuzzy-0.0.6-py3-none-any.whl

---
+++ packages/sklearn-fuzzy/sklearn_fuzzy-0.0.6-py3-none-any.whl/sklearn_fuzzy/__init__.py
@@ -0,0 +1,16 @@
+"""
+sklearn_fuzzy_ (a.k.a. `skfuzzy`): Fuzzy Logic Toolbox for Python.
+
+This package implements many useful tools and functions for computation and
+projects involving fuzzy logic, also known as grey logic.
+
+Most of the functionality is actually located in subpackages, but like numpy we
+bring most of the core functionality into the base namespace.
+
+Recommended Use
+---------------
+>>> import sklearn_fuzzy_
+
+"""
+
+name = "sklearn_fuzzy"
