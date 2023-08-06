6c67f0d8146ffafb2d9c7b5c0b6c5f4363417efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:24 +0000

    Add package tfchain/0.1.0/tfchain-0.1.0.tar.gz

---
+++ packages/tfchain/tfchain-0.1.0.tar.gz/tfchain-0.1.0/setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    # can't have the entry_points option here.
+    from distutils.core import setup
+
+required = []
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(name='tfchain',
+    version='0.1.0',
+    author="Glen De Cauwsemaecker",
+    author_email="glen@threefold.tech",
+    description='pythonic tfchain client',
+    long_description='pythonic tfchain client',
+    packages=['tfchain'],
+    url="https://github.com/glendc/tfchain-py",
+    license='MIT License',
+    install_requires=required,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
---
+++ packages/tfchain/tfchain-0.1.0.tar.gz/tfchain-0.1.0/tfchain/__init__.py
@@ -0,0 +1,3 @@
+from .TFChainClient import TFChainClient as Client
+from .TFChainClient import NetworkType
+from .TFChainWallet import TFChainWallet as Wallet
