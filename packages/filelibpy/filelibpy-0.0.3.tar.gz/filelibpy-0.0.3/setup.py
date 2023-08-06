7b5d9163364814bbad1de3f86a8e8cd2bc70e0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:36 +0000

    Add package filelibpy/0.0.3/filelibpy-0.0.3.tar.gz

---
+++ packages/filelibpy/filelibpy-0.0.3.tar.gz/filelibpy-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+     name='filelibpy',
+     version='0.0.3',
+     scripts=['filelib'],
+     author="filelib",
+     author_email="",
+     description="Filelib offical python package for Filelib API",
+     long_description='',
+     long_description_content_type="text/markdown",
+     url="https://github.com/filelib/filelib-python",
+     packages=setuptools.find_packages(),
+     install_requires=['requests==2.21.0', 'PyJWT==1.7.1', 'pytz==2019.1'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         'License :: OSI Approved :: MIT License',
+         "Operating System :: OS Independent",
+     ]
+ )
