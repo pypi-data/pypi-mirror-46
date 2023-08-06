24fa26ed596a5b7978f4bb5b68ba36eda349f820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:59 +0000

    Add package devo/0.4.2/devo-0.4.2-py3-none-any.whl

---
+++ packages/devo/devo-0.4.2-py3-none-any.whl/devo/generate/commands.py
@@ -0,0 +1,31 @@
+import click
+
+from .utils import generate_gitlab_ci, generate_skaffold, generate_k8s
+
+from devo.config import config_to_context
+
+
+@click.group()
+def generate():
+    pass
+
+
+@generate.command()
+@click.option('--force', is_flag=True, default=False)
+def gitlab_ci(force):
+    ctx = config_to_context()
+    generate_gitlab_ci(ctx, force)
+
+
+@generate.command()
+@click.option('--force', is_flag=True, default=False)
+def skaffold(force):
+    ctx = config_to_context()
+    generate_skaffold(ctx, force)
+
+
+@generate.command()
+@click.option('--force', is_flag=True, default=False)
+def k8s(force):
+    ctx = config_to_context()
+    generate_k8s(ctx, force)
