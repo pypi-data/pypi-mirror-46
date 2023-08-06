2161e29082e7c5d1ec5632fc685bad4215a11b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:51 +0000

    Add package TpModel/0.3/TpModel-0.3.tar.gz

---
+++ packages/TpModel/TpModel-0.3.tar.gz/TpModel-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+
+
+setup(
+    name='TpModel',
+    version='0.3',
+    author='nbl',
+    author_email='',
+    url='',
+    description=u'TpModel',
+    packages=['TpModel'],
+    install_requires=[],
+    entry_points={
+        "console_scripts": ['TpModel = CMD:cmd'
+                            'TpModel = Config:config']
+    }
+)
