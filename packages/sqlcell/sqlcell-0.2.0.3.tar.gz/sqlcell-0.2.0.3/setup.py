bb99cab9648c2bf460fbd77a22abdf58a40435b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:57 +0000

    Add package sqlcell/0.2.0.3/sqlcell-0.2.0.3.tar.gz

---
+++ packages/sqlcell/sqlcell-0.2.0.3.tar.gz/sqlcell-0.2.0.3/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="sqlcell",
+    version="0.2.0.3",
+    description="run sql in jupyter notebooks or jupyter lab",
+    license="MIT",
+    author="Tim Dobbins",
+    author_email="noneya@gmail.com",
+    url="https://github.com/tmthyjames/SQLCell",
+    packages=find_packages(),
+    install_requires=[
+        'ipython',
+        'ipywidgets',
+        'sqlalchemy',
+        'pandas'
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+    ],
+    py_modules=['sqlcell']
+)
