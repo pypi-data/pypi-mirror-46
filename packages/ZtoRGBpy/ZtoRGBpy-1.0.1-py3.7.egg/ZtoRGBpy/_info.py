622c9753bffad47ce7928597ca4c77316cebb0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:01 +0000

    Add package ZtoRGBpy/1.0.1/ZtoRGBpy-1.0.1-py3.7.egg

---
+++ packages/ZtoRGBpy/ZtoRGBpy-1.0.1-py3.7.egg/ZtoRGBpy/_info.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# =================================================================================
+#  Copyright 2019 Glen Fletcher <mail@glenfletcher.com>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  All documentation this file as docstrings or comments are licensed under the
+#  Creative Commons Attribution-ShareAlike 4.0 International License; you may
+#  not use this documentation except in compliance with this License.
+#  You may obtain a copy of this License at
+#
+#    https://creativecommons.org/licenses/by-sa/4.0
+#
+# =================================================================================
+"""
+ZtoRGB information definition module
+
+Special private module used for automatic processing, and inclusion
+
+.. moduleauthor:: Glen Fletcher <mail@glenfletcher.com>
+"""
+
+__authors__ = [
+    ("Glen Fletcher", "mail@glenfletcher.com")]
+__copyright__ = "2019 Glen Fletcher"
+__license__ = """\
+The source code for this package is licensed under the [Apache 2.0 License](http://www.apache.org/licenses/LICENSE-2.0),
+while the documentation including docstrings and comments embedded in the source code are licensed under the
+[Creative Commons Attribution-ShareAlike 4.0 International License](https://creativecommons.org/licenses/by-sa/4.0)
+"""
+__contact__ = "Glen Fletcher <mail@glenfletcher.com>"
+__version__ = "1.0.1"
+__title__ = "ZtoRGBpy"
+__desc__ = """\
+Complex number to perceptually uniform RGB subset mapping library"""
+
+__all__ = [
+    '__authors__', '__copyright__', '__license__',
+    '__contact__', '__version__', '__title__',
+    '__desc__']
