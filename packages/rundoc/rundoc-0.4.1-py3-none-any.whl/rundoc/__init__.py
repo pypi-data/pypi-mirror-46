911e4aea1dc99cdcc685251c6fb89d6099c2ab5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:27 +0000

    Add package rundoc/0.4.1/rundoc-0.4.1-py3-none-any.whl

---
+++ packages/rundoc/rundoc-0.4.1-py3-none-any.whl/rundoc/__init__.py
@@ -0,0 +1,39 @@
+"""
+A command-line utility that runs code blocks from markdown files.
+"""
+__version__ = "0.4.1"
+__license__ = "BSD"
+__year__ = "2017-2018"
+__author__ = "Predrag Mandic"
+__author_email__ = "predrag@eclecticiq.com"
+__copyright__ = "Copyright {} {} <{}>".format(
+    __year__, __author__, __author_email__)
+
+class ansi:
+    ''' 
+    ANSI colors for pretty output.
+    '''
+    red = '\033[91m'
+    green = '\033[92m'
+    blue = '\033[94m'
+    yellow = '\033[93m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+    end = '\033[0m'
+
+class RundocException(Exception):
+    """Generic rundoc exception."""
+    pass
+
+class BadEnv(RundocException):
+    """Attempt to load invalid environment name/value"""
+    pass
+
+class CodeFailed(RundocException):
+    """Raise this when code block returns non-zero exit status."""
+    pass
+
+class BadInterpreter(RundocException):
+    """Selected interpreter not found or is not executable by current user."""
+    pass
+
