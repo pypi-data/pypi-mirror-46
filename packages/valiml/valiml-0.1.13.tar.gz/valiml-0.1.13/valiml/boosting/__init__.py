2f84a9ff8d100c685c83793b84c4ef377af14d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:24 +0000

    Add package valiml/0.1.13/valiml-0.1.13.tar.gz

---
+++ packages/valiml/valiml-0.1.13.tar.gz/valiml-0.1.13/valiml/boosting/__init__.py
@@ -0,0 +1,14 @@
+from valiml.boosting.DecisionStump import DecisionStump
+from valiml.boosting.MulticlassDecisionStump import DecisionStumpSamme, DecisionStumpSammeR
+
+from valiml.boosting.AdaBoost import AdaBoost
+
+__all__ = [
+    'AdaBoost',
+    'DecisionStumpSamme',
+    'DecisionStumpSammeR'
+]
+
+import emtrees
+
+emtrees.ExtraTrees
