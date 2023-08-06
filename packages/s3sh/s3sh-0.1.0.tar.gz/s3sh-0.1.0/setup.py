4328fd740d41871a871372230bf9824b7a0155a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:32 +0000

    Add package s3sh/0.1.0/s3sh-0.1.0.tar.gz

---
+++ packages/s3sh/s3sh-0.1.0.tar.gz/s3sh-0.1.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='s3sh',
+    version='0.1.0',
+    description='A repl for s3',
+    long_description='Traverse s3 just like it would be a filesystem',
+    author='Wim Berchmans',
+    author_email="wimberchmans@gmail.com",
+    license='',
+    url='https://github.com/WRRB/s3sh',
+    include_package_data=True,
+    package_data={},
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=[],
+    entry_points={},
+    zip_safe=False
+)
