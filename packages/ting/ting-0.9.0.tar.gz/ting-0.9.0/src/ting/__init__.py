84f02d061f12b8c7318be28ab9e445abaf0fdfbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:30 +0000

    Add package ting/0.9.0/ting-0.9.0.tar.gz

---
+++ packages/ting/ting-0.9.0.tar.gz/ting-0.9.0/src/ting/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for ting."""
+from pkg_resources import DistributionNotFound, get_distribution
+
+import click
+
+from .ting import Ting  # noqa
+
+__author__ = """Markus Binsteiner"""
+__email__ = "markus@frkl.io"
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = "unknown"
+finally:
+    del get_distribution, DistributionNotFound
+
+
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(__version__)
+    ctx.exit()
