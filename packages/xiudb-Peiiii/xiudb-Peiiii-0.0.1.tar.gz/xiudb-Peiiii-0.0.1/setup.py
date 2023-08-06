eb046d835ba713c909260ad0aa9da43f89ddaaa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:54 +0000

    Add package xiudb-Peiiii/0.0.1/xiudb-Peiiii-0.0.1.tar.gz

---
+++ packages/xiudb-Peiiii/xiudb-Peiiii-0.0.1.tar.gz/xiudb-Peiiii-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xiudb-Peiiii",
+    version="0.0.1",
+    author="Wang Pei",
+    author_email="1535376447@qq.com",
+    description="This is a simple package, which can be used as a substitute of database.\nUnlike a database, this one doesn't store records, but store objects directly .",
+    long_description=None,
+    # long_description_content_type="text/markdown",
+    url="https://github.com/Peiiii/xiudb",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
