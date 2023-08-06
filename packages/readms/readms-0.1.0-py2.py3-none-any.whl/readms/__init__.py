67d0ae49c61e23e4a4b1d54cb28a833db4f31ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:48 +0000

    Add package readms/0.1.0/readms-0.1.0-py2.py3-none-any.whl

---
+++ packages/readms/readms-0.1.0-py2.py3-none-any.whl/readms/__init__.py
@@ -0,0 +1,14 @@
+
+__version__ = "0.1.0"
+
+__title__ = "readms"
+__description__ = "Get the last chapter OUT and be notified !!!"
+__url__ = "https://github.com/nekomamoushi/readms"
+
+__author__ = "Jerome Pradier"
+__author_email__ = "jerome.pradier@gmail.com"
+
+__license__ = "MIT"
+
+def main():
+    print("Using readms")
