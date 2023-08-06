b9360b27652cfbe830f40a0618ce9c22de14ddc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:54 +0000

    Add package logmatic/0.2.11/logmatic-0.2.11.tar.gz

---
+++ packages/logmatic/logmatic-0.2.11.tar.gz/logmatic-0.2.11/logmatic/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Jakrin Juangbhanich"
+__email__ = "juangbhanich.k@gmail.com"
+__version__ = "0.2.11"
+
+import logmatic.log
+from logmatic.log import debug, info, warning, error, critical, increment, gauge, event, get_instance
