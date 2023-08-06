d3ed047a783eceb24569760b13764b2260cd7edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:48 +0000

    Add package django-oidc-user/0.3.3/django-oidc-user-0.3.3.tar.gz

---
+++ packages/django-oidc-user/django-oidc-user-0.3.3.tar.gz/django-oidc-user-0.3.3/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import (
+	find_packages,
+	setup,
+)
+
+# Allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+	name = 'django-oidc-user',
+	packages = find_packages(),
+	version = '0.3.3',
+	description = 'Creates an OpenID Connect compliant User model.',
+	author = 'Monte Hellawell',
+	author_email = 'hellawell@monte.me.uk',
+	url = 'https://github.com/montudor/django-oidc-user',
+	download_url = 'https://github.com/montudor/django-oidc-user/archive/v0.3.3.tar.gz',
+	keywords = ['django'],
+	classifiers = [],
+)
