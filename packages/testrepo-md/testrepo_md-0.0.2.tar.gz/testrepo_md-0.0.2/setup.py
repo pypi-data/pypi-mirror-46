c18c198c989bf6e2a4fafc89328b46ea0199295d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:56 +0000

    Add package testrepo-md/0.0.2/testrepo_md-0.0.2.tar.gz

---
+++ packages/testrepo-md/testrepo_md-0.0.2.tar.gz/testrepo_md-0.0.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="testrepo_md",
+    version="0.0.2",
+    author="Marilia",
+    description="Library for testing",
+    url="https://github.com/mbarandas/testrepo_md/",
+)
