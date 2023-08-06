2d7bc7ede9aa4e5d31077b58e627beb28f3eb800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:40 +0000

    Add package emplo-nameko-zipkin/0.1.6/emplo-nameko-zipkin-0.1.6.tar.gz

---
+++ packages/emplo-nameko-zipkin/emplo-nameko-zipkin-0.1.6.tar.gz/emplo-nameko-zipkin-0.1.6/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+__version__ = '0.1.6'
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='emplo-nameko-zipkin',
+    version=__version__,
+    author="Maxim Kiyan, Zbigniew Siciarz",
+    author_email="maxim.k@fraglab.com, zbigniew.siciarz@emplocity.pl",
+    url='https://github.com/Emplocity/nameko-zipkin',
+    description='Zipkin tracing for nameko framework',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['nameko_zipkin'],
+    install_requires=[
+        'py_zipkin>=0.7.1',
+        'nameko>=2.6.0',
+    ],
+)
