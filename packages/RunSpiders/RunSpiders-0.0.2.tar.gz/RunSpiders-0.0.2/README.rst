5ad33f8e3ca998311a0b87c87189800b11718c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:03:22 +0000

    Add package RunSpiders/0.0.2/RunSpiders-0.0.2.tar.gz

---
+++ packages/RunSpiders/RunSpiders-0.0.2.tar.gz/RunSpiders-0.0.2/README.rst
@@ -0,0 +1,17 @@
+RunSpiders
+==========
+
+|Python3|
+
+A python library contains some predefined web crawlers.
+Attention: this package probably can't work properly because of the correlated webs updates.
+If this situation happens, just fix it on your own.
+
+**Installation**
+
+.. code:: bash
+
+    pip install RunSpiders
+
+
+.. |Python3| image:: https://img.shields.io/badge/python-3-red.svg
---
+++ packages/RunSpiders/RunSpiders-0.0.2.tar.gz/RunSpiders-0.0.2/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="RunSpiders",
+    version="0.0.2",
+    description="Some predefined web crawlers",
+    long_description=open('README.rst').read(),
+    author='Ijustwantyouhappy',
+    author_email='18817363043@163.com',
+    maintainer='',
+    maintainer_email='',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    package_data = {
+        '': ['*.recipe']
+    },
+    platforms=["all"],
+    url='',
+    install_requires=["requests>=2.14.2",
+                      "beautifulsoup4>=4.5.1",
+                      "jinja2>=2.10.1"],
+    classifiers=[
+        "Environment :: Web Environment",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
