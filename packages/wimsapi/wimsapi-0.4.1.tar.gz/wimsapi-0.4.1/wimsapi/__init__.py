415ae69731dd7ddb7ac35a2f9211eb48bb4664fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:23 +0000

    Add package wimsapi/0.4.1/wimsapi-0.4.1.tar.gz

---
+++ packages/wimsapi/wimsapi-0.4.1.tar.gz/wimsapi-0.4.1/wimsapi/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from .api import WimsAPI
+from .exceptions import AdmRawError, InvalidItemTypeError, NotSavedError, WimsAPIError
+from .sheet import Sheet
+from .user import User
+from .wclass import Class
+
+
+name = "wimsapi"
+__title__ = 'wimsapi'
+__version__ = VERSION = '0.4.1'
