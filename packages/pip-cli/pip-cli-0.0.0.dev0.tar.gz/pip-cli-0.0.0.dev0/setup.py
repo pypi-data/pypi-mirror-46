c2acca61190e6e7ea4317c640fdb9e48bc4d4552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:37 +0000

    Add package pip-cli/0.0.0.dev0/pip-cli-0.0.0.dev0.tar.gz

---
+++ packages/pip-cli/pip-cli-0.0.0.dev0.tar.gz/pip-cli-0.0.0.dev0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pip-cli",
+    version="0.0.0.dev0",
+    packages=["pip-cli"],
+    url="https://github.com/pradyunsg/pip-cli",
+    author="Pradyun Gedam",
+    author_email="pradyunsg@gmail.com",
+)
