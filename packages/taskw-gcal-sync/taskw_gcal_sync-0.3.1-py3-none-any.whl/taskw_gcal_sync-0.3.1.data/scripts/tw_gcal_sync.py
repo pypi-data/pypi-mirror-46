4d870c45d3c2de91726aa0d5cfa90a066335e298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:55 +0000

    Add package taskw-gcal-sync/0.3.1/taskw_gcal_sync-0.3.1-py3-none-any.whl

---
+++ packages/taskw-gcal-sync/taskw_gcal_sync-0.3.1-py3-none-any.whl/taskw_gcal_sync-0.3.1.data/scripts/tw_gcal_sync.py
@@ -0,0 +1,35 @@
+#!python
+
+from taskw_gcal_sync import TWGCalAggregator
+from taskw_gcal_sync.clogger import setup_logging
+import logging
+
+
+def main():
+    """Main."""
+
+    logger = logging.getLogger(__name__)
+    setup_logging(__name__)
+    logger.info("Initialising...")
+
+    tw_config = {
+        "tags": ["remindme"],
+    }
+    gcal_config = {
+    }
+
+    with TWGCalAggregator(tw_config=tw_config,
+                          gcal_config=gcal_config) as aggregator:
+
+        aggregator.start()
+
+        tw_items = aggregator.tw_side.get_all_items()
+        gcal_items = aggregator.gcal_side.get_all_items()
+
+        # Check and potentially register items
+        aggregator.register_items(tw_items, "tw")
+        aggregator.register_items(gcal_items, "gcal")
+
+
+if __name__ == "__main__":
+    main()
