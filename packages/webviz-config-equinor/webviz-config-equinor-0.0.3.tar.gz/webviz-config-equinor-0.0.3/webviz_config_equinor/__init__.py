71448b4d5a5cd3cbe95dc6d869dfb6666779e0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:30 +0000

    Add package webviz-config-equinor/0.0.3/webviz-config-equinor-0.0.3.tar.gz

---
+++ packages/webviz-config-equinor/webviz-config-equinor-0.0.3.tar.gz/webviz-config-equinor-0.0.3/webviz_config_equinor/__init__.py
@@ -0,0 +1,27 @@
+import os
+import glob
+from webviz_config.themes import WebvizConfigTheme
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
+equinor_theme = WebvizConfigTheme(theme_name='equinor')
+
+equinor_theme.external_stylesheets = \
+    ['https://webviz-cdn.azureedge.net/fonts/index.css',
+     'https://webviz-cdn.azureedge.net/theme/equinor.theme.css']
+
+equinor_theme.adjust_csp(
+    {'style-src': ['https://webviz-cdn.azureedge.net'],
+     'img-src':  ['https://sibwebvizcdn.blob.core.windows.net'],
+     'font-src': ['https://webviz-cdn.azureedge.net']})
+
+equinor_theme.assets = glob.glob(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'assets',
+    '*')
+)
