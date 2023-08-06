ce505085fc1086217d6036f39407e4dfcb5f8cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:12 +0000

    Add package ethpm/0.1.4a16/ethpm-0.1.4a16.tar.gz

---
+++ packages/ethpm/ethpm-0.1.4a16.tar.gz/ethpm-0.1.4a16/ethpm/backends/registry.py
@@ -0,0 +1,43 @@
+import os
+
+from web3 import Web3
+from web3.providers.auto import load_provider_from_uri
+
+from ethpm.backends.base import BaseURIBackend
+from ethpm.constants import INFURA_API_KEY
+from ethpm.typing import URI
+from ethpm.utils.registry import fetch_standard_registry_abi
+from ethpm.utils.uri import parse_registry_uri
+from ethpm.validation import is_valid_registry_uri
+
+# TODO: Update registry ABI once ERC is finalized.
+REGISTRY_ABI = fetch_standard_registry_abi()
+
+
+class RegistryURIBackend(BaseURIBackend):
+    """
+    Backend class to handle Registry URIs.
+
+    A Registry URI must resolve to a resolvable content-addressed URI.
+    """
+
+    def __init__(self) -> None:
+        os.environ.setdefault("INFUFA_API_KEY", INFURA_API_KEY)
+        infura_url = f"wss://mainnet.infura.io/ws/v3/{INFURA_API_KEY}"
+        self.w3 = Web3(load_provider_from_uri(infura_url))
+
+    def can_translate_uri(self, uri: str) -> bool:
+        return is_valid_registry_uri(uri)
+
+    def can_resolve_uri(self, uri: str) -> bool:
+        return False
+
+    def fetch_uri_contents(self, uri: str) -> URI:
+        """
+        Return content-addressed URI stored at registry URI.
+        """
+        address, pkg_name, pkg_version = parse_registry_uri(uri)
+        self.w3.enable_unstable_package_management_api()
+        self.w3.pm.set_registry(address)
+        _, _, manifest_uri = self.w3.pm.get_release_data(pkg_name, pkg_version)
+        return manifest_uri
---
+++ packages/ethpm/ethpm-0.1.4a16.tar.gz/ethpm-0.1.4a16/setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+readme = open(os.path.join(DIR, 'README.md')).read()
+
+extras_require={
+    'test': [
+        'pytest>=3.2.1,<4',
+        'pytest-ethereum>=0.1.3a.6,<1',
+        'tox>=1.8.0,<2',
+    ],
+    'lint': [
+        'black>=19.3b0,<20',
+        'isort>=4.3.17,<5',   
+        'flake8>=3.7.0,<4',
+        'mypy<0.800',
+    ],
+    'doc': [
+        'Sphinx>=1.5.5,<2',
+        'sphinx_rtd_theme>=0.1.9,<2',
+    ],
+    'dev': [
+        'bumpversion>=0.5.3,<1',
+        'ipython>=7.2.0,<8',
+        'pytest-watch>=4.1.0,<5',
+        'twine',
+        'wheel',
+    ],
+}
+
+extras_require['dev'] = (
+    extras_require['dev']
+    + extras_require['test']
+    + extras_require['lint']
+    + extras_require['doc']
+)
+
+setup(
+    name='ethpm',
+    # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+    version='0.1.4-alpha.16',
+    description="""Python abstraction for ERC190 packages.""",
+    long_description_markdown_filename='README.md',
+    author='Piper Merriam',
+    author_email='pipermerriam@gmail.com',
+    url='https://github.com/ethpm/py-ethpm',
+    include_package_data=True,
+    install_requires=[
+        'eth-utils>=1.4.1,<2',
+        'ipfshttpclient>=0.4.11,<1',
+        'jsonschema>=2.6.0,<3',
+        'protobuf>=3.0.0,<4',
+        'rlp>=1.0.1,<2',
+        'web3[tester]>=5.0.0a3,<6',
+    ],
+    setup_requires=['setuptools-markdown'],
+    python_requires='>=3.6, <4',
+    extras_require=extras_require,
+    py_modules=['ethpm'],
+    license='MIT',
+    zip_safe=False,
+    keywords='ethereum',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
