c3294fbead65f31f20da7dc66aa9d272a2d2ca1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:35 +0000

    Add package KonbiLockerLite/0.2.3/KonbiLockerLite-0.2.3.tar.gz

---
+++ packages/KonbiLockerLite/KonbiLockerLite-0.2.3.tar.gz/KonbiLockerLite-0.2.3/KonbiLockerLite/install/LockerLiteInstall.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+from subprocess import check_call
+
+
+def install_win():
+
+    Path(Path(os.path.expanduser('~')).root +
+         "lockerlite.txt").write_text(Path('KonbiLockerLite/install/lockerlite_linux.py').read_text())
+
+
+def install_linux():
+    print('running install_linux')
+    print(Path('KonbiLockerLite/install/lockerlite_linux.py').absolute())
+    Path("/usr/bin/lockerlite").write_text(
+        Path('KonbiLockerLite/install/lockerlite_linux.py').read_text())
+    check_call("chmod +x /usr/bin/lockerlite".split())
+
+
+def install():
+    # create execution entry
+    if os.name == 'nt':
+        install_win()
+    elif os.name == 'posix':
+        install_linux()
+
+
+Path().root
