bd4c828765fc9cd24ae9be6d6d298af5047b0209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:14 +0000

    Add package hermes-python/0.6.1/hermes_python-0.6.1-cp35-cp35m-macosx_10_14_x86_64.whl

---
+++ packages/hermes-python/hermes_python-0.6.1-cp35-cp35m-macosx_10_14_x86_64.whl/hermes_python/api/dialogue.py
@@ -0,0 +1,33 @@
+class Dialogue(object):
+    def __init__(self):
+        pass
+
+    def on_intent(self):
+        pass
+
+    def on_intents(self):
+        pass
+
+    def on_session_started(self):
+        pass
+
+    def on_session_queued(self):
+        pass
+
+    def on_session_ended(self):
+        pass
+
+    def on_intent_not_recognized(self):
+        pass
+
+    def continue_session(self):
+        pass
+
+    def end_session(self):
+        pass
+
+    def start_session_notification(self):
+        pass
+
+    def start_session_action(self):
+        pass
