aa438c9d9112d547db4562811a02270864f59675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:59 +0000

    Add package pyansys/0.36.1/pyansys-0.36.1-cp36-cp36m-win_amd64.whl

---
+++ packages/pyansys/pyansys-0.36.1-cp36-cp36m-win_amd64.whl/pyansys/__init__.py
@@ -0,0 +1,24 @@
+import warnings
+from pyansys._version import __version__
+
+from pyansys.archive import (Archive, write_cmblock, write_nblock,
+                             save_as_archive)
+
+from pyansys.common import read_binary
+from pyansys.cellquality import *
+from pyansys.convert import convert_script
+
+try:
+    from pyansys.ansys import ANSYS
+    from pyansys.ansys import change_default_ansys_path
+except Exception as e:
+    try:
+        from pyansys.ansysbase import ANSYS
+    except:
+        warnings.warn('Unable to load interactive ANSYS APDL module:\n\n%s' % str(e))
+
+try:
+    from pyansys import ansys
+    has_ansys = ansys.check_valid_ansys()
+except:
+    has_ansys = False
