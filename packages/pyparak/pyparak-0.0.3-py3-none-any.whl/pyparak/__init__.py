df1d5b0ca3a8337acf92fa54f31afc936b7c8a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:05 +0000

    Add package pyparak/0.0.3/pyparak-0.0.3-py3-none-any.whl

---
+++ packages/pyparak/pyparak-0.0.3-py3-none-any.whl/pyparak/__init__.py
@@ -0,0 +1,34 @@
+from zeep import Client
+
+zarinpal_client = Client('https://sandbox.zarinpal.com/pg/services/WebGate/wsdl')
+
+
+class BaseGateway(object):
+    def send(self):
+        raise NotImplementedError()
+
+    def verify(self):
+        raise NotImplementedError()
+
+
+class ZarinpalGateway(BaseGateway):
+    def send(self, user, pk):
+        result = zarinpal_client.service.PaymentRequest(
+            user['merchant'], user['amount'], user['description'], user['email'], user['mobile'],
+            user['CallbackURL'] + str(pk))
+
+        if result.Status == 100:
+            return ('https://sandbox.zarinpal.com/pg/StartPay/' + str(result.Authority))
+        else:
+            return ('Error code: ' + str(result.Status))
+
+    def verify(self, user):
+        result = zarinpal_client.service.PaymentVerificationWithExtra(user['merchant'], user['authority'],
+                                                                      user['amount'])
+
+        if result.Status == 100:
+            return True
+        elif result.Status == 101:
+            return ('Transaction submitted : ' + str(result.Status))
+        else:
+            return ('Transaction failed.\nStatus: ' + str(result.Status))
