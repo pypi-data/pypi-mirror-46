5298f783057a608cc89b72d8613b31de0e286a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:19 +0000

    Add package dephell-licenses/0.1.6/dephell-licenses-0.1.6.tar.gz

---
+++ packages/dephell-licenses/dephell-licenses-0.1.6.tar.gz/dephell-licenses-0.1.6/tests/test_licenses.py
@@ -0,0 +1,16 @@
+from dephell_licenses import licenses
+
+
+def test_get_by_id():
+    license = licenses.get_by_id('MIT')
+    assert license.classifier == 'License :: OSI Approved :: MIT License'
+
+
+def test_get_by_classifier():
+    license = licenses.get_by_classifier('License :: OSI Approved :: MIT License')
+    assert license.id == 'MIT'
+
+
+def test_get_by_name():
+    license = licenses.get_by_name('MIT License')
+    assert license.id == 'MIT'
