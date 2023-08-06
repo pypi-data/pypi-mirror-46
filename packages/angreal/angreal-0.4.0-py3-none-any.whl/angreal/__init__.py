44c1819ff5fd9ccece15de9fd6c5f0035ac20f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:50 +0000

    Add package angreal/0.4.0/angreal-0.4.0-py3-none-any.whl

---
+++ packages/angreal/angreal-0.4.0-py3-none-any.whl/angreal/__init__.py
@@ -0,0 +1,27 @@
+"""
+    angreal
+    ~~~~~~~
+
+
+"""
+import importlib
+import os
+
+from angreal.decorators import *
+from angreal.utils import get_angreal_path, import_from_file
+
+importlib.import_module('click')
+
+__all__ = [
+    # Click Decorators
+    'pass_context', 'pass_obj', 'make_pass_decorator', 'command', 'group',
+    'argument', 'option', 'confirmation_option', 'password_option',
+    'version_option', 'help_option',
+
+    # Click Utilities
+    'echo', 'get_binary_stream', 'get_text_stream', 'open_file',
+    'format_filename', 'get_app_dir', 'get_os_args',
+
+]
+
+__version__ = open( os.path.join( os.path.dirname(__file__), 'VERSION')).read().strip()
