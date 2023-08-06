a12c8b2931020478b631ece7bade198dccfa268e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:50 +0000

    Add package mip/1.1.0/mip-1.1.0.tar.gz

---
+++ packages/mip/mip-1.1.0.tar.gz/mip-1.1.0/mip/constants.py
@@ -0,0 +1,55 @@
+VERSION = '1.1.0'
+
+from enum import Enum
+
+# epsilon number (practical zero)
+EPS = 10e-6
+
+# infinity representation
+INF = float("inf")
+
+
+# constraint senses
+EQUAL = "="
+LESS_OR_EQUAL = "<"
+GREATER_OR_EQUAL = ">"
+
+# optimization directions
+MIN = "MIN"
+MAX = "MAX"
+MINIMIZE = "MIN"
+MAXIMIZE = "MAX"
+
+# solvers
+CBC = "CBC"
+CPX = "CPX"
+CPLEX = "CPX"
+GRB = "GRB"
+GUROBI = "GRB"
+SCIP = "SCIP"
+
+# variable types
+BINARY = "B"
+CONTINUOUS = "C"
+INTEGER = "I"
+
+
+# optimization status
+class OptimizationStatus(Enum):
+    ERROR = -1
+    OPTIMAL = 0
+    INFEASIBLE = 1
+    UNBOUNDED = 2
+    FEASIBLE = 3
+    INT_INFEASIBLE = 4
+    NO_SOLUTION_FOUND = 5
+    LOADED = 6
+    CUTOFF = 7
+    OTHER = 10000
+
+
+# search emphasis
+class SearchEmphasis(Enum):
+    DEFAULT = 0
+    FEASIBILITY = 1
+    OPTIMALITY = 2
