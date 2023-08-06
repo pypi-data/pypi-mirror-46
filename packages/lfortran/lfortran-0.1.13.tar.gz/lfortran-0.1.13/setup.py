f969f022ac95de352d09884b4d043fcf50ff5fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:00 +0000

    Add package lfortran/0.1.13/lfortran-0.1.13.tar.gz

---
+++ packages/lfortran/lfortran-0.1.13.tar.gz/lfortran-0.1.13/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import skbuild
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("version", "r") as fh:
+    version = fh.read().strip()
+
+skbuild.setup(
+    name="lfortran",
+    version=version,
+    author="Ondřej Čertík",
+    author_email="ondrej@certik.us",
+    description="Fortran compiler",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/lfortran/lfortran",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    data_files=[
+        ('share/lfortran/nb', ['share/lfortran/nb/Demo.ipynb']),
+        ],
+    scripts=['lfort'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    cmake_languages=('C'),
+)
