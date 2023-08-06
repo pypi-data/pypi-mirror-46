9cee3c3c2a392e03e80a2ea855f2c0a53409d2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:17 +0000

    Add package borsdata-sdk/0.1.1/borsdata_sdk-0.1.1.tar.gz

---
+++ packages/borsdata-sdk/borsdata_sdk-0.1.1.tar.gz/borsdata_sdk-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="borsdata_sdk",
+    version="0.1.1",
+    author="Joel S. Roxell",
+    author_email="joel.roxell@annevo.se",
+    description="Borsdata api SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joelroxell/borsdata_sdk-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
+    ],
+)
