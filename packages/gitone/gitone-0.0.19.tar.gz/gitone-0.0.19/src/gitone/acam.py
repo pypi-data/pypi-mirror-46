783e53a0a7f0408534b2a1efa0638aeb4063adb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:09 +0000

    Add package gitone/0.0.19/gitone-0.0.19.tar.gz

---
+++ packages/gitone/gitone-0.0.19.tar.gz/gitone-0.0.19/src/gitone/acam.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from typing import Optional
+
+import git
+
+
+def acam(commit_message: Optional[str] = None) -> None:
+    """Amend the previous commit with any new changes.
+
+    :param message: The commit message to be passed to the git commit command.
+    :note: A commit message will be automatically generated
+           if the ``message`` argument is not provided.
+    """
+
+    repo = git.Repo(search_parent_directories=True)
+
+    print(repo.git.add("--all"))
+
+    if commit_message:
+        print(repo.git.commit("--all", "--amend", message=commit_message))
+
+    else:
+        print(repo.git.commit("--amend", "--reuse-message=HEAD"))
+
+
+if __name__ == "__main__":
+    acam()
