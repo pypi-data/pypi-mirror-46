f06cf3eefdd321d31a82537dd9f03779c51eb29c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:32 +0000

    Add package omniscient/0.4/omniscient-0.4.tar.gz

---
+++ packages/omniscient/omniscient-0.4.tar.gz/omniscient-0.4/omniscient/constants.py
@@ -0,0 +1,8 @@
+INPUT_FILE_DIR = '/Users/pasankarunaratne/learning/transfer-learn/data'
+INPUT_FILE_NAME = 'ssd.tsv'
+INPUT_FILE_SEPARATOR = '\t'
+
+PREDICTION_OUTPUT_DIR = '/Users/pasankarunaratne/learning/transfer-learn/data'
+PREDICTION_OUTPUT_FILE = INPUT_FILE_NAME.split('.')[0] + '_predictions.tsv'
+
+IS_ENABLE_PREDICTION_ENDPOINT = True
