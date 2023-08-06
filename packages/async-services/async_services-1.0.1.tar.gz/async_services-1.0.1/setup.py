37f81b5300e8ad505bfd74f1bdfbb6b7132382df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:07 +0000

    Add package async-services/1.0.1/async_services-1.0.1.tar.gz

---
+++ packages/async-services/async_services-1.0.1.tar.gz/async_services-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="async_services",
+    version="1.0.1",
+    description="Synchronus Wrapper for Async Code",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gekco/async_services",
+    author="Ankit Kathuria",
+    author_email="ankitkathuria534@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude="tests"),
+    include_package_data=True,
+)
