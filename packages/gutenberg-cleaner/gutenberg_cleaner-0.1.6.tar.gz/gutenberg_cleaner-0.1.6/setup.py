f36a6cd71b2f2064b74d098cd641684e701eb64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:11 +0000

    Add package gutenberg-cleaner/0.1.6/gutenberg_cleaner-0.1.6.tar.gz

---
+++ packages/gutenberg-cleaner/gutenberg_cleaner-0.1.6.tar.gz/gutenberg_cleaner-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name="gutenberg_cleaner",
+    install_requires=['nltk'],
+    version='0.1.6',
+    description="cleans gutenberg dataset books",
+    author_email='mohsenikiasari@ce.sharif.edu',
+    packages=["_cleaning_options"],
+    py_modules=["gutenberg_cleaner"],
+    url="https://github.com/kiasar/gutenberg_cleaner",
+    license='MIT',
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+)
