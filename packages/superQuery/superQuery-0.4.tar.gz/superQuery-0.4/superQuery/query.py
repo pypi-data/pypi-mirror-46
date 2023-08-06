874903d702e4ba6d994e3ad87886586df6be9cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:55 +0000

    Add package superQuery/0.4/superQuery-0.4.tar.gz

---
+++ packages/superQuery/superQuery-0.4.tar.gz/superQuery-0.4/superQuery/query.py
@@ -0,0 +1,63 @@
+import pymysql.cursors 
+
+class SuperQuery(object):
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+        self.auth = { "username": "", "password": ""}
+        self.explain = {}
+        self.result = None
+        self.connection = None
+
+    def get_data(self, sql, get_stats=False):
+        # Connect to the database.
+        try:
+            with self.connection.cursor() as cursor:
+            
+                # SQL 
+                sql = sql
+                
+                # Execute query.
+                cursor.execute(sql)
+                
+                for row in cursor:
+                    print(row)
+
+                if (get_stats):
+                    print("We made it!")
+                    self.get_statistics(cursor)
+        except:
+            print("We couldn't get your data...")
+            
+        finally:
+            # Close connection.
+            self.connection.close()
+
+
+    def authenticate_connection(self, username, password, project_id, dataset_id):
+        self.connection = pymysql.connect(host='proxy.superquery.io',
+                             user=username,
+                             password=password,                             
+                             db=project_id + "." + dataset_id,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+        print ("Connection to superQuery successful!")
+
+    def get_statistics(cursor):
+
+        # SQL 
+        sql = "explain;"
+        
+        # Execute query.
+        cursor.execute(sql)
+        
+        print("Query statistics: ")
+        for row in cursor:
+            print(row)
+
+    
+
+
