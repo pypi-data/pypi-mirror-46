825947b581b06eb29c05806762cf9c1bfb2bfea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:15 +0000

    Add package gudetama/0.0.1/gudetama-0.0.1.tar.gz

---
+++ packages/gudetama/gudetama-0.0.1.tar.gz/gudetama-0.0.1/setup.py
@@ -0,0 +1,16 @@
+	# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 alvations
+# URL:
+# For license information, see LICENSE.md
+
+from distutils.core import setup
+
+setup(
+    name='gudetama',
+    version='0.0.1',
+    packages=['gudetama',],
+    url = 'https://github.com/alvations/gudetama',
+    description='Lazy python recipes with batteries',
+    license="MIT",
+)
