f619e94624f391338b0c6837b3c8a03d55221c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:37 +0000

    Add package macos-display/1.1/macos_display-1.1.tar.gz

---
+++ packages/macos-display/macos_display-1.1.tar.gz/macos_display-1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, Extension
+
+import sys
+import platform
+
+if platform.system() != 'Darwin':
+    print("The macos_display package can only be installed on macOS", file=sys.stderr)
+    sys.exit(1)
+
+setup(
+    name='macos_display',
+    author='rob@sixty-north.com',
+    author_email="rob@sixty-north.com",
+    description="Retrieve information about macOS displays",
+    version='1.1',
+    url="https://github.com/sixty-north/macos-display",
+    packages = ['macos_display'],
+    ext_modules=[
+        Extension(
+            'macos_display.display',
+            ['macos_display/display_module.c',
+             'macos_display/display.c'],
+            extra_link_args = [
+                '-framework', 'IOKit',
+                '-framework', 'CoreFoundation',
+                '-framework', 'ApplicationServices'
+            ]
+        )
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: zlib/libpng License',
+    ]
+)
