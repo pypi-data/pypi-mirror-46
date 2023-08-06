9c58a64e0b95656b8f208a9205bc891415585ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:02 +0000

    Add package Mark2PY/0.1/Mark2PY-0.1-py3-none-any.whl

---
+++ packages/Mark2PY/Mark2PY-0.1-py3-none-any.whl/Mark2PY/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+""" this script is created by *AMJoshaghani* for helping web
+    developers for using markup and markdown on python script
+    :param MD: help you for showing markdown(md) on your script.
+    for example:
+        Mark2PY.MD.bold('AMJoshaghani')
+
+    :param MU: help you for showing markup(html) on your script.
+    for  example:
+        Mark2PY.MU.center('AMJoshaghani')
+
+    :copyright : © 2019 by Amir Mohammad Joshaghani --> AMJoshaghani @ https://amjoshaghani.ir
+    :license : MIT
+"""
+import M2P
+import MD
+import MU
+md_var = ''
+mu_var = ''
+__version__ = 0.5
+__author__ = 'Amir Mohammad Joshaghani'
+__author_email__ = 'amjoshaghani@gmail.com'
