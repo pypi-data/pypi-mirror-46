23efb52b51e600904799d90ee36a63f3a8be4fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:50 +0000

    Add package dh-potluck/0.1.3/dh-potluck-0.1.3.tar.gz

---
+++ packages/dh-potluck/dh-potluck-0.1.3.tar.gz/dh-potluck-0.1.3/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['dh_potluck']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boltons>=19.1,<20.0',
+ 'ddtrace>=0.25.0,<0.26.0',
+ 'flask>=1.0,<2.0',
+ 'marshmallow>=2.19,<3.0',
+ 'sqlalchemy>=1.3,<2.0',
+ 'webargs>=5.3,<6.0']
+
+setup_kwargs = {
+    'name': 'dh-potluck',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': None,
+    'author_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
