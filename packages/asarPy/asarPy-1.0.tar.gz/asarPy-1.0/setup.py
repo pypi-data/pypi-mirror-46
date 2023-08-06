54899972051116ed4503795f60763b957de09199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:06 +0000

    Add package asarPy/1.0/asarPy-1.0.tar.gz

---
+++ packages/asarPy/asarPy-1.0.tar.gz/asarPy-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='asarPy',
+    packages=['asarPy'],
+    version='1.0',
+    author='AS1337',
+    author_email='as1337@gmail.com',
+    description='Library for working with .asar files',
+    long_description=open("README.rst").read(),
+    url='https://github.com/AS1337/asarPy.git',
+    keywords=['asar', 'electron', 'asarPy', 'pyasar']
+)
