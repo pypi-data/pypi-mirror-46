2989758e0fd442f63255f0cfc8ee032d681f24f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:24 +0000

    Add package citrination-client/5.1.1/citrination-client-5.1.1.tar.gz

---
+++ packages/citrination-client/citrination-client-5.1.1.tar.gz/citrination-client-5.1.1/citrination_client/__init__.py
@@ -0,0 +1,6 @@
+from citrination_client.base import *
+from citrination_client.search import *
+from citrination_client.data import *
+from citrination_client.models import *
+from citrination_client.views.descriptors import *
+from .client import CitrinationClient
