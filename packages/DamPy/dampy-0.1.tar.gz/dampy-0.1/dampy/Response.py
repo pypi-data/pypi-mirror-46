cba6cfde4640de9a5666ae6af4ee5374113499a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:12 +0000

    Add package DamPy/0.1/dampy-0.1.tar.gz

---
+++ packages/DamPy/dampy-0.1.tar.gz/dampy-0.1/dampy/Response.py
@@ -0,0 +1,10 @@
+# ashokkumar.ta@gmail.com / 24-May-2019
+
+class Response:
+    def __init__(self, success, message, data):
+        self.success = success
+        self.message = message
+        self.data = data
+
+    def jsonify(self):
+        self.data = self.data.json()
