9b59c00c64b30ed50190c811b415e446324ad821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:39 +0000

    Add package pybraries/0.0.1/pybraries-0.0.1.tar.gz

---
+++ packages/pybraries/pybraries-0.0.1.tar.gz/pybraries-0.0.1/pybraries/__init__.py
@@ -0,0 +1,7 @@
+from pybraries.subscribe import *
+from pybraries.search import *
+from .helpers import sess
+from .subscription_helpers import sub_api
+from .search_helpers import search_api
+from .set_pages import set_pages
+from .make_request import make_request
---
+++ packages/pybraries/pybraries-0.0.1.tar.gz/pybraries-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup_requirements = ["pytest-runner"]
+
+test_requirements = ["pytest"]
+
+requirements = ["requests>=2"]
+
+setup(
+    name="pybraries",
+    version="0.0.1",
+    author="Jeff Hale",
+    author_email="jeffmshale@gmail.com",
+    description="A Python wrapper for the libraries.io API",
+    url="https://github.com/pybraries/pybraries/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: BSD License",
+    ],
+    include_package_data=True,
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    zip_safe=False,
+)
