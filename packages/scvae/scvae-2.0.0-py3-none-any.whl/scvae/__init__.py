87ce90b29db2fe8d78b395c4179f3b7d21c305c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:04 +0000

    Add package scvae/2.0.0/scvae-2.0.0-py3-none-any.whl

---
+++ packages/scvae/scvae-2.0.0-py3-none-any.whl/scvae/__init__.py
@@ -0,0 +1,10 @@
+from scvae.__version__ import __version__
+
+__title__ = "scvae"
+__description__ = "Model single-cell transcript counts using deep learning."
+__url__ = "http://github.com/chgroenbech/scvae"
+__author__ = "Christopher Heje Grønbech, Maximillian Fornitz Vording"
+__maintainer__ = "Christopher Heje Grønbech"
+__email__ = "christopher.heje.groenbech@regionh.dk"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright (c) 2017 - 2019 scVAE authors"
