caaaac95b685f129f5e26805af00976701f9bfe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:46 +0000

    Add package ddf-utils/1.0.0/ddf_utils-1.0.0.tar.gz

---
+++ packages/ddf-utils/ddf_utils-1.0.0.tar.gz/ddf_utils-1.0.0/setup.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+version = "1.0.0"
+
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+requirements = [
+    'pandas',
+    'unidecode',
+    'pyyaml',
+    'orderedattrdict',
+    'typing',
+    'cookiecutter',
+    'jsonschema',
+    'Click<7',
+    'daff',
+    'tabulate',
+    'dask[dataframe]',
+    'tqdm',
+    'ruamel.yaml',
+    'graphviz',
+    'coloredlogs',
+    'pytz',
+    'requests',
+    'gitpython',
+    'hy',
+    'attrs',
+    'joblib',
+    'lxml'
+]
+
+setup(
+    name='ddf_utils',
+    version=version,
+    description='Commonly used functions/utilities for DDF file model.',
+    long_description='README.md',
+    url='https://github.com/semio/ddf_utils',
+    author='Semio Zheng',
+    author_email='prairy.long@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'res']),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'ddf = ddf_utils.cli:ddf'
+        ]
+    },
+    keywords=['etl', 'ddf', 'datasets', 'recipe'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        # "Programming Language :: Python :: 3.3",
+        # "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Utilities"
+    ]
+)
