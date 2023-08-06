43d53dd751eba984f9829be8d2df8c2fab865cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:10 +0000

    Add package mbq.ranch/2.1.0/mbq.ranch-2.1.0.tar.gz

---
+++ packages/mbq.ranch/mbq.ranch-2.1.0.tar.gz/mbq.ranch-2.1.0/mbq/ranch/__init__.py
@@ -0,0 +1,13 @@
+from .__version__ import (  # noqa
+    __author__,
+    __author_email__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
+
+from . import killswitch  # noqa
+
+default_app_config = "mbq.ranch.apps.RanchConfig"
