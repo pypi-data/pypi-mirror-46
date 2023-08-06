903f59bf72ce965167b27fce67da4247965793d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:02 +0000

    Add package qupy/1.1.1/qupy-1.1.1.tar.gz

---
+++ packages/qupy/qupy-1.1.1.tar.gz/qupy-1.1.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+# with open('README.md') as f:
+#     readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='qupy',
+    version='1.1.1',
+    description='QuPy: A quantum circuit simulator for both CPU and GPU',
+    # long_description=readme,
+    author='Ken Nakanishi',
+    author_email='ikyhn1.ken.n@gmail.com',
+    install_requires=['numpy'],
+    url='https://github.com/ken-nakanishi/qupy',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite='tests'
+)
