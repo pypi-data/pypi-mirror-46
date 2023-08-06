4ce5e9148ab242d0acbfce28494cfd25ffe8a537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:17 +0000

    Add package buvar/0.4.0/buvar-0.4.0.tar.gz

---
+++ packages/buvar/buvar-0.4.0.tar.gz/buvar-0.4.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['buvar']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['attrs', 'pytoml', 'structlog']
+
+setup_kwargs = {
+    'name': 'buvar',
+    'version': '0.4.0',
+    'description': 'General purpose config loader',
+    'long_description': None,
+    'author': 'Oliver Berger',
+    'author_email': 'diefans@gmail.com',
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
