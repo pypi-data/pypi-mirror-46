8efb47a7e87854103baadf3f9a9a708f8f12a6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:27 +0000

    Add package bujo/0.2/bujo-0.2.tar.gz

---
+++ packages/bujo/bujo-0.2.tar.gz/bujo-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import bujo
+
+setup(
+    name='bujo',
+    version='0.2',
+    description = 'A CLI tool for tracking anything and everything',
+    author = bujo.__author__,
+    author_email = 'ferovax@gmail.com',
+    py_modules=['bujo'],
+    install_requires=[
+        'Click',
+        'PyFiglet',
+        'pyyaml',
+        'colorama',
+    ],
+    entry_points='''
+        [console_scripts]
+        bujo= bujo.bujo:cli
+    ''',
+    classifiers = (
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ),
+)
