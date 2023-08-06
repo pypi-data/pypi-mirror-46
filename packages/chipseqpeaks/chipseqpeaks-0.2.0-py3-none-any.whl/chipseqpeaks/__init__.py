b25a4d8b05cce002472e2d35dd5b4d1e69e6b33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:25 +0000

    Add package chipseqpeaks/0.2.0/chipseqpeaks-0.2.0-py3-none-any.whl

---
+++ packages/chipseqpeaks/chipseqpeaks-0.2.0-py3-none-any.whl/chipseqpeaks/__init__.py
@@ -0,0 +1,19 @@
+"""A wrapper for MACS2 that abstracts out some things and makes it easier to use
+
+Blacklist data source: https://www.encodeproject.org/annotations/ENCSR636HFF/
+
+Example
+-------
+from chipseqpeaks import ChIPSeqPeaks
+with ChIPSeqPeaks(<bytes object or path to BAM file>) as cp:
+    cp.cleans_up = False
+    cp.remove_blacklisted_peaks(<path/to/blacklist.bed>)
+    cp.write(<output prefix>)
+
+Classes
+-------
+ChIPSeqPeaks
+    object representing ChIP-seq peaks
+"""
+
+from chipseqpeaks.chip_seq_peaks import ChIPSeqPeaks
