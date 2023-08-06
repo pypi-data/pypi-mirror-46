e32d6ba48ec62117f133bb300fffa108e68b873f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:54 +0000

    Add package calcbench-api-client/1.1.1/calcbench_api_client-1.1.1-py3-none-any.whl

---
+++ packages/calcbench-api-client/calcbench_api_client-1.1.1-py3-none-any.whl/calcbench/listener.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+try:
+    from azure.servicebus import Message
+    from azure.servicebus.control_client import ServiceBusService
+except ImportError:
+    "Will not be able to use the listener"
+    pass
+
+import json
+import warnings
+
+TOPIC = 'filings'
+
+def handle_filings(handler, #type: ()->void
+                    subscription,  
+                    readonly_shared_access_key_value="Cb7VhLR6eJxsurCSPtXQHOJvlkU84CCCx2oB+T/so6Q=", 
+                    service_bus_namespace='calcbench'):
+    
+    bus_service = ServiceBusService(service_namespace=service_bus_namespace,
+                                    shared_access_key_name="public",
+                                    shared_access_key_value=readonly_shared_access_key_value)
+    while True:
+        try:
+            message = bus_service.receive_subscription_message(TOPIC, subscription, peek_lock=True)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception as e:
+            warnings.warn(str(e))
+        else:
+            if(message.body):
+                try:
+                    filing = json.loads(message.body)
+                    handler(filing)
+                    message.delete()
+                except Exception as e:
+                    warnings.warn(str(e))
