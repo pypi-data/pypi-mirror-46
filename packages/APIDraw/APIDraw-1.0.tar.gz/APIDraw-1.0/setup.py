578d0a40b744c433f2a975ec9d84d8530262b27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:06 +0000

    Add package APIDraw/1.0/APIDraw-1.0.tar.gz

---
+++ packages/APIDraw/APIDraw-1.0.tar.gz/APIDraw-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='APIDraw',
+    version='1.0',
+  	author='Ariel Selig',
+  	author_email='support@apidraw.com',
+  	url='http://apidraw.com',
+    packages=['apidraw',],
+    long_description='APIDraw',
+)
