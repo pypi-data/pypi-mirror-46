9aac1f42c4f8d42797234894c99db3db112b0db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:06 +0000

    Add package boofuzz/0.1.5/boofuzz-0.1.5-py3-none-any.whl

---
+++ packages/boofuzz/boofuzz-0.1.5-py3-none-any.whl/boofuzz/exception.py
@@ -0,0 +1,46 @@
+import attr
+
+
+class BoofuzzError(Exception):
+    pass
+
+
+class BoofuzzRestartFailedError(BoofuzzError):
+    pass
+
+
+class BoofuzzTargetConnectionFailedError(BoofuzzError):
+    pass
+
+
+class BoofuzzTargetConnectionReset(BoofuzzError):
+    pass
+
+
+@attr.s
+class BoofuzzTargetConnectionAborted(BoofuzzError):
+    """
+    Raised on `errno.ECONNABORTED`.
+    """
+    socket_errno = attr.ib()
+    socket_errmsg = attr.ib()
+
+
+class BoofuzzNoSuchTestCase(BoofuzzError):
+    pass
+
+
+class BoofuzzRpcError(BoofuzzError):
+    pass
+
+
+class SullyRuntimeError(Exception):
+    pass
+
+
+class SizerNotUtilizedError(Exception):
+    pass
+
+
+class MustImplementException(Exception):
+    pass
