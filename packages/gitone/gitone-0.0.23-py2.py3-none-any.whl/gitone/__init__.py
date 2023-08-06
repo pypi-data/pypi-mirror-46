d7ac99017f107705ab7f38da1700d174df209ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:10 +0000

    Add package gitone/0.0.23/gitone-0.0.23-py2.py3-none-any.whl

---
+++ packages/gitone/gitone-0.0.23-py2.py3-none-any.whl/gitone/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""gitone: a Python package that facilitates git version control."""
+from gitone.camp import camp
+from gitone.acmp import acmp
+from gitone.cam import cam
+from gitone.acm import acm
+from gitone.camendamp import camendamp
+from gitone.acamendmp import acamendmp
+from gitone.camendam import camendam
+from gitone.acamendm import acamendm
+
+__author__ = "Martin Skarzynski"
+__version__ = '0.0.23'
+__all__ = ["camp", "acmp", "cam", "acm", "camendamp", "acamendmp", "camendam", "acamendm"]
