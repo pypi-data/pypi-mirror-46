d1271782879e7ed7c96aec4fcb1259874dc4ec01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:11 +0000

    Add package djangocms-htmlsitemap/0.5.0/djangocms-htmlsitemap-0.5.0.tar.gz

---
+++ packages/djangocms-htmlsitemap/djangocms-htmlsitemap-0.5.0.tar.gz/djangocms-htmlsitemap-0.5.0/tests/_testsite/urls.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.conf.urls import include
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# Local application / specific library imports
+
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
+]
+
+urlpatterns += staticfiles_urlpatterns()
