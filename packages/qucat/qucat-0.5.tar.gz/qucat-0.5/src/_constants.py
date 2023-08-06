21ac7bb1c077e5e1cfb4fa57ed55baef4128d24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package qucat/0.5/qucat-0.5.tar.gz

---
+++ packages/qucat/qucat-0.5.tar.gz/qucat-0.5/src/_constants.py
@@ -0,0 +1,9 @@
+import sys
+max_float = (sys.float_info.max)
+min_float = (sys.float_info.min)
+
+# ANGLES (negative = clock-wise rotation)
+EAST = 0.  # as generated in core.py
+NORTH = 90.
+WEST = 180.
+SOUTH = 270.
