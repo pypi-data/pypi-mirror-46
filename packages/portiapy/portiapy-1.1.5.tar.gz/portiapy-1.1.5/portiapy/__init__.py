5cd4d784138304690286003824f8b218db1a941a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:50 +0000

    Add package portiapy/1.1.5/portiapy-1.1.5.tar.gz

---
+++ packages/portiapy/portiapy-1.1.5.tar.gz/portiapy-1.1.5/portiapy/__init__.py
@@ -0,0 +1,17 @@
+#####################################    
+#             PortiaPy              #
+#####################################
+
+# Variables
+name = "portiapy"
+
+portiaConfigExample = {
+	'baseurl': 'http://localhost',
+	'authorization': '<your API key>',
+	'debug': False,
+	'Accept': 'text/csv'
+}
+
+# Functions
+def about():
+    print("portiapy - an Agriness Edge project")
