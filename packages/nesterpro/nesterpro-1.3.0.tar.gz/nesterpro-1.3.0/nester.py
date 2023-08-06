13d15edcf4af7f0d0dcce7dfda69e834e8d46196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:44 +0000

    Add package nesterpro/1.3.0/nesterpro-1.3.0.tar.gz

---
+++ packages/nesterpro/nesterpro-1.3.0.tar.gz/nesterpro-1.3.0/nester.py
@@ -0,0 +1,14 @@
+""" This is the “nester.py" module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists """
+def print_lol(the_list, indent=False, level=0):
+    """This function takes a positional argument called “the_list", which is any
+    Python list (of, possibly, nested lists). Each data item in the provided list
+    is (recursively) printed to the screen on its own line."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol (each_item, indent, level+1)
+        else:
+            if indent == True:
+                for tab_stop in range(level): #instead of this loop, print('\t' *level, end='')
+                    print ('\t',  end='')
+            print (each_item)
