d6c4fd654415e6a3ee0de70ee8e703730484d3c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:33 +0000

    Add package o2a-potiuk/0.0.1/o2a-potiuk-0.0.1.tar.gz

---
+++ packages/o2a-potiuk/o2a-potiuk-0.0.1.tar.gz/o2a-potiuk-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="o2a-potiuk",
+    version="0.0.1",
+    author="Jarek Potiuk, Szymon Przedwojski, Kamil Breguła, Feng Lu, Cameron Moberg",
+    author_email="jarek.potiuk@polidea.com",
+    description="Oozie To Airflow migration tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GoogleCloudPlatform/oozie-to-airflow",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
