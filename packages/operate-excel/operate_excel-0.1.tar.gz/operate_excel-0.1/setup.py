90bffab9978d0b553092d33aa0686efbc509e150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:33 +0000

    Add package operate-excel/0.1/operate_excel-0.1.tar.gz

---
+++ packages/operate-excel/operate_excel-0.1.tar.gz/operate_excel-0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author:yangtao
+@file: setup.py
+@time: 2019/05/22
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="operate_excel",
+    version="0.1",
+    author="Tao Yang",
+    description="Excel operation method",
+    author_email="1724805202@qq.com",
+    packages=find_packages(),
+    platforms=["all"],
+    license="MIT",
+    url="https://github.com/ChristianYangTao/opetate_excel.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
