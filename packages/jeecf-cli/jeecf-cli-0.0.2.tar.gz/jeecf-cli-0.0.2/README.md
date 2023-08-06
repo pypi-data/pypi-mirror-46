efbc075a96491b684ef73c5844344a0973d6b393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:30 +0000

    Add package jeecf-cli/0.0.2/jeecf-cli-0.0.2.tar.gz

---
+++ packages/jeecf-cli/jeecf-cli-0.0.2.tar.gz/jeecf-cli-0.0.2/README.md
@@ -0,0 +1,103 @@
+# jeecf-cli
+
+![](https://img.shields.io/pypi/v/jeecf-cli.svg?label=jeecf-cli)
+
+## Introduce
+
+Command line toolbox of [Jeecf](https://github.com/cgfly/jeecf)
+
+## Install
+
+```bash
+pip install jeecf-cli
+```
+
+## Commands
+
+### 1. login
+
+```bash
+jeecf login <url>
+```
+
+### 2. namespace
+
+1. 获取命名空间列表并标出当前命名空间
+
+```bash
+jeecf namespace
+```
+
+2. 切换命名空间
+
+```bash
+jeecf namespace use <name>
+```
+
+### 3. dbsource
+
+1. 获取数据源列表并标出当前数据源
+
+```bash
+jeecf dbsource
+```
+
+2. 切换数据源
+
+```bash
+jeecf dbsource use <name>
+```
+
+### 4. plugin
+
+1. 插件列表
+
+```bash
+jeecf plugin
+```
+
+2. 插件支持的语言列表
+
+```bash
+jeecf plugin --language
+```
+
+3. 插件详情
+```bash
+jeecf plugin <name>
+```
+
+### 5. field
+
+1. 属性列表
+```bash
+jeecf field
+```
+
+2. 属性详情
+```bash
+jeecf field <name>
+```
+
+### 6. template
+
+1. 模板列表
+```bash
+jeecf template
+```
+
+2. 拉取模板
+```bash
+jeecf template pull <name>
+```
+
+3. 上传模板
+```bash
+jeecf template push xxx.zip
+```
+
+### 7. gen
+1. 根据模板生成代码
+```bash
+jeecf template gen xxx.yml
+```
---
+++ packages/jeecf-cli/jeecf-cli-0.0.2.tar.gz/jeecf-cli-0.0.2/jeecf/command.py
@@ -0,0 +1,108 @@
+import click
+import getpass
+import sys
+from .jeecf import Jeecf
+
+
+@click.group()
+def main():
+    """
+    Jeecf client commandline toooool.
+
+    Documents: https://github.com/cgfly/jeecf-cli
+    """
+
+
+@main.command()
+def version():
+    click.echo("0.0.2")
+
+
+@main.command()
+@click.argument("path", required=True)
+@click.option("--username", type=click.STRING)
+@click.option("--password", type=click.STRING)
+def login(path, username, password):
+    if path:
+        if not path.startswith("http"):
+            click.echo("Error:")
+            sys.exit("The url should starts with http or https...")
+
+    if not username:
+        username = input("username:")
+    if not password:
+        password = getpass.getpass("password:")
+    Jeecf.login(path, username, password)
+
+
+@main.command()
+@click.argument("command", required=False, type=click.Choice(['use']))
+@click.argument("name", required=False)
+def namespace(command, name):
+    if command == 'use':
+        click.echo(Jeecf().set_current_namespace(name))
+    else:
+        Jeecf().get_namespace_list()
+
+
+@main.command()
+@click.argument("command", required=False, type=click.Choice(['use']))
+@click.argument("name", required=False)
+def dbsource(command, name):
+    if command == 'use':
+        click.echo(Jeecf().set_current_dbsource(name))
+    else:
+        Jeecf().get_dbsource_list()
+
+
+@main.command()
+@click.option("--language", required=False, is_flag=True)
+@click.argument("name", required=False, type=click.STRING)
+def plugin(language, name):
+    if language:
+        Jeecf().get_plugin_language()
+    elif name:
+        Jeecf().get_plugin_detail(name)
+    else:
+        Jeecf().get_plugin_list()
+
+
+@main.command()
+@click.argument("name", required=False, type=click.STRING)
+def field(name):
+    if name:
+        Jeecf().get_field_detail(name)
+    else:
+        Jeecf().get_field_list()
+
+
+@main.command()
+@click.argument("subcommand", required=False, type=click.Choice(['pull', 'push']))
+@click.argument("name", required=False)
+def template(subcommand, name):
+    if subcommand == 'pull':
+        Jeecf().pull_template(name)
+    elif subcommand == 'push':
+        Jeecf().push_template(
+            name,
+            name=click.prompt('name'),
+            field=click.prompt('field', default=''),
+            language=click.prompt('language', default=''),
+            version=click.prompt('version', default=''),
+            namespace=click.prompt('namespace', default=''),
+            description=click.prompt('description', default=''),
+            wiki_uri=click.prompt('wiki_uri', default=''),
+        )
+    else:
+        Jeecf().get_template_list()
+
+
+@main.command()
+@click.argument("name", required=True)
+def gen(name):
+    Jeecf().gen_code(name)
+
+
+@main.command()
+def logout():
+    Jeecf().logout()
