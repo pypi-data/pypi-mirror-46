80b9c981c04f0e591535db5c07ce1d70c5bf8e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:30 +0000

    Add package djangocms-charts/1.2.2/djangocms_charts-1.2.2.tar.gz

---
+++ packages/djangocms-charts/djangocms_charts-1.2.2.tar.gz/djangocms_charts-1.2.2/setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='djangocms_charts',
+    version='1.2.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='DjangoCMS Plugin to add and edit ChartJs charts',
+    long_description=README,
+    url='https://github.com/mcldev/djangocms-charts',
+    author='Michael Carder Ltd',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'six',
+        'django>=1.8',
+        'django-cms>=3.4',
+    ],
+    package_data={
+        'readme': ['README.rst'],
+        'license': ['LICENSE']
+    },
+)
+
