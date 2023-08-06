c06c33bc6e8f3e8dfecb8a7fca563030a2942271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:08 +0000

    Add package instana/1.11.2/instana-1.11.2.tar.gz

---
+++ packages/instana/instana-1.11.2.tar.gz/instana-1.11.2/instana/singletons.py
@@ -0,0 +1,40 @@
+import sys
+import opentracing
+
+from .agent import Agent
+from .tracer import InstanaTracer
+
+from distutils.version import LooseVersion
+
+
+# The Instana Agent which carries along with it a Sensor that collects metrics.
+agent = Agent()
+
+
+# The global OpenTracing compatible tracer used internally by
+# this package.
+#
+# Usage example:
+#
+# import instana
+# instana.tracer.start_span(...)
+#
+tracer = InstanaTracer()
+
+if sys.version_info >= (3,4):
+    from opentracing.scope_managers.asyncio import AsyncioScopeManager
+    async_tracer = InstanaTracer(scope_manager=AsyncioScopeManager())
+
+
+# Mock the tornado tracer until tornado is detected and instrumented first
+tornado_tracer = tracer
+
+
+def setup_tornado_tracer():
+    global tornado_tracer
+    from opentracing.scope_managers.tornado import TornadoScopeManager
+    tornado_tracer = InstanaTracer(scope_manager=TornadoScopeManager())
+
+
+# Set ourselves as the tracer.
+opentracing.tracer = tracer
