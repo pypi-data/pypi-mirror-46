a645cb5ea4ebd4ae09c82054d74e4cb2e11e0d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:23 +0000

    Add package wordcloud-fa/0.1.1/wordcloud_fa-0.1.1.tar.gz

---
+++ packages/wordcloud-fa/wordcloud_fa-0.1.1.tar.gz/wordcloud_fa-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='wordcloud_fa',
+    packages=['wordcloud_fa'],
+    version='0.1.1',
+    license='MIT',
+    description='A wrapper for wordcloud module for creating persian word cloud.',
+    author='Mohammadreza Alihoseiny',
+    author_email='salam@alihoseiny.ir',
+    url='https://github.com/alihoseiny/word_cloud_fa',
+    download_url='https://github.com/alihoseiny/word_cloud_fa/archive/0.1.tar.gz',
+    keywords=['wordcloud', 'word cloud', 'Farsi', 'persian', 'Iran'],
+    install_requires=[
+        'arabic_reshaper',
+        'python-bidi',
+        'wordcloud',
+        'hazm'
+    ],
+    package_data={'wordcloud_fa': ['stopwords', 'Fonts/font.ttf']},
+)
