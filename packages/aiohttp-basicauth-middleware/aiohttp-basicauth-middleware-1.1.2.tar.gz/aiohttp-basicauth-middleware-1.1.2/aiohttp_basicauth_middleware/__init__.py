3eb4ca2ecad5d63f49a113ffb6223958a802a8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:30 +0000

    Add package aiohttp-basicauth-middleware/1.1.2/aiohttp-basicauth-middleware-1.1.2.tar.gz

---
+++ packages/aiohttp-basicauth-middleware/aiohttp-basicauth-middleware-1.1.2.tar.gz/aiohttp-basicauth-middleware-1.1.2/aiohttp_basicauth_middleware/__init__.py
@@ -0,0 +1,82 @@
+import inspect
+import logging
+from typing import (
+    Callable,
+    Iterable,
+    Any,
+    TYPE_CHECKING,
+)
+
+try:
+    from typing import Type, Coroutine
+except ImportError:
+    class _Coroutine:
+        # Fake, so you can do Coroutine[foo, bar, baz]
+        # You could assert the proper number of items are in the slice,
+        # but that seems like overkill, given that mypy will check this
+        # and at runtime you probably no longer care
+        #
+        # See: https://stackoverflow.com/q/44651115/2122401
+        # TODO: remove on drop py35
+        def __getitem__(self, index: Any) -> None:
+            pass
+    if not TYPE_CHECKING:
+        Coroutine = _Coroutine()
+
+from aiohttp import web
+from http_basic_auth import parse_header, BasicAuthException
+from aiohttp_basicauth_middleware.strategy import BaseStrategy
+
+
+log = logging.getLogger(__name__)
+
+
+def check_access(
+    auth_dict: dict,
+    header_value: str,
+    strategy: Callable = lambda x: x
+) -> bool:
+    log.debug('Check access: %r', header_value)
+
+    try:
+        login, password = parse_header(header_value)
+    except BasicAuthException:
+        return False
+
+    hashed_password = auth_dict.get(login)
+    hashed_request_password = strategy(password)
+
+    if hashed_password != hashed_request_password:
+        return False
+
+    return True
+
+
+def basic_auth_middleware(
+    urls: Iterable,
+    auth_dict: dict,
+    strategy: Type[BaseStrategy] = lambda x: x
+) -> Coroutine:
+    async def factory(app, handler) -> Coroutine:
+        async def middleware(request) -> web.Response:
+            for url in urls:
+                if not request.path.startswith(url):
+                    continue
+
+                if inspect.isclass(strategy) and issubclass(strategy, BaseStrategy):
+                    log.debug("Use Strategy: %r", strategy.__name__)
+                    strategy_obj = strategy(
+                        request,
+                        auth_dict,
+                        handler,
+                        request.headers.get('Authorization', '')
+                    )
+                    return await strategy_obj.check()
+
+                if not check_access(auth_dict, request.headers.get('Authorization', ''), strategy):
+                    raise web.HTTPUnauthorized(headers={'WWW-Authenticate': 'Basic'})
+
+                return await handler(request)
+            return await handler(request)
+        return middleware
+    return factory
---
+++ packages/aiohttp-basicauth-middleware/aiohttp-basicauth-middleware-1.1.2.tar.gz/aiohttp-basicauth-middleware-1.1.2/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup
+
+
+requires = ['http_basic_auth', 'aiohttp']
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
+
+
+setup(
+    # Basic package information:
+    name='aiohttp-basicauth-middleware',
+    version='1.1.2',
+    py_modules=('aiohttp_basicauth_middleware',),
+
+    # Packaging options:
+    zip_safe=False,
+    include_package_data=True,
+    packages=('aiohttp_basicauth_middleware',),
+
+    classifiers=[
+        'License :: Public Domain',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    # Package dependencies:
+    requires=requires,
+    tests_require=requires + ["pytest"],
+    setup_requires=requires + ["pytest-runner"],
+    install_requires=requires,
+
+    # Metadata for PyPI:
+    author='Georgy Bazhukov',
+    author_email='georgy.bazhukov@gmail.com',
+    license='BSD',
+    url='https://github.com/bugov/aiohttp-basicauth-middleware',
+    keywords='aiohttp security basicauth http middleware',
+    description='An incredibly simple HTTP basic auth implementation for Aiohttp.',
+    long_description=read('README.rst')
+)
