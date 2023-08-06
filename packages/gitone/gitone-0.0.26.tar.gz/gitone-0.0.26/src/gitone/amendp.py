aa19db0a9da7b10e752972f845e3a115f086dfe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:11 +0000

    Add package gitone/0.0.26/gitone-0.0.26.tar.gz

---
+++ packages/gitone/gitone-0.0.26.tar.gz/gitone-0.0.26/src/gitone/amendp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from typing import Optional
+
+import git
+
+from gitone.amend import amend
+
+
+def amendp(message: Optional[str] = None) -> None:
+    """Amend the previous commit with changes made to tracked files, then push.
+
+    :param message: The commit message to be passed to the git commit command.
+    :note: The previous commit message will be reused
+           if the ``message`` argument is not provided.
+    """
+
+    amend(message=message) if message else amend()
+    repo = git.Repo(search_parent_directories=True)
+    status, stdout, stderr = repo.git.push("--force", extended_output = True)
+    print(f"Status: {status}, Output: {stdout}, Message: {stderr}.")
+
+
+if __name__ == "__main__":
+    amendp()
