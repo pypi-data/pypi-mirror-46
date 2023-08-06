992b39c169b92854b7f56f94a7444fdbcc415aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:30 +0000

    Add package dataship/0.8.2/dataship-0.8.2.tar.gz

---
+++ packages/dataship/dataship-0.8.2.tar.gz/dataship-0.8.2/dataship/beam/__init__.py
@@ -0,0 +1,8 @@
+from .beam import load
+from .beam import read
+from .beam import write
+from .beam import to_dataframe
+from .beam import from_dataframe
+from .beam import write_column
+from .beam import read_column
+__all__ = ['load', 'read', 'write', 'to_dataframe', 'from_dataframe', 'write_column', 'read_column']
