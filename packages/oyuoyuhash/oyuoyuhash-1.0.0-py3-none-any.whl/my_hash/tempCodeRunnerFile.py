b41ec96ef74f69a551c797e7f8ba7209e02c972a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:00 +0000

    Add package oyuoyuhash/1.0.0/oyuoyuhash-1.0.0-py3-none-any.whl

---
+++ packages/oyuoyuhash/oyuoyuhash-1.0.0-py3-none-any.whl/my_hash/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", required=True,
+                help='file name with text')
+args = vars(parser.parse_args())
+
+md5 = hashlib.md5()
+sha1 = hashlib.sha1()
+
+try:
+with open(args["file"], 'rb') as f:
+    buf = f.read()
+    # md5.update(buf)
+    sha1.update(buf)
+print(sha1.hexdigest())
+except FileNotFoundError as e:
+print(e)
