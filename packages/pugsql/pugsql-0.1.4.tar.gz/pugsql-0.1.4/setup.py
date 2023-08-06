01349dcfbcebbf934e2c58a512125886e0c15417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:25 +0000

    Add package pugsql/0.1.4/pugsql-0.1.4.tar.gz

---
+++ packages/pugsql/pugsql-0.1.4.tar.gz/pugsql-0.1.4/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['pugsql']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['sqlalchemy>=1.3,<2.0']
+
+setup_kwargs = {
+    'name': 'pugsql',
+    'version': '0.1.4',
+    'description': 'PugSQL is an anti-ORM that facilitates interacting with databases using SQL in files.',
+    'long_description': None,
+    'author': 'Dan McKinley',
+    'author_email': 'mcfunley@gmail.com',
+    'url': 'https://pugsql.org',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.4',
+}
+
+
+setup(**setup_kwargs)
