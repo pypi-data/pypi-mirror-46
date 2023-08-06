28e53fb3811c6b58cd39a6282955ba6472e00af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:10 +0000

    Add package girder-geospatial/0.1.0a1/girder-geospatial-0.1.0a1.zip

---
+++ packages/girder-geospatial/girder-geospatial-0.1.0a1.zip/girder-geospatial-0.1.0a1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='girder-geospatial',
+    author='Kitware, Inc.',
+    description='Generate metadata for various geospatial datasets',
+    version='0.1.0a1',
+    packages=[
+        'geometa'
+    ],
+    entry_points={
+        'geometa.types': [],
+        'girder.plugin': [
+            'geometa = geometa:GeometaPlugin'
+        ]
+    },
+    install_requires=[
+        'pyproj',
+        'shapely',
+        # Will fix it once marshmallow publishes new version
+        'marshmallow==3.0.0b10',
+        'geojson',
+        'rasterio'
+    ],
+)
