b7d7d1e58a98d4e9763cdbe85517962ec5941093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:48 +0000

    Add package odoo10-addon-sale-commission/10.0.2.2.0.99.dev2/odoo10_addon_sale_commission-10.0.2.2.0.99.dev2-py2-none-any.whl

---
+++ packages/odoo10-addon-sale-commission/odoo10_addon_sale_commission-10.0.2.2.0.99.dev2-py2-none-any.whl/odoo/addons/sale_commission/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sales commissions',
+    'version': '10.0.2.2.0',
+    'author': 'Odoo Community Association (OCA),'
+              'Tecnativa,'
+              'AvanzOSC,'
+              'Agile Business Group',
+    'category': 'Sales Management',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+        'product',
+        'sale',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sale_commission_view.xml',
+        'views/product_template_view.xml',
+        'views/res_partner_view.xml',
+        'views/sale_order_view.xml',
+        'views/account_invoice_view.xml',
+        'views/settlement_view.xml',
+        'wizard/wizard_settle.xml',
+        'wizard/wizard_invoice.xml',
+    ],
+    'demo': [
+        'demo/sale_agent_demo.xml',
+    ],
+    'installable': True,
+}
