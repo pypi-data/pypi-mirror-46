8a9d2762a2017c679ddfe27e1ac7565ff74d6b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:37 +0000

    Add package cmtt-python-wrapper/0.5/cmtt-python-wrapper-0.5.tar.gz

---
+++ packages/cmtt-python-wrapper/cmtt-python-wrapper-0.5.tar.gz/cmtt-python-wrapper-0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cmtt-python-wrapper",
+    version="0.5",
+    author="nenanimalsya",
+    author_email="nenanimalsya@example.com",
+    description="Python wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nenanimalsya/cmtt-python-wrapper/",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'requests',
+          'aiohttp',
+          'aiofiles',
+          'asyncio',
+      ],
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    include_package_data=True,
+)
