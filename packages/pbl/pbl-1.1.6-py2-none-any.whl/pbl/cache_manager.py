7c9d7e5891b0853cce8a045c03f98d335c3cccc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:49 +0000

    Add package pbl/1.1.6/pbl-1.1.6-py2-none-any.whl

---
+++ packages/pbl/pbl-1.1.6-py2-none-any.whl/pbl/cache_manager.py
@@ -0,0 +1,15 @@
+import os
+
+#def get_cache(cache_type="NONE"):
+def get_cache(cache_type=None):
+    if cache_type == None:
+        cache_type = os.environ.get('PBL_CACHE')
+    if cache_type == 'REDIS':
+        import redis_cache as cache
+    elif cache_type == 'LEVELDB':
+        import leveldb_cache as cache
+    else:
+        import nocache as cache
+
+    print 'cache', cache.name
+    return cache
