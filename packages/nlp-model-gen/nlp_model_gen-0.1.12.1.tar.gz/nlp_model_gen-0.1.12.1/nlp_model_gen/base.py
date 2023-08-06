8ace34cef8a2db83c3d146fb2f1aea9beba0e8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:59 +0000

    Add package nlp-model-gen/0.1.12.1/nlp_model_gen-0.1.12.1.tar.gz

---
+++ packages/nlp-model-gen/nlp_model_gen-0.1.12.1.tar.gz/nlp_model_gen-0.1.12.1/nlp_model_gen/base.py
@@ -0,0 +1,8 @@
+# @Vendors
+import os
+
+CURRENT_BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+EXPORT_FORMAT = 'zip'
+FILE_JOIN_OPERATOR = 'cat'
+REMOTE_MODEL_SOURCE = {'path':'https://github.com/galias11', 'remote': True}
+SPLIT_FILE_OPERATOR = 'split -a 1 -b'
