939a0034a7fe29b0ccbea92aaf75dc74c7025482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:13 +0000

    Add package btrdb-admin/5.2/btrdb_admin-5.2.tar.gz

---
+++ packages/btrdb-admin/btrdb_admin-5.2.tar.gz/btrdb_admin-5.2/btrdb_admin/exceptions.py
@@ -0,0 +1,28 @@
+# btrdb_admin.exceptions
+# Module for custom exceptions
+#
+# Author:   PingThings
+# Created:  Wed Mar 27 12:42:26 2019 -0400
+#
+# For license information, see LICENSE.txt
+# ID: exceptions.py [] allen@pingthings.io $
+
+"""
+Module for custom exceptions
+"""
+
+
+class BTRDBError(Exception):
+    pass
+
+class ConnectionError(BTRDBError):
+    """
+    An error has occurred while interacting with the BTrDB server or when trying to establish a connection.
+    """
+    pass
+
+class NotFoundError(BTRDBError):
+    """
+    An error has occurred while attempting to retrieve an item that does not exist
+    """
+    pass
