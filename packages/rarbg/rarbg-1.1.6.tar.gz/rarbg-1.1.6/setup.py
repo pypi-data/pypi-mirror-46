d83c4caac87c95d83646263a7cd90c12ecc67971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:19 +0000

    Add package rarbg/1.1.6/rarbg-1.1.6.tar.gz

---
+++ packages/rarbg/rarbg-1.1.6.tar.gz/rarbg-1.1.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='rarbg',
+    version='1.1.6',
+    description='RSS interface to TorrentAPI',
+    url='https://github.com/banteg/rarbg',
+    py_modules=['rarbg'],
+    install_requires=[
+        'aiohttp',
+        'python-dateutil',
+        'humanize',
+        'jinja2',
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'rarbg = rarbg:main',
+        ],
+    }
+)
