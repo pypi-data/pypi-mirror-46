30985d8dbc090caaaccfc4e64026bdb5aa8550fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:02 +0000

    Add package pymmh3/0.0.4/pymmh3-0.0.4.tar.gz

---
+++ packages/pymmh3/pymmh3-0.0.4.tar.gz/pymmh3-0.0.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+  name = 'pymmh3',
+  packages = ['pymmh3'],
+  version = '0.0.4',
+  license='CC0 1.0',
+  description = 'Pure python mmh3 implementation compatible with python2 and python3',
+  author = 'Ali Ahmed',
+  author_email = 'alahmed.se@gmail.com',
+  url = 'https://github.com/aahmed-se',
+  keywords = ['mmh3', 'hashing'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Utilities',
+    'License :: OSI Approved :: Common Public License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
