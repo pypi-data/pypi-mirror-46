6c95b170fb94e29dd6c964e5a6dfdfcc3e2f24a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:15 +0000

    Add package rezip/0.4.1/rezip-0.4.1.tar.gz

---
+++ packages/rezip/rezip-0.4.1.tar.gz/rezip-0.4.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Run setup"""
+import sys
+
+import setuptools
+
+
+def main():
+    """Run setup"""
+    setuptools.setup()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
