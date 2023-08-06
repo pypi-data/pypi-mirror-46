5b270887b9f7bc67af8c8c5631c9aea9f0cb4dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:22 +0000

    Add package parse-it/0.5.11/parse_it-0.5.11-py3-none-any.whl

---
+++ packages/parse-it/parse_it-0.5.11-py3-none-any.whl/parse_it-0.5.11.data/scripts/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+
+
+__author__ = 'Naor Livne'
+__author_email__ = 'naorlivne@gmail.com'
+__version__ = "0.5.11"
+
+# read the README.md file for the long description of the package
+with open('README.md') as f:
+    long_description = f.read()
+
+# minimum requirement list
+requirements = [
+    "PyYAML",
+    "toml",
+    "configobj",
+    "xmltodict",
+    "pyhcl"
+]
+
+# optional requirements, typing is used for support of Python versions 3.4 & lower
+extra_requirements = {
+          'typing':  ["typing"]
+      }
+
+setup(
+    name='parse_it',
+    author=__author__,
+    author_email=__author_email__,
+    version=__version__,
+    description="A python library for parsing multiple types of config files, envvars and command line arguments "
+                "which takes the headache out of setting app configurations.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    extras_require=extra_requirements,
+    scripts=['setup.py'],
+    license="GPLv3",
+    keywords="parse_it parsing parse config configuration",
+    url="https://github.com/naorlivne/parse_it",
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Other Environment",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+        ]
+)
