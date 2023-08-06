e0c71217c70d3b60ef1a296c2b6ea441772f9ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:12 +0000

    Add package magic-square-dpp/0.2/magic_square_dpp-0.2.tar.gz

---
+++ packages/magic-square-dpp/magic_square_dpp-0.2.tar.gz/magic_square_dpp-0.2/myPackage/magic_square_dpp.py
@@ -0,0 +1,25 @@
+import numpy
+
+def is_magic_square(numbers):
+    print 'Ver 0.2'
+    suma = 0;
+    n = len(numbers)
+    for i in range(n):
+        suma += numbers[i][i]  # Przekatna 1
+    suma_wiersz, suma_kol, suma_przek2 = 0, 0, 0;
+    for i in range(n):
+        for j in range(n):
+            suma_wiersz += numbers[i][j]
+            suma_kol += numbers[j][i]
+        if suma_wiersz != suma or suma_kol != suma:
+            return False
+        suma_wiersz = 0
+        suma_kol = 0
+    for i in range(n):
+        suma_przek2 += numbers[i][n-i-1]
+    if suma_przek2 != suma:
+        return False
+    return True
+
+
+
---
+++ packages/magic-square-dpp/magic_square_dpp-0.2.tar.gz/magic_square_dpp-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='magic_square_dpp',
+    version='0.2',
+    packages=find_packages(exclude=['test*']),
+    license='MIT',
+    description='check if matrix is magic square',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://gitlab.com/DanielSze/dpplab10',
+    author='Daniel Szemik',
+    author_email='szemikd226195@e-science.pl'
+)
