31ed413e0bac9906abd247718f2f86f3547fd839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:53 +0000

    Add package stclassify/0.1.0.5/stclassify-0.1.0.5.tar.gz

---
+++ packages/stclassify/stclassify-0.1.0.5.tar.gz/stclassify-0.1.0.5/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+import os
+
+from setuptools.command.install import install
+
+from setuptools import setup
+
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+
+class MakeCommand(install):
+    def run(self):
+        os.system('make')
+        common_dir = 'tgrocery/learner'
+        target_dir = '%s/%s' % (self.build_lib, common_dir)
+        self.mkpath(target_dir)
+        os.system('cp %s/util.so.1 %s' % (common_dir, target_dir))
+        common_dir = 'tgrocery/learner/liblinear'
+        target_dir = '%s/%s' % (self.build_lib, common_dir)
+        self.mkpath(target_dir)
+        os.system('cp %s/liblinear.so.1 %s' % (common_dir, target_dir))
+        install.run(self)
+
+
+setuptools.setup(
+    name="stclassify",
+    version="0.1.0.5",
+    author="wac",
+    author_email="wuanch@gmail.com",
+    description="short_text_classify",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wac81/short_text_classify",
+    packages=setuptools.find_packages(),
+    install_requires=['jieba', 'numpy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    cmdclass={'install': MakeCommand}
+
+)
