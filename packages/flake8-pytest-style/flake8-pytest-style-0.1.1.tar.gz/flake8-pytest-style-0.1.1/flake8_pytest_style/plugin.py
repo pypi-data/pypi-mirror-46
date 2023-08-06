060dab18ce10afea9d367ee542d3f1cefbac3312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:22 +0000

    Add package flake8-pytest-style/0.1.1/flake8-pytest-style-0.1.1.tar.gz

---
+++ packages/flake8-pytest-style/flake8-pytest-style-0.1.1.tar.gz/flake8-pytest-style-0.1.1/flake8_pytest_style/plugin.py
@@ -0,0 +1,11 @@
+from flake8_plugin_utils import Plugin
+
+from .visitors import PytestStyleVisitor
+
+__version__ = '0.1.1'
+
+
+class PytestStylePlugin(Plugin):
+    name = 'flake8-return'
+    version = __version__
+    visitors = [PytestStyleVisitor]
