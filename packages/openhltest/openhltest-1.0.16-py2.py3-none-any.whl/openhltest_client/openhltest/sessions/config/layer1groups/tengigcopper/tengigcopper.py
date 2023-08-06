16a2fb101b813d36f45629a8001680c9ff5f5e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:00 +0000

    Add package openhltest/1.0.16/openhltest-1.0.16-py2.py3-none-any.whl

---
+++ packages/openhltest/openhltest-1.0.16-py2.py3-none-any.whl/openhltest_client/openhltest/sessions/config/layer1groups/tengigcopper/tengigcopper.py
@@ -0,0 +1,34 @@
+from openhltest_client.base import Base
+
+
+class TenGigCopper(Base):
+	"""TBD
+	"""
+	YANG_NAME = 'ten-gig-copper'
+	YANG_KEYWORD = 'container'
+	YANG_KEY = None
+	YANG_PROPERTY_MAP = {"AdvertiseIeee": "advertise-ieee"}
+	YANG_ACTIONS = []
+
+	def __init__(self, parent):
+		super(TenGigCopper, self).__init__(parent)
+
+	@property
+	def AdvertiseIeee(self):
+		"""TBD
+
+		Getter Returns:
+			boolean
+
+		Setter Allows:
+			boolean
+
+		Setter Raises:
+			ValueError
+			InvalidValueError
+		"""
+		return self._get_value('advertise-ieee')
+	@AdvertiseIeee.setter
+	def AdvertiseIeee(self, value):
+		return self._set_value('advertise-ieee', value)
+
