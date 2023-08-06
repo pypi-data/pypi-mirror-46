cf93cff1d1771896352898368d780e8ec6c14f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:02 +0000

    Add package smolyak/2.6.2/smolyak-2.6.2-py3-none-any.whl

---
+++ packages/smolyak/smolyak-2.6.2-py3-none-any.whl/smolyak/applications/polynomials/__init__.py
@@ -0,0 +1,5 @@
+'''
+Polynomial Approximation
+'''
+from smolyak.applications.polynomial_approximation import PolynomialApproximator
+from smolyak.applications.probability_distributions import ProbabilityDistribution
