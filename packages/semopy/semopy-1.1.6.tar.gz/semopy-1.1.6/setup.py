4834362d1fdb06d9f1c6a9901679d9f2034df4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:43 +0000

    Add package semopy/1.1.6/semopy-1.1.6.tar.gz

---
+++ packages/semopy/semopy-1.1.6.tar.gz/semopy-1.1.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+      package_data = { 'PD': ['pd_data.txt'], 'Example': ['example_data.txt']},
+      install_requires=['scipy', 'numpy', 'pandas', 'graphviz', 'portmin'],
+      name="semopy",
+      version="1.1.6",
+      author="Meshcheryakov A. Georgy",
+      author_email="metsheryakov_ga@spbstu.ru",
+      description="Structural Equation Modelling optimisation package.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://bitbucket.org/herrberg/semopy/",
+      packages=find_packages(),
+      classifiers=(
+              "Programming Language :: Python :: 3",
+              "License :: OSI Approved :: MIT License",
+              "Operating System :: OS Independent"))
