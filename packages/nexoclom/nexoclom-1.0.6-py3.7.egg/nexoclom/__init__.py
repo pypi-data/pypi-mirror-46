a2e19f03fbb77fd8a1ee1924062cb3fb71879c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:08 +0000

    Add package nexoclom/1.0.6/nexoclom-1.0.6-py3.7.egg

---
+++ packages/nexoclom/nexoclom-1.0.6-py3.7.egg/nexoclom/__init__.py
@@ -0,0 +1,13 @@
+from .Input import Input
+from .Output import Output
+from .modeldriver import modeldriver
+from .LOSResult import LOSResult
+from .produce_image import ModelImage
+from .configure_model import configure_model
+
+
+name = 'nexoclom'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '1.0.6'
+database = 'thesolarsystemMB'
