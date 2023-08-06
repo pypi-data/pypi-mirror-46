84c2310876eaa3622886faee50bb2d47a11b2c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:32 +0000

    Add package gitone/0.0.15/gitone-0.0.15-py2.py3-none-any.whl

---
+++ packages/gitone/gitone-0.0.15-py2.py3-none-any.whl/gitone/cam.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from typing import Optional
+
+import git
+
+
+def cam(commit_message: Optional[str] = None) -> None:
+    """Add and commit changes made to tracked files."""
+
+    repo = git.Repo()
+
+    changed_file_lists = [
+        [file.a_path
+         for file in repo.index.diff(None).iter_change_type(change_type)]
+        for change_type in ('D', 'M')
+    ]
+
+    if any(changed_file_lists):
+
+        prefixes = "Deleted files:", "Modified files:"
+
+        deleted, modified = (
+            f"{prefix} {', '.join(changed)}. " if changed else ""
+            for prefix, changed in zip(prefixes, changed_file_lists)
+        )
+
+        print(f"Adding {', '.join(deleted + modified}.",
+              repo.git.add(changed_file_lists))
+
+        if commit_message:
+            print(repo.git.commit(changed_file_lists,
+                                  message=commit_message))
+
+        else:
+
+            print(repo.git.commit(changed_file_lists,
+                                  message=deleted + modified))
+
+    else:
+        print("There are no deleted or modified files.")
+
+
+if __name__ == "__main__":
+    cam()
