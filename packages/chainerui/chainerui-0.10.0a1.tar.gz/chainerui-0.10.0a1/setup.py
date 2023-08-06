f439a350069c47bd5b10e7a512ae98af3732f46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:03:34 +0000

    Add package chainerui/0.10.0a1/chainerui-0.10.0a1.tar.gz

---
+++ packages/chainerui/chainerui-0.10.0a1.tar.gz/chainerui-0.10.0a1/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools.command.sdist import sdist
+from setuptools import find_packages
+from setuptools import setup
+import subprocess
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+# Get __version__ variable
+exec(open(os.path.join(here, 'chainerui', '_version.py')).read())
+
+
+class chainerui_sdist(sdist):
+    def run(self):
+        subprocess.check_call('cd frontend && npm run build', shell=True)
+        sdist.run(self)
+
+
+setup(
+    name='chainerui',
+    version=__version__,  # NOQA
+    description='ChainerUI: User Interface for Chainer',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=required,
+    package_data={
+        'chainerui': [
+            'templates/*',
+            'static/**/*'
+        ],
+    },
+    author='',
+    author_email='',
+    url='https://github.com/chainer/chainerui',
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        "console_scripts": [
+            "chainerui=chainerui.app:main",
+        ]
+    },
+    tests_require=['pytest'],
+    cmdclass={
+        'sdist': chainerui_sdist
+    },
+)
