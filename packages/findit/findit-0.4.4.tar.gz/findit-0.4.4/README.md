3148017de8d82d2835dd681dfddcb54aef4e8b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:23 +0000

    Add package findit/0.4.4/findit-0.4.4.tar.gz

---
+++ packages/findit/findit-0.4.4.tar.gz/findit-0.4.4/README.md
@@ -0,0 +1,30 @@
+<h1 align="center">findit</h1>
+<p align="center">
+    <em>Find target icon on your picture, and get its position.</em>
+</p>
+
+---
+
+[![Python 3.6](https://img.shields.io/badge/python-3.6+-blue.svg)](https://www.python.org/downloads/release/python-360/)
+[![PyPI version](https://badge.fury.io/py/findit.svg)](https://badge.fury.io/py/findit)
+![Travis (.org)](https://img.shields.io/travis/williamfzc/findit.svg?label=Travis%20CI)
+![Docker Cloud Build Status](https://img.shields.io/docker/cloud/build/williamfzc/findit.svg)
+[![Maintainability](https://api.codeclimate.com/v1/badges/d824d06146383ef721c8/maintainability)](https://codeclimate.com/github/williamfzc/findit/maintainability)
+
+---
+
+# 定位
+
+![feature_matching](docs/pics/feature_matching_sample.png)
+
+findit 极大简化了图像识别的繁琐流程，强大而简洁的API让你只需要几行代码就可以在你的目标图片中找到模板图片的位置！
+
+目前，[stagesep2](https://github.com/williamfzc/stagesep2) 与 [fitch](https://github.com/williamfzc/fitch) 均使用该工具作为图像处理引擎。
+
+# 快速开始
+
+ 从我们的 [官方文档](https://williamfzc.github.io/findit/) 开始吧！
+
+# LICENSE
+
+[MIT](LICENSE)
---
+++ packages/findit/findit-0.4.4.tar.gz/findit-0.4.4/findit/server/router.py
@@ -0,0 +1,80 @@
+""" standalone server """
+import os
+import tempfile
+import json
+from collections import namedtuple
+from flask import Flask, request, jsonify
+
+from findit import FindIt
+import findit.server.config as config
+import findit.server.utils as utils
+
+# standard response
+_FindItResponse = namedtuple('FindItResponse', ('status', 'msg', 'request', 'response'))
+STATUS_OK = 'OK'
+STATUS_CLIENT_ERROR = 'CLIENT_ERROR'
+STATUS_SERVER_ERROR = 'SERVER_ERROR'
+
+
+def std_response(**kwargs):
+    _response = _FindItResponse(**kwargs)
+    return jsonify(_response._asdict())
+
+
+# init server
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return std_response(
+        status=STATUS_OK,
+        msg='hello from findit :) response will always contains status/msg/request/response.',
+        request=request.form,
+        response={
+            'hello': 'world',
+        }
+    )
+
+
+@app.route("/analyse", methods=['POST'])
+def analyse():
+    # required
+    template_name = request.form.get('template_name')
+    template_path = utils.get_pic_path_by_name(template_name)
+    if not template_path:
+        return std_response(
+            status=STATUS_CLIENT_ERROR,
+            msg='no template named {}'.format(template_name),
+            request=request.form,
+            response='',
+        )
+
+    # optional
+    extra_dict = json.loads(request.form.get('extras'))
+    new_extra_dict = utils.handle_extras(extra_dict)
+
+    # save target pic
+    target_pic_file = request.files['file']
+    temp_pic_file_object = tempfile.NamedTemporaryFile(mode='wb+', suffix='.png', delete=False)
+    temp_pic_file_object.write(target_pic_file.read())
+    temp_pic_file_object.close()
+
+    # init findit
+    fi = FindIt(need_log=True, **new_extra_dict)
+    fi.load_template(template_path, pic_path=template_path)
+    _response = fi.find(
+        config.DEFAULT_TARGET_NAME,
+        target_pic_path=temp_pic_file_object.name,
+        **new_extra_dict
+    )
+
+    # clean
+    os.remove(temp_pic_file_object.name)
+
+    return std_response(
+        status=STATUS_OK,
+        msg='',
+        request=request.form,
+        response=_response,
+    )
