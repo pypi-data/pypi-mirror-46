78c5a00d674b8655e5ab3eba990786b07c158184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:21 +0000

    Add package pydov/0.1.3/pydov-0.1.3-py2.py3-none-any.whl

---
+++ packages/pydov/pydov-0.1.3-py2.py3-none-any.whl/pydov/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import pydov.util.caching
+from pydov.util.hooks import SimpleStatusHook
+
+__author__ = """DOV-Vlaanderen"""
+__version__ = '0.1.3'
+
+cache = pydov.util.caching.GzipTextFileCache()
+
+hooks = [
+    SimpleStatusHook(),
+]
