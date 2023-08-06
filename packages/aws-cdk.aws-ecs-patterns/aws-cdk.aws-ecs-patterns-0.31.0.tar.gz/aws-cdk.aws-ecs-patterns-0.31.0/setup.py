3f637f0fc2a04db0505cabc1719401eaa4d2551e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:24 +0000

    Add package aws-cdk.aws-ecs-patterns/0.31.0/aws-cdk.aws-ecs-patterns-0.31.0.tar.gz

---
+++ packages/aws-cdk.aws-ecs-patterns/aws-cdk.aws-ecs-patterns-0.31.0.tar.gz/aws-cdk.aws-ecs-patterns-0.31.0/setup.py
@@ -0,0 +1,48 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-ecs-patterns",
+    "version": "0.31.0",
+    "description": "CDK Constructs for AWS ECS",
+    "url": "https://github.com/awslabs/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_ecs_patterns",
+        "aws_cdk.aws_ecs_patterns._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_ecs_patterns._jsii": [
+            "aws-ecs-patterns@0.31.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_ecs_patterns": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.10.5",
+        "publication>=0.0.3",
+        "aws-cdk.aws-ec2~=0.31.0",
+        "aws-cdk.aws-ecs~=0.31.0",
+        "aws-cdk.aws-events~=0.31.0",
+        "aws-cdk.aws-events-targets~=0.31.0",
+        "aws-cdk.aws-iam~=0.31.0",
+        "aws-cdk.cdk~=0.31.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
---
+++ packages/aws-cdk.aws-ecs-patterns/aws-cdk.aws-ecs-patterns-0.31.0.tar.gz/aws-cdk.aws-ecs-patterns-0.31.0/src/aws_cdk/aws_ecs_patterns/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+from jsii.python import classproperty
+
+import aws_cdk.aws_ec2
+import aws_cdk.aws_ecs
+import aws_cdk.aws_events
+import aws_cdk.aws_events_targets
+import aws_cdk.aws_iam
+import aws_cdk.cdk
+__all__ = []
+
+publication.publish()
