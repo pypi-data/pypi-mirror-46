d59ae0c7d3c851650613354a75fe56eef40d4214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:41 +0000

    Add package statgcb/0.1.1/statgcb-0.1.1.tar.gz

---
+++ packages/statgcb/statgcb-0.1.1.tar.gz/statgcb-0.1.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="statgcb",
+    version="0.1.1",
+    author="Francis B. Lavoie",
+    author_email="francis.b.lavoie@usherbrooke.ca",
+    description="Statistiques GCB",
+    long_description="Statistiques GCB",
+    long_description_content_type="text/markdown",
+    url="https://catalyseur.ca",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
