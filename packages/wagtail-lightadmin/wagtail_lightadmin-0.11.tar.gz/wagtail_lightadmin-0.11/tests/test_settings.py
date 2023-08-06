c0286a78f72d361a0273d3d3b377959a74cedea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:28 +0000

    Add package wagtail-lightadmin/0.11/wagtail_lightadmin-0.11.tar.gz

---
+++ packages/wagtail-lightadmin/wagtail_lightadmin-0.11.tar.gz/wagtail_lightadmin-0.11/tests/test_settings.py
@@ -0,0 +1,51 @@
+SECRET_KEY = 'fake-key'
+
+INSTALLED_APPS = [
+    'wagtail.forms',
+    'wagtail.redirects',
+    'wagtail.embeds',
+    'wagtail.sites',
+    'wagtail.users',
+    'wagtail.snippets',
+    'wagtail.documents',
+    'wagtail.images',
+    'wagtail.search',
+    'wagtail.admin',
+    'wagtail.core',
+    'wagtail.contrib.modeladmin',
+
+    'modelcluster',
+    'taggit',
+    'wagtail_lightadmin',
+
+    'django.contrib.contenttypes',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
+}
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug': False,
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
