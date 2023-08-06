04874b1159c9315a02a767ac3f4468defad4e0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:54 +0000

    Add package plopy/1.1/plopy-1.1.tar.gz

---
+++ packages/plopy/plopy-1.1.tar.gz/plopy-1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plopy",
+    version="1.1",
+    author="Finnventor",
+    description="GUI for matplotlib graphing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Finnventor/plopy",
+    py_modules=["my_module"],
+    #packages=setuptools.find_packages(),
+    install_requires=['matplotlib', 'tkinter'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
