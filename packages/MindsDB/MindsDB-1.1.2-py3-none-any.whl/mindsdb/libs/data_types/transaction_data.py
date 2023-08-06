72bba0409e7c07198abff2a4e0a6eade182d497c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:56 +0000

    Add package MindsDB/1.1.2/MindsDB-1.1.2-py3-none-any.whl

---
+++ packages/MindsDB/MindsDB-1.1.2-py3-none-any.whl/mindsdb/libs/data_types/transaction_data.py
@@ -0,0 +1,14 @@
+
+class TransactionData():
+
+    def __init__(self):
+        self.data_array = []
+
+        self.train_indexes = {}
+        self.test_indexes = {}
+        self.validation_indexes = {}
+
+        self.all_indexes = {}
+
+        self.columns = []
+
