afae524b74a6d02b1bf0c575b68294400bf23391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:32 +0000

    Add package byambadorjhash/1.0.0/byambadorjhash-1.0.0.tar.gz

---
+++ packages/byambadorjhash/byambadorjhash-1.0.0.tar.gz/byambadorjhash-1.0.0/my_hash/my_hash.py
@@ -0,0 +1,19 @@
+import os
+import hashlib
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", required=True, help='file name with text')
+args = vars(parser.parse_args())
+
+md5 = hashlib.md5()
+sha1 = hashlib.sha1()
+
+try:
+    with open(args["file"], 'rb') as f:
+        buf = f.read()
+        md5.update(buf)
+        sha1.update(buf)
+    print("{} {} {}".format(os.path.basename(args["file"]), md5.hexdigest(), sha1.hexdigest()))
+except FileNotFoundError as e:
+    print(e)
