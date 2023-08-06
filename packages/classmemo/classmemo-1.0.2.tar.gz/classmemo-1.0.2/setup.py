d980f3688b38804a487cf874b8b52c628655ad37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:21 +0000

    Add package classmemo/1.0.2/classmemo-1.0.2.tar.gz

---
+++ packages/classmemo/classmemo-1.0.2.tar.gz/classmemo-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="classmemo",
+    version="1.0.2",
+    description="Memoizer for classes",
+    url="https://github.com/jonathangjertsen/classmemo",
+    author="Jonathan Reichelt Gjertsen",
+    author_email="jonath.re@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["classmemo"],
+    license="MIT",
+    zip_safe=False,
+)
