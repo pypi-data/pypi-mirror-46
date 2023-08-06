542ebba8c2303444961657673c3e480c13b5c6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:30 +0000

    Add package mummer-idotplot/0.1/mummer-idotplot-0.1.tar.gz

---
+++ packages/mummer-idotplot/mummer-idotplot-0.1.tar.gz/mummer-idotplot-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+        name="mummer-idotplot",
+        version="0.1",
+        description="Generate interactive dotplot from mummer4 output using plotly",
+        url="https://github.com/ryought/mummer-idotplot",
+        author="ryought",
+        author_email="ryonakabayashi@gmail.com",
+        license="MIT",
+        packages=[
+            "mummer_idotplot",
+            ],
+        install_requires=["plotly"],
+        entry_points={
+            "console_scripts": [
+                "mummer-idotplot = mummer_idotplot.main:main"
+                ]
+            }
+        )
