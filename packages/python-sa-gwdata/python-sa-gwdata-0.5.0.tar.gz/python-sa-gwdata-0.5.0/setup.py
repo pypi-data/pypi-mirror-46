064f28876ece1cdc5f866440253774a286176588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:10 +0000

    Add package python-sa-gwdata/0.5.0/python-sa-gwdata-0.5.0.tar.gz

---
+++ packages/python-sa-gwdata/python-sa-gwdata-0.5.0.tar.gz/python-sa-gwdata-0.5.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="python-sa-gwdata",
+    packages=("sa_gwdata",),
+    version="0.5.0",
+    description="Unofficial Python package to ease access to groundwater data in South Australia",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kinverarity1/python-sa-gwdata",
+    author="Kent Inverarity",
+    author_email="kinverarity@hotmail.com",
+    license="MIT",
+    install_requires=("requests", "lxml", "pandas"),
+)
