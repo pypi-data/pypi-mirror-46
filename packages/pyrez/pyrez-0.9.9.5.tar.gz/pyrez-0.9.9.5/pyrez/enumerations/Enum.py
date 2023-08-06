62cf982b51fb0f32b951d15c74af26d03478e166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:52 +0000

    Add package pyrez/0.9.9.5/pyrez-0.9.9.5.tar.gz

---
+++ packages/pyrez/pyrez-0.9.9.5.tar.gz/pyrez-0.9.9.5/pyrez/enumerations/Enum.py
@@ -0,0 +1,25 @@
+import enum
+
+__all__ = (
+    "Enum",
+)
+
+class Enum(enum.Enum):
+    #Unknown = -1#None
+    def __str__(self):#str(Enum)
+        return str(self.getId())
+    def __hash__(self):#[Enum]
+        return hash(self.getId())
+    def equal(self, other):#Enum==3
+        return self.__eq__(other)
+    def __eq__(self, other):#Enum==3
+        if isinstance(other, type(self)):
+            return self.getId() == other.getId()
+        try:
+            return other == type(other)(self.getId())
+        except ValueError:
+            return False
+    def getName(self):
+        return str(self.name.replace('_', ' '))
+    def getId(self):
+        return int(self.value) if str(self.value).isnumeric() else str(self.value)
