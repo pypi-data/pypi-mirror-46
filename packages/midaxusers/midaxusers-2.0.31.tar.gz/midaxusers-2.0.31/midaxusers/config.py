3ec2faf9615a28f028e45a5f82ae3f752af5dd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package midaxusers/2.0.31/midaxusers-2.0.31.tar.gz

---
+++ packages/midaxusers/midaxusers-2.0.31.tar.gz/midaxusers-2.0.31/midaxusers/config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('MUSERS_SECRET_KEY') or 'shpAKIsnmnm6YadSdfXcVbN2G4zbKBZHkjhKSHsJKHsJKHsH'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('MUSERS_DATABASE_URL') or \
+    'mssql+pyodbc://(local)/MidaxUsers?driver=ODBC+Driver+17+for+SQL+Server'
+    #'oracle://MIDAX_USERS:midax_users@192.168.102.80:1521/MIDAX12C'
+    #'mssql+pyodbc://(local)/MidaxUsers?driver=ODBC+Driver+17+for+SQL+Server'
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
