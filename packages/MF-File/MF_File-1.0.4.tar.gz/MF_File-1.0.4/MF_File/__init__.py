3f40a822aa3c6cd53a51ccd27703899bf9a16982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:39 +0000

    Add package MF-File/1.0.4/MF_File-1.0.4.tar.gz

---
+++ packages/MF-File/MF_File-1.0.4.tar.gz/MF_File-1.0.4/MF_File/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# from MF_File.app import File
+# from MF_File.config import *
+
+__version__ = "1.0.2"
---
+++ packages/MF-File/MF_File-1.0.4.tar.gz/MF_File-1.0.4/setup.py
@@ -0,0 +1,16 @@
+# coding=UTF-8
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name="MF_File",
+    version="1.0.4",
+    description="蝰蛇软件工作室-文件操作包",
+    author="林睿霖",
+    author_email="limitlin@outlook.com",
+    url="https://git-codecommit.us-west-2.amazonaws.com/v1/repos/MF_file_python",
+    packages=find_packages(),
+    install_requires=[
+        "MF_StatusCode>=1.0.2"],
+    package_data={'MF_File':['*.json']}
+)
