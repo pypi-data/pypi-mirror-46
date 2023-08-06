8b8ad0ddd22861db4150305dce47af61681be6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:32 +0000

    Add package banana/0.3.7/banana-0.3.7-py3-none-any.whl

---
+++ packages/banana/banana-0.3.7-py3-none-any.whl/banana/__about__.py
@@ -0,0 +1,11 @@
+__version__ = '0.3.7'
+
+__authors__ = [
+    ("Thomas G. Close", "tom.g.close@gmail.com"),
+    ("Francesco Sforazzini", "francesco.sforazzini@gmail.com"),
+    ("Phillip G. D. Ward", "phillipgdward@gmail.com")]
+
+
+install_requires = ['arcana>=0.4.5',
+                    'pydicom>=1.0',
+                    'nibabel>=2.3.0']
