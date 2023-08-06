f82a88122ae940480fefbb45a216166cedec184e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:41 +0000

    Add package mtrx/0.1.0/mtrx-0.1.0.tar.gz

---
+++ packages/mtrx/mtrx-0.1.0.tar.gz/mtrx-0.1.0/mtrx/__init__.py
@@ -0,0 +1,8 @@
+__name__ = 'mtrx'
+__doc__ = 'https://mtrx.ir'
+__author__ = 'Mostafa Vahedi Nejad'
+__version__ = '0.0.9'
+__license__ = 'MIT'
+
+from mtrx.mtrx import *
+from __future__ import absolute_import, division, print_function, with_statement, unicode_literals
