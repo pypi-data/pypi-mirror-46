b50ca4d0d4cc2fccf7a6de5d47cf30b407e5c788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:56 +0000

    Add package valohai-cli/0.9.1/valohai-cli-0.9.1.tar.gz

---
+++ packages/valohai-cli/valohai-cli-0.9.1.tar.gz/valohai-cli-0.9.1/valohai_cli/commands/execution/info.py
@@ -0,0 +1,52 @@
+import click
+
+from valohai_cli.ctx import get_project
+from valohai_cli.table import print_table
+from valohai_cli.utils import humanize_identifier
+from valohai_cli.utils.cli_utils import counter_argument
+
+ignored_keys = {
+    'commit',
+    'counter',
+    'ctime',
+    'environment',
+    'events',
+    'id',
+    'inputs',
+    'metadata',
+    'outputs',
+    'parameters',
+    'project',
+    'tags',
+    'url',
+    'urls',
+}
+
+
+@click.command()
+@counter_argument
+def info(counter):
+    """
+    Show execution info.
+    """
+    execution = get_project(require=True).get_execution_from_counter(
+        counter=counter,
+        params={
+            'exclude': 'metadata,events',
+        },
+    )
+    data = dict((humanize_identifier(key), str(value)) for (key, value) in execution.items() if key not in ignored_keys)
+    data['project name'] = execution['project']['name']
+    data['environment name'] = execution['environment']['name']
+    print_table(data)
+    print()
+    print_table(
+        {input['name']: '; '.join(input['urls']) for input in execution.get('inputs', ())},
+        headers=('input', 'URLs'),
+    )
+    print()
+    print_table(
+        execution.get('parameters', {}),
+        headers=('parameter', 'value'),
+    )
+    print()
