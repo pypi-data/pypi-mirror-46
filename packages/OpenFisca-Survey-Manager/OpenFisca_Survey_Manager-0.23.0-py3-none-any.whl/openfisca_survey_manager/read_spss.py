20cb32d5143897108b18f13eba1f89a82d65c453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:55 +0000

    Add package OpenFisca-Survey-Manager/0.23.0/OpenFisca_Survey_Manager-0.23.0-py3-none-any.whl

---
+++ packages/OpenFisca-Survey-Manager/OpenFisca_Survey_Manager-0.23.0-py3-none-any.whl/openfisca_survey_manager/read_spss.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+from pandas import DataFrame
+from savReaderWriter import SavReader
+
+
+def read_spss(spss_file_path):
+    with SavReader(spss_file_path, returnHeader=True) as reader:
+        for record in reader:
+            print(record)
+            # records_got.append(record)
+
+    data_frame = DataFrame(list(SavReader(spss_file_path)))
+    print(data_frame.info())
+
+    return data_frame
