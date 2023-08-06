53e24d63553ac8e8ea77c3f1869ca0bc411c8c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:09 +0000

    Add package icontract/2.0.5/icontract-2.0.5.tar.gz

---
+++ packages/icontract/icontract-2.0.5.tar.gz/icontract-2.0.5/icontract/__init__.py
@@ -0,0 +1,12 @@
+"""Decorate functions with contracts."""
+
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-instance-attributes
+
+import icontract._represent
+from icontract._decorators import require, snapshot, ensure, invariant
+from icontract._globals import aRepr, SLOW
+from icontract._metaclass import DBCMeta, DBC
+
+from icontract._types import Contract as _Contract, Snapshot as _Snapshot
+from icontract.errors import ViolationError
