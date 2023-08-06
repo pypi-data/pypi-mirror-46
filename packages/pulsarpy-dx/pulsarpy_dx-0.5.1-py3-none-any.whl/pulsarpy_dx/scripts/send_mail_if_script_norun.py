17a9afb81e74452f36926ae4c0da965389a71414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:50 +0000

    Add package pulsarpy-dx/0.5.1/pulsarpy_dx-0.5.1-py3-none-any.whl

---
+++ packages/pulsarpy-dx/pulsarpy_dx-0.5.1-py3-none-any.whl/pulsarpy_dx/scripts/send_mail_if_script_norun.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+###
+#Nathaniel Watson
+#Stanford School of Medicine
+#Nov. 28, 2018
+#nathankw@stanford.edu
+###
+
+"""
+This script is used in the scheduled job I created in Heroku Scheduler addon. 
+"""
+
+import argparse
+import logging
+
+import pulsarpy
+import pulsarpy.utils
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-n", "--name", required=True, help="The name of the script that failed to run.")
+    parser.add_argument("-t", "--to", nargs="+", default=pulsarpy.DEFAULT_TO, help="The recipient email address(s).")
+    return parser
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    name = args.name
+    recipients = args.to
+    # Send email with error details to Admin
+    body = "Oh la la."
+    form = {
+        "subject": "Script {} failed to run.".format(name),
+        "text": body,
+        "to": recipients,
+    }
+    res = pulsarpy.utils.send_mail(form=form, from_name="send_mail_if_script_norun.py")
+
+if __name__ == "__main__":
+    main()
