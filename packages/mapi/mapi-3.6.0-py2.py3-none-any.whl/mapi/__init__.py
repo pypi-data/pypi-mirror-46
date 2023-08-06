794740e0a699113041f61ebc322725280491c239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:32 +0000

    Add package mapi/3.6.0/mapi-3.6.0-py2.py3-none-any.whl

---
+++ packages/mapi/mapi-3.6.0-py2.py3-none-any.whl/mapi/__init__.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+"""
+                       o
+  _  _  _    __,    _
+ / |/ |/ |  /  |  |/ \_|
+   |  |  |_/\_/|_/|__/ |_/
+                 /|
+                 \|
+
+mapi is an API for media database APIs which allows you to lookup and search for
+metadata using simple, common interface.
+
+See https://github.com/jkwill87/mapi for more information.
+
+"""
+
+import logging as _logging
+import sys as _sys
+
+# Set up logging
+log = _logging.getLogger(__name__)
+log.addHandler((_logging.StreamHandler()))
+log.setLevel(_logging.DEBUG if "pydevd" in _sys.modules else _logging.ERROR)
+_logging.getLogger("requests").setLevel(_logging.CRITICAL)
+
+ustr = type(u"")  # unicode string type
