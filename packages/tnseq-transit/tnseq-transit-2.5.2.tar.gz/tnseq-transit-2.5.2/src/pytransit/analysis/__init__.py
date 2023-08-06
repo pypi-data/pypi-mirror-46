6e1ded189ec808e5253bd45693e9e40bd84cb67c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:53 +0000

    Add package tnseq-transit/2.5.2/tnseq-transit-2.5.2.tar.gz

---
+++ packages/tnseq-transit/tnseq-transit-2.5.2.tar.gz/tnseq-transit-2.5.2/src/pytransit/analysis/__init__.py
@@ -0,0 +1,52 @@
+#__all__ = []
+
+from os.path import dirname, basename, isfile
+import glob
+modules = glob.glob(dirname(__file__)+"/*.py")
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
+
+import base
+
+import gumbel
+import example
+import tn5gaps
+import binomial
+import griffin
+import resampling
+import hmm
+import rankproduct
+import gi
+import utest
+import normalize
+import pathway_enrichment #08/22/2018 by Ivan
+import anova
+import zinb
+import tnseq_stats
+import winsorize
+
+methods = {}
+methods["example"] = example.ExampleAnalysis()
+methods["gumbel"] = gumbel.GumbelAnalysis()
+methods["binomial"] = binomial.BinomialAnalysis()
+methods["griffin"] = griffin.GriffinAnalysis()
+methods["hmm"] = hmm.HMMAnalysis()
+methods["resampling"] = resampling.ResamplingAnalysis()
+methods["tn5gaps"] = tn5gaps.Tn5GapsAnalysis()
+methods["rankproduct"] = rankproduct.RankProductAnalysis()
+methods["utest"] = utest.UTestAnalysis()
+methods["GI"] = gi.GIAnalysis()
+methods["anova"] = anova.AnovaAnalysis()
+methods["zinb"] = zinb.ZinbAnalysis()
+#methods["mcce"] = mcce.MCCEAnalysis()
+#methods["mcce2"] = mcce2.MCCE2Analysis()
+#methods["motifhmm"] = motifhmm.MotifHMMAnalysis()
+methods["normalize"] = normalize.Normalize()
+methods["pathway_enrichment"]=pathway_enrichment.GSEAAnalysis()
+methods["tnseq_stats"]=tnseq_stats.TnseqStats()
+methods["winsorize"] = winsorize.Winsorize()
+
+# EXPORT METHODS
+import norm
+export_methods = {}
+export_methods["norm"] = norm.NormAnalysis()
+
