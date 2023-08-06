556a2932595763084a0ec44fe4514d40310bd22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:29 +0000

    Add package django-szuprefix-saas/0.4.1/django_szuprefix_saas-0.4.1-py2-none-any.whl

---
+++ packages/django-szuprefix-saas/django_szuprefix_saas-0.4.1-py2-none-any.whl/django_szuprefix_saas/person/helper.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*- 
+__author__ = 'denishuang'
+from . import models
+from django.db.models.fields import NOT_PROVIDED
+from django_szuprefix.utils.modelutils import translate_model_values
+
+def init_person(worker):
+    profile = worker.profile
+    fns = "email,mobile,id_card,gender,ethnic,city".split(",")
+    ps = translate_model_values(models.Person, profile, fns)
+    ps['name'] = worker.name
+    # print "init_person", ps
+    return models.Person.objects.update_or_create(
+        user=worker.user,
+        defaults=ps)
