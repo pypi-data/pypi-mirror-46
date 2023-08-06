2c57cec6878f998f1deaea01c957667beb4f7070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:49 +0000

    Add package chemeco/0.1.0/chemeco-0.1.0-py3-none-any.whl

---
+++ packages/chemeco/chemeco-0.1.0-py3-none-any.whl/chemeco/wrappers/__init__.py
@@ -0,0 +1,10 @@
+"""
+The 'cheml.wrappers' module includes , 
+last modified date: May 10, 2016
+"""
+
+# from test import test
+
+from chemeco.wrappers.engine import run as ChemEcoRun
+
+__all__ = []
