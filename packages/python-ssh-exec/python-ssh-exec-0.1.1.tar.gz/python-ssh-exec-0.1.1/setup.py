d6c03ddd8ca1acdb007016c7987b5e77f30d9e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:50 +0000

    Add package python-ssh-exec/0.1.1/python-ssh-exec-0.1.1.tar.gz

---
+++ packages/python-ssh-exec/python-ssh-exec-0.1.1.tar.gz/python-ssh-exec-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="python-ssh-exec",
+    version="0.1.1",
+    author="AdamGold",
+    author_email="adamgold7@gmail.com",
+    description="A library to execute local python scripts on remote servers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AdamGold/python-ssh-exec",
+    py_modules=["python-ssh-exec"],
+    install_requires=["Click==7.0", "paramiko==2.4.2", "cryptography==2.4.2"],
+    entry_points="""
+        [console_scripts]
+        pyssh=pysshexec:main
+    """,
+)
