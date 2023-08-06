982927fddc2e34e06d8e1ab340a6b3424e5b531b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:12 +0000

    Add package scamp/0.2.1/scamp-0.2.1-py3-none-any.whl

---
+++ packages/scamp/scamp-0.2.1-py3-none-any.whl/scamp/test_run.py
@@ -0,0 +1,14 @@
+from . import Session
+
+
+def play():
+    session = Session()
+
+    piano = session.new_part()
+
+    session.set_rate_target(4, 10)
+
+    for n in reversed(range(1, 40)):
+        piano.play_note(60 + n * (-1) ** n, 1, 0.25)
+
+    piano.play_note(60, 1.0, 6.0)
