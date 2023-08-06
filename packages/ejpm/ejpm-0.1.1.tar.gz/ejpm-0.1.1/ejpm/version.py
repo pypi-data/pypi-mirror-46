a7b5f38fb155083febdc0e1288088a4e10e31ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:00:00 +0000

    Add package ejpm/0.1.1/ejpm-0.1.1.tar.gz

---
+++ packages/ejpm/ejpm-0.1.1.tar.gz/ejpm-0.1.1/ejpm/version.py
@@ -0,0 +1,6 @@
+
+# Stringified version. Used both for setup.py and for internal code
+version = '0.01.01'
+
+# version as the tuple of ints
+version_tuple = tuple(int(val) for val in version.split('.'))
