4cab4cfcd72ff88d729c94b9057c11e67e6f822c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:45 +0000

    Add package numerapi/1.5.2/numerapi-1.5.2.tar.gz

---
+++ packages/numerapi/numerapi-1.5.2.tar.gz/numerapi-1.5.2/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+def convert_md_to_rst(path):
+    try:
+        from pypandoc import convert_file
+    except ImportError:
+        print("warning: pypandoc module not found, could not convert Markdown to RST")
+        return open(path, 'r').read()
+
+    return convert_file(path, 'rst')
+
+
+numerapi_version = '1.5.2'
+
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering"]
+
+
+if __name__ == "__main__":
+    setup(
+        name="numerapi",
+        version=numerapi_version,
+        maintainer="uuazed",
+        maintainer_email="uuazed@gmail.com",
+        description="Automatically download and upload data for the Numerai machine learning competition",
+        long_description='README.md',
+        long_description_content_type='text/markdown',
+        url='https://github.com/uuazed/numerapi',
+        platforms="OS Independent",
+        classifiers=classifiers,
+        license='MIT License',
+        package_data={'numerai': ['LICENSE', 'README.md']},
+        packages=find_packages(exclude=['tests']),
+        install_requires=["requests", "pytz", "python-dateutil",
+                          "tqdm>=4.29.1", "click>=7.0"],
+        entry_points={
+          'console_scripts': [
+              'numerapi = numerapi.cli:cli'
+          ]
+          },
+        )
