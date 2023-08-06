ac15ba0a4fecbe2a418b1c2784e29aee06233133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:23 +0000

    Add package django-ckeditor-link/0.2.2/django_ckeditor_link-0.2.2-py2.py3-none-any.whl

---
+++ packages/django-ckeditor-link/django_ckeditor_link-0.2.2-py2.py3-none-any.whl/ckeditor_link/tests/urls.py
@@ -0,0 +1,17 @@
+"""URLs to run the tests."""
+from django.conf import settings
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from ckeditor_link.tests.test_app.views import TestModelDetailView
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^testmodel/(?P<pk>\d+)/$', TestModelDetailView.as_view(), name='testmodel_detail'),
+]
+
+# if settings.DEBUG:
+#     urlpatterns += [
+#         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+#     ]
