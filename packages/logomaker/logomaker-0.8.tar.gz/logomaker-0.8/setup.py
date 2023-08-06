058b68d1762527de9323d31bb17e59b26fb2a092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:51 +0000

    Add package logomaker/0.8/logomaker-0.8.tar.gz

---
+++ packages/logomaker/logomaker-0.8.tar.gz/logomaker-0.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='logomaker',
+      version='0.8',
+      description='Package for making Sequence Logos',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='Sequence Logos',
+      url='http://logomaker.readthedocs.io',
+      author='Ammar Tareen, Justin B. Kinney',
+      author_email='tareen@cshl.edu',
+      license='MIT',
+      packages=['logomaker'],
+      include_package_data=True,
+      install_requires=[
+        'numpy',
+		'matplotlib',
+		'pandas'
+      ],
+      zip_safe=False)
