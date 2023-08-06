87537410c6dac6dcd73cb31c1cea65b5370c4721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:19 +0000

    Add package LazyHDF5/0.2.1/LazyHDF5-0.2.1.tar.gz

---
+++ packages/LazyHDF5/LazyHDF5-0.2.1.tar.gz/LazyHDF5-0.2.1/CHANGELOG.rst
@@ -0,0 +1,23 @@
+=========
+Changelog
+=========
+
+0.2.1 (19-05-20)
+-----------------
+-   Bug fixes and tweeks for Python and H5Py library compatibility across versions
+-   Testing on Python 3.4, 3.5, 3.6, and 3.7
+
+0.2.0 (18-07-09)
+-----------------
+
+- Alter attributes with and without ensuring same type
+- Save dataset to file with attributes and chunking
+- Write attributes via dictionary
+- Macros to check file and dataset validity
+- Improved handling of strings in numpy array in attributes
+- Can convert single-entry numpy array numeric attributes to Python native types
+
+0.1.0 (18-05-04)
+----------------
+
+-   Initial version
---
+++ packages/LazyHDF5/LazyHDF5-0.2.1.tar.gz/LazyHDF5-0.2.1/setup.py
@@ -0,0 +1,36 @@
+"""
+
+"""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(name='LazyHDF5',
+      version = '0.2.1',
+      description = 'Python Macros for h5py... because I\'m lazy',
+      long_description = long_description,
+      url = 'https://github.com/CCampJr/LazyHDF5',
+      author = 'Charles H. Camp Jr.',
+      author_email = 'charles.camp@nist.gov',
+      license = 'Public Domain',
+      packages = find_packages(),
+      zip_safe = False,
+      include_package_data = True,
+      install_requires=['numpy', 'h5py>= 2.6.0'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Science/Research',
+                   'Operating System :: OS Independent',
+                   'Environment :: X11 Applications :: Qt',
+                   'Programming Language :: Python :: 3 :: Only',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Database',
+                   'Topic :: Software Development :: Libraries :: Python Modules'
+                  ])
