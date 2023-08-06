fbfffb7349133b63410e03051fa1d3240392a331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:43 +0000

    Add package filmhub-python-api/1.1.4/filmhub_python_api-1.1.4-py2-none-any.whl

---
+++ packages/filmhub-python-api/filmhub_python_api-1.1.4-py2-none-any.whl/filmhub_api/__init__.py
@@ -0,0 +1,10 @@
+'''
+FilmHUB fast filmdelivery and pipeline sync Python API
+
+Talks CRUD REST over HTTPS 443 with FilmHUB cloud server.
+
+'''
+
+from .session import Session
+
+__version__ = Session.__version__
