80b8c7baefbaea02a9cae9502443368744af6add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:00 +0000

    Add package all-purpose-dict/0.2.1/all_purpose_dict-0.2.1.tar.gz

---
+++ packages/all-purpose-dict/all_purpose_dict-0.2.1.tar.gz/all_purpose_dict-0.2.1/all_purpose_dict/fns/__init__.py
@@ -0,0 +1,15 @@
+from .discardWhen import discardWhen
+from .isLaden import isLaden
+from .joinWith import joinWith
+from .map_ import map_
+from .passThrough import passThrough
+from .raise_ import raise_
+
+__all__ = [
+    "discardWhen",
+    "isLaden",
+    "joinWith",
+    "map_",
+    "passThrough",
+    "raise_",
+]
