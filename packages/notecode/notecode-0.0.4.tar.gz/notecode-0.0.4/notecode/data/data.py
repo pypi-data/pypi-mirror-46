845cb6a3b656d76e16f21b210f3bc9132e174bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:39 +0000

    Add package notecode/0.0.4/notecode-0.0.4.tar.gz

---
+++ packages/notecode/notecode-0.0.4.tar.gz/notecode-0.0.4/notecode/data/data.py
@@ -0,0 +1,77 @@
+import zipfile
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+from .download import download_file
+
+data_root = '/content/tmp/'
+
+
+def get_adult_data():
+    train_path = data_root + "/data/adult.data.txt"
+    test_path = data_root + "/data/adult.test.txt"
+
+    download_file(url="https://raw.githubusercontent.com/1007530194/data/master/recommendation/data/adult.data.txt",
+                  path=train_path)
+    download_file(url="https://raw.githubusercontent.com/1007530194/data/master/recommendation/data/adult.test.txt",
+                  path=test_path)
+
+    train_data = pd.read_table(train_path, header=None, delimiter=',')
+    test_data = pd.read_table(test_path, header=None, delimiter=',')
+
+    all_columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num',
+                   'marital-status', 'occupation', 'relationship', 'race', 'sex',
+                   'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'label', 'type']
+
+    continus_columns = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+    dummy_columns = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex',
+                     'native-country']
+
+    train_data['type'] = 1
+    test_data['type'] = 2
+
+    all_data = pd.concat([train_data, test_data], axis=0)
+    all_data.columns = all_columns
+
+    all_data = pd.get_dummies(all_data, columns=dummy_columns)
+
+    test_data = all_data[all_data['type'] == 2].drop(['type'], axis=1)
+    train_data = all_data[all_data['type'] == 1].drop(['type'], axis=1)
+
+    train_data['label'] = train_data['label'].map(lambda x: 1 if x.strip() == '>50K' else 0)
+    test_data['label'] = test_data['label'].map(lambda x: 1 if x.strip() == '>50K.' else 0)
+
+    for col in continus_columns:
+        ss = StandardScaler()
+        train_data[col] = ss.fit_transform(train_data[[col]].astype(np.float64))
+        test_data[col] = ss.transform(test_data[[col]].astype(np.float64))
+
+    train_y = train_data['label']
+    train_x = train_data.drop(['label'], axis=1)
+    test_y = test_data['label']
+    test_x = test_data.drop(['label'], axis=1)
+
+    return train_x, train_y, test_x, test_y
+
+
+def get_electronics_data():
+    path_root = data_root + "/data/electronics/"
+
+    path1 = path_root + "/reviews_Electronics_5.json.gz"
+    path2 = path_root + "/meta_Electronics.json.gz"
+
+    download_file(url="http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Electronics_5.json.gz",
+                  path=path1)
+
+    download_file(url="http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/meta_Electronics.json.gz",
+                  path=path2)
+
+    f = zipfile.ZipFile(path1, 'r')
+    for file in f.namelist():
+        f.extract(file, path_root)
+
+    f = zipfile.ZipFile(path2, 'r')
+    for file in f.namelist():
+        f.extract(file, path_root)
