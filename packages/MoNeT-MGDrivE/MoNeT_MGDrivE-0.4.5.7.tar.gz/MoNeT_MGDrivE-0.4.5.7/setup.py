9e9561ca242e373936c5d70a43f13323eaaee283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:37 +0000

    Add package MoNeT-MGDrivE/0.4.5.7/MoNeT_MGDrivE-0.4.5.7.tar.gz

---
+++ packages/MoNeT-MGDrivE/MoNeT_MGDrivE-0.4.5.7.tar.gz/MoNeT_MGDrivE-0.4.5.7/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='MoNeT_MGDrivE',
+    version='0.4.5.7',
+    url='https://chipdelmal.github.io/MoNeT/',
+    author='Hector M. Sanchez C.',
+    author_email='sanchez.hmsc@berkeley.edu',
+    description="MoNeT python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy', 'scipy', 'matplotlib', 'ipython', 'jupyter', 'pandas', 'sympy'
+    ],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ]
+ )
