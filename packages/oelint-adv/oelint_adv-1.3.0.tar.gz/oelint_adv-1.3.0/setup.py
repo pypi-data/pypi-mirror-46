803ac9391b855b67a9487830db18bc833f6ef9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:46 +0000

    Add package oelint-adv/1.3.0/oelint_adv-1.3.0.tar.gz

---
+++ packages/oelint-adv/oelint_adv-1.3.0.tar.gz/oelint_adv-1.3.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = "See README.md for details"
+
+setuptools.setup(
+    name="oelint_adv",
+    version="1.3.0",
+    author="Konrad Weihmann",
+    author_email="kweihmann@outlook.com",
+    description="Advanced bitbake-recipe linter",
+    long_description=long_description,
+    url="https://github.com/priv-kweihmann/oelint-adv",
+    packages=setuptools.find_packages(),
+    scripts=['bin/oelint-adv'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
+)
