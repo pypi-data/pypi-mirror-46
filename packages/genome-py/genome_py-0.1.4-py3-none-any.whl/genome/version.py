f5a548dc6883030291b44ea59d5c1b788d377c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:51 +0000

    Add package genome-py/0.1.4/genome_py-0.1.4-py3-none-any.whl

---
+++ packages/genome-py/genome_py-0.1.4-py3-none-any.whl/genome/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.1.4'
+version = '0.1.4'
+full_version = '0.1.4'
+dev_version = '1558625594'
+release = True
+if not release:
+    version = full_version
