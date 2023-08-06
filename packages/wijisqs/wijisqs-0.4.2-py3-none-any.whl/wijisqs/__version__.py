9d9a968329a17cfa99e90802d40b11b59cf98e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:59 +0000

    Add package wijisqs/0.4.2/wijisqs-0.4.2-py3-none-any.whl

---
+++ packages/wijisqs/wijisqs-0.4.2-py3-none-any.whl/wijisqs/__version__.py
@@ -0,0 +1,9 @@
+about = {
+    "__title__": "wijisqs",
+    "__description__": "AWS SQS broker for wiji. Wiji<https://github.com/komuw/wiji> is an asyncio distributed task processor/queue.",
+    "__url__": "https://github.com/komuw/wijisqs",
+    "__version__": "v0.4.2",
+    "__author__": "komuW",
+    "__author_email__": "komuw05@gmail.com",
+    "__license__": "MIT",
+}
