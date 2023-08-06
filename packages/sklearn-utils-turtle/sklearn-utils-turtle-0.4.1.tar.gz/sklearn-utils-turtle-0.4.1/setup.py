9244cc3a7d974c24155c7d9a3db90fb6987edfa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:41 +0000

    Add package sklearn-utils-turtle/0.4.1/sklearn-utils-turtle-0.4.1.tar.gz

---
+++ packages/sklearn-utils-turtle/sklearn-utils-turtle-0.4.1.tar.gz/sklearn-utils-turtle-0.4.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+import setuptools
+setup(
+    name='sklearn-utils-turtle',    # This is the name of your PyPI-package.
+    version='0.4.1',                          # Update the version number for new releases
+    packages=setuptools.find_packages(),
+)
