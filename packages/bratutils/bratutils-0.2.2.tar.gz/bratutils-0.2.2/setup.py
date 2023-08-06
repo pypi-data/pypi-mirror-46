f2bb8ffa757739cbebe1147f0c15ffe0b6d3694c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:17 +0000

    Add package bratutils/0.2.2/bratutils-0.2.2.tar.gz

---
+++ packages/bratutils/bratutils-0.2.2.tar.gz/bratutils-0.2.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+
+from distutils.core import setup
+from setuptools import find_packages
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='bratutils',
+    version='0.2.2',
+    packages=find_packages('src', exclude=('tests',)),
+    url='https://github.com/savkov/transcriptor',
+    author='Sasho Savkov',
+    author_email='me@sasho.io',
+    package_dir={'': 'src'},
+    zip_safe=True,
+    include_package_data=False,
+    description='brat utilities',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Operating System :: Unix',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering'
+    ]
+)
