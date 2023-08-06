4f671add44367afa70a7cc09e13e94f40724c305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:03 +0000

    Add package myroot99/0.0.1/myroot99-0.0.1-py3-none-any.whl

---
+++ packages/myroot99/myroot99-0.0.1-py3-none-any.whl/pack1/pack1_module2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+# __all__ = ['sub','add','c','test_class']
+
+c=3
+
+def add():
+  return '这是add方法'
+
+def sub():
+  return '这是sub方法'
+
+class  test_class():
+  def test(self):
+    return '这是test_classd的test方法'
+
+  
