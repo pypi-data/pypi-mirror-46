e5bfb1de4bdab25dd96e3e282936e97b6ef5c21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:48 +0000

    Add package cnx-archive/4.12.0/cnx_archive-4.12.0-py2-none-any.whl

---
+++ packages/cnx-archive/cnx_archive-4.12.0-py2-none-any.whl/cnxarchive/utils/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2015, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+"""Various utility methods for various data types."""
+
+from cnxdb.ident_hash import *  # noqa
+
+from .date import *  # noqa
+from .mimetype import *  # noqa
+from .text import *  # noqa
+from .json import *  # noqa
+from .safe import safe_stat  # noqa
