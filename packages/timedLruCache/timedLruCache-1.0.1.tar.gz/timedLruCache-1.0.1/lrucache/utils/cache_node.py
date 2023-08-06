45fa0fc5bf74c913ef589f7c34093c1f431d4f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:25 +0000

    Add package timedLruCache/1.0.1/timedLruCache-1.0.1.tar.gz

---
+++ packages/timedLruCache/timedLruCache-1.0.1.tar.gz/timedLruCache-1.0.1/lrucache/utils/cache_node.py
@@ -0,0 +1,20 @@
+"""Double linked list node class defined."""
+
+from time import time
+
+
+class DoublylinkedListNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+        self.time_created = time()
+
+    def remove_bindings(self):
+        if self.prev is not None:
+            self.prev.next = self.next
+        if self.next is not None:
+            self.next.prev = self.prev
+        self.next = None
+        self.prev = None
