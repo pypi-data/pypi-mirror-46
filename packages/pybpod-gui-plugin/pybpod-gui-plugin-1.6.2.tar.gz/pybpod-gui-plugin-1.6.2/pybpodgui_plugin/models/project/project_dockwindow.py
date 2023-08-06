7f8485921b996cde8d7492087f88db97089f3195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:45 +0000

    Add package pybpod-gui-plugin/1.6.2/pybpod-gui-plugin-1.6.2.tar.gz

---
+++ packages/pybpod-gui-plugin/pybpod-gui-plugin-1.6.2.tar.gz/pybpod-gui-plugin-1.6.2/pybpodgui_plugin/models/project/project_dockwindow.py
@@ -0,0 +1,28 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+from pybpodgui_plugin.models.project.project_treenode import ProjectTreeNode
+
+
+class ProjectDockWindow(ProjectTreeNode):
+    def show(self):
+        self.mainwindow.details.value = self
+        super(ProjectDockWindow,self).show()
+
+    def focus_name(self):
+        """
+        Sets interface focus on the board name text field
+        """
+        self._name.form.lineEdit.setFocus()
+
+    def close(self, silent=False):
+        self.mainwindow.details.value = None
+        super(ProjectDockWindow, self).close(silent)
+
+    @property
+    def mainwindow(self):
+        return self.projects.mainwindow
