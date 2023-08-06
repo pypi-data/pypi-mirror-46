68644de26eefcc7280e9d4d63e53765bf442b2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:39 +0000

    Add package yzs-work/0.1.9/yzs-work-0.1.9.tar.gz

---
+++ packages/yzs-work/yzs-work-0.1.9.tar.gz/yzs-work-0.1.9/yzs/django_extend/base_model.py
@@ -0,0 +1,42 @@
+from django.db import models
+import uuid
+
+
+class ActiveObjectsManager(models.Manager):
+    """
+    只查询enabled为True的对象
+    """
+
+    def get_queryset(self):
+        return super(ActiveObjectsManager, self).get_queryset().filter(enabled=True)
+
+
+class BaseModel(models.Model):
+    """
+    默认模型
+    """
+    object_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    priority = models.IntegerField(default=0, verbose_name='对象的优先级')
+    enabled = models.BooleanField(default=True, verbose_name='是否启用')
+
+    created_at = models.DateTimeField(verbose_name='创建时间', auto_now_add=True, null=True, editable=False)
+    updated_at = models.DateTimeField(verbose_name='修改时间', auto_now=True, null=True, editable=False)
+
+    objects = models.Manager()  # 全部对象管理器
+    active_objects = ActiveObjectsManager()  # 所有激活状态的查询集合管理器
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"<{self.__class__.__name__}:{self.pk}>"
+
+    def delete(self, using=None, keep_parents=False, real_delete=False):
+        """
+        软删除逻辑
+        """
+        if not real_delete:
+            self.enabled = False
+            self.save()
+        else:
+            super(BaseModel, self).delete(using=using, keep_parents=keep_parents)
