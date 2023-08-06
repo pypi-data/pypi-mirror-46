aa8505e9333546503e4c9088794d57d34ccb2cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:05 +0000

    Add package ergo-ai/1.10.0/ergo-ai-1.10.0.tar.gz

---
+++ packages/ergo-ai/ergo-ai-1.10.0.tar.gz/ergo-ai-1.10.0/ergo/actions/clean.py
@@ -0,0 +1,44 @@
+import os
+import argparse
+
+from ergo.core.utils import clean_if_exist
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(prog="ergo clean", description="Clean a project from temporary datasets and optionally reset it to its initial state.")
+
+    parser.add_argument("path", help="Path of the project to clean.")
+    parser.add_argument( "-a", "--all", dest="all", action="store_true", default=False,
+        help="Remove model weights and training data.")
+
+    args = parser.parse_args(argv)
+    return args
+
+def action_clean(argc, argv):
+    args = parse_args(argv)
+    files = [ \
+        'data-train.csv',
+        'data-test.csv',
+        'data-validation.csv',
+        'data-train.pkl',
+        'data-test.pkl',
+        'data-validation.pkl']
+
+    if args.all:
+        files += [ \
+            '__pycache__',
+            'logs',
+            'model.yml',
+            'model.h5',
+            'model.fdeep',
+            'model.stats', # legacy
+            'model.png', # legacy
+            'test_cm.png',
+            'training_cm.png',
+            'validation_cm.png',
+            'history.png',
+            'roc.png',
+            'stats.txt',
+            'stats.json',
+            'history.json']
+    
+    clean_if_exist(args.path, files)
---
+++ packages/ergo-ai/ergo-ai-1.10.0.tar.gz/ergo-ai-1.10.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from ergo.version import __version__, __author__, __email__, __license__
+
+import os
+
+
+desc = 'ergo is a tool that makes deep learning with Keras easier.'
+
+setup( name                 = 'ergo-ai',
+       version              = __version__,
+       description          = desc,
+       long_description     = desc,
+       long_description_content_type = 'text/plain',
+       author               = __author__,
+       author_email         = __email__,
+       url                  = 'http://www.github.com/evilsocket/ergo',
+       packages             = find_packages(),
+       scripts              = [ 'bin/ergo' ],
+       license              = __license__,
+       classifiers          = [
+            'Programming Language :: Python :: 3',
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Environment :: Console',
+      ],
+)
