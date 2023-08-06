3e13e2444dc445c7e2d6ccb8d92edffa10964925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:48 +0000

    Add package myroot10/0.0.1/myroot10-0.0.1.tar.gz

---
+++ packages/myroot10/myroot10-0.0.1.tar.gz/myroot10-0.0.1/pack1/pack1_module2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+__all__ = ['sub','add','c']
+
+c=3
+
+def add():
+  return '这是add方法'
+
+def sub():
+  return '这是sub方法'
+
+# class  test_class():
+#   def a(self):
+#       return '这是a方法'
+
+  
