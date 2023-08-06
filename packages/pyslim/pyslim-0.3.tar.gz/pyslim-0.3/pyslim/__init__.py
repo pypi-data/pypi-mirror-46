ca6b85129c123503c37d9e5d5cc97955fc5cc4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:56 +0000

    Add package pyslim/0.3/pyslim-0.3.tar.gz

---
+++ packages/pyslim/pyslim-0.3.tar.gz/pyslim-0.3/pyslim/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+from __future__ import division
+
+from pyslim.slim_metadata import *       # NOQA
+from pyslim.slim_tree_sequence import *  # NOQA
+from pyslim.provenance import *          # NOQA
