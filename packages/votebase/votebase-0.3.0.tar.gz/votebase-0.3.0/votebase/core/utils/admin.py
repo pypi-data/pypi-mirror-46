af4e787c9d879999d20a021b404c986a7bd3a33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:23 +0000

    Add package votebase/0.3.0/votebase-0.3.0.tar.gz

---
+++ packages/votebase/votebase-0.3.0.tar.gz/votebase-0.3.0/votebase/core/utils/admin.py
@@ -0,0 +1,6 @@
+from votebase.core.utils.common import duplicate
+
+def duplicate_object(modeladmin, request, queryset):
+    # sd is an instance of SemesterDetails
+    for o in queryset:
+        duplicate(o)
