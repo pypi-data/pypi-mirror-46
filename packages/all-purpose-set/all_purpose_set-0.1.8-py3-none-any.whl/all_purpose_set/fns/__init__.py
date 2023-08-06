7930c06398298c2f0f8d5931e107a7576c1ea7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:14 +0000

    Add package all-purpose-set/0.1.8/all_purpose_set-0.1.8-py3-none-any.whl

---
+++ packages/all-purpose-set/all_purpose_set-0.1.8-py3-none-any.whl/all_purpose_set/fns/__init__.py
@@ -0,0 +1,8 @@
+from .forEach import forEach
+from .isLaden import isLaden
+from .joinWith import joinWith
+from .map_ import map_
+from .passThrough import passThrough
+from .raise_ import raise_
+
+__all__ = ["forEach", "isLaden", "joinWith", "map_", "passThrough", "raise_"]
