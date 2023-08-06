77d170bb7ba170db259c428362a515b99b31295a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:09 +0000

    Add package lmfhawq/1.5.2/lmfhawq-1.5.2.zip

---
+++ packages/lmfhawq/lmfhawq-1.5.2.zip/lmfhawq-1.5.2/lmfhawq/data.py
@@ -0,0 +1,103 @@
+zhulong_diqu_dict={
+    "anhui":["anqing","bengbu","bozhou","chaohu","chizhou","chuzhou","fuyang",
+               "huaibei","huainan","huangshan","luan","maanshan"
+               ,"suzhou","tongling","wuhu",'xuancheng','hefei'
+               ],
+    "fujian":["fujian","fuqing","fuzhou","jianou","longyan",
+             "nanan","nanping","ningde","putian","quanzhou",
+             "sanming","shaowu","wuyishan","xiamen","yongan",
+            "zhangzhou"],
+    "guangdong":["heyuan","huizhou","jiangmen","jieyang","meizhou",
+                "yunfu","zhanjiang","zhaoqing","zhongshan","zhuhai",
+                "lianzhou","nanxiong",'shaoguan',"sihui","yingde"
+                ,"guangzhou"
+
+        ],
+    "guangxi":[
+            "baise", "beihai", "chongzuo", "fangchenggang", "guangxi",
+            "guigang", "guilin", "hechi", "hezhou", "laibin",
+            "liuzhou", "nanning", "qinzhou", "wuzhou", 
+        ],
+    "hainan":["danzhou","dongfang","haikou","hainan","qionghai",
+             "sansha","sanya"],
+    "heilongjiang":["daqing","hegang","heilongjiang",'qiqihaer','yichun'],
+
+    "henan":["anyang","dengfeng","gongyi","hebi","jiaozhuo",
+             "jiyuan","jiyuan1","kaifeng","linzhou","luohe",
+             "luoyang","mengzhou","nanyang","pingdingshan","puyang",
+             "qinyang","ruzhou","sanmenxia","shangqiu","weihui",
+             "wugang","xinmi","xinyang","xinxiang","xinzheng",
+             "xuchang","yanshi","yongcheng","zhengzhou","zhoukou",
+            "zhumadian"],
+
+    "hubei":["enshi","huanggang","huangshi","jingmen","shiyan","suizhou"
+            ,"xiangyang","yichang"
+        ],
+
+    "hunan":["changde","chenzhou","hengyang","huaihua","loudi"
+            ,"shaoyang","xiangtan","yiyang","yongzhou","yueyang"
+            ,"zhangjiajie","zhuzhou",'liling','liuyang','yuanjiang'
+            ,"changsha"
+        ],
+
+    "jiangsu":["changshu", "changzhou", "danyang", "dongtai", "huaian"
+            , "jiangyin", "kunshan", "lianyungang", "nanjing", "nantong"
+            , "jiangsu", "suqian", "suzhou", "taizhou", "wuxi"
+            , "xinyi", "xuzhou", "yancheng", "yangzhou", "yizheng"
+            , "zhangjiagang", "zhenjiang"
+               ],
+
+    'jiangxi':["dexing","fengcheng",'fuzhou','ganzhou','gaoan','jian','jiangxi','jingdezhen',
+               'jinggangshan','lushan','nanchang','ruichang','ruijin','shangrao',
+                'xinyu','yichun','yingtan','zhangshu'],
+
+    "jilin":['baicheng','baishan','changchun','jilin','jilinshi','liaoyuan','siping','songyuan','tonghua'],
+
+    "liaoning": [
+            "anshan", "beizhen", "benxi", "chaoyang", "dalian",
+            "dandong", "donggang", "fushun", "fuxin", "haicheng",
+            "huludao", "jinzhou", "liaoning", "liaoyang", "panjin",
+            "shenyang", "tieling", "yingkou",
+        ],
+
+    "neimenggu": [
+            "baotou", "bayannaoer", "chifeng", "eeduosi", "huhehaote",
+            "hulunbeier", "manzhouli", "xinganmeng", "tongliao", "wuhai",
+            "wulanchabu", "xilinguolemeng", "neimenggu",
+        ],
+
+    "ningxia":['guyuan','ningxia','yinchuan'],
+
+    "qinghai":['qinghai','xining'],
+
+    "shandong":["anqiu","binzhou","dongying","feicheng","jiaozhou",
+             "dezhou","jinan","laiwu","leling","linqing",
+             "pingdu","qingdao","rizhao","rongcheng","rushan",
+            "shandong","taian","tengzhou","weifang","weihai",
+            "xintai","yantai","zaozhuang","zibo","linyi"
+            ,"heze","jining","leling","qufu","zoucheng"
+            ,"liaocheng"
+
+        ],
+
+    "shanxi1":['shanxi'],
+
+    "sichuan":["bazhong","chengdu","chongzhou","dazhou","deyang",
+             "dujiangyan","guangan","guanghan","guangyuan","jiangyou",
+             "jianyang","leshan","longchang","luzhou","meishan",
+            "mianyang1","mianyang2","nanchong","neijiang","panzhihua",
+            "pengzhou","qionglai","shifang","sichuan","suining",
+             "wanyuan","yaan","yibin"
+        ],
+    "xizang":['xizang','lasa','rikaze'],
+
+    "yunnan":['baoshan','chuxiong','dali','lijiang','lincang','puer','tengchong','wenshan','yunnan','yuxi','zhaotong','kunming'],
+
+    "zhejiang":["cixi","dongyang","hangzhou","huzhou","jiaxing",
+             "jinhua","linhai","lishui","longquan","ningbo",
+             "pinghu","quzhou","ruian","shaoxing","shengzhou",
+            "taizhou","tongxiang","wenling","wenzhou","yiwu",
+            "yueqing","yuhuan","zhejiang","zhoushan","zhuji"
+        ]
+
+               }
