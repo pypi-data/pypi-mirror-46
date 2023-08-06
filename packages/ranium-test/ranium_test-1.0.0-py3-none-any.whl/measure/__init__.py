058369db322910a4e0e365c32505453090acc62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:13 +0000

    Add package ranium-test/1.0.0/ranium_test-1.0.0-py3-none-any.whl

---
+++ packages/ranium-test/ranium_test-1.0.0-py3-none-any.whl/measure/__init__.py
@@ -0,0 +1,8 @@
+import os, sys
+name = "ranium_test_package"
+__version__="1.0.0"
+# from builtins import input  # Python 3 compatibility
+
+
+# add to PYTHONPATH, used by Sphinx doc system
+sys.path.insert(1, os.path.dirname(__file__))
