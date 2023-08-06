97bf0271e47921d32153ddfa76ccad9acf604b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:59 +0000

    Add package frd-logging/0.0.5/frd-logging-0.0.5.tar.gz

---
+++ packages/frd-logging/frd-logging-0.0.5.tar.gz/frd-logging-0.0.5/frd_logging/__init__.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+name = 'frd_logging'
+LOG_DIR = 'logfile'
+DEFAULT_FORMATTER = '%(asctime)s : %(levelname)s : %(funceName)s %(message)s'
+
+
+def get_logger():
+    if not os.path.exists(LOG_DIR):
+        os.mkdir(LOG_DIR)
+    filename = LOG_DIR + '/logger.log'
+    logger = logging.getLogger(__name__)
+    ch = logging.StreamHandler()
+    logging.basicConfig(filename=filename,
+                        level=logging.DEBUG,
+                        format=DEFAULT_FORMATTER)
+    return logger
