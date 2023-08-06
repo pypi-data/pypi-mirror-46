d4cce6c8fdb29479592e1d5a431fa5d40b1fab09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:23 +0000

    Add package mastercard-merchant-identifier/2.0.0/mastercard_merchant_identifier-2.0.0.tar.gz

---
+++ packages/mastercard-merchant-identifier/mastercard_merchant_identifier-2.0.0.tar.gz/mastercard_merchant_identifier-2.0.0/mastercardmerchantidentifier/__init__.py
@@ -0,0 +1,8 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardmerchantidentifier.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardmerchantidentifier.merchantids import *
