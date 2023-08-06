a71fa898bbfed0305a82b5e467bcd0f08b597161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:32 +0000

    Add package l-graph/0.2/l-graph-0.2.tar.gz

---
+++ packages/l-graph/l-graph-0.2.tar.gz/l-graph-0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='l-graph',
+     version='0.2',
+     author="Dmitry Rudanov",
+     author_email="rudanov.d@gmail.com",
+     description="Python module for working with L-graphs",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/rudanov/lgraph",
+     packages=['lgraph'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
