fb8bdbd073ef6c5a3392d1284d34b8aa82daeec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:03 +0000

    Add package gitone/0.0.27/gitone-0.0.27.tar.gz

---
+++ packages/gitone/gitone-0.0.27.tar.gz/gitone-0.0.27/src/gitone/acmp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from typing import Optional
+
+import git
+
+from gitone.acm import acm
+
+
+def acmp(message: Optional[str] = None) -> None:
+    """Add and commit all changes, then push the commit.
+
+    :param message: The commit message to be passed to the git commit command.
+    :note: A commit message will be automatically generated
+           if the ``message`` argument is not provided.
+    """
+
+    acm(message=message) if message else acm()
+    repo = git.Repo(search_parent_directories=True)
+    status, stdout, stderr = repo.git.push(with_extended_output=True)
+    print(f"Status: {status}, Output: {stdout}, Message: {stderr}.")
+
+
+if __name__ == "__main__":
+    acmp()
