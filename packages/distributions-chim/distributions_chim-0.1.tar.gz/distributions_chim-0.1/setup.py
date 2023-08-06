a10f3165c7d1150701c3de9a71c08bfda3939be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:21 +0000

    Add package distributions-chim/0.1/distributions_chim-0.1.tar.gz

---
+++ packages/distributions-chim/distributions_chim-0.1.tar.gz/distributions_chim-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_chim',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_chim'],
+      author = 'Tserenchimed Ganbold',
+      author_mail = 'chimdee_5588@icloud.com',
+      zip_safe=False)
