459dd00bd215b1d21a846b90fbb08f265516a7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:33 +0000

    Add package distributions-ZHM/0.1/distributions_ZHM-0.1.tar.gz

---
+++ packages/distributions-ZHM/distributions_ZHM-0.1.tar.gz/distributions_ZHM-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_ZHM',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_ZHM'],
+      zip_safe=False)
