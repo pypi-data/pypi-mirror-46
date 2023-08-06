3535fe2e73dfb50fac7c33f90046d8288025475e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:59 +0000

    Add package nightingale/1.5/nightingale-1.5-py3-none-any.whl

---
+++ packages/nightingale/nightingale-1.5-py3-none-any.whl/nightingale/__init__.py
@@ -0,0 +1,7 @@
+from .get_z_score import get_z_score
+from .PopulationProportion import get_population_proportion_error, PopulationProportion
+from .get_sample_size import get_sample_size
+from .OrdinaryLeastSquares import OrdinaryLeastSquares
+from .OrdinaryLeastSquares import OrdinaryLeastSquares as OLS
+from .OrdinaryLeastSquares import OrdinaryLeastSquares as AnalysisOfVariance
+from .MultipleComparisons import MultipleComparisons
