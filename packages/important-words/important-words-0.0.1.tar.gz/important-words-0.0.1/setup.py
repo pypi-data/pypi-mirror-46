59144ff0aca2b8c8bd0f835aad4d1f1235eabccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:39 +0000

    Add package important-words/0.0.1/important-words-0.0.1.tar.gz

---
+++ packages/important-words/important-words-0.0.1.tar.gz/important-words-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="important-words",
+version="0.0.1",
+    author="Tan Kim Yong",
+    author_email="kimyong95@gmail.com",
+    description="Find important words from a sentence.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "torch",
+        "spacy",
+        "numpy",
+        "sklearn",
+    ],
+)
