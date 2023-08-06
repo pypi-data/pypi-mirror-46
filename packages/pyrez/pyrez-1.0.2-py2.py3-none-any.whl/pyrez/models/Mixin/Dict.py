80ba98d4ad8ce101d262374b2c129d6264828137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:37 +0000

    Add package pyrez/1.0.2/pyrez-1.0.2-py2.py3-none-any.whl

---
+++ packages/pyrez/pyrez-1.0.2-py2.py3-none-any.whl/pyrez/models/Mixin/Dict.py
@@ -0,0 +1,13 @@
+class Dict:
+    def __init__(self, **kwargs):
+        self.__kwargs__ = kwargs or []
+    def __getitem__(self, key):
+        try:
+            return self.__kwargs__[key]
+        except KeyError:
+            return None
+    def __iter__(self):
+        return (self.__kwargs__[key] for key in self.__kwargs__)
+    def __str__(self):
+        import json
+        return json.dumps(self.__kwargs__, sort_keys=True, indent=2) if self.__kwargs__ else '' #return str(self.__kwargs__).replace("'", "\"") if self.__kwargs__ else None
