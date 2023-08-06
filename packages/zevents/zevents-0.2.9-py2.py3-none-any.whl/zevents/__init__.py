5e18e2fb8a14270aa946694a5819fb1b7850d462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:47 +0000

    Add package zevents/0.2.9/zevents-0.2.9-py2.py3-none-any.whl

---
+++ packages/zevents/zevents-0.2.9-py2.py3-none-any.whl/zevents/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for ZEvents."""
+
+__author__ = 'Thierry Chappuis'
+__email__ = 'thierry@chappuis.io'
+__version__ = '0.2.9'
+
+from .core import Event
