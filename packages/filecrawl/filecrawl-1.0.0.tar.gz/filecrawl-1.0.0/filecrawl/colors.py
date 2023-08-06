1dfa94809c3ec4788b96bfd7949f7a6d410762e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:28 +0000

    Add package filecrawl/1.0.0/filecrawl-1.0.0.tar.gz

---
+++ packages/filecrawl/filecrawl-1.0.0.tar.gz/filecrawl-1.0.0/filecrawl/colors.py
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import platform
+from colorama import init, Fore
+
+init()
+
+
+class Color:
+    OK = Fore.BLUE + '[~] ' + Fore.RESET
+    WARNING = Fore.YELLOW + "[!] " + Fore.RESET
+    if platform.system() == "Windows":  # Windows cmd isn't able to output unicode characters correctly
+        ERROR = Fore.RED + "[X] " + Fore.RESET
+        SUCCESS = Fore.GREEN + "[S] " + Fore.RESET
+    else:
+        ERROR = Fore.RED + "[✗] " + Fore.RESET
+        SUCCESS = Fore.GREEN + "[✓] " + Fore.RESET
+
