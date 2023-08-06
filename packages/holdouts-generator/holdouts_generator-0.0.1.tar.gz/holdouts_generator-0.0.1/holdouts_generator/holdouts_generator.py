4190a820c50ce5f8a7479fe77bf61d7656dd3835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:25 +0000

    Add package holdouts-generator/0.0.1/holdouts_generator-0.0.1.tar.gz

---
+++ packages/holdouts-generator/holdouts_generator-0.0.1.tar.gz/holdouts_generator-0.0.1/holdouts_generator/holdouts_generator.py
@@ -0,0 +1,52 @@
+import os
+import pickle
+import shutil
+from auto_tqdm import tqdm
+from typing import List, Tuple
+
+
+def load_cache(dataset, holdout, name, cache_dir:str):
+    """Return given holdout, also handles cache if it is enabled."""
+    path = "{cache_dir}/{name}.pickle".format(cache_dir=cache_dir, name=name)
+    if os.path.exists(path):
+        with open(path, "rb") as f:
+            return pickle.load(f)
+    os.makedirs(cache_dir, exist_ok=True)
+    data = holdout(dataset)
+    with open(path, "wb") as f:
+        pickle.dump(data, f)
+    return data
+
+def holdouts_generator(*dataset, holdouts:List, verbose:bool=True, cache:bool=False, cache_dir:str=".holdouts_cache"):
+    """Return validation dataset and another holdout generator
+        dataset, iterable of datasets to generate holdouts from.
+        holdouts:List, list of holdouts callbacks.
+        verbose:bool=True, whetever to show or not loading bars.
+        cache:bool=False, whetever to cache or not the rendered holdouts.
+        cache_dir:str=".cache", directory where to cache the holdouts.
+    """
+    def generator():
+        for outer_holdout, name, inner_holdouts in tqdm(holdouts, verbose=verbose):
+            validation = load_cache(dataset, outer_holdout, name, cache_dir) if cache else outer_holdout(dataset)
+            if inner_holdouts is None:
+                yield validation, None
+            else:
+                yield validation, holdouts_generator(
+                    *[v for i, v in enumerate(validation) if i%2==0],
+                    holdouts=inner_holdouts,
+                    verbose=verbose,
+                    cache=cache,
+                    cache_dir="{cache_dir}/{name}".format(
+                        cache_dir=cache_dir,
+                        name=name
+                    )
+                )
+    return generator
+
+
+def clear_holdouts_cache(cache_dir:str=".holdouts_cache"):
+    """Remove the holdouts cace directory.
+        cache_dir:str=".holdouts_cache", the holdouts cache directory to be removed.
+    """
+    if os.path.exists(cache_dir):
+        shutil.rmtree(cache_dir)
