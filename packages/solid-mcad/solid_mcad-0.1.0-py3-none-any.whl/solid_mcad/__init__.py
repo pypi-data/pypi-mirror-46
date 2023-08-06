f0f2200b26e8b8290c0dcde0b5612045ace0b09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:28 +0000

    Add package solid-mcad/0.1.0/solid_mcad-0.1.0-py3-none-any.whl

---
+++ packages/solid-mcad/solid_mcad-0.1.0-py3-none-any.whl/solid_mcad/__init__.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import solid
+from types import SimpleNamespace
+
+class SPNamespace(SimpleNamespace):
+    def include_scad(self, scad_path):
+        solid.use(scad_path, dest_namespace_dict=self.__dict__)
+
+def import_all_scad():
+    import os
+    mcad_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'MCAD'))
+    # TODO: This doesn't include any subdirectories
+    for f in os.listdir(mcad_dir):
+        if not f.lower().endswith('.scad'):
+            continue
+        name, ext = os.path.splitext(f)
+        scad_path = os.path.join(mcad_dir, f)
+        n = SPNamespace()
+        try:
+            n.include_scad(scad_path)
+            globals()[name] = n
+        except Exception as e:
+            print(f'Unable to load SCAD code at {scad_path} with error: {e}')
+    return True
+
+# NOTE: simply importing solid_mcad does a lot of work; reading & parsing 20+ files,
+# then creating Python classes for each module in a scad file. It's expensive,
+# but this way you can treat SCAD code as if it were all python with, e.g. 
+# from solid_mcad import involute_gears
+# gears = involute_gears.test_bevel_gear_pair()
+import_all_scad()
