9d9c4791d4cd2788cd76423656637032ead7e4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:18 +0000

    Add package odoo-odoo-test/0.0.1/odoo_odoo_test-0.0.1.tar.gz

---
+++ packages/odoo-odoo-test/odoo_odoo_test-0.0.1.tar.gz/odoo_odoo_test-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="odoo_odoo_test",
+    version="0.0.1",
+    author="Coopdevs",
+    description="Odoo customizations for odoo-test",
+    license="GPL",
+    packages=['odoo_odoo_test'],
+    classifiers=[],
+)
