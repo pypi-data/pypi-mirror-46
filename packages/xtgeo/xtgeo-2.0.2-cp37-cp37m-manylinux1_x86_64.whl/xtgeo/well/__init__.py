9902f5a8faf42dafa7825effe0ef06ff58e00ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:20 +0000

    Add package xtgeo/2.0.2/xtgeo-2.0.2-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/xtgeo/xtgeo-2.0.2-cp37-cp37m-manylinux1_x86_64.whl/xtgeo/well/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, absolute_import
+from __future__ import print_function
+
+# flake8: noqa
+from .well import Well
+from .wells import Wells
+
+from .blocked_well import BlockedWell
+from .blocked_wells import BlockedWells
