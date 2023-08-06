6da208e4495ed22db0c18913ebb5ac22d6bef315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:16 +0000

    Add package aina/0.1.3/aina-0.1.3-py2.py3-none-any.whl

---
+++ packages/aina/aina-0.1.3-py2.py3-none-any.whl/aina/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for aina."""
+
+__author__ = """iLoveTux"""
+__email__ = 'cliffbressette@gmail.com'
+__version__ = '0.1.2'
+
+from aina.render import render
