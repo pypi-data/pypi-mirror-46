84f94b8e14506ce937781d2ff18482c3ffd71a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:39 +0000

    Add package speculationcc/0.1/speculationcc-0.1.tar.gz

---
+++ packages/speculationcc/speculationcc-0.1.tar.gz/speculationcc-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='speculationcc',
+      version='0.1',
+      description='sentiment analysis for speculationcc',
+      url='http://github.com/wilkosz/crypto-profiler',
+      author='joshua wilkosz',
+      author_email='joshua@wilkosz.com.au',
+      license='MIT',
+      packages=['speculationcc'],
+      zip_safe=False)
