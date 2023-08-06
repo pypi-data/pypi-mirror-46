c1d33930be8e6e641dfbe2cd63813afee29ffa2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:05 +0000

    Add package lmfgg/1.1.4/lmfgg-1.1.4.zip

---
+++ packages/lmfgg/lmfgg-1.1.4.zip/lmfgg-1.1.4/lmfgg/table/tbpre.py
@@ -0,0 +1,59 @@
+
+from lmfgg.common import getpage
+from bs4 import BeautifulSoup 
+from lmfgg.table.common import fm 
+
+
+# url="http://aqggzy.anqing.gov.cn/jyxx/012001/012001004/20180112/bee00e3f-473d-4b8d-94b3-07852eb2e23b.html"
+
+# page=getpage(url,'anhui_anqing')
+#对一个table进行清洗，去除无用trtd 
+
+#传入clear的必须为table
+def clear(page):
+
+    soup=BeautifulSoup(page,'html.parser')
+    table=soup.find('table')
+ 
+    trs=table.find_all('tr')
+    for tr in trs:
+        if tr.text.strip()=='':
+            tr.extract()
+        tds=tr.find_all('td')
+        for td in tds:
+            if td.text.strip()=='':
+                td.extract()
+    page=str(table)
+    return page 
+
+#从page中提取无嵌套的table 返回tabled额数组
+def get_tb(page):
+    if page is None:return None
+    data=[]
+    soup=BeautifulSoup(page,'html.parser')
+    tables=soup.find_all('table')
+    if len(tables)==0:return None
+
+    if len(tables)==1:
+        data=[str(tables[0])]
+        return data
+
+    for tb in tables:
+        tbtmp=tb.find('table')
+        if tbtmp is  None:
+            data.append(str(tb))
+            continue
+        if len(tbtmp.text.strip())<10:continue
+        data.append(str(tbtmp))
+    if data==[]:return None
+    return data
+
+
+
+def get_pure_tbs(page):
+
+    tbs=get_tb(page)
+    if tbs is None:return None
+    tbs=[clear(w)  for w in tbs]
+
+    return tbs 
