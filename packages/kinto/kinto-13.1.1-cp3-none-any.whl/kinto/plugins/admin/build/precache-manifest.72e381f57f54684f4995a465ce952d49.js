6569583f8f33a9f4863c0f92496c9dbbdeec87bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:09 +0000

    Add package kinto/13.1.1/kinto-13.1.1-cp3-none-any.whl

---
+++ packages/kinto/kinto-13.1.1-cp3-none-any.whl/kinto/plugins/admin/build/precache-manifest.72e381f57f54684f4995a465ce952d49.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c32e634859dc19d2252dbb5fb9d3c1",
+    "url": "/v1/admin/index.html"
+  },
+  {
+    "revision": "136f9bcda7acfb80a650",
+    "url": "/v1/admin/static/css/2.a5671e0a.chunk.css"
+  },
+  {
+    "revision": "136f9bcda7acfb80a650",
+    "url": "/v1/admin/static/js/2.e1224e18.chunk.js"
+  },
+  {
+    "revision": "56f765c1bfc59bdd8134",
+    "url": "/v1/admin/static/js/main.4bb6b8e7.chunk.js"
+  },
+  {
+    "revision": "dbef967a799aef80d0d1",
+    "url": "/v1/admin/static/js/runtime~main.dde49ca3.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
