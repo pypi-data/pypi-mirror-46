8b6baef18c423796d232de14b1606f6cbfe247d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:26 +0000

    Add package nico-wang-nester/1.4.1/nico_wang_nester-1.4.1.tar.gz

---
+++ packages/nico-wang-nester/nico_wang_nester-1.4.1.tar.gz/nico_wang_nester-1.4.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name              = 'nico_wang_nester',
+	version            = '1.4.1',
+	py_modules    = ['nester'],
+	author            = 'whn',
+	author_email  = '15689678809@163.com',
+	description      = 'A simple printer of nested lists. There is an new optional argument fn for you to print the data to your file or screen.'   
+         )
