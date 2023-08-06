3b0fe03f04fac61affc0c273e3e1b5a0874503ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:28 +0000

    Add package infi.wmpio/0.1.30/infi.wmpio-0.1.30-py2-none-any.whl

---
+++ packages/infi.wmpio/infi.wmpio-0.1.30-py2-none-any.whl/infi/wmpio/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.30"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.30)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '4b391de8c95ea3da1e022b6cea34450ba3953e11'
+__git_head_subject__ = u'Finished Release v0.1.30'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2019-05-13 18:12:10'
