1545a8b10c3d45d0aa2b1787e5d915fe1a557a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:09 +0000

    Add package livelossplot/0.4.1/livelossplot-0.4.1.tar.gz

---
+++ packages/livelossplot/livelossplot-0.4.1.tar.gz/livelossplot-0.4.1/livelossplot/__init__.py
@@ -0,0 +1,25 @@
+import warnings
+from .core import draw_plot
+from .generic_plot import PlotLosses
+from .version import __version__
+
+# keras.PlotLossesCallback and pytoune.PlotLossesCallback
+# NOT loaded, as they depend on other libraries
+
+# open question: keep it as deprecated,
+# or as an alternative (but legit) interface?
+
+def PlotLossesKeras(*args, **kwargs):
+    warnings.warn("From v0.3 onwards, use:\nfrom livelossplot.keras import PlotLossesCallback", DeprecationWarning)
+    from .keras import PlotLossesCallback
+    return PlotLossesCallback(*args, **kwargs)
+
+def PlotLossesTensorFlowKeras(*args, **kwargs):
+    warnings.warn("New and deprecated at the same time!\nFrom v0.3 onwards, use:\nfrom livelossplot.tf_keras import PlotLossesCallback", DeprecationWarning)
+    from .tf_keras import PlotLossesCallback
+    return PlotLossesCallback(*args, **kwargs)
+
+def PlotLossesPytoune(*args, **kwargs):
+    warnings.warn("From v0.3 onwards, use:\nfrom livelossplot.pytoune import PlotLossesCallback", DeprecationWarning)
+    from .pytoune import PlotLossesCallback
+    return PlotLossesCallback(*args, **kwargs)
