95a8477359e1a79eeaddcdb75d517f8f505b3c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:23 +0000

    Add package nico-wang-nester/1.4.0/nico_wang_nester-1.4.0.tar.gz

---
+++ packages/nico-wang-nester/nico_wang_nester-1.4.0.tar.gz/nico_wang_nester-1.4.0/nester.py
@@ -0,0 +1,16 @@
+""" This is the "nester.py" module and it provides one function called print_lot()
+	which prints lists that may or may not include nested lists."""
+
+def print_lol(the_list,intend=False,level=0,fn=sys.stdout):
+	"""This function takes one positional argument called "the_list", which 
+		is any Python list (of - possibly - nested lists). Each data item in the
+		provided list is (recursively) printed to the screen on it's own line.
+		The second argument is to insert the Tap when it comes across a nested list."""
+		
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item,intend,level+1,fn=fn)
+		else:
+			if intend:
+				print('\t' *level, end='',file=fn)
+			print(each_item,file=fn)
