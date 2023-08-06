11cb191817e041ed47592e9b43e2a5f9025e4d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:24 +0000

    Add package raiden-contracts/0.19.1/raiden-contracts-0.19.1.tar.gz

---
+++ packages/raiden-contracts/raiden-contracts-0.19.1.tar.gz/raiden-contracts-0.19.1/raiden_contracts/tests/fixtures/service_registry_fixtures.py
@@ -0,0 +1,8 @@
+import pytest
+
+from raiden_contracts.constants import CONTRACT_SERVICE_REGISTRY
+
+
+@pytest.fixture(scope="session")
+def service_registry(deploy_tester_contract, custom_token):
+    return deploy_tester_contract(CONTRACT_SERVICE_REGISTRY, [custom_token.address])
