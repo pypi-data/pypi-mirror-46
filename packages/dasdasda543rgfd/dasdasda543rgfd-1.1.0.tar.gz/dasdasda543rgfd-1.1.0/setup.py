cbaeb0f38bcacb968ff3262b095aa2b474c9a9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:53 +0000

    Add package dasdasda543rgfd/1.1.0/dasdasda543rgfd-1.1.0.tar.gz

---
+++ packages/dasdasda543rgfd/dasdasda543rgfd-1.1.0.tar.gz/dasdasda543rgfd-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name           = "dasdasda543rgfd",
+    version        = "1.1.0",
+    py_modules     = ["test"],
+    author         = "a",
+    author_email   = "627229645@qq.com",
+    url            = "http://www.aaaa.com",
+    description    = "aaa",
+)
---
+++ packages/dasdasda543rgfd/dasdasda543rgfd-1.1.0.tar.gz/dasdasda543rgfd-1.1.0/test.py
@@ -0,0 +1,6 @@
+def print_lol(list_1qq):
+    for each in list_1:
+        if isinstance(list_1, list):
+            print_lol(each)
+        else:
+            print(each)
