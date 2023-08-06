fb7f25b754a414b9f52c19c9447531553ef16dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:00 +0000

    Add package ninja/1.9.0.post1/ninja-1.9.0.post1.tar.gz

---
+++ packages/ninja/ninja-1.9.0.post1.tar.gz/ninja-1.9.0.post1/NinjaUrls.cmake
@@ -0,0 +1,25 @@
+
+#-----------------------------------------------------------------------------
+# Ninja sources
+set(unix_source_url       "https://github.com/kitware/ninja/archive/v1.9.0.g5b44b.kitware.dyndep-1.jobserver-1.tar.gz")
+set(unix_source_sha256    "449359a402c3adccd37f6fece19ce7d7cda586e837fdf50eb7d53597b7f1ce90")
+
+set(windows_source_url    "https://github.com/kitware/ninja/archive/v1.9.0.g5b44b.kitware.dyndep-1.jobserver-1.zip")
+set(windows_source_sha256 "ff573af7342780afea85ffab9563cc68988c58d7b72f1b2b72754fcc7e771bbf")
+
+#-----------------------------------------------------------------------------
+# Ninja binaries
+set(linux32_binary_url    "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256 "NA")
+
+set(linux64_binary_url    "https://github.com/Kitware/ninja/releases/download/v1.9.0.g5b44b.kitware.dyndep-1.jobserver-1/ninja-1.9.0.g5b44b.kitware.dyndep-1.jobserver-1_x86_64-linux-gnu.tar.gz")
+set(linux64_binary_sha256 "f7e5e4f45b99d85465017caa5e289d8bc7636ee43b9863139e8fd5eb7d9079ee")
+
+set(macosx_binary_url    "https://github.com/Kitware/ninja/releases/download/v1.9.0.g5b44b.kitware.dyndep-1.jobserver-1/ninja-1.9.0.g5b44b.kitware.dyndep-1.jobserver-1_x86_64-apple-darwin.tar.gz")
+set(macosx_binary_sha256 "b757ce95aa9b9682dec76b18c26ec64536ba38ac107780080636014524f32e37")
+
+set(win32_binary_url    "NA")  # Windows 32-bit binaries not available
+set(win32_binary_sha256 "NA")
+
+set(win64_binary_url    "https://github.com/Kitware/ninja/releases/download/v1.9.0.g5b44b.kitware.dyndep-1.jobserver-1/ninja-1.9.0.g5b44b.kitware.dyndep-1.jobserver-1_i686-pc-windows-msvc.zip")
+set(win64_binary_sha256 "4cde85857e18c4a0e3b0a67718a88a1206cfc8323fcd850364c29ea1949959c1")
