7051696cd1cebbc44d9acdd70f68ea43076ea404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:37 +0000

    Add package aws-encryption-sdk/1.4.0/aws-encryption-sdk-1.4.0.tar.gz

---
+++ packages/aws-encryption-sdk/aws-encryption-sdk-1.4.0.tar.gz/aws-encryption-sdk-1.4.0/examples/test/test_i_basic_file_encryption_with_raw_key_provider.py
@@ -0,0 +1,38 @@
+# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""Unit test suite for the Bytes Streams examples in the AWS-hosted documentation."""
+import os
+import sys
+
+sys.path.extend([os.sep.join([os.path.dirname(__file__), "..", "src"])])  # noqa
+import tempfile
+
+import pytest
+
+from basic_file_encryption_with_raw_key_provider import cycle_file
+
+
+pytestmark = [pytest.mark.examples]
+
+
+def test_cycle_file():
+    handle, filename = tempfile.mkstemp()
+    with open(filename, "wb") as f:
+        f.write(os.urandom(1024))
+    try:
+        new_files = cycle_file(source_plaintext_filename=filename)
+        for f in new_files:
+            os.remove(f)
+    finally:
+        os.close(handle)
+        os.remove(filename)
