41f26cd8c6a24613a5f91fdfd4ac653c1db32c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:11 +0000

    Add package nester-reneb/1.0.0/nester-reneb-1.0.0.tar.gz

---
+++ packages/nester-reneb/nester-reneb-1.0.0.tar.gz/nester-reneb-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester-reneb',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'Rene Bruggeman',
+    author_email = 'bruggeman.rene@gmail.com',
+    url = '',
+    description = 'A simple printer of nested lists',
+    )
