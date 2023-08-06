b152daf10dfc1e7bfd443bf0650609568e2c0a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:57 +0000

    Add package scikit-mlm/0.0.7/scikit-mlm-0.0.7.tar.gz

---
+++ packages/scikit-mlm/scikit-mlm-0.0.7.tar.gz/scikit-mlm-0.0.7/setup.py
@@ -0,0 +1,38 @@
+"""Setup file for skmlm."""
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    with open(filename, "r") as fh:
+        return fh.read()
+
+setup(
+    name='scikit-mlm',
+    version='0.0.7',
+    description='Minimal Learning Machine implementation using the scikit-learn API',
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url='https://github.com/omadson/scikit-mlm',
+    author='Madson Dias',
+    author_email='madsonddias@gmail.com',
+    license='BSD',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'numpy>=1.15.4',
+        'scipy>=1.1.0',
+        'scikit-learn>=0.21.0',
+        'pandas>=0.23.4',
+        'fuzzy-c-means>=0.0.3'
+        
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
