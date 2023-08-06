343119c4c5c7561d80e0a7076ec4bd47b907e6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:43 +0000

    Add package testpy9875/1.0.0/testpy9875-1.0.0.tar.gz

---
+++ packages/testpy9875/testpy9875-1.0.0.tar.gz/testpy9875-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup(
+    name='testpy9875',
+    author="zztech",
+    author_email="zz1036@qq.com",
+    version="1.0.0",
+    url="http://blog.yoqi.me",
+    packages=['testpy'],
+)
