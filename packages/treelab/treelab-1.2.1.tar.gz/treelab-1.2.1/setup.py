337cd550521ac21c34127442d67393dc39ed2aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:15 +0000

    Add package treelab/1.2.1/treelab-1.2.1.tar.gz

---
+++ packages/treelab/treelab-1.2.1.tar.gz/treelab-1.2.1/setup.py
@@ -0,0 +1,23 @@
+#coding = utf-8
+from setuptools import setup, find_namespace_packages
+
+# import os
+# base_dir = os.path.dirname(os.path.abspath(__file__))
+# requirements_file = open(os.path.join(base_dir, 'requirements.txt'))
+# requirements = requirements_file.read().splitlines()
+# """
+# python setup.py bdist_wheel
+# python setup.py sdist
+# twine upload dist/*
+
+setup(
+    name="treelab",
+    version="1.2.1",
+    package_dir={'': 'src'},
+    packages=find_namespace_packages(where='src'),
+    install_requires=['Rx>=3.0.0a3', 'grpcio>=1.20.0', 'grpcio-tools>=1.20.0', 'pandas>=0.24.2'],
+    python_requires='>=3',
+    license='BSD License',
+    url='https://github.com/caminerinc/treelab-pySDK',
+    long_description='The Treelab Python API provides an easy way to integrate Treelab with any external system. The API closely follows REST semantics, uses JSON to encode objects, and relies on standard HTTP codes to signal operation outcomes.'
+)
