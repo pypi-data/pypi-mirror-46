86137dcad78e027c56c629fa835147e212864d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:14 +0000

    Add package kafka-utils-netease/1.2/kafka-utils-netease-1.2.tar.gz

---
+++ packages/kafka-utils-netease/kafka-utils-netease-1.2.tar.gz/kafka-utils-netease-1.2/kafka_utils/_default_callbacks.py
@@ -0,0 +1,26 @@
+__all__ = [
+    'sender_suc_cb',
+    'sender_err_cb',
+    'recver_suc_cb',
+    'recver_err_cb',
+    'dummy_cb']
+
+
+def sender_suc_cb(msg):
+    pass
+
+
+def sender_err_cb(err):
+    pass
+
+
+def recver_suc_cb(msg):
+    pass
+
+
+def recver_err_cb(err):
+    pass
+
+
+def dummy_cb(val):
+    pass
