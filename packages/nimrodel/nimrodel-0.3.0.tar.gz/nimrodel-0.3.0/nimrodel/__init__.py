1bc225f5b490ba425aaf42dbe2e108cf671771a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:24 +0000

    Add package nimrodel/0.3.0/nimrodel-0.3.0.tar.gz

---
+++ packages/nimrodel/nimrodel-0.3.0.tar.gz/nimrodel-0.3.0/nimrodel/__init__.py
@@ -0,0 +1,14 @@
+from ._main import API
+
+name = "nimrodel"
+version = 0,3,0
+author = {
+	"name": "Johannes Krattenmacher",
+	"email": "python@krateng.dev",
+	"github": "krateng"
+}
+requires = [
+	"bottle",
+	"waitress",
+	"doreah"
+]
