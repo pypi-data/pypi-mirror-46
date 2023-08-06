231f936c2e7a868527a449e951244df1ea9fbfd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:53 +0000

    Add package schematizer/0.9.0/schematizer-0.9.0.tar.gz

---
+++ packages/schematizer/schematizer-0.9.0.tar.gz/schematizer-0.9.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open('README.rst') as fp:
+    long_description = fp.read()
+
+setup(
+    name='schematizer',
+    version='0.9.0',
+    description='A lightweight library for data marshalling/unmarshalling in Python',
+    long_description=long_description,
+    url='https://github.com/eb08a167/schematizer',
+    author='Andrew Kiyko',
+    author_email='eb08a167@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
