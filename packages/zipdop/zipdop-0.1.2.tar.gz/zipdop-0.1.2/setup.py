b6183b3211dad50d007103322f4c062bf04dc750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:39 +0000

    Add package zipdop/0.1.2/zipdop-0.1.2.tar.gz

---
+++ packages/zipdop/zipdop-0.1.2.tar.gz/zipdop-0.1.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='zipdop',
+    version='0.1.2',
+    description='Tool for downloading only one file from huge zip without downloading whole file',
+    author='Khylia Dmytro',
+    author_email='demonukraine96@gmail.com',
+    packages=['zipdop'],
+    scripts=['zipdop/bin/zipdop'],
+    install_requires=['httpie', 'requests'],
+    keywords=['zip', 'part', 'onefile', 'download'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
