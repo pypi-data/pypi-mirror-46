44feddcb43196f44103b95031661d9534ca6b2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:10 +0000

    Add package thoth-build-analysers/0.1.0/thoth-build-analysers-0.1.0.tar.gz

---
+++ packages/thoth-build-analysers/thoth-build-analysers-0.1.0.tar.gz/thoth-build-analysers-0.1.0/thoth/build_analysers/__about__.py
@@ -0,0 +1,24 @@
+"""About this package."""
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "thoth-build-analysers"
+__summary__ = "Build analysis library and tools to handle and process build logs."
+__uri__ = "https://github.com/thoth-station/build-analysers"
+
+__version__ = "0.1.0"
+
+__author__ = "Marek Cermak"
+__email__ = "macermak@redhat.com"
+
+__license__ = "GPLv3"
+__copyright__ = "Copyright 2019 {0}".format(__author__)
