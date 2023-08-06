d04e5810fad335dd8fc9ce0bc71c07ee9f47919b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:46 +0000

    Add package didery/0.2.0/didery-0.2.0.tar.gz

---
+++ packages/didery/didery-0.2.0.tar.gz/didery-0.2.0/src/didery/core/resting.py
@@ -0,0 +1,108 @@
+import falcon
+
+from ioflo.aid import getConsole
+from ioflo.aid import odict
+from ioflo.aio.http import Valet
+from ioflo.base import doify
+
+from didery import routing
+from didery.db import dbing
+
+console = getConsole()
+
+"""
+Usage pattern
+
+frame server
+    do didery server open at enter
+    do didery server service
+    do didery server close at exit
+"""
+
+
+@doify('DideryServerOpen', ioinits=odict(
+                                        valet="",
+                                        test="",
+                                        port=odict(ival=8080),
+                                        db=odict(ival=""),
+                                        mode=odict(ival=""),
+                                        ))
+def dideryServerOpen(self):
+    """
+    Setup and open a rest server
+
+    Ioinit attributes
+        valet is Valet instance (wsgi server)
+        port is server port
+
+    Context: enter
+
+    Example:
+        do didery server open at enter
+    """
+    port = int(self.port.value)
+    dbing.setupDbEnv(self.db.value, self.port.value, mode=self.mode.value)
+
+    app = falcon.API(middleware=[routing.CORSMiddleware()])
+    routing.loadEndPoints(app, store=self.store, mode=self.mode.value)
+
+    self.valet.value = Valet(
+                            port=port,
+                            bufsize=131072,
+                            wlog=None,
+                            store=self.store,
+                            app=app,
+                            timeout=0.5,
+                            )
+
+    console.terse("IP Address {}\n".format(self.valet.value.servant.ha))
+
+    result = self.valet.value.servant.reopen()
+
+    if not result:
+        console.terse("Error opening server '{0}' at '{1}'\n".format(
+            self.valet.name,
+            self.valet.value.servant.ha))
+        return
+
+    console.concise("Opened server '{0}' at '{1}'\n".format(
+        self.valet.name,
+        self.valet.value.servant.ha, ))
+
+
+@doify('DideryServerService', ioinits=odict(valet=""))
+def dideryServerService(self):
+    """
+    Service server given by valet
+
+    Ioinit attributes:
+        valet is a Valet instance
+
+    Context: recur
+
+    Example:
+        do didery server service
+    """
+    if self.valet.value:
+        self.valet.value.serviceAll()
+
+
+@doify('DideryServerClose', ioinits=odict(valet="",))
+def dideryServerClose(self):
+    """
+    Close server in valet
+
+    Ioinit attributes:
+        valet is a Valet instance
+
+    Context: exit
+
+    Example:
+        do didery server close at exit
+    """
+    if self.valet.value:
+        self.valet.value.servant.closeAll()
+
+        console.concise("Closed server '{0}' at '{1}'\n".format(
+                            self.valet.name,
+                            self.valet.value.servant.eha))
---
+++ packages/didery/didery-0.2.0.tar.gz/didery-0.2.0/src/didery/help/helping.py
@@ -0,0 +1,200 @@
+from collections import OrderedDict as ODict
+import falcon
+import base64
+import base58
+import tempfile
+import os
+import shutil
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+
+def parseSignatureHeader(signature):
+    """
+    Returns ODict of fields and values parsed from signature
+    which is the value portion of a Signature header
+
+    Signature header has format:
+
+        Signature: headervalue
+
+    Headervalue:
+        tag = "signature"
+
+    or
+
+        tag = "signature"; tag = "signature"  ...
+
+    where tag is the name of a field in the body of the request whose value
+    is a DID from which the public key for the signature can be obtained
+    If the same tag appears multiple times then only the last occurrence is returned
+
+    each signature value is a doubly quoted string that contains the actual signature
+    in Base64 url safe format. By default the signatures are EdDSA (Ed25519)
+    which are 88 characters long (with two trailing pad bytes) that represent
+    64 byte EdDSA signatures
+
+    An option tag name = "kind" with values "EdDSA"  "Ed25519" may be present
+    that specifies the type of signature. All signatures within the header
+    must be of the same kind.
+
+    The two tag fields currently supported are "did" and "signer"
+
+
+    """
+    sigs = ODict()
+    if signature:
+        clauses = signature.split(";")
+        for clause in clauses:
+            clause = clause.strip()
+            if not clause:
+                continue
+            try:
+                tag, value = clause.split("=", maxsplit=1)
+            except ValueError as ex:
+                continue
+            tag = tag.strip()
+            if not tag:
+                continue
+            value = value.strip()
+            if not value.startswith('"') or not value.endswith('"') or len(value) < 3:
+                continue
+            value = value[1:-1]
+            value = value.strip()
+            sigs[tag] = value
+
+    return sigs
+
+
+def parseReqBody(req):
+    """
+    :param req: Falcon Request object
+    """
+    try:
+        raw_json = req.stream.read(req.content_length or 0)
+    except Exception as ex:
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Request Error',
+                               'Error reading request body.')
+
+    try:
+        result_json = json.loads(raw_json, encoding='utf-8')
+    except ValueError:
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Malformed JSON',
+                               'Could not decode the request body. The '
+                               'JSON was incorrect.')
+
+    req.body = result_json
+    return raw_json
+
+
+def validateRequiredFields(required, resource):
+    """
+    Validate that all required fields are present in resource
+    :param required: list of required fields
+    :param resource: dict to be checked
+    """
+    for req in required:
+        if req not in resource:
+            raise falcon.HTTPError(falcon.HTTP_400,
+                                   'Missing Required Field',
+                                   'Request must contain {} field.'.format(req))
+
+
+def bytesToStr64u(bytes):
+    """
+    Convert and return bytes string to unicode base64 url-file safe version
+    """
+    return base64.urlsafe_b64encode(bytes).decode("utf-8")
+
+
+def str64uToBytes(str64u):
+    """
+    Convert and return unicode base64 url-file safe string to bytes version
+    """
+    return base64.urlsafe_b64decode(str64u.encode("utf-8"))
+
+
+def bytesToStr58(bytes):
+    """
+    Convert and return bytes string to unicode base58 version
+    """
+    return base58.b58encode(bytes).decode("utf-8")
+
+
+def str58ToBytes(str58):
+    """
+    Convert and return unicode base58 string to bytes version
+    """
+    return base58.b58decode(str58.encode("utf-8"))
+
+
+def makeDid(vk, method="dad"):
+    """
+    Create and return Did from bytes vk.
+    vk is 32 byte verifier key from EdDSA (Ed25519) keypair
+    """
+    # convert verkey to jsonable unicode string of base64 url-file safe
+    vk64u = base64.urlsafe_b64encode(vk).decode("utf-8")
+    did = "did:{}:{}".format(method, vk64u)
+    return did
+
+
+def parseQString(req, resp, resource, params):
+    req.offset = 0
+    req.limit = 10
+
+    if req.query_string:
+        queries = req.query_string.split('&')
+        for query in queries:
+            key, val = qStringValidation(query)
+            if key == 'offset':
+                req.offset = val
+            if key == 'limit':
+                req.limit = val
+
+
+def qStringValidation(query):
+    keyval = query.split('=')
+
+    if len(keyval) != 2:
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Malformed Query String',
+                               'url query string missing value(s).')
+
+    key = keyval[0]
+    val = keyval[1]
+
+    try:
+        val = int(val)
+    except ValueError as ex:
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Malformed Query String',
+                               'url query string value must be a number.')
+
+    if val < 0:
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Malformed Query String',
+                               'url query string value must be a positive number.')
+
+    return key, val
+
+
+def setupTmpBaseDir():
+    """
+    Create temporary directory
+    """
+    return tempfile.mkdtemp(prefix="didery",  suffix="test", dir="/tmp")
+
+
+def cleanupTmpBaseDir(baseDirPath):
+    """
+    Remove temporary root of baseDirPath
+    Ascend tree to find temporary root directory
+    """
+    if os.path.exists(baseDirPath):
+        shutil.rmtree(baseDirPath)
