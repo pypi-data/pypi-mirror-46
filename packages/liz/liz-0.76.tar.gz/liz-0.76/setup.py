3bf05bafcddee6f2a1e5796ce626301d373aba6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:48 +0000

    Add package liz/0.76/liz-0.76.tar.gz

---
+++ packages/liz/liz-0.76.tar.gz/liz-0.76/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'liz',
+  packages = ['liz'],
+  version = '0.76',
+  description = 'A tool for creating websites',
+  author = 'Alex Stachowiak',
+  author_email = 'liz@stachowiak.email',
+  url = 'https://github.com/stakodiak/liz', # use the URL to the github repo
+  keywords = ['static', 'site', 'generator'],
+  install_requires = ['jinja2'],
+  entry_points={
+    'console_scripts': [
+      'liz = liz.main:main',
+    ],
+  },
+)
