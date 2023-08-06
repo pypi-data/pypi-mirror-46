bc4be7578245eaa8f3007d253d584b304e34d1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:31 +0000

    Add package django-campaign/0.4.1/django-campaign-0.4.1.tar.gz

---
+++ packages/django-campaign/django-campaign-0.4.1.tar.gz/django-campaign-0.4.1/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+    name='django-campaign',
+    version=__import__('campaign').__version__,
+    description='A basic newsletter app for the Django webframework',
+    long_description=open('README.rst').read(),
+    author='Arne Brodowski',
+    author_email='arne@rcs4u.de',
+    license="BSD",
+    url='https://github.com/arneb/django-campaign/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    packages = (
+        'campaign',
+        'campaign.backends',
+        'campaign.migrations',
+        'campaign.management',
+        'campaign.management.commands',
+    ),
+    package_data = {
+        'campaign':
+            ['templates/admin/campaign/campaign/*.html',
+             'templates/campaign/*.html'],
+    }
+)
