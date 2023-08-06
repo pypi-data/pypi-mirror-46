eca4648dadce4661c87cf75f021c97d69bf52b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:05 +0000

    Add package catastropy/0.0dev/catastropy-0.0dev.tar.gz

---
+++ packages/catastropy/catastropy-0.0dev.tar.gz/catastropy-0.0dev/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='catastropy',
+      version='0.0dev',
+      description='(cat)astronomy',
+      author='Brigitta Sipocz',
+      )
