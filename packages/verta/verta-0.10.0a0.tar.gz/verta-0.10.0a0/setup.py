c2c35cb885928ff26ccdfbfb4e98bfc7a105451c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:52 +0000

    Add package verta/0.10.0a0/verta-0.10.0a0.tar.gz

---
+++ packages/verta/verta-0.10.0a0.tar.gz/verta-0.10.0a0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="verta",
+    version="0.10.0-alpha",
+    maintainer="Michael Liu",
+    maintainer_email="miliu@verta.ai",
+    description="Python client for interfacing with ModelDB and the Verta platform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.verta.ai/",
+    packages=[
+        "verta",
+        "verta._protos.public.modeldb",
+    ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    install_requires=[
+        "cloudpickle",
+        "googleapis-common-protos>=1.5",
+        "grpcio>=1.16",
+        "pathlib2>=2.1",
+        "pillow>=5.2, <7.0",
+        "protobuf>=3.6",
+        "requests>=2.21",
+        "six>=1.12",
+    ],
+)
