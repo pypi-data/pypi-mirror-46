b0663c72eac23c7be9be55b8b58f07dd6f34d1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:29 +0000

    Add package distributions-me/0.1/distributions_me-0.1.tar.gz

---
+++ packages/distributions-me/distributions_me-0.1.tar.gz/distributions_me-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_me',
+      version='0.1',
+      description='Gaussian distributions',
+      author='Maria',
+      packages=['distributions_me'],
+      zip_safe=False)
