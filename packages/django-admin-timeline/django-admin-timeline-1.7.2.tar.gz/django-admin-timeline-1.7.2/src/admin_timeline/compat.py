cc6352ed2d2fd5bf2ed4a4aac94fc2c03eac14cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:13 +0000

    Add package django-admin-timeline/1.7.2/django-admin-timeline-1.7.2.tar.gz

---
+++ packages/django-admin-timeline/django-admin-timeline-1.7.2.tar.gz/django-admin-timeline-1.7.2/src/admin_timeline/compat.py
@@ -0,0 +1,18 @@
+from nine.versions import DJANGO_LTE_1_4
+
+__title__ = 'admin_timeline.compat'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2013-2019 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = (
+    'TEMPLATE_NAME',
+    'TEMPLATE_NAME_AJAX',
+)
+
+
+TEMPLATE_NAME = "admin_timeline/timeline.html"
+TEMPLATE_NAME_AJAX = "admin_timeline/timeline_ajax.html"
+
+if DJANGO_LTE_1_4:
+    TEMPLATE_NAME = "admin_timeline/django_lte14/timeline.html"
+    TEMPLATE_NAME_AJAX = "admin_timeline/django_lte14/timeline_ajax.html"
