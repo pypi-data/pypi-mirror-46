c30d0dfb7775dfb120062f0376ac2c247227e325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:46 +0000

    Add package pflogger/0.0.1/pflogger-0.0.1.tar.gz

---
+++ packages/pflogger/pflogger-0.0.1.tar.gz/pflogger-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="pflogger",
+    version='0.0.1',
+    description="Configuration of python logging handler for Perfee.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/perfeelab/pflogger",
+    author='ClaireHuang',
+    author_email='clairehf@163.com',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
