30ab6305d224dd29a8512ce79993067da17b2638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:08 +0000

    Add package pugsql/0.1.0/pugsql-0.1.0.tar.gz

---
+++ packages/pugsql/pugsql-0.1.0.tar.gz/pugsql-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['pugsql']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['sqlalchemy>=1.3,<2.0']
+
+setup_kwargs = {
+    'name': 'pugsql',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Dan McKinley',
+    'author_email': 'mcfunley@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
