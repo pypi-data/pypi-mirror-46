981f9c076cb51043ae6721d2d6de20e5ec5d4067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:00 +0000

    Add package mbpprice/0.0.3/mbpprice-0.0.3.tar.gz

---
+++ packages/mbpprice/mbpprice-0.0.3.tar.gz/mbpprice-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mbpprice',
+    version='0.0.3',
+    description="second hand macbook pro price in Taiwan.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    url="https://github.com/allanbian1017/mbpprice",
+    license='MIT',
+    author="Allan Bian",
+    install_requires=[
+        'pyquery',
+        'requests'
+    ],
+    python_requires='>=3.5'
+)
