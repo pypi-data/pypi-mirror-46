390dc7a03048ab32793e6d9030514aa1f5afac20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:58 +0000

    Add package image-layer-decomposition/0.2/image_layer_decomposition-0.2.tar.gz

---
+++ packages/image-layer-decomposition/image_layer_decomposition-0.2.tar.gz/image_layer_decomposition-0.2/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+setup(
+  name = 'image_layer_decomposition',
+  packages = ['image_layer_decomposition'],
+  version = '0.2',
+  license='MIT', 
+  description = 'Color image layer decomposition',
+  author = 'Karolina Tretyakova',
+  author_email = 'tretyakovakarolina@gmail.com',
+  url = 'https://github.com/katretyakova', 
+  download_url = 'https://github.com/katretyakova/image-layer-decomposition/archive/v_02.tar.gz',
+  keywords = ['image', 'decomposition'],
+  install_requires=[
+          'numpy',
+          'scipy',
+          'time',
+          'warnings',
+          'json',
+          'PIL',
+        'cvxopt',
+        'math',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
