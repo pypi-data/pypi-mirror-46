cbfe1e8daa211ef8601d7765bced878ba2ce3f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:27 +0000

    Add package b5/1.1.4/b5-1.1.4.tar.gz

---
+++ packages/b5/b5-1.1.4.tar.gz/b5-1.1.4/b5/init.py
@@ -0,0 +1,85 @@
+import argparse
+import os
+import shutil
+import termcolor
+import subprocess
+import sys
+import re
+
+from .exceptions import B5ExecutionError
+
+
+NON_URL_SKELETON = re.compile('^[A-Za-z0-9_-]+$')
+
+
+def _run_cmd(cmd, error='Command execution failed, see above'):
+    try:
+        subprocess.run(
+            cmd,
+            shell=False,
+            check=True,
+        )
+    except subprocess.CalledProcessError:
+        termcolor.cprint(error, color='red')
+        sys.exit(1)
+
+
+def main():
+    try:
+        parser = argparse.ArgumentParser(
+            prog='b5-init',
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            description='b5-init might be used to setup new projects',
+        )
+        parser.add_argument(
+            '-s', '--skeleton', nargs='?',
+            dest='skeleton', default='basic'
+        )
+        parser.add_argument(
+            '-b', '--branch', nargs='?',
+            dest='branch',
+        )
+        parser.add_argument(
+            dest='path'
+        )
+        args = parser.parse_args()
+
+        skeleton = args.skeleton
+        branch = args.branch
+        path = args.path
+
+        if NON_URL_SKELETON.match(skeleton):
+            skeleton_url = 'https://git.team23.de/build/b5-skel-{skeleton}.git'.format(skeleton=skeleton)
+        else:
+            skeleton_url = skeleton
+
+        full_path = os.path.realpath(os.path.join(os.getcwd(), path))
+        os.makedirs(full_path, exist_ok=True)
+
+        if len(os.listdir(full_path)) > 0:
+            raise B5ExecutionError('Cannot init an existing directory if not empty')
+
+        _run_cmd(['git', 'clone', skeleton_url, full_path], 'Could not clone skeleton repository, see above')
+        os.chdir(full_path)
+        if not branch is None:
+            _run_cmd(['git', 'checkout', branch], 'Could not checkout required branch, see above')
+
+        shutil.rmtree(os.path.join(full_path, '.git'))
+        _run_cmd(['git', 'init', '.'])
+        init_path = os.path.join(full_path, 'init')
+        if os.path.exists(init_path) and os.path.exists(os.path.join(init_path, 'Taskfile')):
+            _run_cmd(['b5',
+                      '--quiet',
+                      '--run-path', 'init',
+                      '--config', 'config.yml',
+                      '--config', 'config.init.yml',
+                      '--config', 'config.local.yml',
+                      'project:init'])
+            shutil.rmtree(init_path)
+        # _run_cmd(['git', 'add', '-A'])
+        termcolor.cprint('Successful initialized {path}'.format(path=path), 'green')
+        termcolor.cprint('  skeleton used: {skeleton_url}'.format(skeleton_url=skeleton_url), 'green')
+        termcolor.cprint('  project path: {full_path}'.format(full_path=full_path), 'green')
+    except B5ExecutionError as e:
+        termcolor.cprint(str(e), 'red')
+        sys.exit(1)
---
+++ packages/b5/b5-1.1.4.tar.gz/b5-1.1.4/b5/lib/state.py
@@ -0,0 +1,56 @@
+import yaml
+import tempfile
+import os
+
+
+class StoredState(object):
+    def __init__(self, state):
+        self.state = state
+        if not self.state.stored_name is None:
+            raise RuntimeError('You may only store the state once')
+
+        self.fh = tempfile.NamedTemporaryFile(suffix='b5-state', mode='w', encoding='utf-8', delete=False)
+        self.state.stored_name = self.name
+        yaml.dump({
+            key: getattr(self.state, key)
+            for key in state.KEYS
+        }, self.fh, default_flow_style=False)
+        self.fh.close()
+
+    def close(self):
+        os.unlink(self.fh.name)
+        self.state.stored_name = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    @property
+    def name(self):
+        return self.fh.name
+
+
+class State(object):
+    KEYS = ('project_path', 'run_path', 'taskfiles', 'configfiles', 'config', 'args', 'stored_name')
+
+    taskfiles = []
+    configfiles = []
+    args = {}
+
+    def __init__(self, **kwargs):
+        for key in self.KEYS:
+            if not hasattr(self, key):
+                setattr(self, key, None)
+        for key in kwargs:
+            if not key in self.KEYS:
+                raise RuntimeError('Key %s is not a valid state attribute' % key)
+            setattr(self, key, kwargs[key])
+
+    def stored(self):
+        return StoredState(self)
+
+    @classmethod
+    def load(cls, fh):
+        return cls(**yaml.safe_load(fh))
