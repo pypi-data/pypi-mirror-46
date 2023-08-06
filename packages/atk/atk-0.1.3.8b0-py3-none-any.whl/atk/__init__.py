1b9df8edef255d0d06c0f67472755b2f4592745c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:03 +0000

    Add package atk/0.1.3.8b0/atk-0.1.3.8b0-py3-none-any.whl

---
+++ packages/atk/atk-0.1.3.8b0-py3-none-any.whl/atk/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+
+from .Docker import Agility_Docker
+from .DockerError import *
+
+name = "atk-(Automation)-(Terminal)-(Kubernetes)"
+version = '0.1.3.8b'
+author = 'haitanghuadeng'
+project = 'https://github.com/haitanghuadeng/havedocker'
+description = "Automate the administration of docker and kubernetes"
+
