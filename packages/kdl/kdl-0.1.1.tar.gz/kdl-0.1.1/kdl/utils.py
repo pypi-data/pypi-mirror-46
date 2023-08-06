1a5b1a80f30582a1675b401d99994b3740078571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:59 +0000

    Add package kdl/0.1.1/kdl-0.1.1.tar.gz

---
+++ packages/kdl/kdl-0.1.1.tar.gz/kdl-0.1.1/kdl/utils.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from functools import wraps
+
+
+def signature_required(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if not self.auth.apiKey:
+            raise NameError("apiKey is required for signature")
+        return func(self, *args, **kwargs)
+    return wrapper
+
+
+class OpsOrderLevel(object):
+    """开放代理订单级别"""
+    NORMAL = "dev"  # 普通
+    VIP = "dev"  # vip
+    SVIP = "svip"  # svip
+    PRO = "ent"  # 专业版
