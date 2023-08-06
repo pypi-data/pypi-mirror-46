141b036ff78f0d871a52565afdabf75bc90e4491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:15 +0000

    Add package almetro/1.0.5/almetro-1.0.5.tar.gz

---
+++ packages/almetro/almetro-1.0.5.tar.gz/almetro-1.0.5/almetro/metro.py
@@ -0,0 +1,71 @@
+import almetro.chart as chart
+import almetro.table as table
+from almetro.complexity import cn
+from copy import deepcopy
+import timeit
+
+
+class Metro:
+    def __init__(self, complexity):
+        self.__start = timeit.default_timer()
+        self.__instances = {}
+        self.__elapsed_time = 0
+        self.__experimental = {}
+        self.__theoretical = {}
+        self.__ratio = {}
+        self.__complexity = complexity
+        self.__processed = False
+
+    @property
+    def elapsed_time(self):
+        return self.__elapsed_time
+
+    @property
+    def experimental(self):
+        return deepcopy(self.__experimental)
+
+    @property
+    def theoretical(self):
+        return deepcopy(self.__theoretical)
+
+    @property
+    def ratio(self):
+        return deepcopy(self.__ratio)
+
+    @property
+    def complexity(self):
+        return self.__complexity
+
+    @property
+    def processed(self):
+        return self.__processed
+
+    def register(self, instance, timestats):
+        self.__elapsed_time = timeit.default_timer() - self.__start
+        self.__instances[instance.size] = instance
+        self.__experimental[instance.size] = min(timestats)
+        self.__processed = False
+
+    def chart(self):
+        self.process()
+        return chart.build_chart(self)
+
+    def table(self):
+        self.process()
+        return table.build_table(self)
+
+    def process(self):
+        if not self.__processed:
+            if not self.__complexity:
+                raise ValueError('complexity must be provided')
+            self.__theoretical = {}
+            self.__ratio = {}
+            for size, instance in self.__instances.items():
+                t_fn = self.__experimental[size]
+                self.__theoretical[size] = self.__complexity.fn(instance.size)
+                self.__ratio[size] = t_fn / max(self.__theoretical[size], 0.1)
+            self.__processed = True
+
+    @staticmethod
+    def new(complexity=cn):
+        return Metro(complexity)
---
+++ packages/almetro/almetro-1.0.5.tar.gz/almetro-1.0.5/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+project = "almetro"
+version = "1.0.5"
+
+setup(
+    name=project,
+    version=version,
+    license="Apache License",
+    description="A python library to measure algorithms execution time and compare with its theoretical complexity",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Arnour Sabino",
+    author_email="arnour.sabino@gmail.com",
+    url="https://github.com/arnour/almetro",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "matplotlib==3.0.3",
+        "tabulate==0.8.3"
+    ],
+    setup_requires=[
+        "nose>=1.3.7",
+    ],
+    dependency_links=[
+    ],
+    entry_points={
+    },
+    extras_require={
+        "test": [
+            "flake8>=3.7.7",
+            "flake8-print>=3.1.0",
+            "coverage>=4.5.3",
+            "PyHamcrest>=1.9.0",
+        ],
+    },
+)
