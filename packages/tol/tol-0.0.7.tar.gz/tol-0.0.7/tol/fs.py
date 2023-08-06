70c285002e4dd634165caf9dd76434e69abd290e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:44 +0000

    Add package tol/0.0.7/tol-0.0.7.tar.gz

---
+++ packages/tol/tol-0.0.7.tar.gz/tol-0.0.7/tol/fs.py
@@ -0,0 +1,8 @@
+def mkdir(path, mode=None):
+    return 'mkdir --parents {mode} {path}'.format(
+        path=path,
+        mode='--mode %s' % mode if mode else '')
+
+
+def chown(user, path):
+    return 'chown --recursive %s:%s %s' % (user, user, path)
