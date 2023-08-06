015262631b990483e60453aeac4e2232bdc05041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:46 +0000

    Add package scraperex/0.0.13/scraperex-0.0.13.tar.gz

---
+++ packages/scraperex/scraperex-0.0.13.tar.gz/scraperex-0.0.13/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='scraperex',
+    version='0.0.13',
+    description='Web scraper using dynamic proxy and user agent.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/userforce/scraper',
+    author='Tudor Corcimar',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    license='MIT',
+    keywords='scraper proxy user-agent',
+    packages=['scraperex'],
+    package_dir={'scraperex': 'scraperex'},
+    package_data={'': ['LICENSE'], 'scraperex': ['data/*.dat'] },
+    python_requires='>=3.7',
+    install_requires=['requests','fake_useragent']
+)
