e7ef8626a8987e428a4c95ba618cfe5f9a2055e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:06 +0000

    Add package crackle-automation-api-client/0.2/crackle-automation-api-client-0.2.tar.gz

---
+++ packages/crackle-automation-api-client/crackle-automation-api-client-0.2.tar.gz/crackle-automation-api-client-0.2/crackle_automation_api_client/__init__.py
@@ -0,0 +1,6 @@
+""" Analytics Reporter API Wrapper setup """
+from crackle_automation_api_client.missing_credentials_error import MissingCredentialsError
+from crackle_automation_api_client.session import SESSION
+
+
+__all__ = ['MissingCredentialsError', 'SESSION']
