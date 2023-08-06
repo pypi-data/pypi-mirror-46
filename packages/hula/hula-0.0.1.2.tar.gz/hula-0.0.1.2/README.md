9877a3ce915b4bcb8dc7ce10fede2e8de714fb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:21 +0000

    Add package hula/0.0.1.2/hula-0.0.1.2.tar.gz

---
+++ packages/hula/hula-0.0.1.2.tar.gz/hula-0.0.1.2/README.md
@@ -0,0 +1,15 @@
+# hula-0.0.1.2
+
+### What is hula?
+
+Hula is a set of unconventional machine learning modules. Hula is designed to unify the desired aspects of most machine learning capabilities out there. 
+
+Within hula are a few machine learning algorithms which I've coined as the following:
+
+-  Deep Recursive Learning
+-  Reinforcement via Similarity
+-  Comprehensive Learning
+
+### Documentation
+
+For Hula's documentation visit https://robert-bassett-coder.gitbook.io/workspace/
---
+++ packages/hula/hula-0.0.1.2.tar.gz/hula-0.0.1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hula",
+    version="0.0.1.2",
+    author="Robert Bassett",
+    author_email="robert.bassett.coder@gmail.com",
+    description="Experimental learning methods. The first appearance of these methods.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy'],
+    url='https://robert-bassett-coder.gitbook.io/workspace/',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+
+    ],
+)
