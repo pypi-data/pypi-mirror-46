d59d90cbd2c7c2584e461d9c8a08602d5bd135cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:49 +0000

    Add package simpler-devops-api/0.2.20190514.2/simpler_devops_api-0.2.20190514.2-py3-none-any.whl

---
+++ packages/simpler-devops-api/simpler_devops_api-0.2.20190514.2-py3-none-any.whl/simpler_devops_api/__init__.py
@@ -0,0 +1,11 @@
+from .base import *
+from .devops_config import *
+from .repos import *
+from .policy import *
+from .policybuilder import *
+from .policy_types import *
+from .projects import *
+
+ 
+name = "simpler_devops_api"
+
