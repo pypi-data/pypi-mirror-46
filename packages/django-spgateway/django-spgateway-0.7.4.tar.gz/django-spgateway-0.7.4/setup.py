3c3a82fc9b8cd1d5a19d4099fdc7e42719a481bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:20 +0000

    Add package django-spgateway/0.7.4/django-spgateway-0.7.4.tar.gz

---
+++ packages/django-spgateway/django-spgateway-0.7.4.tar.gz/django-spgateway-0.7.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+__version__ = "0.7.4"
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name="django-spgateway",
+    version=__version__,
+    description='Django support for Spgateway',
+    keywords="django, spgateway",
+    author="CJLTSOD <github.tsod@tsod.idv.tw>",
+    author_email="github.tsod@tsod.idv.tw",
+    url="https://github.com/cjltsod/django-spgateway",
+    license="MIT",
+    packages=["spgateway"],
+    include_package_data=True,
+    install_requires=["django>=1.10", "pycrypto>=2.6.1"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Framework :: Django :: 2.1",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Web Environment",
+    ],
+    long_description=long_description,
+)
