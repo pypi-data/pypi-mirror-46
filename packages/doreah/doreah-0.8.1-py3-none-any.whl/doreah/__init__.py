ff4c9ece59e53ce223cf3bb65344f86d285c45e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:52 +0000

    Add package doreah/0.8.1/doreah-0.8.1-py3-none-any.whl

---
+++ packages/doreah/doreah-0.8.1-py3-none-any.whl/doreah/__init__.py
@@ -0,0 +1,30 @@
+name = "doreah"
+version = 0,8,1
+
+
+__all__ = [
+	"caching",
+	"logging",
+	"persistence",
+	"pyhp",
+	"regular",
+	"settings",
+	"timing",
+	"tsv"
+
+]
+
+
+
+
+# useful things for everyone
+
+
+def deprecated(func):
+	"""Function decorator to deprecate a function"""
+
+	def newfunc(*args,**kwargs):
+		print("\033[93m" + "Function " + func.__name__ + " is deprecated!" + "\033[0m")
+		return func(*args,**kwargs)
+
+	return newfunc
