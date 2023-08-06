a535b642e978cffba63c74d0f91080955522435c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:08 +0000

    Add package dash-file-dialog-components/0.0.1/dash_file_dialog_components-0.0.1.tar.gz

---
+++ packages/dash-file-dialog-components/dash_file_dialog_components-0.0.1.tar.gz/dash_file_dialog_components-0.0.1/dash_file_dialog_components/DashFileDialogComponents.py
@@ -0,0 +1,46 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class DashFileDialogComponents(Component):
+    """A DashFileDialogComponents component.
+ExampleComponent is an example component.
+It takes a property, `label`, and
+displays it.
+It renders an input with the property `value`
+which is editable by the user.
+
+Keyword arguments:
+- id (string; optional): The ID used to identify this component in Dash callbacks
+- overrideProps (optional): . overrideProps has the following type: dict containing keys 'onChange', 'onError', 'maxSize', 'extensions', 'validateContent', 'style'.
+Those keys have the following types:
+  - onChange (required)
+  - onError (optional)
+  - maxSize (number; optional)
+  - extensions (list; optional)
+  - validateContent (optional)
+  - style (dict; optional)
+- src (string; required)
+- style (dict with strings as keys and values of type string; optional)
+- maxSize (number; optional)
+- extensions (list; optional)"""
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, overrideProps=Component.UNDEFINED, src=Component.REQUIRED, style=Component.UNDEFINED, onChange=Component.REQUIRED, maxSize=Component.UNDEFINED, extensions=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'overrideProps', 'src', 'style', 'maxSize', 'extensions']
+        self._type = 'DashFileDialogComponents'
+        self._namespace = 'dash_file_dialog_components'
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'overrideProps', 'src', 'style', 'maxSize', 'extensions']
+        self.available_wildcard_properties =            []
+
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        for k in ['src', 'onChange']:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(DashFileDialogComponents, self).__init__(**args)
