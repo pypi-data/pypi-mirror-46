6ecd7dbdc0117c9c6417c806d148b66c1ebff9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:38 +0000

    Add package collective.documentgenerator/3.7/collective.documentgenerator-3.7.tar.gz

---
+++ packages/collective.documentgenerator/collective.documentgenerator-3.7.tar.gz/collective.documentgenerator-3.7/src/collective/documentgenerator/config.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+from Products.CMFPlone.utils import safe_unicode
+
+import os
+
+
+ODS_FORMATS = (('ods', 'LibreOffice Calc (.ods)'),
+               ('xls', 'Microsoft Excel (.xls)'),)
+
+ODT_FORMATS = (('odt', 'LibreOffice Writer (.odt)'),
+               ('doc', 'Microsoft Word (.doc)'),
+               ('docx', 'Microsoft Word XML (.docx)'),
+               ('rtf', 'Rich Text Format (.RTF)'),)
+
+NEUTRAL_FORMATS = (('pdf', 'Adobe PDF (.pdf)'),)
+
+POD_FORMATS = ODS_FORMATS + ODT_FORMATS + NEUTRAL_FORMATS
+
+DEFAULT_PYTHON_UNO_PATH = u'/usr/bin/python'
+
+VIEWLET_TYPES = ['PODTemplate', 'ConfigurablePODTemplate']
+
+HAS_PLONE_5 = api.env.plone_version().startswith('5')
+
+
+def get_uno_path():
+    return api.portal.get_registry_record(
+        'collective.documentgenerator.browser.controlpanel.IDocumentGeneratorControlPanelSchema.uno_path'
+    )
+
+
+def get_oo_port():
+    return api.portal.get_registry_record(
+        'collective.documentgenerator.browser.controlpanel.IDocumentGeneratorControlPanelSchema.oo_port'
+    )
+
+
+def get_column_modifier():
+    return api.portal.get_registry_record(
+        'collective.documentgenerator.browser.controlpanel.IDocumentGeneratorControlPanelSchema.column_modifier'
+    )
+
+
+def get_raiseOnError_for_non_managers():
+    return api.portal.get_registry_record(
+        'collective.documentgenerator.browser.controlpanel.'
+        'IDocumentGeneratorControlPanelSchema.raiseOnError_for_non_managers'
+    )
+
+
+def set_oo_port():
+    """ Get environment value in buildout to define port """
+    oo_port = os.getenv('OO_PORT', None)
+    if oo_port:
+        api.portal.set_registry_record('collective.documentgenerator.browser.controlpanel.'
+                                       'IDocumentGeneratorControlPanelSchema.oo_port', int(oo_port))
+
+
+def set_uno_path():
+    """ Get environment value in buildout to define path """
+    python_uno = os.getenv('PYTHON_UNO', None)
+    if python_uno:
+        api.portal.set_registry_record('collective.documentgenerator.browser.controlpanel.'
+                                       'IDocumentGeneratorControlPanelSchema.uno_path', safe_unicode(python_uno))
+
+
+def set_column_modifier(value):
+    api.portal.set_registry_record(
+        'collective.documentgenerator.browser.controlpanel.IDocumentGeneratorControlPanelSchema.column_modifier',
+        value
+    )
+
+
+def set_raiseOnError_for_non_managers(value):
+    api.portal.set_registry_record(
+        'collective.documentgenerator.browser.controlpanel.'
+        'IDocumentGeneratorControlPanelSchema.raiseOnError_for_non_managers',
+        value)
