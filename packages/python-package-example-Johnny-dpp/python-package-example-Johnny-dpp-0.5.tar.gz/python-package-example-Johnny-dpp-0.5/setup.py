74fb9681944c3bc87b06d834a5f30cec49f03b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:08 +0000

    Add package python-package-example-Johnny-dpp/0.5/python-package-example-Johnny-dpp-0.5.tar.gz

---
+++ packages/python-package-example-Johnny-dpp/python-package-example-Johnny-dpp-0.5.tar.gz/python-package-example-Johnny-dpp-0.5/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python-package-example-Johnny-dpp',
+    version='0.5',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An example python package',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
+    url='https://github.com/Johnny205/simplePythonLibrary',
+    author='Mariusz Grabarczyk',
+    author_email='grabmar97@gmaiil.com'
+)
+
+# python setup.py sdist upload
+# python setup.py sdist
