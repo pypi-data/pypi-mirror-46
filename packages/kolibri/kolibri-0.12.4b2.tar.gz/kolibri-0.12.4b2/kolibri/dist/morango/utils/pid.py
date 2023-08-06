56c032e750d9c45293793c01845c397ca2d648d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:11 +0000

    Add package kolibri/0.12.4b2/kolibri-0.12.4b2.tar.gz

---
+++ packages/kolibri/kolibri-0.12.4b2.tar.gz/kolibri-0.12.4b2/kolibri/dist/morango/utils/pid.py
@@ -0,0 +1,37 @@
+import os
+import platform
+import ctypes
+
+# GetExitCodeProcess uses a special exit code to indicate that the process is
+# still running.
+_STILL_ACTIVE = 259
+
+def is_pid_running(pid):
+    return (_is_pid_running_on_windows(pid) if platform.system() == "Windows"
+        else _is_pid_running_on_unix(pid))
+
+def _is_pid_running_on_unix(pid):
+    try:
+        os.kill(pid, 0)
+    except OSError:
+        return False
+    return True
+
+def _is_pid_running_on_windows(pid):
+    import ctypes.wintypes
+
+    kernel32 = ctypes.windll.kernel32
+    handle = kernel32.OpenProcess(1, 0, pid)
+    if handle == 0:
+        return False
+
+    # If the process exited recently, a pid may still exist for the handle.
+    # So, check if we can get the exit code.
+    exit_code = ctypes.wintypes.DWORD()
+    is_running = (
+        kernel32.GetExitCodeProcess(handle, ctypes.byref(exit_code)) == 0)
+    kernel32.CloseHandle(handle)
+
+    # See if we couldn't get the exit code or the exit code indicates that the
+    # process is still running.
+    return is_running or exit_code.value == _STILL_ACTIVE
