993475017a035e85e6145ee998df0b8a3e925006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:28 +0000

    Add package firex-flame/0.1/firex_flame-0.1.tar.gz

---
+++ packages/firex-flame/firex_flame-0.1.tar.gz/firex_flame-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name='firex_flame',
+      version="0.1",
+      description='Core firex libraries',
+      url='https://github.com/FireXStuff/firex-flame',
+      author='Core FireX Team',
+      author_email='firex-dev@gmail.com',
+      license='BSD-3-Clause',
+      packages=find_packages(),
+      zip_safe=True,
+      install_requires=[
+            "flask",
+            "Flask-AutoIndex",
+      ],
+      package_data={
+        'firex_flame': ['ui/*.html', 'ui/js/*.js', 'ui/img/*', 'ui/css/*.css'],
+      },
+      entry_points={
+          'console_scripts': ['firex_flame = firex_flame.__main__:main', ],
+          'firex_tracking_service': ['flame_launcher = firex_flame.launcher:FlameLauncher', ]
+      },)
