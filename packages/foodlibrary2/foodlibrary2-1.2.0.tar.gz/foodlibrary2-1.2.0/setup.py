1fdaea81b95b7a7db003cfc814ed27c44e591d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:39 +0000

    Add package foodlibrary2/1.2.0/foodlibrary2-1.2.0.tar.gz

---
+++ packages/foodlibrary2/foodlibrary2-1.2.0.tar.gz/foodlibrary2-1.2.0/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from distutils.core import setup
+
+setup(name='foodlibrary2',
+	version='1.2.0',
+	author='Aleksy Bernat',
+	license='BEERWARE',
+	author_email='aleksy.bernat@gmail.com',
+	url='https://git.e-science.pl/abernat/foodlibrary',
+	packages=['foodlibrary']
+)
