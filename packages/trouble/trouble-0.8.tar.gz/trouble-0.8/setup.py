073ccc9977c6caa2718e041ecc0b93c1dafdebee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:14 +0000

    Add package trouble/0.8/trouble-0.8.tar.gz

---
+++ packages/trouble/trouble-0.8.tar.gz/trouble-0.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="trouble",
+    version="0.8",
+    packages=['trouble'],
+
+    author="Dan Herrera",
+    author_email="whoahbot@gmail.com",
+    description="The model and corpus files for trouble.whoahbot.com",
+    url="http://github.com/whoahbot/trouble/"
+)
---
+++ packages/trouble/trouble-0.8.tar.gz/trouble-0.8/trouble/__init__.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+
+from torch.autograd import Variable
+
+class RNNModel(nn.Module):
+    """Container module with an encoder, a recurrent module, and a decoder."""
+
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, tie_weights=False):
+        super(RNNModel, self).__init__()
+        self.drop = nn.Dropout(dropout)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        if rnn_type in ['LSTM', 'GRU']:
+            self.rnn = getattr(nn, rnn_type)(ninp, nhid, nlayers, dropout=dropout)
+        else:
+            try:
+                nonlinearity = {'RNN_TANH': 'tanh', 'RNN_RELU': 'relu'}[rnn_type]
+            except KeyError:
+                raise ValueError( """An invalid option for `--model` was supplied,
+                                 options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
+            self.rnn = nn.RNN(ninp, nhid, nlayers, nonlinearity=nonlinearity, dropout=dropout)
+        self.decoder = nn.Linear(nhid, ntoken)
+
+        # Optionally tie weights as in:
+        # "Using the Output Embedding to Improve Language Models" (Press & Wolf 2016)
+        # https://arxiv.org/abs/1608.05859
+        # and
+        # "Tying Word Vectors and Word Classifiers: A Loss Framework for Language Modeling" (Inan et al. 2016)
+        # https://arxiv.org/abs/1611.01462
+        if tie_weights:
+            if nhid != ninp:
+                raise ValueError('When using the tied flag, nhid must be equal to emsize')
+            self.decoder.weight = self.encoder.weight
+
+        self.init_weights()
+
+        self.rnn_type = rnn_type
+        self.nhid = nhid
+        self.nlayers = nlayers
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input, hidden):
+        emb = self.drop(self.encoder(input))
+        output, hidden = self.rnn(emb, hidden)
+        output = self.drop(output)
+        decoded = self.decoder(output.view(output.size(0)*output.size(1), output.size(2)))
+        return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
+
+    def init_hidden(self, bsz):
+        weight = next(self.parameters())
+        if self.rnn_type == 'LSTM':
+            return (weight.new_zeros(self.nlayers, bsz, self.nhid),
+                    weight.new_zeros(self.nlayers, bsz, self.nhid))
+        else:
+            return weight.new_zeros(self.nlayers, bsz, self.nhid)
+
+class Dictionary(object):
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = []
+
+    def add_word(self, word):
+        if word not in self.word2idx:
+            self.idx2word.append(word)
+            self.word2idx[word] = len(self.idx2word) - 1
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.idx2word)
+
+
+class Corpus(object):
+    def __init__(self, train, valid, test):
+        self.dictionary = Dictionary()
+        self.train = self.tokenize(train)
+        self.valid = self.tokenize(valid)
+        self.test = self.tokenize(test)
+
+    def tokenize(self, path):
+        """Tokenizes a random sample of trouble."""
+        # Add words to the dictionary
+        tokens = 0
+
+        with open(path, 'r', encoding='utf-8') as f:
+            trouble_corpus = f.read()
+
+        for line in trouble_corpus.split():
+            words = line.split()
+            tokens += len(words)
+            for word in words:
+                self.dictionary.add_word(word)
+
+        # Tokenize file content
+        ids = torch.LongTensor(tokens)
+        token = 0
+        for line in trouble_corpus.split():
+            words = line.split()
+            for word in words:
+                ids[token] = self.dictionary.word2idx[word]
+                token += 1
+
+        return ids
