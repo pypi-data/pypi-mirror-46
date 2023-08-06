8d3dd09bc2876930f328237ac8d80b05d3e86a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:39 +0000

    Add package ybc-exception/1.1.5/ybc_exception-1.1.5.tar.gz

---
+++ packages/ybc-exception/ybc_exception-1.1.5.tar.gz/ybc_exception-1.1.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='ybc_exception',
+    version='1.1.5',
+    description='define custom exception',
+    long_description='define custom exception',
+    author='mengxf',
+    author_email='mengxf01@fenbi.com',
+    keywords=['pip3', 'exception', 'python3', 'python'],
+    url='http://pip.zhenguanyu.com/',
+    packages=['ybc_exception'],
+    package_data={'ybc_exception': ['*.py']},
+    license='MIT',
+    install_requires=[
+    ],
+)
---
+++ packages/ybc-exception/ybc_exception-1.1.5.tar.gz/ybc_exception-1.1.5/ybc_exception/__init__.py
@@ -0,0 +1,1 @@
+from .ybc_exception import *
