c1e3dd059dc5509132a5dd90aae7469af278b892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:20 +0000

    Add package remot3/0.1.0/remot3-0.1.0-py2.py3-none-any.whl

---
+++ packages/remot3/remot3-0.1.0-py2.py3-none-any.whl/remot3/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for remot3."""
+
+__author__ = """Diego Sandoval"""
+__email__ = 'dsandovalv@gmail.com'
+__version__ = '0.1.0'
+
+from .remot3 import Remot3
