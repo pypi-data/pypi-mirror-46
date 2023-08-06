af48e4fb0da69c6986517fb24454a5ff64cd7239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:58 +0000

    Add package ssim/0.3.0/ssim-0.3.0.tar.gz

---
+++ packages/ssim/ssim-0.3.0.tar.gz/ssim-0.3.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name="ssim",
+    packages=["ssim"],
+    package_dir={"ssim": "ssim"},
+    version="0.3.0",
+    description="IATA SSIM (Standard Schedules Information Manual) file parser is a tool to read the standard IATA "
+    "file format.",
+    author="Rok Mihevc, Ramon Van Schaik, Howard Riddiough, Kevin Haagen",
+    author_email="rok.mihevc@gmail.com",
+    url="https://github.com/rok/ssim",
+    keywords=["parsing", "ssim", "data"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.6",
+    ],
+    install_requires=["python-dateutil"],
+    entry_points={"console_scripts": ["ssim = ssim.__main__:main"]},
+)
---
+++ packages/ssim/ssim-0.3.0.tar.gz/ssim-0.3.0/ssim/__main__.py
@@ -0,0 +1,27 @@
+import csv
+import ssim
+import argparse
+
+parser = argparse.ArgumentParser(description="Converts a slotfile to CSV.")
+parser.add_argument("-i", help="Input slotfile filename", type=str, metavar="input filename", required=True)
+parser.add_argument("-o", help="Output csv filename", type=str, metavar="output filename", required=True)
+args = parser.parse_args()
+input_file = args.i
+output_file = args.o
+
+
+def main():
+
+    records = ssim.read(input_file)
+    flights = ssim.expand_slots(records)
+
+    keys = flights[0].keys()
+
+    with open(output_file, "w") as csvfile:
+        dict_writer = csv.DictWriter(csvfile, keys, restval="")
+        dict_writer.writeheader()
+        dict_writer.writerows(flights)
+
+
+if __name__ == "__main__":
+    main()
