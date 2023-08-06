884764958f838c4c962746d6a4c885ed7dc27981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:16 +0000

    Add package vstutils/1.10.3/vstutils-1.10.3-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/vstutils/vstutils-1.10.3-cp36-cp36m-manylinux1_x86_64.whl/vstutils/static/js/guiDashboard.js
@@ -0,0 +1,6 @@
+var guiDashboard={model:{name:"module"},api:{buttons:[],name:"Dashboard"},tpl_name:'guiDashboard',getTitle:function(){return this.api.name}}
+guiDashboard.model.className="guiDashboard"
+guiDashboard.open=function(holder,menuInfo,data)
+{$(holder).insertTpl(spajs.just.render(this.tpl_name,{guiObj:this}))}
+tabSignal.connect("webGui.start",function()
+{spajs.addMenu({id:"home",urlregexp:[/^home$/,/^$/,/^\/$/],onOpen:function(holder,menuInfo,data){return guiDashboard.open(holder,menuInfo,data);},onClose:function(){},})})
