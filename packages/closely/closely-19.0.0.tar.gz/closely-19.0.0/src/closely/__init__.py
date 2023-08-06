6e09f7cb71068618f722fc33055c9913a3ba3f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:14 +0000

    Add package closely/19.0.0/closely-19.0.0.tar.gz

---
+++ packages/closely/closely-19.0.0.tar.gz/closely-19.0.0/src/closely/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from .main import solution as solve
+
+__version__ = "19.0.0"
+
+__title__ = "closely"
+__description__ = "Closely find closest pairs of points, eg duplicates, in a dataset"
+__url__ = "https://github.com/justinshenk/closely"
+__uri__ = __url__
+__doc__ = __description__ + " <" + __url__ + ">"
+
+__author__ = "Justin Shenk"
+__email__ = "shenkjustin@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2019 " + __author__
