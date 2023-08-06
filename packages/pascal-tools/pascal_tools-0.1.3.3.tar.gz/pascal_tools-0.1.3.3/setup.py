2a2043db679d4cb2c0b3ace6af04bf333062441f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:36 +0000

    Add package pascal-tools/0.1.3.3/pascal_tools-0.1.3.3.tar.gz

---
+++ packages/pascal-tools/pascal_tools-0.1.3.3.tar.gz/pascal_tools-0.1.3.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pascal_tools',
+      version='0.1.3.3',
+      description='Some useful tools for me ! ',
+      url='https://github.com/moreaupascal56/pascal_tools',
+      author='Pascal Moreau',
+      author_email='moreaupascal56@gmail.com',
+      license='free',
+      packages=['pascal_tools'],
+      zip_safe=False)
