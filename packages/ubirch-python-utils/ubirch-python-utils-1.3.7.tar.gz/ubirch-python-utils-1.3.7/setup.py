ca7ac02d596b18e2ac9514d4b164c7023fd9adc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:47 +0000

    Add package ubirch-python-utils/1.3.7/ubirch-python-utils-1.3.7.tar.gz

---
+++ packages/ubirch-python-utils/ubirch-python-utils-1.3.7.tar.gz/ubirch-python-utils-1.3.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ubirch-python-utils",
+    version="1.3.7",
+    author="Michael Merz",
+    author_email="dermicha@ubirch.com",
+    description="A python util for ubirch anchoring services.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ubirch/ubirch-python-utils",
+    packages=setuptools.find_packages(exclude=['bin', 'tests']),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'kafka-python >= 1.4.3',
+        'boto3 >= 1.7.80'
+    ],
+)
