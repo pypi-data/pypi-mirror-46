e2a4e0e7e98efcf3c9348b425478c1e9c911c03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:10 +0000

    Add package errata-tool/1.20.0/errata-tool-1.20.0.tar.gz

---
+++ packages/errata-tool/errata-tool-1.20.0.tar.gz/errata-tool-1.20.0/errata_tool/tests/test_rhsa_synopsis.py
@@ -0,0 +1,18 @@
+import requests
+
+
+class TestRhsaSynopsis(object):
+
+    def test_synopsis(self, monkeypatch, mock_post, mock_put, rhsa):
+        """
+        Verify that we do not include the "Moderate: " prefix in our PUT data.
+        """
+        monkeypatch.setattr(requests, 'post', mock_post)
+        monkeypatch.setattr(requests, 'put', mock_put)
+        # Any typical advisory update would toggle this internal "_update"
+        # attribute. Simulate that here:
+        rhsa._update = True
+        # Now save our "changes":
+        rhsa.commit()
+        expected = 'ceph-iscsi-cli security update'
+        assert mock_put.kwargs['data']['advisory[synopsis]'] == expected
