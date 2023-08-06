3ea210cb6d6d451677cb55909166321d0ee6ba7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:13 +0000

    Add package jake-test-package/0.0.0/jake-test-package-0.0.0.tar.gz

---
+++ packages/jake-test-package/jake-test-package-0.0.0.tar.gz/jake-test-package-0.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="jake-test-package",
+    version="0.0.0",
+    author="Placeholder",
+    author_email="jake@markupisart.com",
+    description="placeholder",
+    packages=setuptools.find_packages(),
+)
