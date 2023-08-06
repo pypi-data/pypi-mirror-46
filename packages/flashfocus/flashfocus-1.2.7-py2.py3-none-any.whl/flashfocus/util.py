8b342031930ade057b35db75d511ab56e75c237f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:03 +0000

    Add package flashfocus/1.2.7/flashfocus-1.2.7-py2.py3-none-any.whl

---
+++ packages/flashfocus/flashfocus-1.2.7-py2.py3-none-any.whl/flashfocus/util.py
@@ -0,0 +1,17 @@
+"""Utility functions."""
+try:
+    from inspect import getfullargspec as getargspec
+except ImportError:
+    from inspect import getargspec
+import os
+
+
+def list_param(f):
+    """List the parameters of a function or method."""
+    return [x for x in getargspec(f).args if x != "self"]
+
+
+def cmd_exists(cmd):
+    return any(
+        os.access(os.path.join(path, cmd), os.X_OK) for path in os.environ["PATH"].split(os.pathsep)
+    )
