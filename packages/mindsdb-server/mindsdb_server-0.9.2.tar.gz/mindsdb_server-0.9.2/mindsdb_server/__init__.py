cef4024c7237d2cbd1dc1fe2f20eba4c855575e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:14 +0000

    Add package mindsdb-server/0.9.2/mindsdb_server-0.9.2.tar.gz

---
+++ packages/mindsdb-server/mindsdb_server-0.9.2.tar.gz/mindsdb_server-0.9.2/mindsdb_server/__init__.py
@@ -0,0 +1,10 @@
+import sys
+if sys.version_info < (3,3):
+    sys.exit('Sorry, For MindsDB Server does not support python < 3.3')
+
+
+from mindsdb_server.server import start as start_server
+
+
+
+
