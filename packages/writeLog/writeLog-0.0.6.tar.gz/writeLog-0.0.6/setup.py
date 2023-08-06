842bd342d2828b598581c6a2e5d9bb6572dd7d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:05 +0000

    Add package writeLog/0.0.6/writeLog-0.0.6.tar.gz

---
+++ packages/writeLog/writeLog-0.0.6.tar.gz/writeLog-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup 
+  
+ 
+# specify requirements of your package here 
+REQUIREMENTS = [''] 
+#CLASSIFIERS=['']#
+  
+# calling the setup function  
+setup(name='writeLog', 
+      version='0.0.6', 
+      description='A log writer function for py based docker containers.', 
+      long_description='',
+      url='', 
+      author='Jacob Stewart', 
+      author_email='jick.68.0@gmail.com', 
+      license='MIT', 
+      packages=[''], 
+#      classifiers=CLASSIFIERS, 
+      install_requires=REQUIREMENTS, 
+      keywords='logger'
+      ) 
+
