02ea7a21ce40bfafaf4334b3cc479a6915b7bd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:59 +0000

    Add package radioz/0.0.dev2/radioz-0.0.dev2-py3-none-any.whl

---
+++ packages/radioz/radioz-0.0.dev2-py3-none-any.whl/plugins/plug_64094.py
@@ -0,0 +1,42 @@
+import json
+import logging
+import requests
+import time
+
+from threading import Thread
+
+from notify import Notification
+
+from plugins import FrozenJson
+
+URL = "http://np.radioplayer.co.uk/qp/v3/onair?rpIds=340&nameSize=200&artistNameSize=200&descriptionSize=200"
+
+logger = logging.getLogger(__name__)
+
+
+def get_data(url):
+    try:
+        resp = requests.get(url)
+        data = json.loads(resp.text[9:-1])
+        return data
+    except (requests.exceptions.RequestException, json.JSONDecodeError) as exc:
+        logger.debug(exc)
+
+
+def run():
+    try:
+        data = FrozenJson(get_data(URL))
+        objs = []
+        if data.results._340:
+            for obj in data.results._340:
+                try:
+                    objs.append(obj)
+                except Exception as exc:
+                    logger.debug("plugin ", exc)
+            service = objs[-1].serviceName
+            artist = objs[-1].artistName
+            song = objs[-1].name
+            return service, artist, song
+    except Exception as exc:
+        logger.debug("plugin ", exc)
+        return "(*_*)", "(*_*)", "(*_*)"
