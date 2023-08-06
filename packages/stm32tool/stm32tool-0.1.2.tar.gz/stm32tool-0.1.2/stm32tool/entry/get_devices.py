19f9dcc0d72cd936113194a601eb1e07619cbaac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:43 +0000

    Add package stm32tool/0.1.2/stm32tool-0.1.2.tar.gz

---
+++ packages/stm32tool/stm32tool-0.1.2.tar.gz/stm32tool-0.1.2/stm32tool/entry/get_devices.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import stm32tool as openmv
+
+
+def main():
+    dev = openmv.OpenMV()
+    dev.connect()
+    print('Board Info: {!r}'.format(dev.arch_id))
+    print('OpenMV Version:', dev.fw_version)
