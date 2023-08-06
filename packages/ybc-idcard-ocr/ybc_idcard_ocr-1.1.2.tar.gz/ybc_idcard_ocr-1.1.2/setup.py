5def945f3aee3ae5a933bb91891d634029f0d8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:16 +0000

    Add package ybc-idcard-ocr/1.1.2/ybc_idcard_ocr-1.1.2.tar.gz

---
+++ packages/ybc-idcard-ocr/ybc_idcard_ocr-1.1.2.tar.gz/ybc_idcard_ocr-1.1.2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ybc_idcard_ocr',
+      version='1.1.2',
+      description='Recognize ID Card By Ocr.',
+      long_description='Recognize ID Card By Ocr.',
+      author='hurs',
+      author_email='hurs@fenbi.com',
+      keywords=['python', 'idcard', 'ocr'],
+      url='http://pip.zhenguanyu.com/',
+      packages=['ybc_idcard_ocr'],
+      package_data={'ybc_idcard_ocr': ['test.jpg', '*.py']},
+      license='MIT',
+      install_requires=['requests', 'ybc_config', 'opencv-python', 'pillow', 'ybc_exception']
+      )
