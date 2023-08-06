60de0734a64e81bf5a2873f3d796f2ed9d5b8cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:53 +0000

    Add package py-popgen/0.0.2/py-popgen-0.0.2.tar.gz

---
+++ packages/py-popgen/py-popgen-0.0.2.tar.gz/py-popgen-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+requirements = [
+      'pysam',
+      'pandas',
+      'pybedtools',
+      'sklearn',
+      'matplotlib'
+
+
+]
+
+setup(name="py-popgen",
+      version="0.0.2",
+      description="first setup file",
+      include_package_data=True,
+      packages=['pgpipe'],
+      install_requires=requirements,
+      scripts=['pgpipe/vcf_phase.py'])
+
+#packages=setuptools.find_packages() to automate package finding?
