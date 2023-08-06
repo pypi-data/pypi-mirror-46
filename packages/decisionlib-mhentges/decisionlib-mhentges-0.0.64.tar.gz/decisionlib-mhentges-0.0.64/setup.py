a385fd4066270052b273ca481493398493d071cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:08 +0000

    Add package decisionlib-mhentges/0.0.64/decisionlib-mhentges-0.0.64.tar.gz

---
+++ packages/decisionlib-mhentges/decisionlib-mhentges-0.0.64.tar.gz/decisionlib-mhentges-0.0.64/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requirements = [line.rstrip('\n') for line in f if not line.startswith('#')]
+
+setuptools.setup(
+    name='decisionlib-mhentges',
+    version='0.0.64',
+    author='Mozilla Release Engineering',
+    author_email='release+python@mozilla.com',
+    description='Taskcluster task-builder',
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'decisionlib = decisionlib:main',
+        ],
+    },
+)
