5dcfa3e3a0ec1a87725cd33e1edcd1119ed930fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:44 +0000

    Add package demojipy/1.2.0/demojipy-1.2.0.tar.gz

---
+++ packages/demojipy/demojipy-1.2.0.tar.gz/demojipy-1.2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='demojipy',
+      version='1.2.0',
+      license='MIT',
+      description='Python API Wrapper for DiscordEmoji',
+      author='Algueem',
+      url='https://github.com/Algueem/DiscordEmojiPy',
+      keywords=['pydemoji', 'discordemoji'],
+      install_requires=[
+          'requests'
+      ],
+      packages=find_packages(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent"
+      ]
+      )
