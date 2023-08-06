5436d15248a3150cdbc4bc048b64dc93e5bab809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:08 +0000

    Add package pykoa/0.1.4/pykoa-0.1.4.tar.gz

---
+++ packages/pykoa/pykoa-0.1.4.tar.gz/pykoa-0.1.4/koa/__init__.py
@@ -0,0 +1,6 @@
+"""
+The pykoa package is KOA's (Keck Online Archive) python client interface for querying KOA's database and downloading KOA data.
+"""
+
+from .core import Koa, Archive, KoaTap, KoaJob
+
