5deef2ce97daee3e4d91ee583f5d926f9cc6ec13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:11 +0000

    Add package nbless/0.2.24/nbless-0.2.24-py3-none-any.whl

---
+++ packages/nbless/nbless-0.2.24-py3-none-any.whl/nbless/nbless.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from typing import List
+
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbformat.notebooknode import NotebookNode
+
+from nbless.nbuild import nbuild
+
+
+def nbless(filenames: List[str], kernel: str = "python3") -> NotebookNode:
+    """Create an executed Jupyter notebook from markdown and code files.
+
+    :param filenames: A list of names of markdown and code files.
+    :param kernel: The programming language used to run the notebook.
+    """
+    nb = nbuild(filenames)
+    ep = ExecutePreprocessor(timeout=600, kernel_name=kernel)
+    ep.preprocess(nb, {"metadata": {"path": "."}})
+    return nb
