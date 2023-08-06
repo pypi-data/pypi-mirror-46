5a0c9a2f96fd085b296d5f891826cdc550dba862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:30 +0000

    Add package ems-gcp-toolkit/0.1.48/ems-gcp-toolkit-0.1.48.tar.gz

---
+++ packages/ems-gcp-toolkit/ems-gcp-toolkit-0.1.48.tar.gz/ems-gcp-toolkit-0.1.48/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ems-gcp-toolkit",
+    version="0.1.48",
+    packages=find_packages(exclude="tests"),
+    url="https://github.com/emartech/ems-gcp-toolkit",
+    license="MIT",
+    author="Emarsys",
+    author_email="",
+    description=""
+)
