a7d5808062d7759cf8f096b3d5fc41689b6bcae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:27 +0000

    Add package cornac/0.2.1/cornac-0.2.1-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/cornac/cornac-0.2.1-cp36-cp36m-manylinux1_x86_64.whl/cornac/models/__init__.py
@@ -0,0 +1,21 @@
+
+from .bpr import BPR
+from .cdl import CDL
+from .c2pf import C2PF
+from .hpf import HPF
+from .pcrl import PCRL
+from .pmf import PMF
+from .mcf import MCF
+from .vmf import VMF
+from .ibpr import IBPR
+from .coe import COE
+from .skm import SKMeans
+from .vbpr import VBPR
+from .online_ibpr import OnlineIBPR
+from .mf import MF
+from .conv_mf import ConvMF
+from .cdr import CDR
+from .sbpr import SBPR
+from .sorec import SoRec
+from .cvae import CVAE
+
