897e8f84d4f9e2750f886eb07ea15dbccdc44264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:45 +0000

    Add package lmfgg/1.1.7/lmfgg-1.1.7.zip

---
+++ packages/lmfgg/lmfgg-1.1.7.zip/lmfgg-1.1.7/lmfgg/ps/pspre.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+def get_ps(page):
+    data=[]
+    soup=BeautifulSoup(page,'html.parser')
+    tables=soup.find_all('table')
+    if tables !=[]:
+        for tb in tables:
+            tbtmp=tb.find('table')
+            if tbtmp is  None:
+                tb.extract()
+    return str(soup)
+
+
+
+def get_pure_ps(page):
+
+    page=get_ps(page)
+    return page
