4af5d59b73d21906b290ad0662e1466af475be9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:16 +0000

    Add package yapo/0.1.0.dev20190515174257/yapo-0.1.0.dev20190515174257.tar.gz

---
+++ packages/yapo/yapo-0.1.0.dev20190515174257.tar.gz/yapo-0.1.0.dev20190515174257/yapo/__init__.py
@@ -0,0 +1,6 @@
+from ._instance import information, \
+                       portfolio, \
+                       portfolio_asset, \
+                       available_names, \
+                       search, \
+                       inflation
