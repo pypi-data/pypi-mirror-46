479f4f93f281dd9bc5188e44a5b8a02f7484e520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:33 +0000

    Add package rand-string/0.41/rand-string-0.41.tar.gz

---
+++ packages/rand-string/rand-string-0.41.tar.gz/rand-string-0.41/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+  readme = f.read()
+
+setup(
+  name = "rand-string",
+  packages = ["rand_string"],
+  version = "0.41",
+  license="MIT",
+  description = "Generates a random string of a specified length.",
+  long_description=readme,
+  long_description_content_type="text/markdown",
+  author = "TheSongList",
+  author_email = "the@songlist.moe",
+  url = "https://github.com/TheSongList/random-string",
+  download_url = "https://github.com/TheSongList/random-string/archive/v0.1.tar.gz",
+  keywords = ["RANDOM", "STRING", "GENERATOR"],
+  install_requires=[
+      ],
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
+
+    "License :: OSI Approved :: MIT License",
+
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+  ],
+)
