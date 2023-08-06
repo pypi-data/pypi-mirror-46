98e1385c5fbb08c78f6f0035018381a8e4bc4b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:59 +0000

    Add package mdtraj/1.9.3/mdtraj-1.9.3.tar.gz

---
+++ packages/mdtraj/mdtraj-1.9.3.tar.gz/mdtraj-1.9.3/mdtraj/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '1.9.3'
+short_version = '1.9.3'
+full_version = '1.9.3'
+git_revision = 'a7d574e3af52b61028da3bdf13afb37728bd77a6'
+release = True
