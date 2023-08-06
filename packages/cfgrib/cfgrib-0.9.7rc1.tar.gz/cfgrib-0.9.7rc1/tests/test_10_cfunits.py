9e1d674a3239566d58c788cc23e7f3572d1aa855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:31 +0000

    Add package cfgrib/0.9.7rc1/cfgrib-0.9.7rc1.tar.gz

---
+++ packages/cfgrib/cfgrib-0.9.7rc1.tar.gz/cfgrib-0.9.7rc1/tests/test_10_cfunits.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pytest
+
+xr = pytest.importorskip('xarray')  # noqa
+
+from cf2cdm import cfunits
+
+
+def test_are_convertible():
+    assert cfunits.are_convertible('K', 'K')
+    assert cfunits.are_convertible('m', 'meters')
+    assert cfunits.are_convertible('hPa', 'Pa')
+    assert not cfunits.are_convertible('m', 'Pa')
---
+++ packages/cfgrib/cfgrib-0.9.7rc1.tar.gz/cfgrib-0.9.7rc1/tests/test_20_main.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import click.testing
+
+from cfgrib import __main__
+
+
+def test_cfgrib_cli_selfcheck():
+    runner = click.testing.CliRunner()
+
+    res = runner.invoke(__main__.cfgrib_cli, ['selfcheck'])
+
+    assert res.exit_code == 0
+    assert 'Your system is ready.' in res.output
+
+    res = runner.invoke(__main__.cfgrib_cli, ['non-existent-command'])
+    assert res.exit_code == 2
