caabf90b6db64cbbab87beac3f434a6cc152dff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:20 +0000

    Add package ImageD11/1.9.2/ImageD11-1.9.2.tar.gz

---
+++ packages/ImageD11/ImageD11-1.9.2.tar.gz/ImageD11-1.9.2/ImageD11/ImageD11_file_series.py
@@ -0,0 +1,228 @@
+
+from __future__ import print_function
+
+"""
+To be moved to fabio sometime
+"""
+
+import fabio.file_series
+import fabio.fabioimage
+import fabio.openimage
+import numpy, h5py
+import gzip, bz2
+
+def get_options(parser):
+
+    parser.add_option("-5","--hdf5",action="store", type="string",
+                      dest = "hdf5", default = None,
+                      help = "hdf file containing input image series")
+    # or, eventually:
+    # stem, first, last, format, (omegas better be in the headers)
+    parser.add_option("-n","--stem",action="store", type="string",
+                      dest = "stem", default = None,
+                      help = "stem name for input image series")
+    parser.add_option("-f","--first",action="store", type="int",
+                      dest = "first", default = None,
+                      help = "first number for input image series")
+    parser.add_option("-l","--last",action="store", type="int",
+                      dest = "last", default = None,
+                      help = "last number for input image series")
+    parser.add_option("--ndigits", action="store", type="int",
+                      dest = "ndigits", default = 4,
+                      help = "Number of digits in file numbering [4]")
+    parser.add_option("-P", "--padding", action="store",
+                      type="choice", choices=["Y","N"], 
+                      default="Y", dest="padding",
+                      help="Is the image number to padded Y|N, e.g. "\
+                          "should 1 be 0001 or just 1 in image name, default=Y")
+    parser.add_option("-F","--format",action="store", type="string",
+                      dest = "format", default = ".edf",
+                      help = "format [.edf] for input image series")
+
+    parser.add_option("-O", "--flood", action="store", type="string",
+                      dest = "flood", default = None,
+                      help = "Flood")
+    
+    parser.add_option("-d", "--dark", action="store", type="string",
+                      dest = "dark", default = None,
+                      help = "Dark image")
+    parser.add_option("-S", "--step", action="store", type="float",
+                      dest = "OMEGASTEP", default = None,
+                      help = "omega step size")
+    parser.add_option("-T", "--start", action="store", type="float",
+                      dest = "OMEGA", default = None,
+                      help = "start omega")
+    parser.add_option("--omega_motor", action="store", type="string",
+                      dest = "omegamotor", default = "Omega",
+       help = "Header value to use for rotation motor position [Omega]")
+    parser.add_option("--omega_motor_step", action="store", type="string",
+                      dest = "omegamotorstep", default = "OmegaStep",
+       help = "Header value to use for rotation width [OmegaStep]")
+
+    return parser
+
+
+def get_series_from_hdf( hdf_file, dark = None, flood = None ):
+    groups = hdf_file.listnames()
+    for group in groups:
+        imagenames = hdf_file[group].listnames()
+        for image in imagenames:
+            im = hdf_file[group][image]
+            om = float(im.attrs['Omega'])
+            data = im[:,:]
+            if (dark, flood) is not (None, None):
+                data = data.astype(numpy.float32)
+            if dark is not None:
+                numpy.subtract( data, dark, data )
+            if flood is not None:
+                numpy.divide( data, flood, data )
+            yield fabio.fabioimage.fabioimage( data = data,
+                                               header = {
+                    'Omega': om } )
+
+def series_from_fabioseries( fabioseries, dark, flood, options ):
+    
+    for filename in fabioseries:
+        try:
+            fim = fabio.openimage.openimage(filename)
+        except:
+            print("Missing image",filename)
+            continue
+        if (dark, flood) is not (None, None):
+            fim.data = fim.data.astype(numpy.float32)
+        if dark is not None:
+            numpy.subtract( fim.data, dark, fim.data )
+        if flood is not None:
+            numpy.divide( fim.data, flood, fim.data )
+        if options.omegamotor in fim.header:
+            fim.header['Omega'] = float(fim.header[options.omegamotor])
+            try:
+                fim.header['OmegaStep'] = float(fim.header[options.omegamotorstep])
+            except:
+                fim.header['OmegaStep'] = float(options.OMEGASTEP)            
+        else:
+            fim.header['Omega'] = float(options.OMEGA)
+            fim.header['OmegaStep'] = float(options.OMEGASTEP)            
+            options.OMEGA = float(options.OMEGA) + float(options.OMEGASTEP)
+        yield fim
+        
+
+
+def get_series_from_stemnum( options, args, dark = None, flood = None ):
+    """
+    Returns a file series thing - not a fabio one
+    """
+    if options.format in ['bruker', 'BRUKER', 'Bruker']:
+        extn = ""
+    elif options.format == 'GE':
+        extn = ""
+    else:
+        extn = options.format
+        
+    fso = fabio.file_series.numbered_file_series(
+        options.stem,
+        options.first,
+        options.last,
+        extn,
+        digits = options.ndigits,
+        padding = options.padding )
+    return series_from_fabioseries( fso , dark, flood, options )
+    
+
+def get_series_from_options( options, args ):
+    """
+    Returns a file series thing - not a fabio one
+
+    This gives back a fabioimage object with dark and flood
+    corrected data
+    """
+
+    try:
+        if options.dark is not None:
+            dark = fabio.openimage.openimage( options.dark ).data
+        else:
+            dark = None
+    except:
+        print("Problem with your dark",options.dark)
+        raise
+    
+    try:
+        if options.flood is not None:
+            flood = fabio.openimage.openimage( options.flood ).data
+        else:
+            flood = None
+    except:
+        print("Problem with your flood",options.flood)
+        raise
+        
+
+    if len(args) > 0 :
+        # We assume unlabelled arguments are filenames 
+        fso = fabio.file_series.file_series(args)
+        return series_from_fabioseries( fso, dark, flood, options )
+
+    if options.hdf5 is not None:
+        hf = h5py.File(options.hdf5)
+        # print "Getting images from",options.hdf5
+        return get_series_from_hdf( hf, dark, flood )
+    
+    return get_series_from_stemnum( options, args,
+                                     dark, flood) 
+    
+
+
+def getedfheader(filename):
+    """
+    Reads a header from an edf file in 1024 byte chunks.
+    Assumes enclosing { }
+    Returns string enclosed
+    Adds a filename key at the top
+    """
+    h = "filename = "
+    if filename[-3:]==".gz":
+        fp=gzip.GzipFile(filename,"rb")
+    elif filename [-4:]==".bz2":
+        fp=bz2.BZ2File(filename,"rb")
+    else:
+        try:
+            fp=open(filename,"rb")
+        except IOError:
+            return ""
+    h=h+filename+";\n"
+    s=fp.read(1024)
+    if s.find("{")==-1:
+        raise Exception("Not an edf file")
+    while 1:
+        if s.find("}")>=0:
+            h=h+s[0:s.find("}")+2]
+            break
+        else:
+            h=h+s
+        s=fp.read(1024)
+    return h
+
+def motor_mne(hd):
+    """
+    expands the _mne and _pos header items of edf headers
+    """
+    h = {}
+    order = []
+    for line in hd.split(";"):
+        try:
+            key,vals = line.split("=")
+        except ValueError:
+            continue
+        key = key.lstrip().rstrip()
+        h[key] = vals.split(";")[0]
+        order.append( key )
+    for k in order:
+        if k.endswith("_mne"):
+            stem = k.split("_")[0]
+            p = k.replace("_mne","_pos")
+            newkeys = h[k].split()
+            newvals = h[p].split()
+            for ik, iv in zip(newkeys, newvals):
+                kk = stem+":"+ik
+                h[kk]=iv
+                order.append( kk )
+    return h, order
---
+++ packages/ImageD11/ImageD11-1.9.2.tar.gz/ImageD11-1.9.2/setup.py
@@ -0,0 +1,128 @@
+
+from __future__ import print_function
+
+# ImageD11_v1.0 Software for beamline ID11
+# Copyright (C) 2005-2018  Jon Wright
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""
+Setup script
+
+Do:
+  python setup.py build --force
+   -> it will build static libraries in the src folder
+   -> compile wrappers them here
+"""
+
+# For pip / bdist_wheel etc
+import setuptools
+import os, sys
+# Need to use numpy.distutils so it works for f2py
+from numpy.distutils.core import setup, Extension
+from numpy import get_include
+# Get the path for the static libraries        
+import src.bldlib
+
+if "build" in sys.argv:
+    """ Ugly - requires setup.py build 
+    Should learn to use build_clib at some point
+    """
+    os.chdir("src")
+    print("Call write_check")
+    os.system("python write_check.py")
+    print("Call bldlib")
+    # transmit compiler from command line
+    ok = os.system("python bldlib.py "+ " ".join(sys.argv))
+    os.chdir("..")
+    if ok != 0:
+        print("Return was",ok)
+        sys.exit(ok)
+    else:
+        print("Seems to build OK")
+
+
+ekwds = { 'include_dirs' : [get_include(), 'src' ],
+          'library_dirs' : ['./src'],
+          'extra_compile_args' : src.bldlib.arg + \
+               ["-DF2PY_REPORT_ON_ARRAY_COPY=1.",],
+          'extra_link_args' : src.bldlib.arg + \
+               ["-DF2PY_REPORT_ON_ARRAY_COPY=1.",],
+        }
+
+# Compiled extensions:
+extensions = [ Extension( "cImageD11_sse2", 
+                          sources = ["src/cImageD11_sse2.pyf",],
+                          libraries = [src.bldlib.sse2libname],
+                          **ekwds ),
+               Extension( "cImageD11_avx",
+                          sources = ["src/cImageD11_avx.pyf",],
+                          libraries = [src.bldlib.avx2libname],
+                          **ekwds) ]
+
+
+# Removed list of dependencies from setup file
+#  borked something, not sure what or why
+needed =[]
+# ["xfab", "fabio", "pyopengl",  "matplotlib", "numpy", "scipy", "six", "h5py",
+#  "pyopengltk", "FitAllB", ... ]
+
+# See the distutils docs...
+setup(name='ImageD11',
+      version='1.9.2',
+      author='Jon Wright',
+      author_email='wright@esrf.fr',
+      description='ImageD11',
+      license = "GPL",
+      ext_package = "ImageD11",   # Puts extensions in the ImageD11 directory
+      ext_modules = extensions,
+      install_requires = needed,
+      packages = ["ImageD11"],
+      package_dir = {"ImageD11":"ImageD11"},
+      url = "http://github.com/jonwright/ImageD11",
+      package_data = {"ImageD11" : ["doc/*.html", "data/*" ]},
+      scripts = ["ImageD11/rsv_mapper.py",
+                 "scripts/peaksearch.py",
+                 "scripts/fitgrain.py",
+                 "scripts/tomapper.py",
+                 "scripts/ubi2cellpars.py",
+                 "scripts/filtergrain.py",
+                 "scripts/filterout.py",
+                 "ImageD11/plot3d.py",
+                 "scripts/pars_2_sweeper.py",
+                 "scripts/ImageD11_2_shelx.py",
+                 "scripts/fit2dcake.py",
+                 "scripts/fix_spline.py",
+                 "scripts/edfheader.py",
+                 "ImageD11/plot3d.py",
+                 "scripts/huber2bruker.py",
+                 "scripts/id11_summarize.py",
+                 "scripts/ImageD11_gui.py",
+                 "scripts/bgmaker.py",
+                 "scripts/merge_flt.py",
+                 "scripts/makemap.py",
+                 "scripts/plotlayer.py",
+                 "scripts/plotedf.py",
+                 "scripts/plotgrainhist.py",
+                 "scripts/rubber.py",
+                 "scripts/plotImageD11map.py",
+                 "scripts/cutgrains.py",
+                 "scripts/index_unknown.py",
+                 "scripts/spatialfix.py",
+       	         "scripts/refine_em.py",
+                 "scripts/avg_par.py",
+                 "scripts/powderimagetopeaks.py"])
+
+
