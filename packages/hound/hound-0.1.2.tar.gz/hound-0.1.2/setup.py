6acdb96ff664e4ff6743a79f6c773de4ae5c454f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:42 +0000

    Add package hound/0.1.2/hound-0.1.2.tar.gz

---
+++ packages/hound/hound-0.1.2.tar.gz/hound-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from hound import __version__
+
+setup(
+    name = 'hound',
+    version = __version__,
+    packages = [
+        'hound',
+    ],
+    description = 'A FireCloud database extension',
+    url = 'https://github.com/broadinstitute/hound',
+    author = 'Aaron Graubert - Broad Institute - Cancer Genome Computational Analysis',
+    author_email = 'aarong@broadinstitute.org',
+    long_description = 'A FireCloud database extension',
+    install_requires = [
+        "google-cloud-storage>=1.13.2",
+        "pandas"
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+    ],
+    license="BSD3"
+)
