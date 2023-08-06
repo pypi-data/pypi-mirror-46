1b01d13b52fb99348f8ef0524303e31f40b9aa11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:07 +0000

    Add package libcolgraph/0.0.1.post4/libcolgraph-0.0.1.post4.tar.gz

---
+++ packages/libcolgraph/libcolgraph-0.0.1.post4.tar.gz/libcolgraph-0.0.1.post4/libcolgraph/libcc/Graph.h
@@ -0,0 +1,57 @@
+#ifndef __GRAPH_H__
+#define __GRAPH_H__
+
+#include <map>
+#include <string>
+#include <fstream>
+#include "Vertex.h"
+
+
+/*
+ *  the OG graph class
+ */
+class Graph
+{
+    private:
+
+    protected:
+        std::map<long, Vertex> vertices;
+
+    public:
+
+        Graph();
+        ~Graph();
+
+        void load_txt(char* path);
+
+        long size();
+
+        void add_vertex(int name);
+
+        std::map<long, Vertex>::iterator get_vertices();
+
+};
+
+
+class BaseGraph : Graph
+{
+    private:
+
+    protected:
+
+    public:
+};
+
+
+class ColoringGraph
+{
+    private:
+        Graph* base;
+
+    protected:
+
+    public:
+};
+
+
+#endif
