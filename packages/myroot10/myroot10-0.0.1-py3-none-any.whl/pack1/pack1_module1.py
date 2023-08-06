26aa0a8ae0dc25d01f12dc9d0959875849fb09b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:48 +0000

    Add package myroot10/0.0.1/myroot10-0.0.1-py3-none-any.whl

---
+++ packages/myroot10/myroot10-0.0.1-py3-none-any.whl/pack1/pack1_module1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# import sys
+# print(sys.path)
+# # sys.path.append('')
+# import pack1_module2
+# print(pack1_module2.add())
---
+++ packages/myroot10/myroot10-0.0.1-py3-none-any.whl/pack2/pack2_module1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+# import sys
+# # dd = sys
+# # print(dd.path)
+# print(sys.path)
+# sys.path.append('f:\\Root1')
+# print(sys.path)
+
+# import pack1.pack1_module2 as _iu
+# print(_iu.add())
+
+from pack1.pack1_module2 import add
+print(add())
+# from pack1 import pack1_module2
+
+def a():
+  from pack1.pack1_module2 import c,sub
+  print(sub())
+  print(c)
+  return sub()
+
+a()
+# print(sub())
+
+
