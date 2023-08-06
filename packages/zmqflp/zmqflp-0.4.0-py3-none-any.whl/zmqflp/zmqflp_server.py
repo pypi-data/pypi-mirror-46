5971a65e65ac014c679844e7fef66cb317aa8c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:23 +0000

    Add package zmqflp/0.4.0/zmqflp-0.4.0-py3-none-any.whl

---
+++ packages/zmqflp/zmqflp-0.4.0-py3-none-any.whl/zmqflp/zmqflp_server.py
@@ -0,0 +1,68 @@
+import asyncio
+import zmq
+import zmq.asyncio
+import socket
+import cbor2
+import logging
+
+class ZMQFLPServer(object):
+    def __init__(self, custom_identity = None, str_port = '9001'):
+        ctx = zmq.asyncio.Context()
+        # Prepare server socket with predictable identity
+        if custom_identity:
+            identity = custom_identity
+        else:
+            identity = socket.gethostbyname(socket.gethostname())
+        bind_endpoint = "tcp://*:"+str(str_port)
+        connect_endpoint = ('tcp://'+identity+':'+str(str_port)).encode('utf8')
+        self.server = ctx.socket(zmq.ROUTER)
+        self.server.setsockopt(zmq.IDENTITY, connect_endpoint)
+        self.server.bind(bind_endpoint)
+        logging.info("I: service is ready with identity " + str(connect_endpoint))
+        logging.info("I: service is bound to " + str(bind_endpoint))
+
+        self.message_table = {}
+
+
+    async def receive(self):
+        # Frame 0: identity of client
+        # Frame 1: PING, or client control frame
+        # Frame 2: request body
+        try:
+            request = await self.server.recv_multipart()
+            control = request[1].decode('utf8')
+        except asyncio.CancelledError:
+            return (None, None) # Interrupted
+        except Exception as e:
+            logging.exception(e)
+            return (None, None) # Interrupted
+        if control == "PING":
+            await self.send([request[0]], "PONG".encode('utf8'), mpack=False)
+            return (request[1].decode('utf8'), [request[0]])
+        else:
+            if request[0] not in self.message_table:
+                self.message_table[request[0]] = request[1]
+                return (await asyncio.get_running_loop().run_in_executor(None,
+                                                                         cbor2.loads,
+                                                                         request[2]),
+                        request[0:2])  # , raw=False, encoding="utf-8"
+            elif self.message_table[request[0]] == request[1]:
+                return (None, None)
+            else:
+                self.message_table[request[0]] = request[1]
+                return (await asyncio.get_running_loop().run_in_executor(None,
+                                                                         cbor2.loads,
+                                                                         request[2]),
+                        request[0:2]) # , raw=False, encoding="utf-8"
+
+
+    async def send(self, orig_req_headers, str_resp, mpack=True):
+        out_message = orig_req_headers
+        if mpack:
+            out_message.append(
+                await asyncio.get_running_loop().run_in_executor(None,
+                                                                 cbor2.dumps,
+                                                                 str_resp))  # , use_bin_type=True))#.encode('utf8'))
+        else:
+            out_message.append(str_resp)
+        await self.server.send_multipart(out_message)
---
+++ packages/zmqflp/zmqflp-0.4.0-py3-none-any.whl/zmqflp/zmqflp_tests.py
@@ -0,0 +1,77 @@
+import zmqflp_managed_client
+import zmqflp_server
+import time
+from multiprocessing import Process
+import socket
+import asyncio
+import logging
+import cbor2
+
+LEN_TEST_MESSAGE = 100
+log_handlers = [logging.StreamHandler()]
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=log_handlers,
+    level=logging.DEBUG)
+
+
+def server_main():
+    asyncio.run(server_loop())
+    return 0
+
+
+async def server_loop():
+    server = zmqflp_server.ZMQFLPServer(str_port='9001')
+    keep_running = True
+    while keep_running:
+        # handle the "TEST" requests
+        (str_request, orig_headers) = await server.receive()
+        req_object = cbor2.loads(str_request)
+        if req_object != "EXIT":
+            await server.send(orig_headers, req_object)
+        elif req_object == "EXIT":
+            logging.info('server exiting...')
+            await server.send(orig_headers, "EXITING")
+            keep_running = False
+    if keep_running is False:
+        return
+
+
+def run_test(num_of_tests):
+    with zmqflp_managed_client.ZMQFLPManagedClient([socket.gethostbyname(socket.gethostname()) + ':9001']) as client:
+        for i in range(num_of_tests):
+            test_message = ["TEST" for i in range(LEN_TEST_MESSAGE)]
+            # logging.info('hi!')
+            reply = client.send_and_receive(cbor2.dumps(test_message))  # , use_bin_type=True))
+            # logging.info(reply)
+            # logging.debug('reply: '+str(reply))
+            if (len(reply) != LEN_TEST_MESSAGE) and (reply[-1] != "TEST"):  # "TEST_OK":
+                logging.debug("TEST_FAILURE")
+                raise ValueError()
+    logging.debug("ending client send")
+    return
+
+
+def main():
+    requests = 2000
+    server_process = Process(target=server_main, daemon=True)
+    server_process.start()
+    time.sleep(0.5)
+    # client_process = Process(target=client_main, args=(requests,))
+    # client = zmqflp_client.ZMQFLPClient([socket.gethostbyname(socket.gethostname()) + ':9001'])
+
+    logging.debug(">> starting zmq freelance protocol test!")
+    start = time.time()
+    run_test(requests)
+    # client_process.start()
+    # client_process.join()
+    avg_time = ((time.time() - start) / requests)
+    logging.debug(">> waiting for server to exit...")
+    server_process.join(timeout=1)
+    logging.debug("Average RT time (sec): " + str(avg_time))
+    return
+
+
+if __name__ == '__main__':
+    main()
