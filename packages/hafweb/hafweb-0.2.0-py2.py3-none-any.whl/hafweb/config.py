fb11ce88d73a560014e631af25c279350a38f6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:02 +0000

    Add package hafweb/0.2.0/hafweb-0.2.0-py2.py3-none-any.whl

---
+++ packages/hafweb/hafweb-0.2.0-py2.py3-none-any.whl/hafweb/config.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+WEB_SERVER_PORT = 8081
+
+RESOURCE_PATH = f"{os.path.dirname(__file__)}/resource"
+
+Base = declarative_base()
+
+MAIN_VERSION = 0
+SUB_VERSION = 2
+FIX_VERSION = 0
+
+VERSION = f"{MAIN_VERSION}.{SUB_VERSION}.{FIX_VERSION}"
