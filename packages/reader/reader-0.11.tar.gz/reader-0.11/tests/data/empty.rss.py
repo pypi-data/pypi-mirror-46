57f431836f5bf2d4e28f498e46233dc6e0369861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:04 +0000

    Add package reader/0.11/reader-0.11.tar.gz

---
+++ packages/reader/reader-0.11.tar.gz/reader-0.11/tests/data/empty.rss.py
@@ -0,0 +1,16 @@
+import datetime
+
+from reader import Feed, Entry, Content, Enclosure
+
+
+feed = Feed(
+    url='{}empty.rss'.format(url_base),
+)
+
+entries = [
+    Entry(
+        id='7bd204c6-1655-4c27-aeee-53f933c5395f',
+        updated=None,
+    ),
+]
+
