af739013686110369399b55b5dcf12e7c75e6b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:26 +0000

    Add package algo-base/0.0.3/algo-base-0.0.3.tar.gz

---
+++ packages/algo-base/algo-base-0.0.3.tar.gz/algo-base-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    # Metadata
+    name='algo-base',
+    version='0.0.3',
+    author='Contributors',
+    author_email='mli@amazon.com',
+    url='https://www.baidu.com',
+    description='组件开发基础工具',
+    long_description='组件开发基础工具',
+    license='Apache-2.0',
+    platforms=["all"],
+    # Package info
+    packages=['base'],
+
+    zip_safe=True,
+)
