5dd6a22f7c8a5237edf55cde121f1df82ca72c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:24 +0000

    Add package nimrodel/0.3.0/nimrodel-0.3.0-py3-none-any.whl

---
+++ packages/nimrodel/nimrodel-0.3.0-py3-none-any.whl/nimrodel/_misc.py
@@ -0,0 +1,42 @@
+import re
+
+regex_param = re.compile(r":param (.*)? (.*)?: (.*)")
+regex_return = re.compile(r":return: (.*)")
+regex_rtype = re.compile(r":rtype: (.*)")
+
+# default function to extract information from functions
+def docstring(func):
+	try:
+		docstr = func.__doc__
+		docstr = docstr.replace("\t","")
+
+		params = {}
+		returns = {}
+
+		lines = docstr.split("\n")
+		desc = []
+		for l in lines:
+			if l.startswith(":"):
+				match = regex_param.match(l)
+				if match is not None:
+					type, name, d = match.groups()
+					params[name] = {"type":type,"desc":d}
+					continue
+				match = regex_return.match(l)
+				if match is not None:
+					returns["desc"], = match.groups()
+					continue
+				match = regex_rtype.match(l)
+				if match is not None:
+					returns["type"], = match.groups()
+					continue
+
+			else:
+				desc.append(l)
+
+		desc
+		desc = "\n".join(desc).strip("\n")
+
+		return {"desc":desc,"params":params,"returns":returns}
+	except:
+		return {"desc":"","params":{},"returns":{}}
