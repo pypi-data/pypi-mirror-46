5b32273c6ed544a3b77b909e3fbb64f30ac08008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:57 +0000

    Add package QATechBasic/0.0.1/QATechBasic-0.0.1.zip

---
+++ packages/QATechBasic/QATechBasic-0.0.1.zip/QATechBasic-0.0.1/qa_tech_core_setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# author:      hj
+# create_time: 2019/5/14 16:12
+# update_time: 2019/5/14 16:12
+from distutils.core import setup
+
+setup(
+    name='QATechBasic',  # 对外我们模块的名字
+    version='0.0.1',  # 版本号
+    description='齐安科技基础包',  # 描述
+    author='hj',  # 作者
+    author_email='56172032@qq.com',
+    py_modules=['core.configuration']  # 要发布的模块
+)
