7b7927587ffb9e7dea563ee2568fb68f66df9599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:21 +0000

    Add package ambition-screening/0.1.28/ambition_screening-0.1.28-py3-none-any.whl

---
+++ packages/ambition-screening/ambition_screening-0.1.28-py3-none-any.whl/ambition_screening/choices.py
@@ -0,0 +1,7 @@
+from edc_constants.constants import YES, NO, NOT_APPLICABLE
+
+PREG_YES_NO_NA = (
+    (YES, "Yes"),
+    (NO, "No"),
+    (NOT_APPLICABLE, "Not Applicable: e.g. male, post-menopausal"),
+)
