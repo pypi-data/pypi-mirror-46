7595ca239ab3a1d3e0f6fd033b667e77399e3971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:54 +0000

    Add package dsnd-lz-distributions/0.1/dsnd_lz_distributions-0.1.tar.gz

---
+++ packages/dsnd-lz-distributions/dsnd_lz_distributions-0.1.tar.gz/dsnd_lz_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_lz_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_lz_distributions'],
+      author='Liankun Zou',
+      author_email = 'zz.guku@gmail.com',
+      zip_safe=False)
