2fb802187cfb5eb604808c98bbba37761d749c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:24 +0000

    Add package COWBAT/0.5.0.0/COWBAT-0.5.0.0.tar.gz

---
+++ packages/COWBAT/COWBAT-0.5.0.0.tar.gz/COWBAT-0.5.0.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+__author__ = 'adamkoziol'
+setup(
+    name="COWBAT",
+    version="0.5.0.0",
+    include_package_data=True,
+    packages=find_packages(),
+    scripts=['cowbat/assembly_pipeline.py', 'cowbat/assembly_typing.py', 'cowbat/validation/validate_data.py'],
+    license='MIT',
+    author='Adam Koziol',
+    author_email='adam.koziol@canada.ca',
+    description='CFIA OLC Workflow for Bacterial Assembly and Typing',
+    url='https://github.com/OLC-Bioinformatics/COWBAT',
+    long_description=open('README.md').read(),
+    install_requires=['validator_helper',
+                      'olctools']
+)
