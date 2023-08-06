054b39020ad029be5e7c3c6348564bd90a9b8850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:02 +0000

    Add package clicmod/0.7.9/clicmod-0.7.9.tar.gz

---
+++ packages/clicmod/clicmod-0.7.9.tar.gz/clicmod-0.7.9/onedesk/client/client.py
@@ -0,0 +1,21 @@
+import logging
+
+logger = logging.getLogger('main')
+
+
+def get_client(session, instance, client_name):
+    params = {'size': 10000}  # TODO this could be better
+    client_list = session.get(instance + "/api/clients", params=params)
+    logger.debug('Get client %s Response: %s', client_name, client_list.status_code)
+    logger.debug(client_list.json())
+
+    for client in client_list.json()['content']:
+        if client['code'] == client_name:
+            return client
+
+    logger.debug("No client with the provided name found, exiting...")
+    raise SystemExit
+
+
+def create_client(name):  # TODO implement this
+    raise NotImplementedError
