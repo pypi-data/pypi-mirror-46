ae0c6a4a570c89e545e7e73189fb3f8355655266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:03 +0000

    Add package fortiosapi/0.10.6/fortiosapi-0.10.6.tar.gz

---
+++ packages/fortiosapi/fortiosapi-0.10.6.tar.gz/fortiosapi-0.10.6/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import setuptools
+
+
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='fortiosapi',
+    version='0.10.6',
+    description="Python modules to use Fortigate APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # Valid Classifiers are here:
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python ',
+        'Topic :: Security',
+    ],
+    keywords='Fortinet fortigate fortios rest api',
+    packages=setuptools.find_packages(),
+    author='Nicolas Thomas',
+    author_email='nthomas@fortinet.com',
+    url='https://github.com/fortinet-solutions-cse/fortiosapi',
+    include_package_data=True,
+)
