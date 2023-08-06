6c4f8a1a59508fecb117399671c8e0f8adb1d954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:54 +0000

    Add package pyrolite/0.1.16/pyrolite-0.1.16-py3-none-any.whl

---
+++ packages/pyrolite/pyrolite-0.1.16-py3-none-any.whl/pyrolite/mineral/__init__.py
@@ -0,0 +1,13 @@
+"""
+Submodule for working with mineral data.
+"""
+import logging
+from .mineral import Mineral
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logger = logging.getLogger(__name__)
+
+
+# Todo: class attributes to aggregate all collected minerals
+
+__db__ = Mineral.db
