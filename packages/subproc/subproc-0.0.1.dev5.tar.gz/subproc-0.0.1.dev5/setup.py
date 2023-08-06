2336016db13e6ec2c94667299191804ff5262bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:39 +0000

    Add package subproc/0.0.1.dev5/subproc-0.0.1.dev5.tar.gz

---
+++ packages/subproc/subproc-0.0.1.dev5.tar.gz/subproc-0.0.1.dev5/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['subproc']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'subproc',
+    'version': '0.0.1.dev5',
+    'description': 'Provides utility functions around subprocess module.',
+    'long_description': None,
+    'author': 'Mustafa Caylak',
+    'author_email': 'mustafa.caylak@web.de',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+}
+
+
+setup(**setup_kwargs)
