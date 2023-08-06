31beee36c5ae0f31631bf5cfb426ed617646bdc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:12 +0000

    Add package restream/0.0.2/restream-0.0.2.tar.gz

---
+++ packages/restream/restream-0.0.2.tar.gz/restream-0.0.2/setup.py
@@ -0,0 +1,16 @@
+"""
+Setup file
+"""
+
+
+from setuptools import setup
+
+
+setup(
+    name='restream',
+    version='0.0.2',
+    description='A python library to parse strings based on tokens and grammar rules',
+    url='https://github.com/mohitudupa/restream.git',
+    py_modules=['restream'],
+    install_requires=[],
+)
