581b72bdd3c04492cda8b122ae72bd2b78a38cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:12 +0000

    Add package docker-hub/2.0.0/docker_hub-2.0.0-py2.py3-none-any.whl

---
+++ packages/docker-hub/docker_hub-2.0.0-py2.py3-none-any.whl/src/commands/tags.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+
+import dateutil.parser
+
+from ..libs.utils import *
+from ..libs.config import Config
+
+def run(docker_hub_client, args):
+    """ The command to list tags for given repo on docker hub
+
+        >>> from ..tests.docker_hub_client import TestingDockerHubClient
+        >>> from collections import namedtuple
+        >>> args = namedtuple('args', 'orgname reponame page')
+        >>> docker_hub_client = TestingDockerHubClient()
+        >>> run(docker_hub_client,
+        ...     args(orgname='docker', reponame='docker', page='1'))
+    """
+    config = Config()
+    orgname = args.orgname or config.get('orgname')
+    resp = docker_hub_client.get_tags(orgname, args.reponame, args.page)
+
+    if resp['code'] == 200:
+        if resp['content']['count'] > 0:
+            rows = []
+            for repo in resp['content']['results']:
+                formatted_date = ''
+                if repo['last_updated']:
+                    formatted_date = dateutil.parser \
+                                      .parse(repo['last_updated'])
+                    formatted_date = formatted_date.strftime("%Y-%m-%d %H:%M")
+                # Convert full_size in bytes to KB
+                size_in_kb = repo['full_size'] / 1024
+                formatted_size = readableMemoryFormat(size_in_kb)
+                rows.append([repo['name'], formatted_size, formatted_date])
+            header = ['Name', 'Size', 'Last updated']
+            print_result(args.format, rows, header, resp['content']['count'],
+                         args.page)
+        else:
+            print('This repo has no tags')
+    else:
+        print('Error fetching tags for: {0}/{1}'.
+              format(orgname, args.reponame))
