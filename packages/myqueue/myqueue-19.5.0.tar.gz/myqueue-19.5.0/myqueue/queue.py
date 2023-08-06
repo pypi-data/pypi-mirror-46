67a0734b02f5cfc51476f9ef51d056c2758cf039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:04 +0000

    Add package myqueue/19.5.0/myqueue-19.5.0.tar.gz

---
+++ packages/myqueue/myqueue-19.5.0.tar.gz/myqueue-19.5.0/myqueue/queue.py
@@ -0,0 +1,43 @@
+from myqueue.task import Task
+from typing import Set
+
+
+class Queue:
+    def submit(self, task: Task) -> None:
+        pass
+
+    def update(self, id: int, state: str) -> None:
+        pass
+
+    def kick(self) -> None:
+        pass
+
+    def timeout(self, Task) -> bool:
+        return False
+
+    def cancel(self, task: Task) -> None:
+        raise NotImplementedError
+
+    def get_ids(self) -> Set[int]:
+        raise NotImplementedError
+
+    def hold(self, task: Task) -> None:
+        raise NotImplementedError
+
+    def release_hold(self, task: Task) -> None:
+        raise NotImplementedError
+
+
+def get_queue(name: str) -> Queue:
+    name = name.lower()
+    if name == 'local':
+        from myqueue.local import LocalQueue
+        return LocalQueue()
+    if name == 'slurm':
+        from myqueue.slurm import SLURM
+        return SLURM()
+    if name == 'pbs':
+        from myqueue.pbs import PBS
+        return PBS()
+    else:
+        assert 0
