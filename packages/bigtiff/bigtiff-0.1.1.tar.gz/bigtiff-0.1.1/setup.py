0cfa9feba084435fe86705f3ee36a6738a8bb76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:16 +0000

    Add package bigtiff/0.1.1/bigtiff-0.1.1.tar.gz

---
+++ packages/bigtiff/bigtiff-0.1.1.tar.gz/bigtiff-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='bigtiff',
+    version='0.1.1',
+    description='A library for random access for large (Big-)TIFF files',
+    author='Manuel Schölling',
+    author_email='manuel.schoelling@dzne.de',
+    license='GPL3',
+    packages=['bigtiff'],
+    zip_safe=False,
+    install_requires=[
+        'numpy',
+        'kaitaistruct==0.8.0'
+    ],
+)
