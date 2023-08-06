9dfd8ca93391350ee98840f486e16caa9fbdf601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:58 +0000

    Add package dcacheclient/0.0.5/dcacheclient-0.0.5.tar.gz

---
+++ packages/dcacheclient/dcacheclient-0.0.5.tar.gz/dcacheclient-0.0.5/setup.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+
+"""
+    User and administration interaction with dCache  # noqa: E501
+"""
+
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "dcacheclient"
+VERSION = "0.0.5"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = [
+#   "urllib3 >= 1.23",
+    "six >= 1.10",
+    "requests >= 2.6.0",
+    "urllib3<1.25,>=1.21.1",
+    "certifi",
+    "python-dateutil",
+    "argcomplete >= 1.9.4",
+    "sseclient"]
+#    "rucio-clients"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="",
+    author_email="support@dCache.org",
+    url="https://www.dcache.org",
+    keywords=["dCache", "storage"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    User and administration interaction with dCache  # noqa: E501
+    """,
+    entry_points={
+        'console_scripts': ['dcache-admin=dcacheclient.dcache_admin:main',]
+    },
+)
