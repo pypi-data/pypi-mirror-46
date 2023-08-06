42e64d062893f0f8d4609ed2f056379b97081853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:24 +0000

    Add package pulp-file-client/0.0.1b10.dev1557780140/pulp_file-client-0.0.1b10.dev1557780140.tar.gz

---
+++ packages/pulp-file-client/pulp_file-client-0.0.1b10.dev1557780140.tar.gz/pulp_file-client-0.0.1b10.dev1557780140/pulpcore/client/pulp_file/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_file.api.content_api import ContentApi
+from pulpcore.client.pulp_file.api.distributions_api import DistributionsApi
+from pulpcore.client.pulp_file.api.publications_api import PublicationsApi
+from pulpcore.client.pulp_file.api.publishers_api import PublishersApi
+from pulpcore.client.pulp_file.api.remotes_api import RemotesApi
