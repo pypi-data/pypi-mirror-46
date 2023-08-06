589c41cfa2ab363c09375779b0edf627cd52f8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:14 +0000

    Add package mindsdb-server/0.9.2/mindsdb_server-0.9.2-py3-none-any.whl

---
+++ packages/mindsdb-server/mindsdb_server-0.9.2-py3-none-any.whl/mindsdb_server/server.py
@@ -0,0 +1,22 @@
+from mindsdb_server.namespaces.predictor import ns_conf as predictor_ns
+from mindsdb_server.namespaces.datasource import ns_conf as datasource_ns
+from mindsdb_server.shared_ressources import get_shared
+import json
+import os
+
+
+
+
+def start():
+    app, api = get_shared()
+
+    api.add_namespace(predictor_ns)
+    api.add_namespace(datasource_ns)
+    os.makedirs('storage', exist_ok=True)
+    os.makedirs('tmp', exist_ok=True)
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+
+    start()
