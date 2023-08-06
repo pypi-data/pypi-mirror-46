7a2aefdc48336ccfb1ac46eaf95e1cf03481837c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:18 +0000

    Add package nodejsscan/3.6/nodejsscan-3.6.tar.gz

---
+++ packages/nodejsscan/nodejsscan-3.6.tar.gz/nodejsscan-3.6/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nodejsscan",
+    description='Static security code scanner (SAST) for Node.js applications',
+    version="3.6",
+    author="Ajin Abraham",
+    author_email="ajin25@gmail.com",
+    license='GPL v3',
+    packages=find_packages(include=[
+        "core",
+        "core.*",
+    ]),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3'
+    ],
+    entry_points={
+        'console_scripts': [
+            "nodejsscan = core.cli:main",
+        ]
+    },
+
+    # Include additional files into the package
+    include_package_data=True,
+    # Details
+    url="http://pypi.python.org/pypi/nodejsscan/",
+
+    long_description='Static security code scanner (SAST) for Node.js applications',
+    # Dependent packages (distributions)
+    install_requires=[
+        "jsbeautifier==1.10.0",
+        "defusedxml==0.6.0",
+    ],
+)
