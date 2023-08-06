345d7455f232781c4f653d2bb2895ae525425db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:17 +0000

    Add package mlcollect/0.0.1/mlcollect-0.0.1-py3-none-any.whl

---
+++ packages/mlcollect/mlcollect-0.0.1-py3-none-any.whl/cnn/__init__.py
@@ -0,0 +1,14 @@
+from .lenet import LeNet
+from .resnet import ResNet
+from .alexnet import AlexNet
+from .minivggnet import MiniVGGNet
+from .shallownet import ShallowNet
+from .minigooglenet import MiniGoogLeNet
+from .mxagegendernet import MxAgeGenderNet
+from .deepergooglenet import DeeperGoogLeNet
+
+from keras.applications import VGG16
+from keras.applications import VGG19
+from keras.applications import Xception
+from keras.applications import ResNet50
+from keras.applications import InceptionV3
