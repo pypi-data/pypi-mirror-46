1da2a86ce93c72bd242e0435f4b344c0fe9ab888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:17 +0000

    Add package remediate/0.0.7/remediate-0.0.7.tar.gz

---
+++ packages/remediate/remediate-0.0.7.tar.gz/remediate-0.0.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="remediate",
+    version="0.0.7",
+    packages=['remediate'],
+    include_package_data=True,
+    install_requires=['boto3'],
+    python_requires='~=3.7',
+    license='MIT',
+    author='cloudcraeft',
+    author_email='cloudcraeft@outloook.com',
+    url='https://github.com/cloudcraeft/remediate',
+    description='integrate previously written remediation code',
+)
