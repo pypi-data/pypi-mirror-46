9765fe993e4c3c9248ae02322757237c717c23be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:56 +0000

    Add package ruengdech/0.2/ruengdech-0.2.tar.gz

---
+++ packages/ruengdech/ruengdech-0.2.tar.gz/ruengdech-0.2/ruengdech/myname.py
@@ -0,0 +1,7 @@
+#myname.py
+#this is my Test first Pypi
+def my_info():
+	print('Name: Ruengdech La-orpongpaisan')
+	print('Tel: 6681443772')
+	print('Email: ruegndech@gmail.com')
+	print('VERSION 2')
