28497fa2e5fdfd5aa66a40f133a5f1a374831cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:42 +0000

    Add package deadjson/1.0.6/deadjson-1.0.6.tar.gz

---
+++ packages/deadjson/deadjson-1.0.6.tar.gz/deadjson-1.0.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='deadjson',
+    version='1.0.6',
+    description='A Python library implementing a dict-like object that writes to a JSON file as it is modified',
+    url='https://github.com/adityanugraha98/deadjson',
+    packages= ["deadjson"],
+    author='ADITYA NUGRAHA',
+    author_email='adityana987@gmail.com',
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Games/Entertainment',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    py_modules=["deadjson"],
+    project_urls={
+        'Bug Reports': 'https://github.com/adityanugraha98/deadjson/issues',
+    },
+)
