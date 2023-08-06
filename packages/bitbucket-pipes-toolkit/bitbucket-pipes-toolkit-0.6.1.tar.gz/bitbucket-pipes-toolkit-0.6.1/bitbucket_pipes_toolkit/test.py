4f8f8e06a3190e669013abc5dd180ac74f661409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:46 +0000

    Add package bitbucket-pipes-toolkit/0.6.1/bitbucket-pipes-toolkit-0.6.1.tar.gz

---
+++ packages/bitbucket-pipes-toolkit/bitbucket-pipes-toolkit-0.6.1.tar.gz/bitbucket-pipes-toolkit-0.6.1/bitbucket_pipes_toolkit/test.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+import os
+
+import docker
+from docker.errors import ContainerError
+
+class PipeTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.image_tag = 'bitbucketpipelines/demo-pipe-python:ci' + \
+            os.getenv('BITBUCKET_BUILD_NUMBER', 'local')
+        cls.docker_client = docker.from_env()
+        cls.docker_client.images.build(
+            path='.', tag=cls.image_tag)
+
+    def run_container(self, cmd=None, **kwargs):
+        cwd = os.getcwd()
+        try:
+            return self.docker_client.containers.run(self.image_tag, 
+                                                     command=cmd, 
+                                                     volumes={cwd: {'bind': cwd, 'mode': 'rw'}},
+                                                     working_dir=cwd,
+                                                     **kwargs).decode()
+        except ContainerError as e:
+            return e.container.logs().decode()
+
+
+class PipeTestCaseTestCase(PipeTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        with open('Dockerfile', 'w') as f:
+            f.write("FROM python:3.7")
+        super().setUpClass()
+
+    def test_the_test(self):
+        result = self.run_container('echo hello world')
+        self.assertIn(b'hello world', result)
