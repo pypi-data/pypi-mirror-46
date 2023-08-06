160eaae85ba1e08961c40dbde8e22e11055005f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:45 +0000

    Add package Py-SearchAddress/0.0.1/Py-SearchAddress-0.0.1.tar.gz

---
+++ packages/Py-SearchAddress/Py-SearchAddress-0.0.1.tar.gz/Py-SearchAddress-0.0.1/Py_search_address/__init__.py
@@ -0,0 +1,2 @@
+from Py_search_address import search_address
+from Py_search_address.search_address import Get_Address
---
+++ packages/Py-SearchAddress/Py-SearchAddress-0.0.1.tar.gz/Py-SearchAddress-0.0.1/Py_search_address/py_search_address.py
@@ -0,0 +1,76 @@
+import requests
+import  xml.etree.ElementTree as ET
+
+
+class Search_Cep:
+        
+    def __request_WebService(self):
+        
+        try:
+            #print(self.__cep)
+            #url webservice com o paramentro cep
+            url="https://viacep.com.br/ws/{}/xml/".format(self.__cep)
+            #informa o tipo do retorno que deseja do webservice (XML/JSon e etc).. Armazena em um dict
+            retorno_xml={ 'Accept': 'application/xml' }
+            #Realizando a requisição pro webservice com GET
+            requisicao=requests.get(url,headers=retorno_xml)
+            #Guardando o resultado da requição na bibilioteca xml.etree.ElementTree que nos ajudar a acessar as tags XML
+            ELEMENTO=ET.ElementTree(ET.fromstring(requisicao.content))
+            #agora só acessar as tags e atribuir em suas variaveis ou nos atributos da classe
+            #atribuindo os valores nos atributos das classes.
+            #print(ELEMENTO.find("logradouro").text)
+            self.__logradouro = ELEMENTO.find("logradouro").text
+            self.__bairro = ELEMENTO.find("bairro").text
+            self.__cidade=ELEMENTO.find("localidade").text
+            self.__uf=ELEMENTO.find("uf").text
+            return 1
+
+        except:
+            print("Não foi possivel consultar no webservice, verifique se o CEP está correto ou se existe! ") 
+            return 0
+        
+
+
+  
+        
+    def __init__(self,CEP):
+        
+        CEP = CEP.replace("-","")
+        CEP = CEP.replace("/","")
+        CEP = CEP.replace(" ","")
+        CEP = CEP.replace(".","")
+        CEP = CEP.strip()
+        self.__cep=CEP
+        self.__logradouro=""
+        self.__bairro=""
+        self.__cidade=""
+        self.__uf=""
+        self.__request_WebService()  
+     
+    @property
+    def cep(self):
+        return self.__cep
+    @property
+    def logradouro(self):
+        return self.__logradouro
+    @property
+    def bairro(self):
+        return self.__bairro
+    @property
+    def cidade(self):
+        return self.__cidade
+    @property
+    def uf(self):
+        return self.__uf
+    
+    @cep.setter
+    def cep(self,new_cep):
+        self.__cep=new_cep
+        self.__request_WebService() 
+    
+    
+    def __str__(self):
+        return f"Rua : {self.__logradouro} bairro:{self.__bairro} cidade: {self.__cidade} Estado: {self.__uf} cep:{self.__cep}"
+        
+        
+    
