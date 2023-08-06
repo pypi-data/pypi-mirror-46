33bbcaa12098529dd1811f66a87cfb58eabe560a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:08 +0000

    Add package khaliunmyfindmax/1.0.0/khaliunmyfindmax-1.0.0.tar.gz

---
+++ packages/khaliunmyfindmax/khaliunmyfindmax-1.0.0.tar.gz/khaliunmyfindmax-1.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="khaliunmyfindmax",
+    version="1.0.0",
+    long_description=Path("README.md").read_text(),
+    # long_description="",
+    packages=setuptools.find_packages(exclude=[".vscode"])
+)
