97e892f45aa70bd24de87877af2973a3688d10fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:11 +0000

    Add package libnum/1.5/libnum-1.5.tar.gz

---
+++ packages/libnum/libnum-1.5.tar.gz/libnum-1.5/libnum/__init__.py
@@ -0,0 +1,19 @@
+#-*- coding:utf-8 -*-
+"""
+libnum - Python library for some numbers functions:
+  - working with primes (generating, primality tests)
+  - common maths (gcd, lcm, modulo inverse, Jacobi symbol, sqrt)
+  - elliptic curve cryptography functions
+"""
+
+from libnum.lib.primes import *
+from libnum.lib.factorize import *
+from libnum.lib.common import *
+from libnum.lib.modular import *
+from libnum.lib.sqrtmod import *
+from libnum.lib.stuff import *
+from libnum.lib.chains import *
+
+__author__ = "Jafar Akhondali (jafar.akhondali@yahoo.com)"
+__license__ = "MIT"
+__version__ = "1.5"
