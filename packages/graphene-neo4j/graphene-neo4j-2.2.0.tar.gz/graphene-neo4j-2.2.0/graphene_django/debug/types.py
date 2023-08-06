7091bd6d1a3adaf2fc36b5c1d3bbb96e3cb6e4bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:44 +0000

    Add package graphene-neo4j/2.2.0/graphene-neo4j-2.2.0.tar.gz

---
+++ packages/graphene-neo4j/graphene-neo4j-2.2.0.tar.gz/graphene-neo4j-2.2.0/graphene_django/debug/types.py
@@ -0,0 +1,13 @@
+from graphene import List, ObjectType
+
+from .sql.types import DjangoDebugSQL
+
+
+class DjangoDebug(ObjectType):
+    class Meta:
+        description = "Debugging information for the current query."
+
+    sql = List(
+        DjangoDebugSQL,
+        description="Executed SQL queries for this API query.",
+    )
