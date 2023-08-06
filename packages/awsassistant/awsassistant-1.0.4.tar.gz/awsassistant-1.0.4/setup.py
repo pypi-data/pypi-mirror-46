eb6d6d66fe86b8ec3a5a13bf77cfc6e330698be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:33 +0000

    Add package awsassistant/1.0.4/awsassistant-1.0.4.tar.gz

---
+++ packages/awsassistant/awsassistant-1.0.4.tar.gz/awsassistant-1.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+version = "1.0.4"
+
+long_descr = "This is awshelper develope for developer to easy develope aws services"
+
+
+
+setup(name = "awsassistant",
+    packages = ["Services","Services.cloudwatch","Services.display"],
+    entry_points = {
+        "console_scripts": ['awsassistant = Services.awshelper:main']
+        },
+    version = version,
+    description = "Python command line application to help us with aws api",
+    long_description = long_descr,
+    author = "devesh bajaj",
+    author_email = "deveshbajaj59@gmail.com",
+    install_requires=[
+        'pytz','prettytable','colorama','boto3'
+        ],
+    )
