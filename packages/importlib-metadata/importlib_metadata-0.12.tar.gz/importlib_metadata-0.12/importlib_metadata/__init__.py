a2fed3e00ebb5d25fff8145b17b34e7ad563956f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:55 +0000

    Add package importlib-metadata/0.12/importlib_metadata-0.12.tar.gz

---
+++ packages/importlib-metadata/importlib_metadata-0.12.tar.gz/importlib_metadata-0.12/importlib_metadata/__init__.py
@@ -0,0 +1,22 @@
+from .api import (
+    Distribution, PackageNotFoundError, distribution, distributions,
+    entry_points, files, metadata, requires, version)
+
+# Import for installation side-effects.
+from . import _hooks  # noqa: F401
+
+
+__all__ = [
+    'Distribution',
+    'PackageNotFoundError',
+    'distribution',
+    'distributions',
+    'entry_points',
+    'files',
+    'metadata',
+    'requires',
+    'version',
+    ]
+
+
+__version__ = version(__name__)
