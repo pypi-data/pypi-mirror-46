66a2acf754e80ffcf15d8d244650b1ec5708171d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:12 +0000

    Add package xgo-spider-log/0.0.10/xgo-spider-log-0.0.10.tar.gz

---
+++ packages/xgo-spider-log/xgo-spider-log-0.0.10.tar.gz/xgo-spider-log-0.0.10/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='xgo-spider-log',
+    version='0.0.10',
+    author='cielpy',
+    author_email='beijiu572@gmail.com',
+    url='https://github.com/xgoteam/xgo-spider-log',
+    description='XGo 爬虫日志',
+    packages=find_packages(),
+)
