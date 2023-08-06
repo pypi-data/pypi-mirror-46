027384957700621cc2220a58654f67282e3c6107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:03 +0000

    Add package myroot99/0.0.1/myroot99-0.0.1.tar.gz

---
+++ packages/myroot99/myroot99-0.0.1.tar.gz/myroot99-0.0.1/pack1/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# __all__ = ['','']
+# __all__ = ['pack1_module1']
+# __all__ = ['pack1_module2','pack1_module1']
---
+++ packages/myroot99/myroot99-0.0.1.tar.gz/myroot99-0.0.1/pack1/pack1_module1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# import sys
+# print(sys.path)
+# # sys.path.append('')
+# import pack1_module2
+# print(pack1_module2.add())
+
+import sys
+# dd = sys
+print(sys.path)
+# print(sys.path) 
+# sys.path.append('f:\\Root1')
+# print(sys.path)
+
+import pack1.pack1_module2 as _iu
+print(_iu.add())
