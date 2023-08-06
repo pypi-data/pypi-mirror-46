b48eec9bfa86f798065cbfd8f1726f5b1313ea62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:47 +0000

    Add package lbforaging/1.0.6/lbforaging-1.0.6.tar.gz

---
+++ packages/lbforaging/lbforaging-1.0.6.tar.gz/lbforaging-1.0.6/lbforaging/__init__.py
@@ -0,0 +1,20 @@
+from gym.envs.registration import registry, register, make, spec
+from itertools import product
+
+sizes = range(5,11)
+players = range(2,5)
+foods = range(2,5)
+
+for s, p, f in product(sizes, players, foods):
+    register(
+        id="Foraging-{0}x{0}-{1}p-{2}f-v0".format(s, p, f),
+        entry_point="lbforaging.foraging:ForagingEnv",
+        kwargs={
+            "players": p,
+            "max_player_level": 3,
+            "field_size": (s, s),
+            "max_food": f,
+            "sight": s,
+            "max_episode_steps": 50,
+        },
+    )
