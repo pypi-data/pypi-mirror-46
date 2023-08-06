e00bad37e5790864200349faff08c46a32450c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:10 +0000

    Add package python-starter-pack/0.1.5/python_starter_pack-0.1.5-py3-none-any.whl

---
+++ packages/python-starter-pack/python_starter_pack-0.1.5-py3-none-any.whl/python_starter_pack/funcs.py
@@ -0,0 +1,12 @@
+# coding: utf8
+
+def some_func():
+    """Some random function that can be imported from unit tests.
+    """
+    return True
+
+
+def say_hello():
+    """Entry-point function
+    """
+    print("This is the Python Starter Pack.\nCheck details in Github: https://github.com/apiad/python-starter-pack.")
