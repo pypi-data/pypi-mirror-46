53894fb3f7a0ec56694600e1f20869899386aeef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:40 +0000

    Add package fileparser/0.0/fileparser-0.0.tar.gz

---
+++ packages/fileparser/fileparser-0.0.tar.gz/fileparser-0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import fileparser
+
+
+setup(
+    name='fileparser',
+    version='0.0',
+    author='HH-MWB',
+    author_email='h.hong@mail.com',
+    description='',
+    long_description='',
+    long_description_content_type='text/markdown',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    py_modules=['fileparser'],
+    python_requires='>=3',
+)
