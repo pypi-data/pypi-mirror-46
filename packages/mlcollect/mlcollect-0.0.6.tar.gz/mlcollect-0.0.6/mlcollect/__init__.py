e0d025d498dfe48ecb5dda4a4a1041c30d4eb946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:25 +0000

    Add package mlcollect/0.0.6/mlcollect-0.0.6.tar.gz

---
+++ packages/mlcollect/mlcollect-0.0.6.tar.gz/mlcollect-0.0.6/mlcollect/__init__.py
@@ -0,0 +1,10 @@
+from cnn.lenet import LeNet
+from cnn.resnet import ResNet
+from cnn.alexnet import AlexNet
+from cnn.shallownet import ShallowNet 
+from cnn.minivggnet import MiniVGGNet
+from cnn.agegendernet import AgeGenderNet
+from cnn.minigooglenet import MiniGoogLeNet
+from cnn.deepergooglenet import DeeperGoogLeNet
+
+from visualize.scatter import point_scatter
