7a3a391fa3cf3c406222265ee9d6edf3b56ae331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:48 +0000

    Add package aimmo/0.20.0b4021/aimmo-0.20.0.b4021.tar.gz

---
+++ packages/aimmo/aimmo-0.20.0.b4021.tar.gz/aimmo-0.20.0.b4021/aimmo-game/simulation/django_communicator.py
@@ -0,0 +1,18 @@
+import requests
+
+
+class DjangoCommunicator(object):
+    """
+    This class encapsulates the communication between aimmo-game
+    and the django server
+    """
+
+    def __init__(self, django_api_url, completion_url):
+        self.django_api_url = django_api_url
+        self.completion_url = completion_url
+
+    def get_game_metadata(self):
+        return requests.get(self.django_api_url).json()
+
+    def mark_game_complete(self, data=None):
+        return requests.post(requests.post(self.completion_url, json=data))
