e7823a5e3942f4e24065049949fc2f8b29a02002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:57 +0000

    Add package ifolder/1.3.9/ifolder-1.3.9.tar.gz

---
+++ packages/ifolder/ifolder-1.3.9.tar.gz/ifolder-1.3.9/ifolder/send.py
@@ -0,0 +1,16 @@
+from socketTransfering import Sender
+
+ip = ('192.168.1.127',10086)
+ip = ('127.0.0.1',10087)
+
+# files or folders to be sent. list
+files = [
+'/Users/vt/Desktop/TEST/TEST.pdf',
+'/Users/vt/Desktop/TEST/icon',
+'/Users/vt/Desktop/TEST/wonderbits', 
+]
+
+tx = Sender(ip)
+
+tx.send(files)
+
