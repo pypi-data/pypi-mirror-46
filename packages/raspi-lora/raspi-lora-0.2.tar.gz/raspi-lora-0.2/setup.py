6f573f91affb1e569a250217a1c9bced7d85fd68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:06 +0000

    Add package raspi-lora/0.2/raspi-lora-0.2.tar.gz

---
+++ packages/raspi-lora/raspi-lora-0.2.tar.gz/raspi-lora-0.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='raspi-lora',
+    version='0.2',
+    packages=['raspi_lora'],
+    url='https://gitlab.com/the-plant/raspi-lora',
+    license='MIT',
+    author='Anne Wood',
+    author_email='anne.w@fastmail.us',
+    description='LoRa RFM9x library for Raspberry Pi inspired by RadioHead',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Hardware',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='lora rfm95 rfm9x rfm96 rfm97 rfm98 hardware raspberrypi',
+    install_requires=[
+        'RPi.GPIO',
+        'spidev'],
+)
