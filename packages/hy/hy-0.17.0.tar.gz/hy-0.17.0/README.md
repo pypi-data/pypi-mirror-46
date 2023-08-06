e818df0aa59ff69e2db9111a30f9dfd54860ddef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:25 +0000

    Add package hy/0.17.0/hy-0.17.0.tar.gz

---
+++ packages/hy/hy-0.17.0.tar.gz/hy-0.17.0/README.md
@@ -0,0 +1,50 @@
+Hy
+==
+
+[![Build Status](https://img.shields.io/travis/hylang/hy/master.svg)](https://travis-ci.org/hylang/hy)
+[![Version](https://img.shields.io/pypi/v/hy.svg)](https://pypi.python.org/pypi/hy)
+
+<a href="https://xkcd.com/224/"><img title="We lost the documentation on quantum mechanics. You'll have to decode the regexes yourself." alt="XKCD #224" src="https://raw.github.com/hylang/shyte/18f6925e08684b0e1f52b2cc2c803989cd62cd91/imgs/xkcd.png"></a>
+
+Lisp and Python should love each other. Let's make it happen. [Try it](http://try-hy.appspot.com/).
+
+Hylarious Hacks
+---------------
+
+* [Django + Lisp](https://github.com/paultag/djlisp/tree/master/djlisp)
+* [Python `sh` Fun](https://twitter.com/paultag/status/314925996442796032)
+* [Hy IRC Bot](https://github.com/hylang/hygdrop)
+* [miniKanren in Hy](https://github.com/algernon/adderall)
+
+OK, so, why?
+------------
+
+Well. Python is awesome. So awesome, that we have so many tools to alter the
+language in a *core* way, but we never use them.
+
+Why?
+
+Well, I wrote Hy to help people realize one thing about Python:
+
+It's really awesome.
+
+Oh, and lisps are neat.
+
+![Cuddles the Hacker](https://i.imgur.com/QbPMXTN.png)
+
+(fan art from the one and only [doctormo](http://doctormo.deviantart.com/art/Cuddles-the-Hacker-372184766))
+
+Project
+-------
+
+* Code: https://github.com/hylang/hy
+* Documentation:
+  * stable, for use with the latest stable release: http://hylang.org/
+  * master, for use with the latest revision on GitHub: http://docs.hylang.org/en/master
+* Quickstart: http://hylang.org/en/stable/quickstart.html
+* Bug reports: We have no bugs! Your bugs are your own! (https://github.com/hylang/hy/issues)
+* License: MIT (Expat)
+* [Hacking on Hy](http://docs.hylang.org/en/master/hacking.html)
+  * [Contributor Guidelines](http://docs.hylang.org/en/master/hacking.html#contributor-guidelines)
+  * [Code of Conduct](http://docs.hylang.org/en/master/hacking.html#contributor-code-of-conduct)
+* IRC: Join #hy on [freenode](https://webchat.freenode.net/)
---
+++ packages/hy/hy-0.17.0.tar.gz/hy-0.17.0/setup.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# Copyright 2019 the authors.
+# This file is part of Hy, which is free software licensed under the Expat
+# license. See the LICENSE.
+
+import sys, os
+
+from setuptools import find_packages, setup
+from setuptools.command.install import install
+import fastentrypoints   # Monkey-patches setuptools.
+
+from get_version import __version__
+
+os.chdir(os.path.split(os.path.abspath(__file__))[0])
+
+PKG = "hy"
+
+long_description = """Hy is a Python <--> Lisp layer. It helps
+make things work nicer, and lets Python and the Hy lisp variant play
+nice together. """
+
+class Install(install):
+    def run(self):
+        # Import each Hy module to ensure it's compiled.
+        import os, importlib
+        for dirpath, _, filenames in sorted(os.walk("hy")):
+            for filename in sorted(filenames):
+                if filename.endswith(".hy"):
+                    importlib.import_module(
+                        dirpath.replace("/", ".").replace("\\", ".") +
+                        "." + filename[:-len(".hy")])
+        install.run(self)
+
+install_requires = [
+    'rply>=0.7.7',
+    'astor>=0.8',
+    'funcparserlib>=0.3.6',
+    'clint>=0.4']
+if os.name == 'nt':
+    install_requires.append('pyreadline>=2.1')
+
+ver = sys.version_info[0]
+
+setup(
+    name=PKG,
+    version=__version__,
+    install_requires=install_requires,
+    cmdclass=dict(install=Install),
+    entry_points={
+        'console_scripts': [
+            'hy = hy.cmdline:hy_main',
+            'hy%d = hy.cmdline:hy_main' % ver,
+            'hyc = hy.cmdline:hyc_main',
+            'hyc%d = hy.cmdline:hyc_main' % ver,
+            'hy2py = hy.cmdline:hy2py_main',
+            'hy2py%d = hy.cmdline:hy2py_main' % ver,
+        ]
+    },
+    packages=find_packages(exclude=['tests*']),
+    package_data={
+        'hy.contrib': ['*.hy', '__pycache__/*'],
+        'hy.core': ['*.hy', '__pycache__/*'],
+        'hy.extra': ['*.hy', '__pycache__/*'],
+    },
+    data_files=[
+        ('get_version', ['get_version.py'])
+    ],
+    author="Paul Tagliamonte",
+    author_email="tag@pault.ag",
+    long_description=long_description,
+    description='Lisp and Python love each other.',
+    license="Expat",
+    url="http://hylang.org/",
+    platforms=['any'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: DFSG approved",
+        "License :: OSI Approved :: MIT License",  # Really "Expat". Ugh.
+        "Operating System :: OS Independent",
+        "Programming Language :: Lisp",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development :: Compilers",
+        "Topic :: Software Development :: Libraries",
+    ]
+)
