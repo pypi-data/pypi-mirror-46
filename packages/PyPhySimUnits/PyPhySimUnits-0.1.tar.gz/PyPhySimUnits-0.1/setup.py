c51739c3ae1d82553be6a2e7b96adbbba2eeeb53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:53 +0000

    Add package PyPhySimUnits/0.1/PyPhySimUnits-0.1.tar.gz

---
+++ packages/PyPhySimUnits/PyPhySimUnits-0.1.tar.gz/PyPhySimUnits-0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'PyPhySimUnits',
+  packages = ['PyPhySimUnits'],
+  version = '0.1',
+  license='MIT',
+  description = 'Repo for double-checking unit conversions and calculations',
+  author = 'Christopher Auld',
+  author_email = 'auld.chris.10@gmail.com',
+  url = 'https://github.com/user/chupin10',
+  download_url = 'https://github.com/chupin10/PyPhySimUnits/archive/0.1.tar.gz',
+  keywords = ['Units', 'Engineering', 'Physics', 'Simulation'],
+  install_requires=[
+          'numpy',
+          'typing',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
