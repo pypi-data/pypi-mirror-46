92407683a414ec2e50f28d57dbb4f3875b2e27c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:47 +0000

    Add package xyscript/0.1.8.13/xyscript-0.1.8.13.tar.gz

---
+++ packages/xyscript/xyscript-0.1.8.13.tar.gz/xyscript-0.1.8.13/xyscript/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+#-*- encoding:utf-8 -*-
+# from __future__ import print_function
+
+__version__ = '0.1.8.13'
+
+log_store_local = "./dist/"
+
+class Config:
+    def get(self,project):
+        print(project)
+        # pass
+    def get_version(self):
+        return __version__
+        
