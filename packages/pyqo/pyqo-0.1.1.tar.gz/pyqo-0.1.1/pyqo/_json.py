f7274d7c1957fc18eef23bb04ebd86d00e399df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:42 +0000

    Add package pyqo/0.1.1/pyqo-0.1.1.tar.gz

---
+++ packages/pyqo/pyqo-0.1.1.tar.gz/pyqo-0.1.1/pyqo/_json.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python3
+"""
+    The ``._json`` module
+    ======================
+    Contains all the functions related to the reading and mofifications of the ``.json`` files
+"""
+
+import json
+import os
+import sys
+
+PYTHON_PATH = os.path.dirname(os.path.realpath(__file__))
+HOME_PATH = os.path.expanduser('~')
+CONFIG_PATH = os.path.join(HOME_PATH,'.config')
+DATA_PATH = os.path.join(CONFIG_PATH, 'pyqo')
+
+def resolve_json_filename(command):
+    if not os.path.isdir(CONFIG_PATH):
+        os.mkdir(CONFIG_PATH)
+    if not os.path.isdir(DATA_PATH):
+        os.mkdir(DATA_PATH)
+    #init default filename
+    filename = os.path.join(DATA_PATH,'{}.json'.format(command))
+    #test if config
+    config = read_json(os.path.join(DATA_PATH,'config.json'))
+    key_name = '{}_json'.format(command)
+    if key_name in config:
+            filename = config[key_name]
+
+    return filename
+
+def set_config(command, datafile):
+    config_file = os.path.join(DATA_PATH,'config.json')
+    key_name = '{}_json'.format(command)
+    data = read_json(config_file)
+    data[key_name] = datafile
+    write_json(config_file, data)
+
+def read_json(filename):
+    if os.path.isfile(filename):
+        with open(filename, encoding='utf-8') as f:
+            data = json.loads(f.read())
+    else:
+        data = {}
+    return data
+
+def list_json(filename, keys):
+    from ._printer import print_map
+    if keys is None or len(keys)<1 or keys[0] is None:
+        data = read_json(filename)
+        print_map(data)
+    else:
+        values = get_json(filename, keys)
+        dico = {keys[i] : values[i] for i in range(len(keys))}
+        print_map(dico)
+
+def get_json(filename, keys):
+    data = read_json(filename)
+    l = []
+    for key in keys:
+        if key in data:
+            l.append(data[key])
+        else:
+            print('The key "{}" has no attributed value.'.format(key))
+            del key
+    return l
+
+def write_json(filename, data):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        json.dump(data, f)
+
+def set_json(filename, map):
+    data = read_json(filename)
+    for key, value in map.items():
+        data[key] = value
+    write_json(filename, data)
+
+def remove_json(filename, keys):
+    data = read_json(filename)
+    for key in keys:
+        data.pop(key)
+    write_json(filename, data)
---
+++ packages/pyqo/pyqo-0.1.1.tar.gz/pyqo-0.1.1/pyqo/c_.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+"""
+## Command ``c``
+
+Set the working directory of the command line to your favourite directories with ease.
+For a script to alter the current environment, it requires `source`'ing in linux.
+We suggest you to create an alias to avoid doing it manually : `alias c="source c"`.
+The command `c` shares its data with the command `d`.
+See `c --help` for more details.
+
+### Example
+
+```
+$ cd ~/Documents/games
+$ # associate permanently the key 'games' to '~/Documents/games'
+$ c games -a .
+$ # associate permanently the key 'films' to '~/Documents/films'
+$ c films -a /home/pyqo/Documents/films
+$ # equivalent to 'cd ~/Documents/films'
+$ c films
+$ # equivalent to 'cd ~/Documents/games'
+$ c games
+```
+"""
+
+import click
+import sys, os
+from ._json import *
+from ._srl import *
+
+@click.command()
+@click.argument('keys', required = False, nargs = -1)
+@decorate_srl
+def main(keys, **kwargs):
+    """Navigate through directories."""
+
+    command = 'd'
+    filename = resolve_json_filename(command)
+
+    if handle_srl(command, filename, keys, type='dir', **kwargs):
+        exit()
+
+    if len(keys)!=1:
+        print("When changing directory, you should provide exactly one key.")
+        exit()
+
+    values = get_json(filename, keys)
+
+    if len(values)<1:
+        print("Key not known, aborting.")
+        exit()
+    ret = values[0] if sys.platform in ['linux','linux2'] else '"'+values[0]+'"'
+    exit(ret)
+
+if __name__ == "__main__":
+    main()
