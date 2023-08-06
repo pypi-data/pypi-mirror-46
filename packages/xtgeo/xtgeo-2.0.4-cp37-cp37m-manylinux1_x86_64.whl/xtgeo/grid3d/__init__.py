6bc017eb39df20108dcfe8b0f3a5dfa3f0bfb467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:58 +0000

    Add package xtgeo/2.0.4/xtgeo-2.0.4-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/xtgeo/xtgeo-2.0.4-cp37-cp37m-manylinux1_x86_64.whl/xtgeo/grid3d/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+from __future__ import division, absolute_import
+from __future__ import print_function
+
+from xtgeo.common.exceptions import (
+    DateNotFoundError,
+    KeywordFoundNoDateError,
+    KeywordNotFoundError,
+)
+
+from .grid import Grid
+from .grid_property import GridProperty
+from .grid_properties import GridProperties
