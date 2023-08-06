a94065203f739476af4c2f764f4f0cce093ba3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:46 +0000

    Add package pytorchcheckpoint/0.0.2/pytorchcheckpoint-0.0.2.tar.gz

---
+++ packages/pytorchcheckpoint/pytorchcheckpoint-0.0.2.tar.gz/pytorchcheckpoint-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+"""
+python3 setup.py sdist bdist_wheel
+twine upload dist/*
+"""
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytorchcheckpoint",
+    version="0.0.2",
+    author="Omri Bar",
+    author_email="baromri@gmail.com",
+    description="Support PyTorch checkpoints",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bomri/pytorch-checkpoint.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
