d6dd757cc5bf3ba3741c3e51a3dd9a8befc53dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:44 +0000

    Add package fb-duckling/0.1.9/fb_duckling-0.1.9-py2.py3-none-any.whl

---
+++ packages/fb-duckling/fb_duckling-0.1.9-py2.py3-none-any.whl/fb_duckling/utils.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from os import path, getenv
+
+
+env_path = "./.env"
+env_default_path = "./.env.default"
+
+if path.isfile(env_path):
+    load_dotenv(env_path)
+    print("loaded env variables from: {}".format(env_path))
+elif path.isfile(env_default_path):
+    load_dotenv(env_default_path)
+    print("loaded env variables from: {}".format(env_default_path))
+else:
+    raise Exception("No .env file found please make sure that a .env file of the .env.default file"
+                    "is present at the root of the project")
+
+
+def get_default_port():
+    return int(getenv("DEFAULT_PORT"))
+
+
+def get_default_url():
+    return str(getenv("DEFAULT_URL"))
+
+
+def get_default_locale():
+    return str(getenv("DEFAULT_LOCALE"))
