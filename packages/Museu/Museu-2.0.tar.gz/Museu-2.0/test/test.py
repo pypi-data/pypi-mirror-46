158ea7715c19241d975886d959db40f394dad57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:57 +0000

    Add package Museu/2.0/Museu-2.0.tar.gz

---
+++ packages/Museu/Museu-2.0.tar.gz/Museu-2.0/test/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-04-26 09:28:08
+# @Author  : Blackstone
+# @to      :
+import sys
+
+sys.path.append("..")
+
+
+from mt.common import func
+
+import test2
+
+
+
+print(func.dot_name_to_object("test2.vv"))
+
