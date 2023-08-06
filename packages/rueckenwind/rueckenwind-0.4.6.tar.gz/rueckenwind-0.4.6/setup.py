78e47f961089cfb66233a79e2e1a2c16d01e408b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:33 +0000

    Add package rueckenwind/0.4.6/rueckenwind-0.4.6.tar.gz

---
+++ packages/rueckenwind/rueckenwind-0.4.6.tar.gz/rueckenwind-0.4.6/setup.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+from distutils.command.sdist import sdist
+from setuptools import setup, find_packages
+import setuptools.command.test
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+version_suffix = ''
+
+
+class TestCommand(setuptools.command.test.test):
+    def finalize_options(self):
+        setuptools.command.test.test.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        fails = []
+        from tox.config import parseconfig
+        from tox.session import Session
+
+        config = parseconfig(self.test_args)
+        retcode = Session(config).runcommand()
+        if retcode != 0:
+            fails.append('tox returned errors')
+
+        import pep8
+        style_guide = pep8.StyleGuide(config_file=BASE_PATH + '/.pep8')
+        style_guide.input_dir(BASE_PATH + '/rw')
+        if style_guide.options.report.get_count() != 0:
+            fails.append('pep8 returned errros for rw/')
+
+        style_guide = pep8.StyleGuide(config_file=BASE_PATH + '/.pep8')
+        style_guide.input_dir(BASE_PATH + '/test')
+        if style_guide.options.report.get_count() != 0:
+            fails.append('pep8 returned errros for test/')
+
+        if fails:
+            print('\n'.join(fails))
+            sys.exit(1)
+
+
+setup(
+    name="rueckenwind",
+    version="0.4.6",
+    url='https://github.com/FlorianLudwig/rueckenwind',
+    description='tornado based webframework',
+    author='Florian Ludwig',
+    author_email='vierzigundzwei@gmail.com',
+    install_requires=['tornado>=4.0.0,<5.0',
+                      'jinja2',
+                      'babel',
+                      'argcomplete>=0.6.6,<1.0',
+                      'configobj',
+                      'chardet',
+                      'pytz',
+                      'PyYAML>=3.10',
+                      'future'
+                      ],
+    extras_requires={
+        'test': ['tox', 'pytest', 'pep8'],
+        'docs': ['sphinx_rtd_theme']
+    },
+    packages=find_packages(exclude=['*.test', '*.test.*']),
+    include_package_data=True,
+    package_data={
+        'rw': ['*.html', '*.css', 'templates/html5', 'templates/form', 'templates/nginx']
+    },
+    entry_points={
+        'console_scripts': [
+            'rw = rw.cli:main',
+        ],
+    },
+    cmdclass={
+        'test': TestCommand
+    },
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
