c59974c7988da2599405daeb5e42c556fe40032a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:18 +0000

    Add package Munin/0.1.0/Munin-0.1.0-py36-none-any.whl

---
+++ packages/Munin/Munin-0.1.0-py36-none-any.whl/munin/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from munin.version import get_version
+from .generate_report import generate_html, generate_pdf
+
+__author__ = "cnheider"
+__version__ = get_version()
+__doc__ = """
+Created on 27/04/2019
+
+@author: cnheider
+"""
