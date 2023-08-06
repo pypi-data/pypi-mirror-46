c4e04cc763d820b75ccb10f312ca65cd71222ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:39 +0000

    Add package dash-router/0.1.1/dash_router-0.1.1.tar.gz

---
+++ packages/dash-router/dash_router-0.1.1.tar.gz/dash_router-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dash_router',
+    version='0.1.1',
+    description='A simple router for multi page Dash applications.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Mercy University Hosptial',
+    author_email='jharrington@muh.ie',
+    url='https://github.com/mercyuniversityhospital/dash_router',
+    license='BSD',
+    packages=find_packages(exclude=('tests',)),
+)
