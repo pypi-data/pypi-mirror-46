42189c0ad78d1a6add3ba6e8e7dcb209194a7237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:38 +0000

    Add package yachain/0.1.4/yachain-0.1.4-py2.py3-none-any.whl

---
+++ packages/yachain/yachain-0.1.4-py2.py3-none-any.whl/yachain/__init__.py
@@ -0,0 +1,6 @@
+__title__ = "yachain yaml config parser"
+__version__ = "0.1.4"
+__author__ = "Feite Brekeveld"
+__license__ = "MIT"
+__copyright__ = "Copyright 2017 - 2019 Feite Brekeveld"
+from yachain.config import Config, NoDataError
