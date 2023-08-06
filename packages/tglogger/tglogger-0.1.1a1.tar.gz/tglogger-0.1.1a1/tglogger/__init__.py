756a55edff325f1ef31a23194ffbc57254c8aa86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:32 +0000

    Add package tglogger/0.1.1a1/tglogger-0.1.1a1.tar.gz

---
+++ packages/tglogger/tglogger-0.1.1a1.tar.gz/tglogger-0.1.1a1/tglogger/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "0.1.1a1"
+__author__ = "Eray Erdin"
---
+++ packages/tglogger/tglogger-0.1.1a1.tar.gz/tglogger-0.1.1a1/tglogger/handler.py
@@ -0,0 +1,49 @@
+import logging
+import os
+import uuid
+
+from tglogger import request
+
+_old_factory = logging.getLogRecordFactory()
+
+
+def _log_record_uuid(*args, **kwargs):
+    record = _old_factory(*args, **kwargs)
+    record.uuid = uuid.uuid4()
+    return record
+
+
+logging.setLogRecordFactory(_log_record_uuid)
+
+
+class InvalidBotError(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class TelegramHandler(logging.Handler):
+    def __init__(
+        self,
+        level=logging.ERROR,
+        bot_token: str = os.environ["TELEGRAM_BOT_TOKEN"],
+        receiver: str = os.environ["TELEGRAM_RECEIVER"],
+        **kwargs
+    ):
+        self.bot_token = bot_token
+        self.receiver = receiver
+        super().__init__(level)
+
+        if not kwargs.get("bypass_auth", False):
+            self._authorize()
+
+    def _authorize(self):
+        url = request._BASE_URL.format(token=self.bot_token, method="getMe")
+        response = request.requests.get(url)
+
+        if not response.ok:
+            raise InvalidBotError(
+                "No bot exists with given token. (Hidden for security.)"
+            )
+
+    def emit(self, record):
+        return request.send_log(self, record, self.receiver)
