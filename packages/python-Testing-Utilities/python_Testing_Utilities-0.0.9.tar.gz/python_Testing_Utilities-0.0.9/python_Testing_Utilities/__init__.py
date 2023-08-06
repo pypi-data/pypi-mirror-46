ff089fc6d4a0104911b2f47735f533aa0fa3a239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:07 +0000

    Add package python-Testing-Utilities/0.0.9/python_Testing_Utilities-0.0.9.tar.gz

---
+++ packages/python-Testing-Utilities/python_Testing_Utilities-0.0.9.tar.gz/python_Testing_Utilities-0.0.9/python_Testing_Utilities/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+    python_Testing_Utilities
+    ~~~~~~~~
+    python_Testing_Utilities
+    :copyright: (c) 2019 by Robert Metcalf.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from .callService import callService, callGetService, callPostService, callPutService, callDeleteService
+
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
