7b96b8c2f3304d34c0477ff35fbbe22a27066194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:59 +0000

    Add package pythsr/0.4/pythsr-0.4.tar.gz

---
+++ packages/pythsr/pythsr-0.4.tar.gz/pythsr-0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README')) as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pythsr',
+    version='0.4',
+    description='A collection of python utils for personal use',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://github.com/thsr/pythsr',
+    author='thsr',
+    author_email='437808@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
