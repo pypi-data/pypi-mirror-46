8a4f316b2d6501c7ecdfcc935b937fb6786cd2d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:47 +0000

    Add package pygecko/1.3.0/pygecko-1.3.0-py3-none-any.whl

---
+++ packages/pygecko/pygecko-1.3.0-py3-none-any.whl/pygecko-1.3.0.data/scripts/pycore.py
@@ -0,0 +1,13 @@
+#!python
+
+import threading
+import time
+from pygecko.pycore.mbeacon import BeaconCoreServer
+from pygecko.pycore.transport import Ascii
+
+
+if __name__ == "__main__":
+
+    bs = BeaconCoreServer(key='local',handler=Ascii)
+    bs.start()
+    bs.run()
