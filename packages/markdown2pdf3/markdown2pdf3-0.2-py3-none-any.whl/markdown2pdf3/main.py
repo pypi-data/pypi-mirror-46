4a702b6396351aaf404692ebb2ab9de54794d1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:14 +0000

    Add package markdown2pdf3/0.2/markdown2pdf3-0.2-py3-none-any.whl

---
+++ packages/markdown2pdf3/markdown2pdf3-0.2-py3-none-any.whl/markdown2pdf3/main.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import pypandoc
+from . import utils
+
+
+@utils.save_cwd
+def convert_markdown_to_pdf(
+        markdown_path,
+        pdf_path=None,
+        pdf_engine=None,
+):
+    markdown_dir, _ = os.path.split(markdown_path)
+    os.chdir(markdown_dir)
+
+    if not pdf_engine:
+        pdf_engine = {
+            'win32': 'latex',
+        }[sys.platform]
+
+    if not pdf_path:
+        pdf_path = os.path.splitext(markdown_path)[0] + '.pdf'
+
+    pypandoc.convert_file(
+        source_file=markdown_path,
+        outputfile=pdf_path,
+        to='pdf',
+        format='md',
+        extra_args=[
+            f'--pdf-engine={pdf_engine}'
+        ]
+    )
+
+
+if __name__ == '__main__':
+    convert_markdown_to_pdf(r'C:\Users\wolf\Downloads\4.8.md')
