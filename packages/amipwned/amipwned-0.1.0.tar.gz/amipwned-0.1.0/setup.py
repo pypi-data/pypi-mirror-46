50d60895ded573eeaa62611b71967a52d23f6643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:12 +0000

    Add package amipwned/0.1.0/amipwned-0.1.0.tar.gz

---
+++ packages/amipwned/amipwned-0.1.0.tar.gz/amipwned-0.1.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['amipwned']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiodns>=2.0,<3.0',
+ 'aiohttp>=3.5,<4.0',
+ 'aioredis>=1.2,<2.0',
+ 'asyncpg>=0.18.3,<0.19.0',
+ 'cchardet>=2.1,<3.0']
+
+entry_points = \
+{'console_scripts': ['amipwned = amipwned.amipwned:main']}
+
+setup_kwargs = {
+    'name': 'amipwned',
+    'version': '0.1.0',
+    'description': 'A minimal self-hosted haveibeenpwned application.',
+    'long_description': None,
+    'author': 'Michael',
+    'author_email': 'michael@dubell.io',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
