5eaad73b08b79fb5e9122a543084db3a5a44e5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:02 +0000

    Add package haohaninfo/0.0.10/haohaninfo-0.0.10-py3-none-any.whl

---
+++ packages/haohaninfo/haohaninfo-0.0.10-py3-none-any.whl/haohaninfo/__init__.py
@@ -0,0 +1,13 @@
+import json
+import subprocess
+import sys
+
+from haohaninfo import GOrder
+#from haohaninfo.GOrder import GOQuote
+pip_list = subprocess.check_output([sys.executable, '-m', 'pip', 'list', '--outdated', '--format=json']).decode()
+json_list = json.loads(pip_list)
+
+for i in json_list:
+    if(i['name'] == 'haohaninfo'):
+        print('已有新版本: ' + i['latest_version'])
+        
