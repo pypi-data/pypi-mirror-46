ebc180f4119c7915c3bbb7e7347c76a987449d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:20 +0000

    Add package yunetkomymath/1.0.0/yunetkomymath-1.0.0.tar.gz

---
+++ packages/yunetkomymath/yunetkomymath-1.0.0.tar.gz/yunetkomymath-1.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from pathlib import Path
+
+# from pathlib import Path
+
+setuptools.setup(
+name="yunetkomymath",
+version="1.0.0",
+long_description=Path("README.md").read_text(),
+packages=setuptools.find_packages(exclude=[".vscode"])
+)
