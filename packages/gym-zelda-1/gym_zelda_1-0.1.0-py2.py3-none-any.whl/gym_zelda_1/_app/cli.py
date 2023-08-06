f9b3081f8678cb236402f9b094da706249b72f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:34 +0000

    Add package gym-zelda-1/0.1.0/gym_zelda_1-0.1.0-py2.py3-none-any.whl

---
+++ packages/gym-zelda-1/gym_zelda_1-0.1.0-py2.py3-none-any.whl/gym_zelda_1/_app/cli.py
@@ -0,0 +1,49 @@
+"""Super Mario Bros for OpenAI Gym."""
+import argparse
+import gym
+from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
+from nes_py.app.play_human import play_human
+from nes_py.app.play_random import play_random
+
+
+def _get_args():
+    """Parse command line arguments and return them."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    # add the argument for the Super Mario Bros environment to run
+    parser.add_argument('--env', '-e',
+        type=str,
+        default='Zelda1-v0',
+        help='The name of the environment to play'
+    )
+    # add the argument for the mode of execution as either human or random
+    parser.add_argument('--mode', '-m',
+        type=str,
+        default='human',
+        choices=['human', 'random'],
+        help='The execution mode for the emulation'
+    )
+    # add the argument for the number of steps to take in random mode
+    parser.add_argument('--steps', '-s',
+        type=int,
+        default=500,
+        help='The number of random steps to take.',
+    )
+    # parse arguments and return them
+    return parser.parse_args()
+
+
+def main():
+    """The main entry point for the command line interface."""
+    # parse arguments from the command line (argparse validates arguments)
+    args = _get_args()
+    # build the environment with the given ID
+    env = gym.make(args.env)
+    # play the environment with the given mode
+    if args.mode == 'human':
+        play_human(env)
+    else:
+        play_random(env, args.steps)
+
+
+# explicitly define the outward facing API of this module
+__all__ = [main.__name__]
