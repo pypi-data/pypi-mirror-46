0083551d51576582cafd0bead305a32c22e06325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:44 +0000

    Add package tap-braintree/0.8.2/tap-braintree-0.8.2.tar.gz

---
+++ packages/tap-braintree/tap-braintree-0.8.2.tar.gz/tap-braintree-0.8.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-braintree',
+      version='0.8.2',
+      description='Singer.io tap for extracting data from the Braintree API',
+      author='Stitch',
+      url='http://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_braintree'],
+      install_requires=[
+          'singer-python==5.0.15',
+          'requests==2.20.0',
+          'braintree==3.53.0',
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-braintree=tap_braintree:main
+      ''',
+      packages=['tap_braintree'],
+      package_data = {
+          'tap_braintree/schemas': [
+              'transactions.json',
+          ],
+      },
+      include_package_data=True,
+)
