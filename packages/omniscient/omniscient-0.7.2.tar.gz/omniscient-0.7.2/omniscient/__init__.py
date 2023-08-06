4d6352edd5c4c0e31822424510f66854f5d942b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:36 +0000

    Add package omniscient/0.7.2/omniscient-0.7.2.tar.gz

---
+++ packages/omniscient/omniscient-0.7.2.tar.gz/omniscient-0.7.2/omniscient/__init__.py
@@ -0,0 +1,3 @@
+from omniscient.run import init_endpoint, init_model
+from omniscient.prediction.predictor import Predictor
+from omniscient.utils.file_handler import FileHandler
---
+++ packages/omniscient/omniscient-0.7.2.tar.gz/omniscient-0.7.2/omniscient/run.py
@@ -0,0 +1,37 @@
+from deeppavlov import build_model, configs
+from .endpoint.rest_endpoint import RestEndpoint
+from deeppavlov.core.common.chainer import Chainer
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def init_model()->Chainer:
+    """
+    Initialises model. Downloads pretrained weights if necessary (hash check conducted before download)
+    :return: The initialised model that can be passed to a Predictor object
+    """
+    logging.info("Initialising SQuAD model")
+    model = build_model(configs.squad.squad, download=True)
+
+    return model
+
+
+def init_endpoint(model: Chainer, host: str='0.0.0.0', port: int=5000):
+    """
+    Initialises an REST endpoint to query for predictions
+    :param model: The model initialised via init_model()
+    :param host: Host address to run endpoint. NOTE '0.0.0.0' for externally visible server
+    :param port: Port to run endpoint on
+    :return: The initialised REST endpoint
+    """
+    restendpoint = RestEndpoint(model, host, port)
+
+
+if __name__ == '__main__':
+    # If run as main will initialise model and an endpoint that uses that model to provide predictions
+    logging.basicConfig(level=logging.DEBUG)
+
+    mdl = init_model()
+    init_endpoint(mdl)
