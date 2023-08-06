3f73db1907a9eb83b80f04f35b3d7cf1867a1fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:40 +0000

    Add package cloud-logger/0.0.4/cloud-logger-0.0.4.tar.gz

---
+++ packages/cloud-logger/cloud-logger-0.0.4.tar.gz/cloud-logger-0.0.4/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+        name="cloud-logger",
+        version="0.0.4",
+        author="furodrive",
+        author_email="furodrive@gmail.com",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License"
+        ]
+)
