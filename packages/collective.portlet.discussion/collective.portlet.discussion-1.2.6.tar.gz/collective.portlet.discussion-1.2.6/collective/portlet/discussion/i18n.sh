a291df34029e535130ef5c0f4eafed0b6fc8554d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:10 +0000

    Add package collective.portlet.discussion/1.2.6/collective.portlet.discussion-1.2.6.tar.gz

---
+++ packages/collective.portlet.discussion/collective.portlet.discussion-1.2.6.tar.gz/collective.portlet.discussion-1.2.6/collective/portlet/discussion/i18n.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+DOMAIN='collective.portlet.discussion'
+
+i18ndude rebuild-pot --pot locales/${DOMAIN}.pot --create ${DOMAIN} .
+i18ndude sync --pot locales/${DOMAIN}.pot locales/*/LC_MESSAGES/${DOMAIN}.po
+
+# Compile po files
+for lang in $(find locales -mindepth 1 -maxdepth 1 -type d); do
+    if test -d $lang/LC_MESSAGES; then
+        msgfmt -o $lang/LC_MESSAGES/${DOMAIN}.mo $lang/LC_MESSAGES/${DOMAIN}.po
+    fi
+done
+
+DOMAIN='plone'
+
+i18ndude rebuild-pot --pot locales/${DOMAIN}.pot --create ${DOMAIN} profiles
+i18ndude sync --pot locales/${DOMAIN}.pot locales/*/LC_MESSAGES/${DOMAIN}.po
+
+# Compile po files
+for lang in $(find locales -mindepth 1 -maxdepth 1 -type d); do
+    if test -d $lang/LC_MESSAGES; then
+        msgfmt -o $lang/LC_MESSAGES/${DOMAIN}.mo $lang/LC_MESSAGES/${DOMAIN}.po
+    fi
+done
