b9a4c39d13a19e9900d16d67c1f77c2884290f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:06 +0000

    Add package django-ok-seo/0.6.1/django-ok-seo-0.6.1.tar.gz

---
+++ packages/django-ok-seo/django-ok-seo-0.6.1.tar.gz/django-ok-seo-0.6.1/seo/admin/__init__.py
@@ -0,0 +1,11 @@
+from ..settings import SEO_USE_URL_SEO
+
+from .filters import *
+from .inline import *
+from .utils import *
+
+if SEO_USE_URL_SEO:
+    from .url_seo import *
+else:
+    from .model_instance_seo import *
+    from .view_seo import *
