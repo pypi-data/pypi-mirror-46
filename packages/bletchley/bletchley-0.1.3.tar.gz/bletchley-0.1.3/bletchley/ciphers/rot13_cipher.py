45d9238e75bd18a60d6cbd03d30109c5004f90f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:05 +0000

    Add package bletchley/0.1.3/bletchley-0.1.3.tar.gz

---
+++ packages/bletchley/bletchley-0.1.3.tar.gz/bletchley-0.1.3/bletchley/ciphers/rot13_cipher.py
@@ -0,0 +1,34 @@
+""" Module containing rot13 cipher """
+import logging
+from ciphers.cipher import Cipher
+
+
+class Rot13Cipher(Cipher):
+    """ Implementation of the rot13 cipher """
+    def __init__(self):
+        logging.info("Created rot13 cipher")
+        pass
+
+    def encrypt(self, plaintext):
+        """ Encrypt plain text using rot13 cipher """
+        logging.info("Attempting to encrypt plain text...")
+        ciphertext = ''
+        plaintext = self.sanitise(plaintext)
+        plaintext_ordinal = [(ord(letter)-65) for letter in plaintext]
+        for i in range(len(plaintext)):
+            encrypted_value = (plaintext_ordinal[i] + 13) % 26
+            ciphertext += chr(encrypted_value + 65)
+        logging.info("Successfully encrypted plain text...")
+        return ciphertext
+
+    def decrypt(self, ciphertext):
+        """ Decrypt cipher text using rot13 cipher """
+        logging.info("Attempting to decrypt cipher text...")
+        plaintext = ''
+        ciphertext = self.sanitise(ciphertext)
+        ciphertext_ordinal = [(ord(letter)-65) for letter in ciphertext]
+        for i in range(len(ciphertext)):
+            unencrypted_value = (ciphertext_ordinal[i] - 13) % 26
+            plaintext += chr(unencrypted_value + 65)
+        logging.info("Successfully decrypted cipher text...")
+        return plaintext
