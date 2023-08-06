95d42d1027f758ba7409004e57f095298c4d2200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:43 +0000

    Add package treeutils/0.1.0/treeutils-0.1.0.tar.gz

---
+++ packages/treeutils/treeutils-0.1.0.tar.gz/treeutils-0.1.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import setuptools
+import codecs
+
+long_description = codecs.open('README.rst', 'r', 'utf-8').read()
+
+setuptools.setup(
+      name='treeutils',
+      version='0.1.0',
+      description=('For developers who have to fit real world data into '
+                   'tree structures like the D3 tree.'), 
+      long_description=long_description,
+      author='Charles Kaminski',
+      author_email='CharlesKaminski@gmail.com',
+      url='https://github.com/Charles-Kaminski/treeutils',
+      keywords = ['Tree', 'Cluster', 'Parent', 'Child', 'Branch', 'Root', 'D3'],
+      license='BSD 3-Clause License',
+      py_modules=['treeutils.__init__',],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Topic :: Sociology',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Visualization',
+          'Topic :: Utilities',
+      ] 
+)
