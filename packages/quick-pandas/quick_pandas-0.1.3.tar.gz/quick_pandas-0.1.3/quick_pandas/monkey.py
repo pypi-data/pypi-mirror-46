0a4f192101cdb55f46bad276a97d7dbc66852316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:46 +0000

    Add package quick-pandas/0.1.3/quick_pandas-0.1.3.tar.gz

---
+++ packages/quick-pandas/quick_pandas-0.1.3.tar.gz/quick_pandas-0.1.3/quick_pandas/monkey.py
@@ -0,0 +1,25 @@
+import pandas
+
+from quick_pandas import sort_api, config
+from quick_pandas.wrappers.pandas_wrapper import pandas_core_sorting_nargsort
+
+
+def patch_numpy():
+    # np.ndarray = ndarray_wrapper
+    # np.empty = NdarrayFunctionWrapper(np.empty)
+    pass
+
+
+def patch_pandas():
+    pandas.core.sorting.nargsort = pandas_core_sorting_nargsort
+
+
+def init(ascii_sort=False):
+    config.ASCII_SORT = ascii_sort
+    sort_api.init()
+
+
+def patch_all():
+    init()
+    patch_numpy()
+    patch_pandas()
