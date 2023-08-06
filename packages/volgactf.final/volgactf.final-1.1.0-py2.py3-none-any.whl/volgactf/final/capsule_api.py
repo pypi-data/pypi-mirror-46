489813d62b771517e93bdbf87b3a115bf1982e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:21 +0000

    Add package volgactf.final/1.1.0/volgactf.final-1.1.0-py2.py3-none-any.whl

---
+++ packages/volgactf.final/volgactf.final-1.1.0-py2.py3-none-any.whl/volgactf/final/capsule_api.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+import jwt
+import requests
+
+
+class GetPublicKeyResult(Enum):
+    SUCCESS = 0
+    ERROR = 1
+
+
+class DecodeResult(Enum):
+    SUCCESS = 0
+    ERROR = 1
+
+
+class CapsuleAPIHelper(object):
+    def __init__(self, host):
+        self._host = host
+        self._port = 80
+        self._url_path = 'api/capsule/v1'
+
+    @property
+    def public_key_url(self):
+        return 'http://{0}:{1:d}/{2}/public_key'.format(
+            self._host,
+            self._port,
+            self._url_path
+        )
+
+    def get_public_key(self):
+        try:
+            r = requests.get(self.public_key_url)
+            if r is not None and r.status_code == requests.codes.ok:
+                return dict(
+                    code=GetPublicKeyResult.SUCCESS,
+                    public_key=r.text
+                )
+            else:
+                return dict(code=GetPublicKeyResult.ERROR)
+        except Exception:
+            return dict(code=GetPublicKeyResult.ERROR)
+
+    def decode(self, capsule):
+        try:
+            call = self.get_public_key()
+            if call['code'] != GetPublicKeyResult.SUCCESS:
+                raise Exception('Invalid public key')
+            payload = jwt.decode(
+                capsule,
+                algorithms=['ES256', 'RS256'],
+                key=call['public_key']
+            )
+            return dict(
+                code=DecodeResult.SUCCESS,
+                decoded=payload
+            )
+        except Exception:
+            return dict(code=DecodeResult.ERROR)
