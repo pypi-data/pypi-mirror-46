d7041d294c327df6daf7322486cd8ea9c76abd99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:56 +0000

    Add package pypsa/0.14.0/pypsa-0.14.0.tar.gz

---
+++ packages/pypsa/pypsa-0.14.0.tar.gz/pypsa-0.14.0/setup.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+
+
+
+from setuptools import setup, find_packages
+from codecs import open
+
+
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pypsa',
+    version='0.14.0',
+    author='Tom Brown (FIAS), Jonas Hoersch (FIAS), David Schlachtberger (FIAS)',
+    author_email='brown@fias.uni-frankfurt.de',
+    description='Python for Power Systems Analysis',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/PyPSA/PyPSA',
+    license='GPLv3',
+    packages=find_packages(exclude=['doc', 'test']),
+    include_package_data=True,
+    install_requires=['numpy','pyomo>=5.3','scipy','pandas>=0.19.0','networkx>=1.10', 'cartopy>=0.16'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+    ])
