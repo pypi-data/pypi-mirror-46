bd12280b5feea1d19ea1f5859c1215d744dbf2f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:17 +0000

    Add package markdown2pdf3/0.2.2/markdown2pdf3-0.2.2.tar.gz

---
+++ packages/markdown2pdf3/markdown2pdf3-0.2.2.tar.gz/markdown2pdf3-0.2.2/README.rst
@@ -0,0 +1,28 @@
+Markdown2PDF
+============
+
+It works well on Python3 and Windows 10,
+and not tested on other platforms.
+
+Markdown2pdf is a command-line tool to convert markdown file into pdf.
+
+Installation
+------------
+
+To install Markdown2PDF you may need to install PDF convertion tool first,
+
+Install Markdown2PDF by pip:
+
+.. code-block:: shell
+
+    pip install markdown2pdf3
+
+
+Usage
+-----
+
+You can use Markdown2PDF via simple command ``md2pdf``:
+
+.. code-block:: shell
+
+    md2pdf article.md
---
+++ packages/markdown2pdf3/markdown2pdf3-0.2.2.tar.gz/markdown2pdf3-0.2.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+
+def fread(filepath):
+    with open(filepath, 'r') as f:
+        return f.read()
+
+
+setup(
+    name='markdown2pdf3',
+    version='0.2.2',
+    url='https://github.com/panhaoyu/markdown2pdf',
+    license='MIT',
+    author='panhaoyu',
+    author_email='haoyupan@aliyun.com',
+    description='A tool converts Markdown file to PDF.',
+    long_description=fread('README.rst'),
+    packages=['markdown2pdf3'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'md2pdf = markdown2pdf3:execute',
+        ]
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
