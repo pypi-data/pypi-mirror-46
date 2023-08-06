43f35c5acd087cbc40882e5ab58a9ae88a4b954d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:48 +0000

    Add package forecastlib/0.1.1/forecastlib-0.1.1-py3-none-any.whl

---
+++ packages/forecastlib/forecastlib-0.1.1-py3-none-any.whl/forecastlib/evaluation/evaluation.py
@@ -0,0 +1,107 @@
+from typing import List
+
+
+class CatalogProduct:
+	"""Product definition"""
+
+	def __init__(self, product_code: int, std_price: float, npu_ratio: float, cost: float, cons_cost: float):
+		self.product_code = product_code
+		self.std_price = std_price
+		self.npu_ratio = npu_ratio
+		self.cost = cost
+		self.cons_cost = cons_cost
+
+
+class EvaluatedProduct:
+	"""Product evaluation for specific discount and v1
+
+	discount - pass real number, not percent value (0.5 instead of 50%)
+	"""
+
+	def __init__(self, product: CatalogProduct, discount: float, forecast: float):
+		self.product = product
+		self.discount = discount
+		self.forecast = forecast
+
+	@property
+	def gross_price(self) -> float:
+		"""Return the gross price of the product (price - discount)"""
+		return self.product.std_price * (1 - self.discount)
+
+	@property
+	def net_price(self) -> float:
+		"""Return the net price of the product ((price - discount) * NPU)"""
+		return self.gross_price / self.product.npu_ratio
+
+	@property
+	def net_sales(self) -> float:
+		"""Return sales of the product (net price * v1)"""
+		return self.net_price * self.forecast
+
+	@property
+	def cost_of_sales(self) -> float:
+		"""Return cost of sales for the product and v1 (cost * v1)"""
+		return self.product.cost * self.forecast
+
+	@property
+	def cons_cost_of_sales(self) -> float:
+		"""Return consolidated cost of sales for the product and v1 (cons.cost * v1)"""
+		return self.product.cons_cost * self.forecast
+
+
+class EvaluatedCatalog:
+	"""Catalog evaluation for set of evaluated products"""
+
+	def __init__(self, products: List[EvaluatedProduct], active_count: int):
+		self.products = products
+		self.active_count = active_count
+
+	@property
+	def net_sales(self) -> float:
+		"""Return total sales (sum of NetSales)"""
+		return sum(p.net_sales for p in self.products)
+
+	@property
+	def cost_of_sales(self) -> float:
+		"""Return total costs (sum of CostOfSales)"""
+		return sum(p.cost_of_sales for p in self.products)
+
+	@property
+	def cons_cost_of_sales(self) -> float:
+		"""Return total consolidated costs (sum of ConsCostOfSales)"""
+		return sum(p.cons_cost_of_sales for p in self.products)
+
+	@property
+	def profit(self) -> float:
+		"""Return total profit (sales - costs)"""
+		return self.net_sales - self.cost_of_sales
+
+	@property
+	def cons_profit(self) -> float:
+		"""Return total consolidated profit (sales - consolidated costs)"""
+		return self.net_sales - self.cons_cost_of_sales
+
+	@property
+	def gross_margin(self) -> float:
+		"""Return total gross margin"""
+		return 1 - (self.cost_of_sales / self.net_sales)
+
+	@property
+	def cons_gross_margin(self) -> float:
+		"""Return total consolidated gross margin"""
+		return 1 - (self.cons_cost_of_sales / self.net_sales)
+
+	@property
+	def upa(self) -> float:
+		"""Return catalog-wide units-per-active"""
+		return sum(p.forecast for p in self.products) / self.activeCount
+
+	@property
+	def npu(self) -> float:
+		"""Return catalog-wide netprice-per-unit"""
+		return self.net_sales / sum(p.forecast for p in self.products)
+
+	@property
+	def spa(self) -> float:
+		"""Return catalog-wide sales-per-active"""
+		return self.net_sales / self.activeCount
