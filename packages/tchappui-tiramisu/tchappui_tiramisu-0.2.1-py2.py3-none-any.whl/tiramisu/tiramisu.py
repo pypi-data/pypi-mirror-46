90619899741ce40aa37d2903368858aecbe62592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:05 +0000

    Add package tchappui-tiramisu/0.2.1/tchappui_tiramisu-0.2.1-py2.py3-none-any.whl

---
+++ packages/tchappui-tiramisu/tchappui_tiramisu-0.2.1-py2.py3-none-any.whl/tiramisu/tiramisu.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+
+def get_ingredients():
+    """Prints ingredients to prepare tiramisu."""
+    print("""1. pellerines
+2. cognac + café
+3. mascapone
+4. oeufs et sucre
+5. beaucoup d'amour
+""")
