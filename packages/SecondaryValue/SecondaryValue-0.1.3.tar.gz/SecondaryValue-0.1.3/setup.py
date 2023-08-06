dd34a8cefb63ce2d8f23e86f540b140becf70b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:36 +0000

    Add package SecondaryValue/0.1.3/SecondaryValue-0.1.3.tar.gz

---
+++ packages/SecondaryValue/SecondaryValue-0.1.3.tar.gz/SecondaryValue-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+def readme():
+    with open('Readme.md') as f:
+        return f.read()
+
+setup(name='SecondaryValue',
+      version='0.1.3',
+      description='A helper to calculate the gaussian error propagation.',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/vale981/SecondaryValue',
+      author='Valentin Boettcher',
+      author_email='hiro@protagon.space',
+      license='GPLv3.0',
+      packages=['SecondaryValue'],
+      keywords='gaussian error propagation',
+      install_requires=[
+          'numpy',
+          'sympy',
+      ],
+      zip_safe=True)
