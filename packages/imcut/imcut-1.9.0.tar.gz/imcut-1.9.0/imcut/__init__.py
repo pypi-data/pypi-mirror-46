c8b069ff97de0c04f37356fcd89f517ca7adec3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:28 +0000

    Add package imcut/1.9.0/imcut-1.9.0.tar.gz

---
+++ packages/imcut/imcut-1.9.0.tar.gz/imcut-1.9.0/imcut/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+__all__ = ['pycut',
+           # 'seed_editor_qt',
+           'dcmreaddata']
+__version__ = "1.9.0"
+# DeprecationWarning("imcut package is renamed to imcut")
+# from .pycut import Model, ImageGraphCut
+# from .seed_editor_qt import QTSeedEditor
+# from .dcmreaddata import DicomReader
