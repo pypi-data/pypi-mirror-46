4ab33da673b8139a358bc6c9a1599fc5b94d4956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:14 +0000

    Add package quart-events/0.2.2/quart_events-0.2.2-py2.py3-none-any.whl

---
+++ packages/quart-events/quart_events-0.2.2-py2.py3-none-any.whl/quart_events/__init__.py
@@ -0,0 +1,12 @@
+import sys
+
+
+__version__ = '0.2.2'
+__MIN_PYTHON__ = (3, 7)
+
+
+if sys.version_info < __MIN_PYTHON__:
+    sys.exit('python {}.{} or later is required'.format(*__MIN_PYTHON__))
+
+
+from .broker import EventBroker
