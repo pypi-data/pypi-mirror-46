4580bb51bed4f869ba12a28a4e44adfbf3ab643d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:06 +0000

    Add package crackle-automation-api-client/0.1/crackle-automation-api-client-0.1.tar.gz

---
+++ packages/crackle-automation-api-client/crackle-automation-api-client-0.1.tar.gz/crackle-automation-api-client-0.1/setup.py
@@ -0,0 +1,21 @@
+''' Setup for package '''
+from setuptools import setup, find_packages
+
+setup(
+    name='crackle-automation-api-client',
+    version='0.1',
+    description="Python API Wrapper for Crackle Automation Reporter Web Service",
+    long_description=""" """,
+    classifiers=[],
+    keywords='',
+    author='Clark Mckenzie',
+    author_email='clarkmckenzie@gmail.com',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        "requests"
+    ],
+)
