734d467012087262690cc9fdde5f575b30c60a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:23 +0000

    Add package cbr/0.0/cbr-0.0.tar.gz

---
+++ packages/cbr/cbr-0.0.tar.gz/cbr-0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import cbr
+
+
+setup(
+    name='cbr',
+    version='0.0',
+    author='HH-MWB',
+    author_email='h.hong@mail.com',
+    description='',
+    long_description='',
+    long_description_content_type='text/markdown',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    py_modules=['cbr'],
+    python_requires='>=3.6',
+)
