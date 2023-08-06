a4bf36cb1c499ef6af43eef3074a7696626bc8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:41 +0000

    Add package adhesive/0.2.11/adhesive-0.2.11.tar.gz

---
+++ packages/adhesive/adhesive-0.2.11.tar.gz/adhesive-0.2.11/adhesive/mainapp.py
@@ -0,0 +1,13 @@
+_globals = globals()
+_locals = locals()
+
+
+def main():
+    with open('_adhesive.py', 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    exec(content, _globals, _locals)
+
+
+if __name__ == "__main__":
+    main()
