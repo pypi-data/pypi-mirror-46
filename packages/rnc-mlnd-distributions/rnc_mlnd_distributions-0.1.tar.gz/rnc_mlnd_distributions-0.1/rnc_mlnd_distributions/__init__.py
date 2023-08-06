167d0b3c842d9cecd6c00120933c44a1023aa5c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:40 +0000

    Add package rnc-mlnd-distributions/0.1/rnc_mlnd_distributions-0.1.tar.gz

---
+++ packages/rnc-mlnd-distributions/rnc_mlnd_distributions-0.1.tar.gz/rnc_mlnd_distributions-0.1/rnc_mlnd_distributions/__init__.py
@@ -0,0 +1,6 @@
+from .Gaussiandistribution import Gaussian
+#from .Binomialdistribution_challenge import Binomial
+from .Binomialdistribution import Binomial
+
+
+# TODO: import the Binomial class from the Binomialdistribution module
---
+++ packages/rnc-mlnd-distributions/rnc_mlnd_distributions-0.1.tar.gz/rnc_mlnd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rnc_mlnd_distributions',
+      version='0.1',
+      description='Gaussian & Binomial probability distributions',
+      author='Raghu Nandan Chilukuri',
+      packages=['rnc_mlnd_distributions'],
+      author_email='raghu.rnc@gmail.com',
+      zip_safe=False)
