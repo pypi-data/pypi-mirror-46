4b5c4693f9134aeed20f2eeaa868ac1034870f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:50 +0000

    Add package celery-pantry/1.1.2/celery_pantry-1.1.2-py3-none-any.whl

---
+++ packages/celery-pantry/celery_pantry-1.1.2-py3-none-any.whl/celery_pantry/custom_handler.py
@@ -0,0 +1,14 @@
+import abc
+
+
+class CustomPantryHandler(metaclass=abc.ABCMeta):
+
+    def __init__(self, state):
+        self.state = state
+
+    @abc.abstractmethod
+    def process_event(self, event, task):
+        pass
+
+    def stop(self):
+        pass
