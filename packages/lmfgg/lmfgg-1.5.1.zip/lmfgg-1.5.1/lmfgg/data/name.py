48b7c3fd0000999d503a716f3ea808aaaf7ad1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:04 +0000

    Add package lmfgg/1.5.1/lmfgg-1.5.1.zip

---
+++ packages/lmfgg/lmfgg-1.5.1.zip/lmfgg-1.5.1/lmfgg/data/name.py
@@ -0,0 +1,25 @@
+import re 
+
+__krr1=["中标公示$",'招标公告$',"变更通知$","流标公示$","修改说明$","投标邀请公告$","招标失败公告$","流标公告$","中标结果公示$"
+,"补充说明$","询价公告","暂停的公告","成交公示"
+]
+
+def extname(name):
+
+    for w in __krr1:
+            name1=re.sub(w,'',name)
+    if name1==name:
+        return None
+    else:
+        return name1
+    
+
+
+
+def reclear_qyname(qyname):
+    if qyname is None:return None
+    a=re.findall("^[\u4e00-\u9fa5（）\(\)]*公司",qyname)
+    if a!=[]:
+        return a[0]
+    else:
+        return qyname
