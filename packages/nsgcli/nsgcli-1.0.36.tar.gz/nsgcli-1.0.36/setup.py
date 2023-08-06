724921458348977d35ac0801dab49e6f5b7ce0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:03 +0000

    Add package nsgcli/1.0.36/nsgcli-1.0.36.tar.gz

---
+++ packages/nsgcli/nsgcli-1.0.36.tar.gz/nsgcli-1.0.36/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='nsgcli',
+                 version='1.0.36',
+                 description='NetSpyGlass CLI',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 classifiers=[
+                     'Development Status :: 4 - Beta',
+                     'License :: OSI Approved :: Apache Software License',
+                     'Programming Language :: Python :: 2.7',
+                     'Topic :: System :: Networking :: Monitoring',
+                 ],
+                 keywords='network monitoring NMS netspyglass',
+                 url='https://github.com/happygears/nsgcli',
+                 author='Happy Gears, Inc',
+                 author_email='vadim@happygears.net',
+                 license='Apache',
+                 packages=setuptools.find_packages(),
+                 install_requires=[
+                     'requests', 'requests-unixsocket', 'pyhocon', 'typing', 'python-dateutil'
+                 ],
+                 scripts=['bin/nsgcli', 'bin/nsgql', 'bin/silence'],
+                 include_package_data=True,
+                 zip_safe=False)
