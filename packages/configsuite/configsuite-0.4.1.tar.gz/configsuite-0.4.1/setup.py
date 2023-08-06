ff6bac1bd0e094782719d011971ed421255dbc56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:36 +0000

    Add package configsuite/0.4.1/configsuite-0.4.1.tar.gz

---
+++ packages/configsuite/configsuite-0.4.1.tar.gz/configsuite-0.4.1/setup.py
@@ -0,0 +1,47 @@
+"""Copyright 2018 Equinor ASA and The Netherlands Organisation for
+Applied Scientific Research TNO.
+
+Licensed under the MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the conditions stated in the LICENSE file in the project root for
+details.
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+"""
+
+
+import setuptools
+
+
+setuptools.setup(
+    name="configsuite",
+    packages=["configsuite", "configsuite.docs"],
+    author="Software Innovation Bergen, Statoil ASA and TNO",
+    use_scm_version={"write_to": "configsuite/_version.py"},
+    install_requires=[
+        "enum34 >= 1.0 ; python_version<'3.4'",
+        "six >= 1.0 ; python_version<'3'",
+    ],
+    setup_requires=[
+        "setuptools_scm",
+        "setuptools_scm_about",
+    ],
+    tests_require=[
+        "pytest",
+        "pytest-runner",
+        "pytest-pylint",
+        "black ; python_version>='3.6'",
+        "click ; python_version>='3.6'",
+        "pylint",
+        "pytest-cov",
+        "jinja2",
+    ],
+    test_suite="tests",
+)
