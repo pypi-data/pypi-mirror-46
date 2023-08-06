a6cb43552e05662e5585837751c2838119c0be9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:25 +0000

    Add package Easy-GRID/0.0.4/Easy_GRID-0.0.4.tar.gz

---
+++ packages/Easy-GRID/Easy_GRID-0.0.4.tar.gz/Easy_GRID-0.0.4/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(name='Easy_GRID',
+      version='0.0.04',
+      description='A GUI for field segmentation',
+      url='https://github.com/Zhiwu-Zhang-Lab/James_Git',
+      author='James Chen',
+      author_email='chun-peng.chen@wsu.edu',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      install_requires=['numpy',
+                        'pandas>=0.19.2',
+                        'sklearn',
+                        'scipy',
+                        'opencv-python',
+                        # 'gdal',
+                        'image',
+                        'PyQt5',
+                        'tqdm'])
