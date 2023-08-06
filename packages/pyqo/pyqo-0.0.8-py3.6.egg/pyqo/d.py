f0abd6ade90a2744944adfae6fb6d0e82d824765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:11 +0000

    Add package pyqo/0.0.8/pyqo-0.0.8-py3.6.egg

---
+++ packages/pyqo/pyqo-0.0.8-py3.6.egg/pyqo/d.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python3
+"""
+## Command ``d``
+
+Open the file manager to your favourite directories with ease.
+The command `d` shares its data with the command `c`.
+See `d --help` for more details.
+
+### Example
+
+```
+$ cd ~/Documents/games
+$ # open the current working directory, here '~/Documents/games'
+$ d
+$ # associate permanently the key 'films' to '~/Documents/films'
+$ d films -a /home/pyqo/Documents/films
+$ # open '~/Documents/films'
+$ d films
+```
+"""
+import click
+import sys, os
+from ._json import *
+from ._srl import *
+import subprocess
+from subprocess import DEVNULL
+
+@click.command()
+@click.argument('keys', required = False, nargs=-1)
+@decorate_srl
+
+def main(keys, **kwargs):
+    """Open directories."""
+    command = 'd'
+    filename = resolve_json_filename(command)
+
+    if handle_srl(command, filename, keys, type='dir', **kwargs):
+        return
+
+    cmd = 'xdg-open {}' if sys.platform in ['linux','linux2'] else 'start "" "{}"'
+
+    if len(keys)<1:
+        dirs = [os.getcwd()]
+    else:
+        dirs = get_json(filename, keys)
+
+    for dir in dirs:
+        subprocess.call(cmd.format(dir), shell=True, stderr=DEVNULL, stdout=DEVNULL)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pyqo/pyqo-0.0.8-py3.6.egg/pyqo/f.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+"""
+## Command ``f``
+
+Open your favourite files with ease. See `f --help` for more details.
+
+### Example
+
+```
+$ cd ~
+$ # associate permanently the key 'bashrc' to the file '~/.bashrc'
+$ f bashrc -a .bashrc
+$ cd ~/Documents/games
+$ # open the '~/.bashrc' file
+$ f bashrc
+```
+"""
+
+import click
+import sys, os
+from ._json import *
+from ._srl import *
+import subprocess
+from subprocess import DEVNULL
+
+
+@click.command()
+@click.argument('keys', required = False, nargs=-1)
+@decorate_srl
+def main(keys, **kwargs):
+    """open directories"""
+
+    command = 'f'
+    filename = resolve_json_filename(command)
+
+    if handle_srl(command, filename, keys, type='file', **kwargs):
+        return
+
+    files = get_json(filename, keys)
+
+    cmd = 'xdg-open {}' if sys.platform in ['linux','linux2'] else 'start "" "{}"'
+    for file in files:
+        subprocess.call(cmd.format(file), shell=True, stderr=DEVNULL, stdout=DEVNULL)
+
+
+if __name__ == "__main__":
+    main()
