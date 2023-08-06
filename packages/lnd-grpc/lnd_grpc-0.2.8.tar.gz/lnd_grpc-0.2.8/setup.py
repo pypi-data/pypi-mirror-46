3055ceb2962ad734e845415295d5de56aa60cc6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:02 +0000

    Add package lnd-grpc/0.2.8/lnd_grpc-0.2.8.tar.gz

---
+++ packages/lnd-grpc/lnd_grpc-0.2.8.tar.gz/lnd_grpc-0.2.8/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lnd_grpc",
+        version="0.2.8",
+    author="Will Clark",
+    author_email="will8clark@gmail.com",
+        description="An LND gRPC client for Python 3.7",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/willcl-ark/lnd_grpc",
+    packages=setuptools.find_packages(
+            exclude=['googleapis', 'misc']),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="lnd grpc",
+    install_requires=[
+        "grpcio",
+        "grpcio-tools",
+        "googleapis-common-protos"
+    ],
+        python_requires='>=3.7',
+)
