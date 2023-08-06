d2f9437c7bcf24af71fb4b6f0a62c4ca10a6b340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:47 +0000

    Add package simplog/0.0.1/simplog-0.0.1.tar.gz

---
+++ packages/simplog/simplog-0.0.1.tar.gz/simplog-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+  name = 'simplog',
+  packages = ['simplog'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'A simple, functional, opinionated logger for python',   
+  author = 'Joel Smith',
+  author_email = 'joel.pypi@gmail.com',
+  url = 'https://github.com/JoelAtDeluxe/simplog',
+  download_url = 'https://github.com/JoelAtDeluxe/simplog/archive/v0.0.1.tar.gz', 
+  keywords = ['logging', 'functional', 'splunk'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
---
+++ packages/simplog/simplog-0.0.1.tar.gz/simplog-0.0.1/simplog/__init__.py
@@ -0,0 +1,13 @@
+from logger import (
+    make_logger,
+    refine_logger,
+
+    force_string,
+    escape_value,
+    escape_backslash,
+    escape_newline,
+    escape_tab,
+    escape_null,
+    escape_quotes,
+    quote_string
+)
